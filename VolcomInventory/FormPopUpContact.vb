55b2913969c3d097585ef2a84441d6831385f0d4
Satrya Anas <catur.satrya@gmail.com>
2016-04-29 10:40:37 +0800

    Browse SO only for store & WH

--- VolcomInventory/FormPopUpContact.vb
+++ VolcomInventory/FormPopUpContact.vb
@@ -99,6 +99,10 @@
         If id_cat <> "-1" Then
             query += "AND tb_m_comp.id_comp_cat = '" + id_cat + "' "
         End If
+        If id_pop_up = "38" Then
+            query += "AND (tb_m_comp.id_comp_cat = '5' OR tb_m_comp.id_comp_cat = '6') "
+        End If
+
         If id_departement <> "-1" Then
             query += "AND tb_m_comp.id_departement = '" + id_departement + "' "
         End If
