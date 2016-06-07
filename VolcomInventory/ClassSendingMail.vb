1af14e01eaf37b8bb56d10953f51e88f0d5eaad0
Septian Primadewa <iansaviour@gmail.com>
2016-06-07 16:47:22 +0700

    Add class mail sending

---
+++ VolcomInventory/ClassSendingMail.vb
@@ -0,0 +1,18 @@
+﻿Imports System.Net.Mail
+
+Public Class ClassSendingMail
+
+    Sub send_mail()
+        Dim mail As MailMessage = New MailMessage("septian@volcom.mail", "user@hotmail.com")
+        Dim client As SmtpClient = New SmtpClient()
+        client.Port = 25
+        client.DeliveryMethod = SmtpDeliveryMethod.Network
+        client.UseDefaultCredentials = False
+        client.Host = "192.168.1.4"
+        client.Credentials = New System.Net.NetworkCredential("septian", "volcom10")
+        mail.Subject = "No reply ceritanya"
+        mail.Body = "This is my test email  body"
+        client.Send(mail)
+    End Sub
+
+End Class
