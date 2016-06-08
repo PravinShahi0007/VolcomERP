ad82008a5fbb9ee13177a6112890407f9a235adc
Satrya Anas <catur.satrya@gmail.com>
2016-06-08 08:57:56 +0800

    Check mark in finalize status

--- VolcomInventory/FormChangeStatus.vb
+++ VolcomInventory/FormChangeStatus.vb
@@ -17,157 +17,173 @@
     End Sub
 
     Private Sub BtnUpdateRec_Click(sender As Object, e As EventArgs) Handles BtnUpdateRec.Click
+        'var
+        Dim assigned As Boolean = False
+        Dim query_jml As String
+        Dim jml As Integer
         Dim note As String = MEComment.Text
-        If id_pop_up = "1" Then
-            Dim check_stt As Boolean = False
-            For c As Integer = 0 To ((FormSalesOrderSvcLevel.GVPL.RowCount - 1) - GetGroupRowCount(FormSalesOrderSvcLevel.GVPL))
-                Dim rs As String = FormSalesOrderSvcLevel.GVPL.GetRowCellValue(c, "id_report_status").ToString
-                If rs = "5" Or rs = "6" Then
-                    check_stt = True
-                    Exit For
-                End If
-            Next
+        Dim id_status_reportx As String = SLEStatusRec.EditValue.ToString
+        If id_status_reportx = "6" Or id_status_reportx = "4" Then
+            'completed or processed
+            assigned = True
+            query_jml = String.Format("SELECT count(id_report_mark) FROM tb_report_mark WHERE report_mark_type='{0}' AND id_report='{1}' AND id_report_status <= '3' AND id_mark != '2' AND is_use='1'", report_mark_type, id_report)
+            jml = execute_query(query_jml, 0, True, "", "", "", "")
+        End If
 
-            If check_stt Then
-                stopCustom("Can't update status because data is already locked.")
+        If (jml < 1 And assigned = True) Or id_status_reportx = "5" Then
+            If id_pop_up = "1" Then
+                Dim check_stt As Boolean = False
+                For c As Integer = 0 To ((FormSalesOrderSvcLevel.GVPL.RowCount - 1) - GetGroupRowCount(FormSalesOrderSvcLevel.GVPL))
+                    Dim rs As String = FormSalesOrderSvcLevel.GVPL.GetRowCellValue(c, "id_report_status").ToString
+                    If rs = "5" Or rs = "6" Then
+                        check_stt = True
+                        Exit For
+                    End If
+                Next
+
+                If check_stt Then
+                    stopCustom("Can't update status because data is already locked.")
+                    FormSalesOrderSvcLevel.GVPL.ActiveFilterString = ""
+                    Close()
+                Else
+                    Dim confirm As DialogResult = DevExpress.XtraEditors.XtraMessageBox.Show("Are you sure want to set " + SLEStatusRec.Text.ToLower.ToString + " status for these data?", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button2)
+                    If confirm = Windows.Forms.DialogResult.Yes Then
+                        Cursor = Cursors.WaitCursor
+                        For i As Integer = 0 To ((FormSalesOrderSvcLevel.GVPL.RowCount - 1) - GetGroupRowCount(FormSalesOrderSvcLevel.GVPL))
+                            Dim ch_stt As ClassProductionPLToWHRec = New ClassProductionPLToWHRec()
+                            ch_stt.changeStatus(FormSalesOrderSvcLevel.GVPL.GetRowCellValue(i, "id_pl_prod_order_rec").ToString, SLEStatusRec.EditValue.ToString)
+                            PBC.PerformStep()
+                            PBC.Update()
+                        Next
+                        Cursor = Cursors.Default
+                    End If
+                End If
                 FormSalesOrderSvcLevel.GVPL.ActiveFilterString = ""
+                FormSalesOrderSvcLevel.viewRecProduct()
                 Close()
-            Else
-                Dim confirm As DialogResult = DevExpress.XtraEditors.XtraMessageBox.Show("Are you sure want to set " + SLEStatusRec.Text.ToLower.ToString + " status for these data?", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button2)
-                If confirm = Windows.Forms.DialogResult.Yes Then
-                    Cursor = Cursors.WaitCursor
-                    For i As Integer = 0 To ((FormSalesOrderSvcLevel.GVPL.RowCount - 1) - GetGroupRowCount(FormSalesOrderSvcLevel.GVPL))
-                        Dim ch_stt As ClassProductionPLToWHRec = New ClassProductionPLToWHRec()
-                        ch_stt.changeStatus(FormSalesOrderSvcLevel.GVPL.GetRowCellValue(i, "id_pl_prod_order_rec").ToString, SLEStatusRec.EditValue.ToString)
-                        PBC.PerformStep()
-                        PBC.Update()
-                    Next
-                    Cursor = Cursors.Default
-                End If
-            End If
-            FormSalesOrderSvcLevel.GVPL.ActiveFilterString = ""
-            FormSalesOrderSvcLevel.viewRecProduct()
-            Close()
-        ElseIf id_pop_up = "2" Then
-            Dim check_stt As Boolean = False
-            For c As Integer = 0 To ((FormSalesOrderSvcLevel.GVSalesDelOrder.RowCount - 1) - GetGroupRowCount(FormSalesOrderSvcLevel.GVSalesDelOrder))
-                Dim rs As String = FormSalesOrderSvcLevel.GVSalesDelOrder.GetRowCellValue(c, "id_report_status").ToString
-                If rs = "5" Or rs = "6" Then
-                    check_stt = True
-                    Exit For
-                End If
-            Next
+            ElseIf id_pop_up = "2" Then
+                Dim check_stt As Boolean = False
+                For c As Integer = 0 To ((FormSalesOrderSvcLevel.GVSalesDelOrder.RowCount - 1) - GetGroupRowCount(FormSalesOrderSvcLevel.GVSalesDelOrder))
+                    Dim rs As String = FormSalesOrderSvcLevel.GVSalesDelOrder.GetRowCellValue(c, "id_report_status").ToString
+                    If rs = "5" Or rs = "6" Then
+                        check_stt = True
+                        Exit For
+                    End If
+                Next
 
-            If check_stt Then
-                stopCustom("Can't update status because data is already locked.")
+                If check_stt Then
+                    stopCustom("Can't update status because data is already locked.")
+                    FormSalesOrderSvcLevel.GVSalesDelOrder.ActiveFilterString = ""
+                    Close()
+                Else
+                    Dim confirm As DialogResult = DevExpress.XtraEditors.XtraMessageBox.Show("Are you sure want to set " + SLEStatusRec.Text.ToLower.ToString + " status for these data?", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button2)
+                    If confirm = Windows.Forms.DialogResult.Yes Then
+                        Cursor = Cursors.WaitCursor
+                        For i As Integer = 0 To ((FormSalesOrderSvcLevel.GVSalesDelOrder.RowCount - 1) - GetGroupRowCount(FormSalesOrderSvcLevel.GVSalesDelOrder))
+                            Dim stt As ClassSalesDelOrder = New ClassSalesDelOrder()
+                            stt.changeStatus(FormSalesOrderSvcLevel.GVSalesDelOrder.GetRowCellValue(i, "id_pl_sales_order_del").ToString, SLEStatusRec.EditValue.ToString)
+                            PBC.PerformStep()
+                            PBC.Update()
+                        Next
+                        Cursor = Cursors.Default
+                    End If
+                End If
                 FormSalesOrderSvcLevel.GVSalesDelOrder.ActiveFilterString = ""
+                FormSalesOrderSvcLevel.viewDO()
                 Close()
-            Else
-                Dim confirm As DialogResult = DevExpress.XtraEditors.XtraMessageBox.Show("Are you sure want to set " + SLEStatusRec.Text.ToLower.ToString + " status for these data?", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button2)
-                If confirm = Windows.Forms.DialogResult.Yes Then
-                    Cursor = Cursors.WaitCursor
-                    For i As Integer = 0 To ((FormSalesOrderSvcLevel.GVSalesDelOrder.RowCount - 1) - GetGroupRowCount(FormSalesOrderSvcLevel.GVSalesDelOrder))
-                        Dim stt As ClassSalesDelOrder = New ClassSalesDelOrder()
-                        stt.changeStatus(FormSalesOrderSvcLevel.GVSalesDelOrder.GetRowCellValue(i, "id_pl_sales_order_del").ToString, SLEStatusRec.EditValue.ToString)
-                        PBC.PerformStep()
-                        PBC.Update()
-                    Next
-                    Cursor = Cursors.Default
-                End If
-            End If
-            FormSalesOrderSvcLevel.GVSalesDelOrder.ActiveFilterString = ""
-            FormSalesOrderSvcLevel.viewDO()
-            Close()
-        ElseIf id_pop_up = "3" Then
-            Dim check_stt As Boolean = False
-            For c As Integer = 0 To ((FormSalesOrderSvcLevel.GVSalesReturn.RowCount - 1) - GetGroupRowCount(FormSalesOrderSvcLevel.GVSalesReturn))
-                Dim rs As String = FormSalesOrderSvcLevel.GVSalesReturn.GetRowCellValue(c, "id_report_status").ToString
-                If rs = "5" Or rs = "6" Then
-                    check_stt = True
-                    Exit For
-                End If
-            Next
+            ElseIf id_pop_up = "3" Then
+                Dim check_stt As Boolean = False
+                For c As Integer = 0 To ((FormSalesOrderSvcLevel.GVSalesReturn.RowCount - 1) - GetGroupRowCount(FormSalesOrderSvcLevel.GVSalesReturn))
+                    Dim rs As String = FormSalesOrderSvcLevel.GVSalesReturn.GetRowCellValue(c, "id_report_status").ToString
+                    If rs = "5" Or rs = "6" Then
+                        check_stt = True
+                        Exit For
+                    End If
+                Next
 
-            If check_stt Then
-                stopCustom("Can't update status because data is already locked.")
+                If check_stt Then
+                    stopCustom("Can't update status because data is already locked.")
+                    FormSalesOrderSvcLevel.GVSalesReturn.ActiveFilterString = ""
+                    Close()
+                Else
+                    Dim confirm As DialogResult = DevExpress.XtraEditors.XtraMessageBox.Show("Are you sure want to set " + SLEStatusRec.Text.ToLower.ToString + " status for these data?", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button2)
+                    If confirm = Windows.Forms.DialogResult.Yes Then
+                        Cursor = Cursors.WaitCursor
+                        For i As Integer = 0 To ((FormSalesOrderSvcLevel.GVSalesReturn.RowCount - 1) - GetGroupRowCount(FormSalesOrderSvcLevel.GVSalesReturn))
+                            Dim stt As ClassSalesReturn = New ClassSalesReturn()
+                            stt.changeStatus(FormSalesOrderSvcLevel.GVSalesReturn.GetRowCellValue(i, "id_sales_return").ToString, SLEStatusRec.EditValue.ToString)
+                            PBC.PerformStep()
+                            PBC.Update()
+                        Next
+                        Cursor = Cursors.Default
+                    End If
+                End If
                 FormSalesOrderSvcLevel.GVSalesReturn.ActiveFilterString = ""
+                FormSalesOrderSvcLevel.viewReturn()
                 Close()
-            Else
-                Dim confirm As DialogResult = DevExpress.XtraEditors.XtraMessageBox.Show("Are you sure want to set " + SLEStatusRec.Text.ToLower.ToString + " status for these data?", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button2)
-                If confirm = Windows.Forms.DialogResult.Yes Then
-                    Cursor = Cursors.WaitCursor
-                    For i As Integer = 0 To ((FormSalesOrderSvcLevel.GVSalesReturn.RowCount - 1) - GetGroupRowCount(FormSalesOrderSvcLevel.GVSalesReturn))
-                        Dim stt As ClassSalesReturn = New ClassSalesReturn()
-                        stt.changeStatus(FormSalesOrderSvcLevel.GVSalesReturn.GetRowCellValue(i, "id_sales_return").ToString, SLEStatusRec.EditValue.ToString)
-                        PBC.PerformStep()
-                        PBC.Update()
-                    Next
-                    Cursor = Cursors.Default
-                End If
-            End If
-            FormSalesOrderSvcLevel.GVSalesReturn.ActiveFilterString = ""
-            FormSalesOrderSvcLevel.viewReturn()
-            Close()
-        ElseIf id_pop_up = "4" Then
-            Dim check_stt As Boolean = False
-            For c As Integer = 0 To ((FormSalesOrderSvcLevel.GVSalesReturnQC.RowCount - 1) - GetGroupRowCount(FormSalesOrderSvcLevel.GVSalesReturnQC))
-                Dim rs As String = FormSalesOrderSvcLevel.GVSalesReturnQC.GetRowCellValue(c, "id_report_status").ToString
-                If rs = "5" Or rs = "6" Then
-                    check_stt = True
-                    Exit For
-                End If
-            Next
+            ElseIf id_pop_up = "4" Then
+                Dim check_stt As Boolean = False
+                For c As Integer = 0 To ((FormSalesOrderSvcLevel.GVSalesReturnQC.RowCount - 1) - GetGroupRowCount(FormSalesOrderSvcLevel.GVSalesReturnQC))
+                    Dim rs As String = FormSalesOrderSvcLevel.GVSalesReturnQC.GetRowCellValue(c, "id_report_status").ToString
+                    If rs = "5" Or rs = "6" Then
+                        check_stt = True
+                        Exit For
+                    End If
+                Next
 
-            If check_stt Then
-                stopCustom("Can't update status because data is already locked.")
+                If check_stt Then
+                    stopCustom("Can't update status because data is already locked.")
+                    FormSalesOrderSvcLevel.GVSalesReturnQC.ActiveFilterString = ""
+                    Close()
+                Else
+                    Dim confirm As DialogResult = DevExpress.XtraEditors.XtraMessageBox.Show("Are you sure want to set " + SLEStatusRec.Text.ToLower.ToString + " status for these data?", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button2)
+                    If confirm = Windows.Forms.DialogResult.Yes Then
+                        Cursor = Cursors.WaitCursor
+                        For i As Integer = 0 To ((FormSalesOrderSvcLevel.GVSalesReturnQC.RowCount - 1) - GetGroupRowCount(FormSalesOrderSvcLevel.GVSalesReturnQC))
+                            Dim stt As ClassSalesReturnQC = New ClassSalesReturnQC()
+                            stt.changeStatus(FormSalesOrderSvcLevel.GVSalesReturnQC.GetRowCellValue(i, "id_sales_return_qc").ToString, SLEStatusRec.EditValue.ToString)
+                            PBC.PerformStep()
+                            PBC.Update()
+                        Next
+                        Cursor = Cursors.Default
+                    End If
+                End If
                 FormSalesOrderSvcLevel.GVSalesReturnQC.ActiveFilterString = ""
+                FormSalesOrderSvcLevel.viewReturnQC()
                 Close()
-            Else
-                Dim confirm As DialogResult = DevExpress.XtraEditors.XtraMessageBox.Show("Are you sure want to set " + SLEStatusRec.Text.ToLower.ToString + " status for these data?", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button2)
-                If confirm = Windows.Forms.DialogResult.Yes Then
-                    Cursor = Cursors.WaitCursor
-                    For i As Integer = 0 To ((FormSalesOrderSvcLevel.GVSalesReturnQC.RowCount - 1) - GetGroupRowCount(FormSalesOrderSvcLevel.GVSalesReturnQC))
-                        Dim stt As ClassSalesReturnQC = New ClassSalesReturnQC()
-                        stt.changeStatus(FormSalesOrderSvcLevel.GVSalesReturnQC.GetRowCellValue(i, "id_sales_return_qc").ToString, SLEStatusRec.EditValue.ToString)
-                        PBC.PerformStep()
-                        PBC.Update()
-                    Next
-                    Cursor = Cursors.Default
-                End If
-            End If
-            FormSalesOrderSvcLevel.GVSalesReturnQC.ActiveFilterString = ""
-            FormSalesOrderSvcLevel.viewReturnQC()
-            Close()
-        ElseIf id_pop_up = "5" Then
-            Dim check_stt As Boolean = False
-            For c As Integer = 0 To ((FormSalesOrderSvcLevel.GVFGTrf.RowCount - 1) - GetGroupRowCount(FormSalesOrderSvcLevel.GVFGTrf))
-                Dim rs As String = FormSalesOrderSvcLevel.GVFGTrf.GetRowCellValue(c, "id_report_status").ToString
-                If rs = "5" Or rs = "6" Then
-                    check_stt = True
-                    Exit For
-                End If
-            Next
+            ElseIf id_pop_up = "5" Then
+                Dim check_stt As Boolean = False
+                For c As Integer = 0 To ((FormSalesOrderSvcLevel.GVFGTrf.RowCount - 1) - GetGroupRowCount(FormSalesOrderSvcLevel.GVFGTrf))
+                    Dim rs As String = FormSalesOrderSvcLevel.GVFGTrf.GetRowCellValue(c, "id_report_status").ToString
+                    If rs = "5" Or rs = "6" Then
+                        check_stt = True
+                        Exit For
+                    End If
+                Next
 
-            If check_stt Then
-                stopCustom("Can't update status because data is already locked.")
-                FormSalesOrderSvcLevel.GVFGTrf.ActiveFilterString = ""
-                Close()
-            Else
-                Dim confirm As DialogResult = DevExpress.XtraEditors.XtraMessageBox.Show("Are you sure want to set " + SLEStatusRec.Text.ToLower.ToString + " status for these data?", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button2)
-                If confirm = Windows.Forms.DialogResult.Yes Then
-                    Cursor = Cursors.WaitCursor
-                    For i As Integer = 0 To ((FormSalesOrderSvcLevel.GVFGTrf.RowCount - 1) - GetGroupRowCount(FormSalesOrderSvcLevel.GVFGTrf))
-                        Dim stt As ClassFGTrf = New ClassFGTrf()
-                        stt.changeStatus(FormSalesOrderSvcLevel.GVFGTrf.GetRowCellValue(i, "id_fg_trf").ToString, SLEStatusRec.EditValue.ToString)
-                        PBC.PerformStep()
-                        PBC.Update()
-                    Next
-                    Cursor = Cursors.Default
+                If check_stt Then
+                    stopCustom("Can't update status because data is already locked.")
+                    FormSalesOrderSvcLevel.GVFGTrf.ActiveFilterString = ""
+                    Close()
+                Else
+                    Dim confirm As DialogResult = DevExpress.XtraEditors.XtraMessageBox.Show("Are you sure want to set " + SLEStatusRec.Text.ToLower.ToString + " status for these data?", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button2)
+                    If confirm = Windows.Forms.DialogResult.Yes Then
+                        Cursor = Cursors.WaitCursor
+                        For i As Integer = 0 To ((FormSalesOrderSvcLevel.GVFGTrf.RowCount - 1) - GetGroupRowCount(FormSalesOrderSvcLevel.GVFGTrf))
+                            Dim stt As ClassFGTrf = New ClassFGTrf()
+                            stt.changeStatus(FormSalesOrderSvcLevel.GVFGTrf.GetRowCellValue(i, "id_fg_trf").ToString, SLEStatusRec.EditValue.ToString)
+                            PBC.PerformStep()
+                            PBC.Update()
+                        Next
+                        Cursor = Cursors.Default
+                    End If
                 End If
+                FormSalesOrderSvcLevel.GVFGTrf.ActiveFilterString = ""
+                FormSalesOrderSvcLevel.viewTrf()
+                Close()
             End If
-            FormSalesOrderSvcLevel.GVFGTrf.ActiveFilterString = ""
-            FormSalesOrderSvcLevel.viewTrf()
-            Close()
+        Else
+            stopCustom("Unable change to this status, report doesn't meet requirement to this status.")
         End If
     End Sub
 
