1dedbf233957a662e8040afa3992ab4a08345a9f
Septian Primadewa <iansaviour@gmail.com>
2016-08-12 10:12:47 +0700

    Changes Class Fingerprint

--- VolcomInventory/FormEmpAttn.vb
+++ VolcomInventory/FormEmpAttn.vb
@@ -1,5 +1,21 @@
 ﻿Public Class FormEmpAttn
+    Dim fp As New ClassFingerPrint
     Private Sub BGetData_Click(sender As Object, e As EventArgs) Handles BGetData.Click
+        Cursor = Cursors.WaitCursor
+        fp.connect()
 
+        fp.get_attlog()
+        fp.clear_attlog()
+
+        fp.disconnect()
+        Cursor = Cursors.Default
+    End Sub
+
+    Private Sub BView_Click(sender As Object, e As EventArgs) Handles BView.Click
+        Dim query As String = ""
+        query = "SELECT * FROM tb_emp_attn emp_attn"
+        query += " INNER JOIN tb_m_employee emp ON emp.id_employee=emp_attn.id_employee"
+        Dim data As DataTable = execute_query(query, -1, True, "", "", "", "")
+        GCSchedule.DataSource = data
     End Sub
 End Class
