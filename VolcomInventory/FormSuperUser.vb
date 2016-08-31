787e47db0e222d10548335747f1618e17566c54b
Satrya Anas <catur.satrya@gmail.com>
2016-08-31 11:56:33 +0800

    Download FP & Face template

--- VolcomInventory/FormSuperUser.vb
+++ VolcomInventory/FormSuperUser.vb
@@ -69,6 +69,18 @@
     End Sub
 
     Private Sub BTest_Click(sender As Object, e As EventArgs) Handles BTest.Click
-        FormEmpHoliday.ShowDialog()
+        Cursor = Cursors.WaitCursor
+        Try
+            Dim fp As New ClassFingerPrint()
+            Dim data_fp As DataTable = fp.get_fp_register()
+            fp.ip = data_fp.Rows(0)("ip").ToString
+            fp.port = data_fp.Rows(0)("port").ToString
+            fp.download_fp_tmp()
+            fp.download_face_tmp()
+            infoCustom("OK")
+        Catch ex As Exception
+            stopCustom("syncronization problems, please try again later.")
+        End Try
+        Cursor = Cursors.Default
     End Sub
 End Class
