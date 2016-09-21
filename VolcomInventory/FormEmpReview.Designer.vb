28b9fb4abb0cf812327e818f5ee1ebca2ecf7d70
Satrya Anas <catur.satrya@gmail.com>
2016-09-21 12:05:24 +0800

    Date panel for review menu

--- VolcomInventory/FormEmpReview.Designer.vb
+++ VolcomInventory/FormEmpReview.Designer.vb
@@ -21,9 +21,31 @@ Partial Class FormEmpReview
     Private Sub InitializeComponent()
         Me.XTCEmpReview = New DevExpress.XtraTab.XtraTabControl()
         Me.XTPContract = New DevExpress.XtraTab.XtraTabPage()
+        Me.GCFilter = New DevExpress.XtraEditors.GroupControl()
+        Me.BtnView = New DevExpress.XtraEditors.SimpleButton()
+        Me.BHide = New DevExpress.XtraEditors.SimpleButton()
+        Me.BExpand = New DevExpress.XtraEditors.SimpleButton()
+        Me.DEFrom = New DevExpress.XtraEditors.DateEdit()
+        Me.LabelControl3 = New DevExpress.XtraEditors.LabelControl()
         Me.XTPAge = New DevExpress.XtraTab.XtraTabPage()
+        Me.GroupControl1 = New DevExpress.XtraEditors.GroupControl()
+        Me.SimpleButton1 = New DevExpress.XtraEditors.SimpleButton()
+        Me.SimpleButton2 = New DevExpress.XtraEditors.SimpleButton()
+        Me.SimpleButton3 = New DevExpress.XtraEditors.SimpleButton()
+        Me.DateEdit1 = New DevExpress.XtraEditors.DateEdit()
+        Me.LabelControl1 = New DevExpress.XtraEditors.LabelControl()
         CType(Me.XTCEmpReview, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.XTCEmpReview.SuspendLayout()
+        Me.XTPContract.SuspendLayout()
+        CType(Me.GCFilter, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GCFilter.SuspendLayout()
+        CType(Me.DEFrom.Properties.CalendarTimeProperties, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DEFrom.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.XTPAge.SuspendLayout()
+        CType(Me.GroupControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupControl1.SuspendLayout()
+        CType(Me.DateEdit1.Properties.CalendarTimeProperties, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DateEdit1.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'XTCEmpReview
@@ -33,27 +55,155 @@ Partial Class FormEmpReview
         Me.XTCEmpReview.Location = New System.Drawing.Point(0, 0)
         Me.XTCEmpReview.Name = "XTCEmpReview"
         Me.XTCEmpReview.SelectedTabPage = Me.XTPContract
-        Me.XTCEmpReview.Size = New System.Drawing.Size(581, 262)
+        Me.XTCEmpReview.Size = New System.Drawing.Size(713, 310)
         Me.XTCEmpReview.TabIndex = 0
         Me.XTCEmpReview.TabPages.AddRange(New DevExpress.XtraTab.XtraTabPage() {Me.XTPContract, Me.XTPAge})
         '
         'XTPContract
         '
+        Me.XTPContract.Controls.Add(Me.GCFilter)
         Me.XTPContract.Name = "XTPContract"
-        Me.XTPContract.Size = New System.Drawing.Size(552, 256)
+        Me.XTPContract.Size = New System.Drawing.Size(684, 304)
         Me.XTPContract.Text = "Contract"
+        '
+        'GCFilter
+        '
+        Me.GCFilter.CaptionLocation = DevExpress.Utils.Locations.Left
+        Me.GCFilter.Controls.Add(Me.BtnView)
+        Me.GCFilter.Controls.Add(Me.BHide)
+        Me.GCFilter.Controls.Add(Me.BExpand)
+        Me.GCFilter.Controls.Add(Me.DEFrom)
+        Me.GCFilter.Controls.Add(Me.LabelControl3)
+        Me.GCFilter.Dock = System.Windows.Forms.DockStyle.Top
+        Me.GCFilter.Location = New System.Drawing.Point(0, 0)
+        Me.GCFilter.Name = "GCFilter"
+        Me.GCFilter.Size = New System.Drawing.Size(684, 39)
+        Me.GCFilter.TabIndex = 3
+        '
+        'BtnView
+        '
+        Me.BtnView.Location = New System.Drawing.Point(216, 9)
+        Me.BtnView.LookAndFeel.SkinName = "Blue"
+        Me.BtnView.Name = "BtnView"
+        Me.BtnView.Size = New System.Drawing.Size(75, 20)
+        Me.BtnView.TabIndex = 8896
+        Me.BtnView.Text = "View"
+        '
+        'BHide
+        '
+        Me.BHide.ImageIndex = 9
+        Me.BHide.Location = New System.Drawing.Point(938, 14)
+        Me.BHide.Name = "BHide"
+        Me.BHide.Size = New System.Drawing.Size(104, 20)
+        Me.BHide.TabIndex = 8898
+        Me.BHide.Text = "Hide All Detail"
+        Me.BHide.Visible = False
+        '
+        'BExpand
+        '
+        Me.BExpand.ImageIndex = 8
+        Me.BExpand.Location = New System.Drawing.Point(835, 14)
+        Me.BExpand.Name = "BExpand"
+        Me.BExpand.Size = New System.Drawing.Size(99, 20)
+        Me.BExpand.TabIndex = 8897
+        Me.BExpand.Text = "Expand All Detail"
+        Me.BExpand.Visible = False
+        '
+        'DEFrom
+        '
+        Me.DEFrom.EditValue = Nothing
+        Me.DEFrom.Location = New System.Drawing.Point(58, 9)
+        Me.DEFrom.Name = "DEFrom"
+        Me.DEFrom.Properties.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() {New DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)})
+        Me.DEFrom.Properties.CalendarTimeProperties.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() {New DevExpress.XtraEditors.Controls.EditorButton()})
+        Me.DEFrom.Properties.DisplayFormat.FormatString = "dd MMM yyyy"
+        Me.DEFrom.Properties.DisplayFormat.FormatType = DevExpress.Utils.FormatType.DateTime
+        Me.DEFrom.Size = New System.Drawing.Size(152, 20)
+        Me.DEFrom.TabIndex = 8894
+        '
+        'LabelControl3
+        '
+        Me.LabelControl3.Location = New System.Drawing.Point(28, 12)
+        Me.LabelControl3.Name = "LabelControl3"
+        Me.LabelControl3.Size = New System.Drawing.Size(23, 13)
+        Me.LabelControl3.TabIndex = 8892
+        Me.LabelControl3.Text = "Date"
         '
         'XTPAge
         '
+        Me.XTPAge.Controls.Add(Me.GroupControl1)
         Me.XTPAge.Name = "XTPAge"
-        Me.XTPAge.Size = New System.Drawing.Size(294, 272)
+        Me.XTPAge.Size = New System.Drawing.Size(684, 304)
         Me.XTPAge.Text = "Age"
+        '
+        'GroupControl1
+        '
+        Me.GroupControl1.CaptionLocation = DevExpress.Utils.Locations.Left
+        Me.GroupControl1.Controls.Add(Me.SimpleButton1)
+        Me.GroupControl1.Controls.Add(Me.SimpleButton2)
+        Me.GroupControl1.Controls.Add(Me.SimpleButton3)
+        Me.GroupControl1.Controls.Add(Me.DateEdit1)
+        Me.GroupControl1.Controls.Add(Me.LabelControl1)
+        Me.GroupControl1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.GroupControl1.Location = New System.Drawing.Point(0, 0)
+        Me.GroupControl1.Name = "GroupControl1"
+        Me.GroupControl1.Size = New System.Drawing.Size(684, 39)
+        Me.GroupControl1.TabIndex = 4
+        '
+        'SimpleButton1
+        '
+        Me.SimpleButton1.Location = New System.Drawing.Point(216, 9)
+        Me.SimpleButton1.LookAndFeel.SkinName = "Blue"
+        Me.SimpleButton1.Name = "SimpleButton1"
+        Me.SimpleButton1.Size = New System.Drawing.Size(75, 20)
+        Me.SimpleButton1.TabIndex = 8896
+        Me.SimpleButton1.Text = "View"
+        '
+        'SimpleButton2
+        '
+        Me.SimpleButton2.ImageIndex = 9
+        Me.SimpleButton2.Location = New System.Drawing.Point(938, 14)
+        Me.SimpleButton2.Name = "SimpleButton2"
+        Me.SimpleButton2.Size = New System.Drawing.Size(104, 20)
+        Me.SimpleButton2.TabIndex = 8898
+        Me.SimpleButton2.Text = "Hide All Detail"
+        Me.SimpleButton2.Visible = False
+        '
+        'SimpleButton3
+        '
+        Me.SimpleButton3.ImageIndex = 8
+        Me.SimpleButton3.Location = New System.Drawing.Point(835, 14)
+        Me.SimpleButton3.Name = "SimpleButton3"
+        Me.SimpleButton3.Size = New System.Drawing.Size(99, 20)
+        Me.SimpleButton3.TabIndex = 8897
+        Me.SimpleButton3.Text = "Expand All Detail"
+        Me.SimpleButton3.Visible = False
+        '
+        'DateEdit1
+        '
+        Me.DateEdit1.EditValue = Nothing
+        Me.DateEdit1.Location = New System.Drawing.Point(58, 9)
+        Me.DateEdit1.Name = "DateEdit1"
+        Me.DateEdit1.Properties.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() {New DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)})
+        Me.DateEdit1.Properties.CalendarTimeProperties.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() {New DevExpress.XtraEditors.Controls.EditorButton()})
+        Me.DateEdit1.Properties.DisplayFormat.FormatString = "dd MMM yyyy"
+        Me.DateEdit1.Properties.DisplayFormat.FormatType = DevExpress.Utils.FormatType.DateTime
+        Me.DateEdit1.Size = New System.Drawing.Size(152, 20)
+        Me.DateEdit1.TabIndex = 8894
+        '
+        'LabelControl1
+        '
+        Me.LabelControl1.Location = New System.Drawing.Point(28, 12)
+        Me.LabelControl1.Name = "LabelControl1"
+        Me.LabelControl1.Size = New System.Drawing.Size(23, 13)
+        Me.LabelControl1.TabIndex = 8892
+        Me.LabelControl1.Text = "Date"
         '
         'FormEmpReview
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(581, 262)
+        Me.ClientSize = New System.Drawing.Size(713, 310)
         Me.Controls.Add(Me.XTCEmpReview)
         Me.MaximizeBox = False
         Me.MinimizeBox = False
@@ -62,6 +212,18 @@ Partial Class FormEmpReview
         Me.Text = "Employee Review"
         CType(Me.XTCEmpReview, System.ComponentModel.ISupportInitialize).EndInit()
         Me.XTCEmpReview.ResumeLayout(False)
+        Me.XTPContract.ResumeLayout(False)
+        CType(Me.GCFilter, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GCFilter.ResumeLayout(False)
+        Me.GCFilter.PerformLayout()
+        CType(Me.DEFrom.Properties.CalendarTimeProperties, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DEFrom.Properties, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.XTPAge.ResumeLayout(False)
+        CType(Me.GroupControl1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupControl1.ResumeLayout(False)
+        Me.GroupControl1.PerformLayout()
+        CType(Me.DateEdit1.Properties.CalendarTimeProperties, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DateEdit1.Properties, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
@@ -69,4 +231,16 @@ Partial Class FormEmpReview
     Friend WithEvents XTCEmpReview As DevExpress.XtraTab.XtraTabControl
     Friend WithEvents XTPContract As DevExpress.XtraTab.XtraTabPage
     Friend WithEvents XTPAge As DevExpress.XtraTab.XtraTabPage
+    Friend WithEvents GCFilter As DevExpress.XtraEditors.GroupControl
+    Friend WithEvents BtnView As DevExpress.XtraEditors.SimpleButton
+    Friend WithEvents BHide As DevExpress.XtraEditors.SimpleButton
+    Friend WithEvents BExpand As DevExpress.XtraEditors.SimpleButton
+    Friend WithEvents DEFrom As DevExpress.XtraEditors.DateEdit
+    Friend WithEvents LabelControl3 As DevExpress.XtraEditors.LabelControl
+    Friend WithEvents GroupControl1 As DevExpress.XtraEditors.GroupControl
+    Friend WithEvents SimpleButton1 As DevExpress.XtraEditors.SimpleButton
+    Friend WithEvents SimpleButton2 As DevExpress.XtraEditors.SimpleButton
+    Friend WithEvents SimpleButton3 As DevExpress.XtraEditors.SimpleButton
+    Friend WithEvents DateEdit1 As DevExpress.XtraEditors.DateEdit
+    Friend WithEvents LabelControl1 As DevExpress.XtraEditors.LabelControl
 End Class
