9488f573eeddc2840e64bcaea2ab1bd64c9ed90f
Septian Primadewa <iansaviour@gmail.com>
2016-10-05 15:22:12 +0700

    Initialization Employee Holiday

--- VolcomInventory/FormMain.vb
+++ VolcomInventory/FormMain.vb
@@ -2223,7 +2223,7 @@ Public Class FormMain
                 FormEmpShiftDet.ShowDialog()
             ElseIf formName = "FormEmpHoliday" Then
                 'Template Shift Employee
-                FormEmpHolidayDet.id_holiday = FormEmpHoliday.GVHoliday.GetFocusedRowCellValue("id_holiday").ToString
+                FormEmpHolidayDet.id_holiday = FormEmpHoliday.GVHoliday.GetFocusedRowCellValue("id_emp_holiday").ToString
                 FormEmpHolidayDet.ShowDialog()
             Else
                 RPSubMenu.Visible = False
@@ -5101,8 +5101,8 @@ Public Class FormMain
             confirm = XtraMessageBox.Show("Are you sure want to delete?", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button2)
             If confirm = DialogResult.Yes Then
                 Try
-                    Dim id_holiday As String = FormEmpHoliday.GVHoliday.GetFocusedRowCellValue("id_holiday").ToString
-                    query = String.Format("DELETE FROM tb_emp_holiday WHERE id_holiday='{0}'", id_holiday)
+                    Dim id_holiday As String = FormEmpHoliday.GVHoliday.GetFocusedRowCellValue("id_emp_holiday").ToString
+                    query = String.Format("DELETE FROM tb_emp_holiday WHERE id_emp_holiday='{0}'", id_holiday)
                     execute_non_query(query, True, "", "", "", "")
                     FormEmpHoliday.view_holiday()
                 Catch ex As Exception
@@ -6226,6 +6226,8 @@ Public Class FormMain
             print(FormEmpShift.GCShift, "Template Shift")
         ElseIf formName = "FormEmpAttnInd" Then
             print(FormEmpAttnInd.GCEmployee, "Employee List")
+        ElseIf formName = "FormEmpHoliday" Then
+            print(FormEmpAttnInd.GCEmployee, "Holiday List")
         Else
             RPSubMenu.Visible = False
         End If
@@ -6734,6 +6736,10 @@ Public Class FormMain
             'Employee Attn Report Summary
             FormEmpAttnSum.Close()
             FormEmpAttnSum.Dispose()
+        ElseIf formName = "FormEmpHoliday" Then
+            'Employee Holiday
+            FormEmpHoliday.Close()
+            FormEmpHoliday.Dispose()
         Else
             RPSubMenu.Visible = False
         End If
