5aa66a19923250c25d89bd4be65f7b3ab9e2e38b
Septian Primadewa <iansaviour@gmail.com>
2016-10-17 10:27:20 +0700

    Save schedule

--- VolcomInventory/FormEmpScheduleTable.vb
+++ VolcomInventory/FormEmpScheduleTable.vb
@@ -13,6 +13,7 @@
                 For j As Integer = 0 To GVSchedule.Columns.Count - 1
                     If Not (GVSchedule.Columns(j).FieldName = "id_employee" Or GVSchedule.Columns(j).FieldName = "employee_code" Or GVSchedule.Columns(j).FieldName = "employee_name") Then
                         Dim date_string As String = GVSchedule.Columns(j).FieldName.ToString
+                        MsgBox(get_schedule(GVSchedule.GetRowCellValue(i, GVSchedule.Columns(j).FieldName.ToString).ToString))
                     End If
                 Next
             Next
@@ -21,4 +22,14 @@
             FormEmpScheduleTableSet.ShowDialog()
         End If
     End Sub
+    Function get_schedule(ByVal shift_code As String)
+        Dim id_schedule = ""
+        If Not shift_code = "" Then
+            Dim query As String = "SELECT id_schedule FROM tb_emp_schedule WHERE shift_code='" & shift_code & "' LIMIT 1"
+            Dim data As DataTable = execute_query(query, -1, True, "", "", "", "")
+            id_schedule = data.Rows(0)("id_schedule").ToString
+        End If
+        Return id_schedule
+    End Function
+
 End Class
