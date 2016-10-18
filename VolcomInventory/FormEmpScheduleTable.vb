dcc6bb2ce5a4e9ae3f405e05a1079d124304af82
Septian Primadewa <iansaviour@gmail.com>
2016-10-18 11:20:27 +0700

    Add shift table with date

--- VolcomInventory/FormEmpScheduleTable.vb
+++ VolcomInventory/FormEmpScheduleTable.vb
@@ -12,24 +12,32 @@
             For i As Integer = 0 To GVSchedule.RowCount - 1
                 For j As Integer = 0 To GVSchedule.Columns.Count - 1
                     If Not (GVSchedule.Columns(j).FieldName = "id_employee" Or GVSchedule.Columns(j).FieldName = "employee_code" Or GVSchedule.Columns(j).FieldName = "employee_name") Then
-                        Dim date_string As String = GVSchedule.Columns(j).FieldName.ToString
-                        MsgBox(get_schedule(GVSchedule.GetRowCellValue(i, GVSchedule.Columns(j).FieldName.ToString).ToString))
+                        get_schedule(GVSchedule.GetRowCellValue(i, GVSchedule.Columns(j).FieldName.ToString), GVSchedule.Columns(j).FieldName.ToString, GVSchedule.GetRowCellValue(i, "id_employee").ToString)
                     End If
                 Next
             Next
+            infoCustom("Employee Scheduled.")
+            Close()
         Else
-            stopCustom("Please choose the employee.")
+            stopCustom("Please choose the employee first.")
             FormEmpScheduleTableSet.ShowDialog()
         End If
     End Sub
-    Function get_schedule(ByVal shift_code As String)
-        Dim id_schedule = ""
+    Sub get_schedule(ByVal shift_code As String, ByVal date_var As String, ByVal id_employee_var As String)
+        Dim id_shift = ""
         If Not shift_code = "" Then
-            Dim query As String = "SELECT id_schedule FROM tb_emp_schedule WHERE shift_code='" & shift_code & "' LIMIT 1"
-            Dim data As DataTable = execute_query(query, -1, True, "", "", "", "")
-            id_schedule = data.Rows(0)("id_schedule").ToString
+            If shift_code = "OFF" Then
+                Dim query_shift As String = "CALL add_shift(" & id_employee_var & ",1,'" & date_var & "','" & date_var & "',2)"
+                execute_non_query(query_shift, True, "", "", "", "")
+            Else
+                Dim query As String = "SELECT id_shift FROM tb_emp_shift WHERE shift_code='" & shift_code & "' LIMIT 1"
+                Dim data As DataTable = execute_query(query, -1, True, "", "", "", "")
+                If data.Rows.Count > 0 Then
+                    Dim query_shift As String = "CALL add_shift(" & id_employee_var & "," & data.Rows(0)("id_shift").ToString & ",'" & date_var & "','" & date_var & "',1)"
+                    execute_non_query(query_shift, True, "", "", "", "")
+                End If
+            End If
         End If
-        Return id_schedule
-    End Function
+    End Sub
 
 End Class
