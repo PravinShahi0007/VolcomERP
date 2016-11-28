0d9f5aeadce976ab3bf823e412d793349751b5b8
Septian Primadewa <iansaviour@gmail.com>
2016-11-28 15:56:35 +0700

    DP Form

---
+++ VolcomInventory/FormEmpDP.vb
@@ -0,0 +1,9 @@
+﻿Public Class FormEmpDP
+    Private Sub FormEmpDP_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        load_date()
+    End Sub
+    Sub load_date()
+        DEStart.EditValue = Now
+        DEUntil.EditValue = Now
+    End Sub
+End Class
