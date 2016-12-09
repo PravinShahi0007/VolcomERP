bf2df62d2cf2d8571edd6432b3acae2e57887689
Septian Primadewa <iansaviour@gmail.com>
2016-12-09 16:30:38 +0700

    Add Form Change Schedule

---
+++ VolcomInventory/FormEmpChSchedule.vb
@@ -0,0 +1,20 @@
+﻿Public Class FormEmpChSchedule
+    Dim bnew_active As String = "1"
+    Dim bedit_active As String = "1"
+    Dim bdel_active As String = "1"
+    '
+
+    Private Sub FormEmpChSchedule_Activated(sender As Object, e As EventArgs) Handles MyBase.Activated
+        FormMain.show_rb(Name)
+        checkFormAccess(Name)
+        button_main(bnew_active, bedit_active, bdel_active)
+    End Sub
+
+    Private Sub FormEmpChSchedule_FormClosed(sender As Object, e As FormClosedEventArgs) Handles MyBase.FormClosed
+        Dispose()
+    End Sub
+
+    Private Sub FormEmpChSchedule_Deactivate(sender As Object, e As EventArgs) Handles MyBase.Deactivate
+        FormMain.hide_rb()
+    End Sub
+End Class
