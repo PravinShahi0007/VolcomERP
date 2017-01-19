ac0511cb564a1da976a7bd3b6138d973f2cfc83e
Satrya Anas <catur.satrya@gmail.com>
2017-01-19 16:03:16 +0800

    new move

--- VolcomInventory/FormFGLineListMoveSeason.vb
+++ VolcomInventory/FormFGLineListMoveSeason.vb
@@ -4,6 +4,8 @@
     Private Sub FormFGLineListMoveSeason_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         viewSeason()
         viewPlanOrder()
+        SLESeasonFrom.EditValue = FormFGLineList.SLESeason.EditValue.ToString
+        SLESeason.EditValue = FormFGLineList.SLESeason.EditValue.ToString
     End Sub
 
     Sub viewPlanOrder()
@@ -22,6 +24,7 @@
         End If
         query += "ORDER BY b.range DESC"
         viewSearchLookupQuery(SLESeason, query, "id_season", "season", "id_season")
+        viewSearchLookupQuery(SLESeasonFrom, query, "id_season", "season", "id_season")
     End Sub
 
     Private Sub LEPlanStatus_EditValueChanged(sender As Object, e As EventArgs) Handles LEPlanStatus.EditValueChanged
@@ -30,47 +33,64 @@
             stt = LEPlanStatus.EditValue.ToString
         Catch ex As Exception
         End Try
-        If stt <> "3" Then
-            LabelSeason.Visible = False
-            SLESeason.Visible = False
+        If stt = "1" Then
+            LabelSeason.Enabled = False
+            SLESeason.Enabled = False
+            LabelSeasonFrom.Enabled = False
+            SLESeasonFrom.Enabled = False
+            SLESeason.EditValue = FormFGLineList.SLESeason.EditValue.ToString
+        ElseIf stt = "2" Then
+            LabelSeason.Enabled = False
+            SLESeason.Enabled = False
+            LabelSeasonFrom.Enabled = False
+            SLESeasonFrom.Enabled = False
         Else
-            LabelSeason.Visible = True
-            SLESeason.Visible = True
+            LabelSeason.Enabled = True
+            SLESeason.Enabled = True
+            LabelSeasonFrom.Enabled = False
+            SLESeasonFrom.Enabled = False
         End If
     End Sub
 
     Private Sub BtnUpdateRec_Click(sender As Object, e As EventArgs) Handles BtnUpdateRec.Click
-        Dim confirm As DialogResult = DevExpress.XtraEditors.XtraMessageBox.Show("Are you sure want to change line list status for these data?", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button2)
-        If confirm = Windows.Forms.DialogResult.Yes Then
-            Cursor = Cursors.WaitCursor
-            Dim id_lookup_status_order As String = LEPlanStatus.EditValue.ToString
-            Dim id_season_to As String = SLESeason.EditValue.ToString
-            For i As Integer = 0 To ((FormFGLineList.BGVLineList.RowCount - 1) - GetGroupRowCount(FormFGLineList.BGVLineList))
-                Dim id_design As String = FormFGLineList.BGVLineList.GetRowCellValue(i, "id_design").ToString
-                Dim id_season_from As String = FormFGLineList.BGVLineList.GetRowCellValue(i, "id_season").ToString
+        If LEPlanStatus.EditValue.ToString = "3" And (SLESeason.EditValue.ToString = SLESeasonFrom.EditValue.ToString) Then
+            errorCustom("Season must different")
+        Else
+            Dim confirm As DialogResult = DevExpress.XtraEditors.XtraMessageBox.Show("Are you sure want to change line list status for these data?", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button2)
+            If confirm = Windows.Forms.DialogResult.Yes Then
+                Cursor = Cursors.WaitCursor
+                Dim id_lookup_status_order As String = LEPlanStatus.EditValue.ToString
+                Dim id_season_to As String = SLESeason.EditValue.ToString
+                For i As Integer = 0 To ((FormFGLineList.BGVLineList.RowCount - 1) - GetGroupRowCount(FormFGLineList.BGVLineList))
+                    Dim id_design As String = FormFGLineList.BGVLineList.GetRowCellValue(i, "id_design").ToString
+                    Dim id_season_from As String = SLESeasonFrom.EditValue.ToString
 
-                'update line list status
-                Dim query_upd As String = "UPDATE tb_m_design SET id_lookup_status_order='" + id_lookup_status_order + "' "
-                If id_lookup_status_order = "2" Then 'drop
-                    query_upd += ", id_active=2 "
-                ElseIf id_lookup_status_order = "3" Then 'move
-                    query_upd += ", id_season_move='" + id_season_to + "' "
-                End If
-                query_upd += "WHERE id_design='" + id_design + "' "
-                execute_non_query(query_upd, True, "", "", "", "")
+                    'update line list status
+                    Dim query_upd As String = "UPDATE tb_m_design SET id_lookup_status_order='" + id_lookup_status_order + "' "
+                    If id_lookup_status_order = "2" Then 'drop
+                        query_upd += ", id_active=2 "
+                    ElseIf id_lookup_status_order = "3" Then 'move
+                        query_upd += ",id_season='" + id_season_to + "', id_season_move=IF(ISNULL(id_season_move),'" + id_season_from + "',id_season_move) "
+                    Else
+                        query_upd += ", id_season='" + id_season_to + "', id_season_move=NULL "
+                    End If
+                    query_upd += "WHERE id_design='" + id_design + "' "
+                    execute_non_query(query_upd, True, "", "", "", "")
 
 
-                'update time
-                Dim upd As New ClassDesign
-                upd.updatedTime(id_design)
+                    'update time
+                    Dim upd As New ClassDesign
+                    upd.updatedTime(id_design)
 
-                PBC.PerformStep()
-                PBC.Update()
-            Next
-            FormFGLineList.viewLineList()
-            Close()
-            Cursor = Cursors.Default
+                    PBC.PerformStep()
+                    PBC.Update()
+                Next
+                FormFGLineList.viewLineList()
+                Close()
+                Cursor = Cursors.Default
+            End If
         End If
+
     End Sub
 
     Private Sub FormFGLineListMoveSeason_FormClosed(sender As Object, e As FormClosedEventArgs) Handles MyBase.FormClosed
