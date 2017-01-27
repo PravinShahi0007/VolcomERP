c15f954b5ea1333474bbc90617b88155ba9d87d9
Septian Primadewa <iansaviour@gmail.com>
2017-01-27 16:32:59 +0700

    Add propose from

--- VolcomInventory/FormEmpAttnAssignDet.Designer.vb
+++ VolcomInventory/FormEmpAttnAssignDet.Designer.vb
@@ -22,38 +22,41 @@ Partial Class FormEmpAttnAssignDet
         Me.components = New System.ComponentModel.Container()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FormEmpAttnAssignDet))
         Me.PanelControl1 = New DevExpress.XtraEditors.PanelControl()
-        Me.LargeImageCollection = New DevExpress.Utils.ImageCollection(Me.components)
-        Me.BSetSchedule = New DevExpress.XtraEditors.SimpleButton()
-        Me.SimpleButton1 = New DevExpress.XtraEditors.SimpleButton()
         Me.SimpleButton2 = New DevExpress.XtraEditors.SimpleButton()
+        Me.LargeImageCollection = New DevExpress.Utils.ImageCollection(Me.components)
+        Me.SimpleButton1 = New DevExpress.XtraEditors.SimpleButton()
+        Me.BSetSchedule = New DevExpress.XtraEditors.SimpleButton()
         Me.GroupControl1 = New DevExpress.XtraEditors.GroupControl()
+        Me.TextEdit1 = New DevExpress.XtraEditors.TextEdit()
+        Me.LabelControl1 = New DevExpress.XtraEditors.LabelControl()
         Me.TENumber = New DevExpress.XtraEditors.TextEdit()
         Me.LabelControl7 = New DevExpress.XtraEditors.LabelControl()
         Me.DEJoinDate = New DevExpress.XtraEditors.DateEdit()
         Me.LabelControl4 = New DevExpress.XtraEditors.LabelControl()
         Me.XtraTabControl1 = New DevExpress.XtraTab.XtraTabControl()
         Me.XTPBefore = New DevExpress.XtraTab.XtraTabPage()
+        Me.GCScheduleBefore = New DevExpress.XtraGrid.GridControl()
+        Me.GVScheduleBefore = New DevExpress.XtraGrid.Views.Grid.GridView()
         Me.XTPAfter = New DevExpress.XtraTab.XtraTabPage()
-        Me.GCSchedule = New DevExpress.XtraGrid.GridControl()
-        Me.GVSchedule = New DevExpress.XtraGrid.Views.Grid.GridView()
-        Me.GridControl1 = New DevExpress.XtraGrid.GridControl()
-        Me.GridView1 = New DevExpress.XtraGrid.Views.Grid.GridView()
+        Me.GCScheduleAfter = New DevExpress.XtraGrid.GridControl()
+        Me.GVScheduleAfter = New DevExpress.XtraGrid.Views.Grid.GridView()
         CType(Me.PanelControl1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.PanelControl1.SuspendLayout()
         CType(Me.LargeImageCollection, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.GroupControl1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.GroupControl1.SuspendLayout()
+        CType(Me.TextEdit1.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.TENumber.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.DEJoinDate.Properties.CalendarTimeProperties, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.DEJoinDate.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.XtraTabControl1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.XtraTabControl1.SuspendLayout()
         Me.XTPBefore.SuspendLayout()
+        CType(Me.GCScheduleBefore, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.GVScheduleBefore, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.XTPAfter.SuspendLayout()
-        CType(Me.GCSchedule, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.GVSchedule, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.GridControl1, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.GridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.GCScheduleAfter, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.GVScheduleAfter, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'PanelControl1
@@ -66,6 +69,17 @@ Partial Class FormEmpAttnAssignDet
         Me.PanelControl1.Name = "PanelControl1"
         Me.PanelControl1.Size = New System.Drawing.Size(714, 36)
         Me.PanelControl1.TabIndex = 2
+        '
+        'SimpleButton2
+        '
+        Me.SimpleButton2.Dock = System.Windows.Forms.DockStyle.Left
+        Me.SimpleButton2.ImageIndex = 4
+        Me.SimpleButton2.ImageList = Me.LargeImageCollection
+        Me.SimpleButton2.Location = New System.Drawing.Point(2, 2)
+        Me.SimpleButton2.Name = "SimpleButton2"
+        Me.SimpleButton2.Size = New System.Drawing.Size(71, 32)
+        Me.SimpleButton2.TabIndex = 2
+        Me.SimpleButton2.Text = "Mark"
         '
         'LargeImageCollection
         '
@@ -92,17 +106,6 @@ Partial Class FormEmpAttnAssignDet
         Me.LargeImageCollection.Images.SetKeyName(18, "estimate_icon32.png")
         Me.LargeImageCollection.Images.SetKeyName(19, "copy_icon.png")
         '
-        'BSetSchedule
-        '
-        Me.BSetSchedule.Dock = System.Windows.Forms.DockStyle.Right
-        Me.BSetSchedule.ImageIndex = 19
-        Me.BSetSchedule.ImageList = Me.LargeImageCollection
-        Me.BSetSchedule.Location = New System.Drawing.Point(573, 2)
-        Me.BSetSchedule.Name = "BSetSchedule"
-        Me.BSetSchedule.Size = New System.Drawing.Size(139, 32)
-        Me.BSetSchedule.TabIndex = 0
-        Me.BSetSchedule.Text = "Propose Schedule"
-        '
         'SimpleButton1
         '
         Me.SimpleButton1.Dock = System.Windows.Forms.DockStyle.Right
@@ -114,20 +117,22 @@ Partial Class FormEmpAttnAssignDet
         Me.SimpleButton1.TabIndex = 1
         Me.SimpleButton1.Text = "Cancel"
         '
-        'SimpleButton2
+        'BSetSchedule
         '
-        Me.SimpleButton2.Dock = System.Windows.Forms.DockStyle.Left
-        Me.SimpleButton2.ImageIndex = 4
-        Me.SimpleButton2.ImageList = Me.LargeImageCollection
-        Me.SimpleButton2.Location = New System.Drawing.Point(2, 2)
-        Me.SimpleButton2.Name = "SimpleButton2"
-        Me.SimpleButton2.Size = New System.Drawing.Size(71, 32)
-        Me.SimpleButton2.TabIndex = 2
-        Me.SimpleButton2.Text = "Mark"
+        Me.BSetSchedule.Dock = System.Windows.Forms.DockStyle.Right
+        Me.BSetSchedule.ImageIndex = 19
+        Me.BSetSchedule.ImageList = Me.LargeImageCollection
+        Me.BSetSchedule.Location = New System.Drawing.Point(573, 2)
+        Me.BSetSchedule.Name = "BSetSchedule"
+        Me.BSetSchedule.Size = New System.Drawing.Size(139, 32)
+        Me.BSetSchedule.TabIndex = 0
+        Me.BSetSchedule.Text = "Propose Schedule"
         '
         'GroupControl1
         '
         Me.GroupControl1.CaptionLocation = DevExpress.Utils.Locations.Left
+        Me.GroupControl1.Controls.Add(Me.TextEdit1)
+        Me.GroupControl1.Controls.Add(Me.LabelControl1)
         Me.GroupControl1.Controls.Add(Me.TENumber)
         Me.GroupControl1.Controls.Add(Me.LabelControl7)
         Me.GroupControl1.Controls.Add(Me.DEJoinDate)
@@ -137,6 +142,24 @@ Partial Class FormEmpAttnAssignDet
         Me.GroupControl1.Name = "GroupControl1"
         Me.GroupControl1.Size = New System.Drawing.Size(714, 75)
         Me.GroupControl1.TabIndex = 3
+        '
+        'TextEdit1
+        '
+        Me.TextEdit1.EditValue = ""
+        Me.TextEdit1.Location = New System.Drawing.Point(478, 12)
+        Me.TextEdit1.Name = "TextEdit1"
+        Me.TextEdit1.Properties.EditValueChangedDelay = 1
+        Me.TextEdit1.Properties.ReadOnly = True
+        Me.TextEdit1.Size = New System.Drawing.Size(224, 20)
+        Me.TextEdit1.TabIndex = 97
+        '
+        'LabelControl1
+        '
+        Me.LabelControl1.Location = New System.Drawing.Point(412, 15)
+        Me.LabelControl1.Name = "LabelControl1"
+        Me.LabelControl1.Size = New System.Drawing.Size(60, 13)
+        Me.LabelControl1.TabIndex = 98
+        Me.LabelControl1.Text = "Proposed By"
         '
         'TENumber
         '
@@ -189,51 +212,51 @@ Partial Class FormEmpAttnAssignDet
         '
         'XTPBefore
         '
-        Me.XTPBefore.Controls.Add(Me.GCSchedule)
+        Me.XTPBefore.Controls.Add(Me.GCScheduleBefore)
         Me.XTPBefore.Name = "XTPBefore"
         Me.XTPBefore.Size = New System.Drawing.Size(708, 172)
         Me.XTPBefore.Text = "Before"
         '
+        'GCScheduleBefore
+        '
+        Me.GCScheduleBefore.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.GCScheduleBefore.Location = New System.Drawing.Point(0, 0)
+        Me.GCScheduleBefore.MainView = Me.GVScheduleBefore
+        Me.GCScheduleBefore.Name = "GCScheduleBefore"
+        Me.GCScheduleBefore.Size = New System.Drawing.Size(708, 172)
+        Me.GCScheduleBefore.TabIndex = 3
+        Me.GCScheduleBefore.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() {Me.GVScheduleBefore})
+        '
+        'GVScheduleBefore
+        '
+        Me.GVScheduleBefore.GridControl = Me.GCScheduleBefore
+        Me.GVScheduleBefore.Name = "GVScheduleBefore"
+        Me.GVScheduleBefore.OptionsView.ColumnAutoWidth = False
+        Me.GVScheduleBefore.OptionsView.ShowGroupPanel = False
+        '
         'XTPAfter
         '
-        Me.XTPAfter.Controls.Add(Me.GridControl1)
+        Me.XTPAfter.Controls.Add(Me.GCScheduleAfter)
         Me.XTPAfter.Name = "XTPAfter"
         Me.XTPAfter.Size = New System.Drawing.Size(708, 172)
         Me.XTPAfter.Text = "After"
         '
-        'GCSchedule
+        'GCScheduleAfter
         '
-        Me.GCSchedule.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.GCSchedule.Location = New System.Drawing.Point(0, 0)
-        Me.GCSchedule.MainView = Me.GVSchedule
-        Me.GCSchedule.Name = "GCSchedule"
-        Me.GCSchedule.Size = New System.Drawing.Size(708, 172)
-        Me.GCSchedule.TabIndex = 3
-        Me.GCSchedule.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() {Me.GVSchedule})
+        Me.GCScheduleAfter.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.GCScheduleAfter.Location = New System.Drawing.Point(0, 0)
+        Me.GCScheduleAfter.MainView = Me.GVScheduleAfter
+        Me.GCScheduleAfter.Name = "GCScheduleAfter"
+        Me.GCScheduleAfter.Size = New System.Drawing.Size(708, 172)
+        Me.GCScheduleAfter.TabIndex = 3
+        Me.GCScheduleAfter.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() {Me.GVScheduleAfter})
         '
-        'GVSchedule
+        'GVScheduleAfter
         '
-        Me.GVSchedule.GridControl = Me.GCSchedule
-        Me.GVSchedule.Name = "GVSchedule"
-        Me.GVSchedule.OptionsView.ColumnAutoWidth = False
-        Me.GVSchedule.OptionsView.ShowGroupPanel = False
-        '
-        'GridControl1
-        '
-        Me.GridControl1.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.GridControl1.Location = New System.Drawing.Point(0, 0)
-        Me.GridControl1.MainView = Me.GridView1
-        Me.GridControl1.Name = "GridControl1"
-        Me.GridControl1.Size = New System.Drawing.Size(708, 172)
-        Me.GridControl1.TabIndex = 3
-        Me.GridControl1.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() {Me.GridView1})
-        '
-        'GridView1
-        '
-        Me.GridView1.GridControl = Me.GridControl1
-        Me.GridView1.Name = "GridView1"
-        Me.GridView1.OptionsView.ColumnAutoWidth = False
-        Me.GridView1.OptionsView.ShowGroupPanel = False
+        Me.GVScheduleAfter.GridControl = Me.GCScheduleAfter
+        Me.GVScheduleAfter.Name = "GVScheduleAfter"
+        Me.GVScheduleAfter.OptionsView.ColumnAutoWidth = False
+        Me.GVScheduleAfter.OptionsView.ShowGroupPanel = False
         '
         'FormEmpAttnAssignDet
         '
@@ -251,17 +274,18 @@ Partial Class FormEmpAttnAssignDet
         CType(Me.GroupControl1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.GroupControl1.ResumeLayout(False)
         Me.GroupControl1.PerformLayout()
+        CType(Me.TextEdit1.Properties, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.TENumber.Properties, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.DEJoinDate.Properties.CalendarTimeProperties, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.DEJoinDate.Properties, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.XtraTabControl1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.XtraTabControl1.ResumeLayout(False)
         Me.XTPBefore.ResumeLayout(False)
+        CType(Me.GCScheduleBefore, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.GVScheduleBefore, System.ComponentModel.ISupportInitialize).EndInit()
         Me.XTPAfter.ResumeLayout(False)
-        CType(Me.GCSchedule, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.GVSchedule, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.GridControl1, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.GridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.GCScheduleAfter, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.GVScheduleAfter, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
@@ -278,8 +302,10 @@ Partial Class FormEmpAttnAssignDet
     Friend WithEvents XtraTabControl1 As DevExpress.XtraTab.XtraTabControl
     Friend WithEvents XTPBefore As DevExpress.XtraTab.XtraTabPage
     Friend WithEvents XTPAfter As DevExpress.XtraTab.XtraTabPage
-    Friend WithEvents GCSchedule As DevExpress.XtraGrid.GridControl
-    Friend WithEvents GVSchedule As DevExpress.XtraGrid.Views.Grid.GridView
-    Friend WithEvents GridControl1 As DevExpress.XtraGrid.GridControl
-    Friend WithEvents GridView1 As DevExpress.XtraGrid.Views.Grid.GridView
+    Friend WithEvents GCScheduleBefore As DevExpress.XtraGrid.GridControl
+    Friend WithEvents GVScheduleBefore As DevExpress.XtraGrid.Views.Grid.GridView
+    Friend WithEvents GCScheduleAfter As DevExpress.XtraGrid.GridControl
+    Friend WithEvents GVScheduleAfter As DevExpress.XtraGrid.Views.Grid.GridView
+    Friend WithEvents TextEdit1 As DevExpress.XtraEditors.TextEdit
+    Friend WithEvents LabelControl1 As DevExpress.XtraEditors.LabelControl
 End Class
