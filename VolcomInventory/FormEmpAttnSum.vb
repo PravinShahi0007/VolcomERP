b95b58999edf7c1ad332bad01f57f5673d44e512
Septian Primadewa <iansaviour@gmail.com>
2017-02-14 14:26:01 +0700

    Add Table Schedule

--- VolcomInventory/FormEmpAttnSum.vb
+++ VolcomInventory/FormEmpAttnSum.vb
@@ -185,54 +185,55 @@
             dept = LEDeptSum.EditValue.ToString
         End If
 
-        Dim query As String = "SELECT * FROM tb_m_employee "
+        Dim query As String = "SELECT * FROM tb_m_employee WHERE id_departement LIKE '" & dept & "' AND id_employee_active='1'"
+        Dim data As DataTable = execute_query(query, -1, True, "", "", "", "")
 
-        'If GVEmploye.RowCount > 0 Then
-        '    '
-        '    FormEmpScheduleTable.GVSchedule.Columns.AddVisible("id_employee", "ID")
-        '    FormEmpScheduleTable.GVSchedule.Columns("id_employee").OptionsColumn.AllowEdit = False
-        '    FormEmpScheduleTable.GVSchedule.Columns("id_employee").Visible = False
+        GVScheduleTable.Columns.Clear()
 
-        '    FormEmpScheduleTable.GVSchedule.Columns.AddVisible("employee_code", "NIP")
-        '    FormEmpScheduleTable.GVSchedule.Columns("employee_code").OptionsColumn.AllowEdit = False
+        If data.Rows.Count > 0 Then
+            '
+            GVScheduleTable.Columns.AddVisible("id_employee", "ID")
+            GVScheduleTable.Columns("id_employee").OptionsColumn.AllowEdit = False
+            GVScheduleTable.Columns("id_employee").Visible = False
 
-        '    FormEmpScheduleTable.GVSchedule.Columns.AddVisible("employee_name", "Name")
-        '    FormEmpScheduleTable.GVSchedule.Columns("employee_name").OptionsColumn.AllowEdit = False
+            GVScheduleTable.Columns.AddVisible("employee_code", "NIP")
+            GVScheduleTable.Columns("employee_code").OptionsColumn.AllowEdit = False
 
-        '    While (curD <= endP)
-        '        FormEmpScheduleTable.GVSchedule.Columns.AddVisible(curD.ToString("yyyy-MM-dd"), curD.ToString("dd MMM yyyy"))
-        '        string_date += ",'" & curD.ToString("yyyy-MM-dd") & "'"
-        '        curD = curD.AddDays(1)
-        '    End While
-        '    '
-        '    Dim query As String = "SELECT '' as id_employee,'' as employee_code,'' as employee_name" & string_date
-        '    Dim data As DataTable = execute_query(query, -1, True, "", "", "", "")
-        '    FormEmpScheduleTable.GCSchedule.DataSource = data
-        '    FormEmpScheduleTable.GVSchedule.DeleteRow(0)
-        '    '
-        '    For i As Integer = 0 To GVEmployee.RowCount - 1
-        '        Dim query_emp As String = "SELECT emp.date,emp.shift_code FROM tb_emp_schedule emp WHERE emp.id_employee='" & GVEmployee.GetRowCellValue(i, "id_employee").ToString & "' AND emp.date >= '" & startP.ToString("yyyy-MM-dd") & "' AND emp.date <= '" & endP.ToString("yyyy-MM-dd") & "'"
-        '        Dim data_emp As DataTable = execute_query(query_emp, -1, True, "", "", "", "")
+            GVScheduleTable.Columns.AddVisible("employee_name", "Name")
+            GVScheduleTable.Columns("employee_name").OptionsColumn.AllowEdit = False
 
-        '        Dim newRow As DataRow = (TryCast(FormEmpScheduleTable.GCSchedule.DataSource, DataTable)).NewRow()
-        '        newRow("id_employee") = GVEmployee.GetRowCellValue(i, "id_employee").ToString
-        '        newRow("employee_code") = GVEmployee.GetRowCellValue(i, "employee_code").ToString
-        '        newRow("employee_name") = GVEmployee.GetRowCellValue(i, "employee_name").ToString
-        '        If data_emp.Rows.Count > 0 Then
-        '            For j As Integer = 0 To data_emp.Rows.Count - 1
-        '                newRow(Date.Parse(data_emp.Rows(j)("date").ToString).ToString("yyyy-MM-dd")) = data_emp.Rows(j)("shift_code").ToString.ToUpper
-        '            Next
-        '        End If
+            While (curD <= endP)
+                GVScheduleTable.Columns.AddVisible(curD.ToString("yyyy-MM-dd"), curD.ToString("dddd, dd MMM yyyy"))
+                string_date += ",'" & curD.ToString("yyyy-MM-dd") & "'"
+                curD = curD.AddDays(1)
+            End While
+            '
+            Dim query_x As String = "SELECT '' as id_employee,'' as employee_code,'' as employee_name" & string_date
+            Dim data_x As DataTable = execute_query(query_x, -1, True, "", "", "", "")
+            GCScheduleTable.DataSource = data_x
+            GVScheduleTable.DeleteRow(0)
+            '
+            For i As Integer = 0 To data.Rows.Count - 1
+                Dim query_emp As String = "SELECT emp.date,emp.shift_code FROM tb_emp_schedule emp WHERE emp.id_employee='" & data.Rows(i)("id_employee").ToString & "' AND emp.date >= '" & startP.ToString("yyyy-MM-dd") & "' AND emp.date <= '" & endP.ToString("yyyy-MM-dd") & "'"
+                Dim data_emp As DataTable = execute_query(query_emp, -1, True, "", "", "", "")
 
-        '        TryCast(FormEmpScheduleTable.GCSchedule.DataSource, DataTable).Rows.Add(newRow)
-        '        FormEmpScheduleTable.GCSchedule.RefreshDataSource()
-        '    Next
-        '    FormEmpScheduleTable.GVSchedule.BestFitColumns()
-        '    '
-        '    Close()
-        'Else
-        '    stopCustom("Please select employee first.")
-        'End If
+                Dim newRow As DataRow = (TryCast(GCScheduleTable.DataSource, DataTable)).NewRow()
+                newRow("id_employee") = data.Rows(i)("id_employee").ToString
+                newRow("employee_code") = data.Rows(i)("employee_code").ToString
+                newRow("employee_name") = data.Rows(i)("employee_name").ToString
+                If data_emp.Rows.Count > 0 Then
+                    For j As Integer = 0 To data_emp.Rows.Count - 1
+                        newRow(Date.Parse(data_emp.Rows(j)("date").ToString).ToString("yyyy-MM-dd")) = data_emp.Rows(j)("shift_code").ToString.ToUpper
+                    Next
+                End If
+
+                TryCast(GCScheduleTable.DataSource, DataTable).Rows.Add(newRow)
+                GCScheduleTable.RefreshDataSource()
+            Next
+            GVScheduleTable.BestFitColumns()
+        Else
+            stopCustom("Please select employee first.")
+        End If
 
     End Sub
 
