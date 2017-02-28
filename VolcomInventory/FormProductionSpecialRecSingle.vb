63c877a3eced15eec8bd40c507b77291b8820be7
Septian Primadewa <iansaviour@gmail.com>
2017-02-28 16:14:06 +0700

    Add Date to tolerance

--- VolcomInventory/FormProductionSpecialRecSingle.vb
+++ VolcomInventory/FormProductionSpecialRecSingle.vb
@@ -11,9 +11,12 @@
         Dim confirm As DialogResult = DevExpress.XtraEditors.XtraMessageBox.Show("Are you sure want to disable receving tolerance for this PO ?", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button2)
         If confirm = DialogResult.Yes Then
             If Not TEMemoNumber.Text = "" Then
-                Dim query As String = "UPDATE tb_prod_order SET is_special_rec='1',special_rec_memo='" & TEMemoNumber.Text & "',special_rec_datetime=NOW() WHERE id_prod_order='" & FormProductionSpecialRec.GVProd.GetFocusedRowCellValue("id_prod_order").ToString & "'"
+                Dim id_prod_order As String = FormProductionSpecialRec.GVProd.GetFocusedRowCellValue("id_prod_order").ToString
+                Dim query As String = "UPDATE tb_prod_order SET is_special_rec='1',special_rec_memo='" & addSlashes(TEMemoNumber.Text) & "',special_rec_datetime=NOW() WHERE id_prod_order='" & id_prod_order & "'"
                 execute_non_query(query, True, "", "", "", "")
                 infoCustom("Receving tolerance disabled")
+                FormProductionSpecialRec.view_production_order()
+                FormProductionSpecialRec.GVProd.FocusedRowHandle = find_row(FormProductionSpecialRec.GVProd, "id_prod_order", id_prod_order)
                 Close()
             End If
         End If
