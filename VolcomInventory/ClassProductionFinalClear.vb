d61aa91fb5be13cd24c62695377bb4f3c9b3a1b2
Satrya Anas <catur.satrya@gmail.com>
2017-03-03 11:50:34 +0800

    main view

---
+++ VolcomInventory/ClassProductionFinalClear.vb
@@ -0,0 +1,35 @@
+﻿Public Class ClassProductionFinalClear
+    Public Function queryMain(ByVal condition As String, ByVal order_type As String) As String
+        If order_type = "1" Then
+            order_type = "ASC "
+        ElseIf order_type = "2" Then
+            order_type = "DESC "
+        End If
+
+        If condition <> "-1" Then
+            condition = condition
+        Else
+            condition = ""
+        End If
+
+        Dim query As String = "SELECT f.id_prod_fc, f.id_prod_order, 
+        f.id_comp_from, cf.comp_number AS `comp_from_number`, cf.comp_name AS `comp_from_name`, CONCAT(cf.comp_number, ' - ', cf.comp_name) AS `comp_from`,
+        f.id_comp_to, ct.comp_number AS `comp_to_number`, ct.comp_name AS `comp_to_name`, CONCAT(ct.comp_number, ' - ', ct.comp_name) AS `comp_to`,
+        f.id_pl_category, c.pl_category,
+        f.prod_fc_number, f.prod_fc_date, f.prod_fc_note, d.`total`, f.id_report_status, r.report_status 
+        FROM tb_prod_fc f 
+        INNER JOIN tb_m_comp cf ON cf.id_comp = f.id_comp_from 
+        INNER JOIN tb_m_comp ct ON ct.id_comp = f.id_comp_to 
+        INNER JOIN tb_lookup_pl_category c ON c.id_pl_category = f.id_pl_category
+        INNER JOIN tb_lookup_report_status r ON r.id_report_status = f.id_report_status
+        LEFT JOIN(
+	        SELECT det.id_prod_fc, SUM(det.prod_fc_det_qty) AS `total`
+	        FROM tb_prod_fc_det det
+	        GROUP BY det.id_prod_fc
+        ) d ON d.id_prod_fc = f.id_prod_fc
+        WHERE f.id_prod_fc>0 "
+        query += condition + " "
+        query += "ORDER BY f.id_prod_fc " + order_type
+        Return query
+    End Function
+End Class
--- VolcomInventory/FormProductionFinalClear.Designer.vb
+++ VolcomInventory/FormProductionFinalClear.Designer.vb
@@ -19,8 +19,162 @@ Partial Class FormProductionFinalClear
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.GCFilter = New DevExpress.XtraEditors.GroupControl()
+        Me.BtnView = New DevExpress.XtraEditors.SimpleButton()
+        Me.BHide = New DevExpress.XtraEditors.SimpleButton()
+        Me.BExpand = New DevExpress.XtraEditors.SimpleButton()
+        Me.DEUntil = New DevExpress.XtraEditors.DateEdit()
+        Me.DEFrom = New DevExpress.XtraEditors.DateEdit()
+        Me.LabelControl2 = New DevExpress.XtraEditors.LabelControl()
+        Me.LabelControl3 = New DevExpress.XtraEditors.LabelControl()
+        Me.GCFinalClear = New DevExpress.XtraGrid.GridControl()
+        Me.GVFinalClear = New DevExpress.XtraGrid.Views.Grid.GridView()
+        CType(Me.GCFilter, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GCFilter.SuspendLayout()
+        CType(Me.DEUntil.Properties.CalendarTimeProperties, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DEUntil.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DEFrom.Properties.CalendarTimeProperties, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DEFrom.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.GCFinalClear, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.GVFinalClear, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'GCFilter
+        '
+        Me.GCFilter.CaptionLocation = DevExpress.Utils.Locations.Left
+        Me.GCFilter.Controls.Add(Me.BtnView)
+        Me.GCFilter.Controls.Add(Me.BHide)
+        Me.GCFilter.Controls.Add(Me.BExpand)
+        Me.GCFilter.Controls.Add(Me.DEUntil)
+        Me.GCFilter.Controls.Add(Me.DEFrom)
+        Me.GCFilter.Controls.Add(Me.LabelControl2)
+        Me.GCFilter.Controls.Add(Me.LabelControl3)
+        Me.GCFilter.Dock = System.Windows.Forms.DockStyle.Top
+        Me.GCFilter.Location = New System.Drawing.Point(0, 0)
+        Me.GCFilter.Name = "GCFilter"
+        Me.GCFilter.Size = New System.Drawing.Size(754, 39)
+        Me.GCFilter.TabIndex = 3
+        '
+        'BtnView
+        '
+        Me.BtnView.Location = New System.Drawing.Point(319, 9)
+        Me.BtnView.LookAndFeel.SkinName = "Blue"
+        Me.BtnView.Name = "BtnView"
+        Me.BtnView.Size = New System.Drawing.Size(63, 20)
+        Me.BtnView.TabIndex = 8896
+        Me.BtnView.Text = "View"
+        '
+        'BHide
+        '
+        Me.BHide.ImageIndex = 9
+        Me.BHide.Location = New System.Drawing.Point(938, 14)
+        Me.BHide.Name = "BHide"
+        Me.BHide.Size = New System.Drawing.Size(104, 20)
+        Me.BHide.TabIndex = 8898
+        Me.BHide.Text = "Hide All Detail"
+        Me.BHide.Visible = False
+        '
+        'BExpand
+        '
+        Me.BExpand.ImageIndex = 8
+        Me.BExpand.Location = New System.Drawing.Point(835, 14)
+        Me.BExpand.Name = "BExpand"
+        Me.BExpand.Size = New System.Drawing.Size(99, 20)
+        Me.BExpand.TabIndex = 8897
+        Me.BExpand.Text = "Expand All Detail"
+        Me.BExpand.Visible = False
+        '
+        'DEUntil
+        '
+        Me.DEUntil.EditValue = Nothing
+        Me.DEUntil.Location = New System.Drawing.Point(202, 9)
+        Me.DEUntil.Name = "DEUntil"
+        Me.DEUntil.Properties.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() {New DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)})
+        Me.DEUntil.Properties.CalendarTimeProperties.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() {New DevExpress.XtraEditors.Controls.EditorButton()})
+        Me.DEUntil.Properties.DisplayFormat.FormatString = "dd MMM yyyy"
+        Me.DEUntil.Properties.DisplayFormat.FormatType = DevExpress.Utils.FormatType.DateTime
+        Me.DEUntil.Size = New System.Drawing.Size(111, 20)
+        Me.DEUntil.TabIndex = 8895
+        '
+        'DEFrom
+        '
+        Me.DEFrom.EditValue = Nothing
+        Me.DEFrom.Location = New System.Drawing.Point(58, 9)
+        Me.DEFrom.Name = "DEFrom"
+        Me.DEFrom.Properties.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() {New DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)})
+        Me.DEFrom.Properties.CalendarTimeProperties.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() {New DevExpress.XtraEditors.Controls.EditorButton()})
+        Me.DEFrom.Properties.DisplayFormat.FormatString = "dd MMM yyyy"
+        Me.DEFrom.Properties.DisplayFormat.FormatType = DevExpress.Utils.FormatType.DateTime
+        Me.DEFrom.Size = New System.Drawing.Size(111, 20)
+        Me.DEFrom.TabIndex = 8894
+        '
+        'LabelControl2
+        '
+        Me.LabelControl2.Location = New System.Drawing.Point(175, 12)
+        Me.LabelControl2.Name = "LabelControl2"
+        Me.LabelControl2.Size = New System.Drawing.Size(21, 13)
+        Me.LabelControl2.TabIndex = 8893
+        Me.LabelControl2.Text = "Until"
+        '
+        'LabelControl3
+        '
+        Me.LabelControl3.Location = New System.Drawing.Point(28, 12)
+        Me.LabelControl3.Name = "LabelControl3"
+        Me.LabelControl3.Size = New System.Drawing.Size(24, 13)
+        Me.LabelControl3.TabIndex = 8892
+        Me.LabelControl3.Text = "From"
+        '
+        'GCFinalClear
+        '
+        Me.GCFinalClear.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.GCFinalClear.Location = New System.Drawing.Point(0, 39)
+        Me.GCFinalClear.MainView = Me.GVFinalClear
+        Me.GCFinalClear.Name = "GCFinalClear"
+        Me.GCFinalClear.Size = New System.Drawing.Size(754, 277)
+        Me.GCFinalClear.TabIndex = 4
+        Me.GCFinalClear.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() {Me.GVFinalClear})
+        '
+        'GVFinalClear
+        '
+        Me.GVFinalClear.GridControl = Me.GCFinalClear
+        Me.GVFinalClear.Name = "GVFinalClear"
+        Me.GVFinalClear.OptionsBehavior.Editable = False
+        Me.GVFinalClear.OptionsView.ShowFooter = True
+        Me.GVFinalClear.OptionsView.ShowGroupPanel = False
+        '
+        'FormProductionFinalClear
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "FormProductionFinalClear"
+        Me.ClientSize = New System.Drawing.Size(754, 316)
+        Me.Controls.Add(Me.GCFinalClear)
+        Me.Controls.Add(Me.GCFilter)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "FormProductionFinalClear"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Final Clearance"
+        CType(Me.GCFilter, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GCFilter.ResumeLayout(False)
+        Me.GCFilter.PerformLayout()
+        CType(Me.DEUntil.Properties.CalendarTimeProperties, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DEUntil.Properties, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DEFrom.Properties.CalendarTimeProperties, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DEFrom.Properties, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.GCFinalClear, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.GVFinalClear, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
     End Sub
+
+    Friend WithEvents GCFilter As DevExpress.XtraEditors.GroupControl
+    Friend WithEvents BtnView As DevExpress.XtraEditors.SimpleButton
+    Friend WithEvents BHide As DevExpress.XtraEditors.SimpleButton
+    Friend WithEvents BExpand As DevExpress.XtraEditors.SimpleButton
+    Friend WithEvents DEUntil As DevExpress.XtraEditors.DateEdit
+    Friend WithEvents DEFrom As DevExpress.XtraEditors.DateEdit
+    Friend WithEvents LabelControl2 As DevExpress.XtraEditors.LabelControl
+    Friend WithEvents LabelControl3 As DevExpress.XtraEditors.LabelControl
+    Friend WithEvents GCFinalClear As DevExpress.XtraGrid.GridControl
+    Friend WithEvents GVFinalClear As DevExpress.XtraGrid.Views.Grid.GridView
 End Class
