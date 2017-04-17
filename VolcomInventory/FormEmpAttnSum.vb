f80ade917e0f8e774313824d959706da7216f16e
Septian Primadewa <iansaviour@gmail.com>
2017-04-17 16:09:48 +0700

    Fix Late and over if leave

--- VolcomInventory/FormEmpAttnSum.vb
+++ VolcomInventory/FormEmpAttnSum.vb
@@ -31,12 +31,18 @@
                                  sch.break_in,MAX(at_brin.datetime) AS end_break, 
                                  scht.schedule_type,note ,
                                  sch.minutes_work,
-                                 IF(MIN(at_in.datetime)>sch.in_tolerance,TIMESTAMPDIFF(MINUTE,sch.in_tolerance,MIN(at_in.datetime)),0) AS late ,
-                                 TIMESTAMPDIFF(MINUTE,sch.out,MAX(at_out.datetime)) AS over ,
+                                 IF(IF(MIN(at_in.datetime)>sch.in_tolerance,TIMESTAMPDIFF(MINUTE,sch.in_tolerance,MIN(at_in.datetime)),0) - IF(lv.is_full_day=1 OR ISNULL(lv.datetime_until),0,IF(lv.datetime_until=sch.out,0,lv.minutes_total+60))<0,0,IF(MIN(at_in.datetime)>sch.in_tolerance,TIMESTAMPDIFF(MINUTE,sch.in_tolerance,MIN(at_in.datetime)),0) - IF(lv.is_full_day=1 OR ISNULL(lv.datetime_until),0,IF(lv.datetime_until=sch.out,0,lv.minutes_total+60))) AS late ,
+                                 TIMESTAMPDIFF(MINUTE,sch.out,MAX(at_out.datetime)) + IF(lv.is_full_day=1 OR ISNULL(lv.datetime_until),0,IF(lv.datetime_start=sch.in_tolerance,0,lv.minutes_total+60)) AS over ,
                                 IF(TIMESTAMPDIFF(MINUTE,MIN(at_brout.datetime),MAX(at_brin.datetime))>TIMESTAMPDIFF(MINUTE,sch.break_out,sch.break_in),
                                 TIMESTAMPDIFF(MINUTE,MIN(at_brout.datetime),MAX(at_brin.datetime))-TIMESTAMPDIFF(MINUTE,sch.break_out,sch.break_in),0) AS over_break ,
                                 TIMESTAMPDIFF(MINUTE,MIN(at_in.datetime),MAX(at_out.datetime)) AS actual_work_hour 
                                 FROM tb_emp_schedule sch 
+                                LEFT JOIN
+                                (
+                                SELECT eld.* FROM tb_emp_leave_det eld
+                                INNER JOIN tb_emp_leave el ON el.id_emp_leave=eld.id_emp_leave
+                                WHERE el.id_report_status='6' 
+                                ) lv ON lv.id_schedule=sch.id_schedule
                                 LEFT JOIN tb_lookup_leave_type ket ON ket.id_leave_type=sch.id_leave_type 
                                 INNER JOIN tb_m_employee emp ON emp.id_employee=sch.id_employee 
                                 INNER JOIN tb_lookup_employee_level lvl ON lvl.id_employee_level=emp.id_employee_level 
@@ -251,41 +257,42 @@
 
         Dim query As String = ""
         query = "SELECT tb.id_schedule,tb.employee_level,tb.employee_position,tb.id_leave_type,tb.leave_type,tb.info_leave,tb.employee_active,tb.id_employee_active,tb.id_employee,tb.employee_name,tb.employee_code,tb.id_departement,dep.departement,SUM(tb.late) AS late,SUM(tb.over) AS over,SUM(tb.over_break) AS over_break,
-                    SUM(IF(NOT ISNULL(tb.att_in) AND NOT ISNULL(tb.att_out),(tb.minutes_work-tb.over_break-tb.late+IF(tb.over<0,tb.over,0)),0)) AS work_hour,
-                    SUM(tb.actual_work_hour) AS actual_work_hour,SUM((tb.over-tb.late-tb.over_break)) AS balance,SUM(IF(NOT ISNULL(tb.att_in) AND NOT ISNULL(tb.att_out),1,0)) AS present,SUM(IF(tb.id_schedule_type=1,1,0)) AS workday "
-        query += " FROM "
-        query += " ("
-        query += " SELECT sch.id_schedule,lvl.employee_level,emp.employee_position,ket.id_leave_type,ket.leave_type,sch.info_leave,active.employee_active,active.id_employee_active,sch.id_schedule_type,sch.id_employee,emp.employee_name,emp.employee_code,emp.id_departement,sch.date, "
-        query += " sch.minutes_work,sch.in,sch.in_tolerance,MIN(at_in.datetime) As `att_in`, "
-        query += " sch.out,MAX(at_out.datetime) AS `att_out`, "
-        query += " sch.break_out,MIN(at_brout.datetime) As start_break, "
-        query += " sch.break_in,MAX(at_brin.datetime) AS end_break, "
-        query += " scht.schedule_type,note,"
-        query += " IF(MIN(at_in.datetime)>sch.in_tolerance,TIMESTAMPDIFF(MINUTE,sch.in_tolerance,MIN(at_in.datetime)),0) AS late ,"
-        'query += " If(MAX(at_out.datetime)>sch.out,TIMESTAMPDIFF(MINUTE,sch.out,MAX(at_out.datetime)),0) As over ,"
-        query += " TIMESTAMPDIFF(MINUTE,sch.out,MAX(at_out.datetime)) As over ,"
-        query += " IF(TIMESTAMPDIFF(MINUTE,MIN(at_brout.datetime),MAX(at_brin.datetime))>TIMESTAMPDIFF(MINUTE,sch.break_out,sch.break_in),"
-        query += " TIMESTAMPDIFF(MINUTE,MIN(at_brout.datetime),MAX(at_brin.datetime))-TIMESTAMPDIFF(MINUTE,sch.break_out,sch.break_in),0) As over_break ,"
-        query += " TIMESTAMPDIFF(MINUTE,MIN(at_in.datetime),MAX(at_out.datetime)) AS actual_work_hour"
-        'query += " TIMESTAMPDIFF(MINUTE,If(MIN(at_in.datetime)<sch.In,sch.In,If(MIN(at_in.datetime)<sch.in_tolerance,sch.In,MIN(at_in.datetime))),If(MAX(at_out.datetime)>sch.out,sch.out,MAX(at_out.datetime))) As work_hour "
-        query += " FROM tb_emp_schedule sch "
-        query += " LEFT JOIN tb_lookup_leave_type ket ON ket.id_leave_type=sch.id_leave_type "
-        query += " INNER JOIN tb_m_employee emp On emp.id_employee=sch.id_employee "
-        query += " INNER JOIN tb_lookup_employee_level lvl ON lvl.id_employee_level=emp.id_employee_level "
-        query += " INNER JOIN tb_lookup_schedule_type scht On scht.id_schedule_type=sch.id_schedule_type "
-        query += " INNER JOIN tb_lookup_employee_active active on emp.id_employee_active=active.id_employee_active"
-        'query attendance old
-        'query += " LEFT JOIN tb_emp_attn at_in On at_in.id_employee=sch.id_employee And Date(at_in.datetime) = sch.Date And at_in.type_log = 1 "
-        'query += " LEFT JOIN tb_emp_attn at_out On at_out.id_employee=sch.id_employee And Date(at_out.datetime) = sch.Date And at_out.type_log = 2 "
-        query += " LEFT JOIN tb_emp_attn at_in ON at_in.id_employee = sch.id_employee AND (at_in.datetime>=(sch.out - INTERVAL 1 DAY) AND at_in.datetime<=sch.out) AND at_in.type_log = 1 "
-        query += " LEFT JOIN tb_emp_attn at_out ON at_out.id_employee = sch.id_employee AND (at_out.datetime>=sch.in AND at_out.datetime<=(sch.in + INTERVAL 1 DAY)) AND at_out.type_log = 2 "
-        query += " LEFT JOIN tb_emp_attn at_brout On at_brout.id_employee=sch.id_employee And Date(at_brout.datetime) = sch.Date And at_brout.type_log = 3 "
-        query += " LEFT JOIN tb_emp_attn at_brin On at_brin.id_employee=sch.id_employee And Date(at_brin.datetime) = sch.Date And at_brin.type_log = 4 "
-        query += " WHERE emp.id_departement Like '" & dept & "' AND sch.date >='" & date_start & "' AND sch.date <='" & date_until & "' "
-        query += " GROUP BY sch.id_schedule"
-        query += " ) tb"
-        query += " INNER JOIN tb_m_departement dep On dep.id_departement=tb.id_departement"
-        query += " GROUP BY tb.id_employee"
+                SUM(IF(NOT ISNULL(tb.att_in) AND NOT ISNULL(tb.att_out),(tb.minutes_work-tb.over_break-tb.late+IF(tb.over<0,tb.over,0)),0)) AS work_hour,
+                SUM(tb.actual_work_hour) AS actual_work_hour,SUM((tb.over-tb.late-tb.over_break)) AS balance,SUM(IF(NOT ISNULL(tb.att_in) AND NOT ISNULL(tb.att_out),1,0)) AS present,SUM(IF(tb.id_schedule_type=1,1,0)) AS workday 
+                FROM
+                (
+                Select sch.id_schedule,lvl.employee_level,emp.employee_position,ket.id_leave_type,ket.leave_type,sch.info_leave,active.employee_active,active.id_employee_active,sch.id_schedule_type,sch.id_employee,emp.employee_name,emp.employee_code,emp.id_departement,sch.Date, 
+                sch.minutes_work,sch.In,sch.in_tolerance,MIN(at_in.datetime) As `att_in`, 
+                sch.out,MAX(at_out.datetime) As `att_out`, 
+                sch.break_out,MIN(at_brout.datetime) As start_break, 
+                sch.break_in,MAX(at_brin.datetime) As end_break, 
+                scht.schedule_type,note,
+                IF(IF(MIN(at_in.datetime)>sch.in_tolerance,TIMESTAMPDIFF(MINUTE,sch.in_tolerance,MIN(at_in.datetime)),0) - IF(lv.is_full_day=1 OR ISNULL(lv.datetime_until),0,IF(lv.datetime_until=sch.out,0,lv.minutes_total+60))<0,0,IF(MIN(at_in.datetime)>sch.in_tolerance,TIMESTAMPDIFF(MINUTE,sch.in_tolerance,MIN(at_in.datetime)),0) - IF(lv.is_full_day=1 OR ISNULL(lv.datetime_until),0,IF(lv.datetime_until=sch.out,0,lv.minutes_total+60))) AS late ,
+                TIMESTAMPDIFF(MINUTE,sch.out,MAX(at_out.datetime)) + IF(lv.is_full_day=1 OR ISNULL(lv.datetime_until),0,IF(lv.datetime_start=sch.in_tolerance,0,lv.minutes_total+60)) As over ,
+                If(TIMESTAMPDIFF(MINUTE,MIN(at_brout.datetime),MAX(at_brin.datetime))>TIMESTAMPDIFF(MINUTE,sch.break_out,sch.break_in),
+                TIMESTAMPDIFF(MINUTE,MIN(at_brout.datetime),MAX(at_brin.datetime))-TIMESTAMPDIFF(MINUTE,sch.break_out,sch.break_in),0) As over_break ,
+                TIMESTAMPDIFF(MINUTE,MIN(at_in.datetime),MAX(at_out.datetime)) As actual_work_hour
+                FROM tb_emp_schedule sch 
+                LEFT JOIN
+                (
+                SELECT eld.* FROM tb_emp_leave_det eld
+                INNER JOIN tb_emp_leave el ON el.id_emp_leave=eld.id_emp_leave
+                WHERE el.id_report_status='6' 
+                ) lv ON lv.id_schedule=sch.id_schedule
+                LEFT JOIN tb_lookup_leave_type ket On ket.id_leave_type=sch.id_leave_type 
+                INNER JOIN tb_m_employee emp On emp.id_employee=sch.id_employee 
+                INNER JOIN tb_lookup_employee_level lvl On lvl.id_employee_level=emp.id_employee_level 
+                INNER JOIN tb_lookup_schedule_type scht On scht.id_schedule_type=sch.id_schedule_type 
+                INNER JOIN tb_lookup_employee_active active On emp.id_employee_active=active.id_employee_active
+                LEFT JOIN tb_emp_attn at_in On at_in.id_employee = sch.id_employee And (at_in.datetime>=(sch.out - INTERVAL 1 DAY) And at_in.datetime<=sch.out) And at_in.type_log = 1 
+                LEFT JOIN tb_emp_attn at_out On at_out.id_employee = sch.id_employee And (at_out.datetime>=sch.In And at_out.datetime<=(sch.In + INTERVAL 1 DAY)) And at_out.type_log = 2 
+                LEFT JOIN tb_emp_attn at_brout On at_brout.id_employee=sch.id_employee And Date(at_brout.datetime) = sch.Date And at_brout.type_log = 3 
+                LEFT JOIN tb_emp_attn at_brin On at_brin.id_employee=sch.id_employee And Date(at_brin.datetime) = sch.Date And at_brin.type_log = 4 
+                WHERE emp.id_departement Like '" & dept & "' AND sch.date >='" & date_start & "' AND sch.date <='" & date_until & "' 
+                GROUP BY sch.id_schedule
+                )tb
+                INNER Join tb_m_departement dep On dep.id_departement=tb.id_departement
+                GROUP BY tb.id_employee"
 
         Dim data As DataTable = execute_query(query, -1, True, "", "", "", "")
         GCSum.DataSource = data
