52ae586be333c4f7d56677163ccf5f1b42e0ffc3
Satrya Anas <catur.satrya@gmail.com>
2017-07-10 16:26:26 +0800

    new form uniform

---
+++ VolcomInventory/ClassEmpUni.vb
@@ -0,0 +1,23 @@
+﻿Public Class ClassEmpUni
+    Public Function queryMain(ByVal condition As String, ByVal order_type As String) As String
+        If order_type = "1" Then
+            order_type = "ASC "
+        ElseIf order_type = "2" Then
+            order_type = "DESC "
+        End If
+
+        If condition <> "-1" Then
+            condition = condition
+        Else
+            condition = ""
+        End If
+
+        Dim query As String = "SELECT u.id_emp_uni_period, u.period_name, u.selection_date_start, u.selection_date_start, u.selection_date_end, u.created_date, u.distribution_date 
+        FROM tb_emp_uni_period u
+        WHERE u.id_emp_uni_period>0 "
+        query += condition + " "
+        query += "GROUP BY u.id_emp_uni_period  "
+        query += "ORDER BY u.id_emp_uni_period " + order_type
+        Return query
+    End Function
+End Class
--- VolcomInventory/FormEmpUniPeriod.vb
+++ VolcomInventory/FormEmpUniPeriod.vb
@@ -1,3 +1,68 @@
 ﻿Public Class FormEmpUniPeriod
+    Dim bnew_active As String = "1"
+    Dim bedit_active As String = "1"
+    Dim bdel_active As String = "1"
 
+    Private Sub FormEmpUniPeriod_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        viewUniformPeriod()
+    End Sub
+
+    Private Sub FormEmpUniPeriod_Activated(sender As Object, e As EventArgs) Handles MyBase.Activated
+        FormMain.show_rb(Name)
+        check_menu()
+    End Sub
+
+    Private Sub FormEmpUniPeriod_Deactivate(sender As Object, e As EventArgs) Handles MyBase.Deactivate
+        FormMain.hide_rb()
+    End Sub
+
+    Sub check_menu()
+        If GVUni.RowCount < 1 Then
+            'hide all except new
+            bnew_active = "1"
+            bedit_active = "0"
+            bdel_active = "0"
+            checkFormAccess(Name)
+            button_main(bnew_active, bedit_active, bdel_active)
+            noManipulating()
+        Else
+            'show all
+            bnew_active = "1"
+            bedit_active = "1"
+            bdel_active = "1"
+            checkFormAccess(Name)
+            button_main(bnew_active, bedit_active, bdel_active)
+            noManipulating()
+        End If
+    End Sub
+
+    Sub noManipulating()
+        Dim indeks As Integer = 0
+        Try
+            indeks = GVUni.FocusedRowHandle
+        Catch ex As Exception
+        End Try
+        If indeks < 0 Then
+            bnew_active = "1"
+            bedit_active = "0"
+            bdel_active = "0"
+        Else
+            bnew_active = "1"
+            bedit_active = "1"
+            bdel_active = "1"
+        End If
+        checkFormAccess(Name)
+        button_main(bnew_active, bedit_active, bdel_active)
+    End Sub
+
+    Sub viewUniformPeriod()
+        Cursor = Cursors.WaitCursor
+
+        Dim query_c As ClassEmpUni = New ClassEmpUni()
+        Dim query As String = query_c.queryMain("-1", "2")
+        Dim data As DataTable = execute_query(query, -1, True, "", "", "", "")
+        GCUni.DataSource = data
+        check_menu()
+        Cursor = Cursors.Default
+    End Sub
 End Class
