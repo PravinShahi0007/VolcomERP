7d990bf7fbd1a69b31531ebed6dda9f5da52af9b
Satrya Anas <catur.satrya@gmail.com>
2017-09-13 15:56:41 +0800

    menu auth first dev

---
+++ VolcomInventory/FormMenuAuth.Designer.vb
@@ -0,0 +1,112 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FormMenuAuth
+    Inherits DevExpress.XtraEditors.XtraForm
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        If disposing AndAlso components IsNot Nothing Then
+            components.Dispose()
+        End If
+        MyBase.Dispose(disposing)
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.LabelControl1 = New DevExpress.XtraEditors.LabelControl()
+        Me.LabelControl2 = New DevExpress.XtraEditors.LabelControl()
+        Me.TxtUsername = New DevExpress.XtraEditors.TextEdit()
+        Me.TxtPass = New DevExpress.XtraEditors.TextEdit()
+        Me.SimpleButton1 = New DevExpress.XtraEditors.SimpleButton()
+        Me.SimpleButton2 = New DevExpress.XtraEditors.SimpleButton()
+        CType(Me.TxtUsername.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TxtPass.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'LabelControl1
+        '
+        Me.LabelControl1.Location = New System.Drawing.Point(18, 21)
+        Me.LabelControl1.Name = "LabelControl1"
+        Me.LabelControl1.Size = New System.Drawing.Size(48, 13)
+        Me.LabelControl1.TabIndex = 0
+        Me.LabelControl1.Text = "Username"
+        '
+        'LabelControl2
+        '
+        Me.LabelControl2.Location = New System.Drawing.Point(18, 47)
+        Me.LabelControl2.Name = "LabelControl2"
+        Me.LabelControl2.Size = New System.Drawing.Size(46, 13)
+        Me.LabelControl2.TabIndex = 1
+        Me.LabelControl2.Text = "Password"
+        '
+        'TxtUsername
+        '
+        Me.TxtUsername.Location = New System.Drawing.Point(90, 18)
+        Me.TxtUsername.Name = "TxtUsername"
+        Me.TxtUsername.Size = New System.Drawing.Size(180, 20)
+        Me.TxtUsername.TabIndex = 0
+        '
+        'TxtPass
+        '
+        Me.TxtPass.Location = New System.Drawing.Point(90, 44)
+        Me.TxtPass.Name = "TxtPass"
+        Me.TxtPass.Size = New System.Drawing.Size(180, 20)
+        Me.TxtPass.TabIndex = 1
+        '
+        'SimpleButton1
+        '
+        Me.SimpleButton1.Location = New System.Drawing.Point(195, 73)
+        Me.SimpleButton1.Name = "SimpleButton1"
+        Me.SimpleButton1.Size = New System.Drawing.Size(75, 23)
+        Me.SimpleButton1.TabIndex = 2
+        Me.SimpleButton1.Text = "OK"
+        '
+        'SimpleButton2
+        '
+        Me.SimpleButton2.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.SimpleButton2.Location = New System.Drawing.Point(115, 73)
+        Me.SimpleButton2.Name = "SimpleButton2"
+        Me.SimpleButton2.Size = New System.Drawing.Size(75, 23)
+        Me.SimpleButton2.TabIndex = 3
+        Me.SimpleButton2.Text = "Cancel"
+        '
+        'FormMenuAuth
+        '
+        Me.AcceptButton = Me.SimpleButton1
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.SimpleButton2
+        Me.ClientSize = New System.Drawing.Size(294, 118)
+        Me.Controls.Add(Me.SimpleButton2)
+        Me.Controls.Add(Me.SimpleButton1)
+        Me.Controls.Add(Me.TxtPass)
+        Me.Controls.Add(Me.TxtUsername)
+        Me.Controls.Add(Me.LabelControl2)
+        Me.Controls.Add(Me.LabelControl1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "FormMenuAuth"
+        Me.ShowInTaskbar = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Authorization"
+        CType(Me.TxtUsername.Properties, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.TxtPass.Properties, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents LabelControl1 As DevExpress.XtraEditors.LabelControl
+    Friend WithEvents LabelControl2 As DevExpress.XtraEditors.LabelControl
+    Friend WithEvents TxtUsername As DevExpress.XtraEditors.TextEdit
+    Friend WithEvents TxtPass As DevExpress.XtraEditors.TextEdit
+    Friend WithEvents SimpleButton1 As DevExpress.XtraEditors.SimpleButton
+    Friend WithEvents SimpleButton2 As DevExpress.XtraEditors.SimpleButton
+End Class
