d66559ee99f862cc4619e929a0ea3320ef64e38d
Satrya Anas <catur.satrya@gmail.com>
2017-10-16 17:01:35 +0800

    Print Form Fix

--- VolcomInventory/ReportEmpUni.vb
+++ VolcomInventory/ReportEmpUni.vb
@@ -1,6 +1,7 @@
 ﻿Public Class ReportEmpUni
+    Public Shared id_period As String = "-1"
+
     Private Sub ReportEmpUni_BeforePrint(sender As Object, e As Printing.PrintEventArgs) Handles MyBase.BeforePrint
-        Dim id_period As String = "4"
         Dim query As String = "CALL view_emp_uni_budget(" & id_period & ")"
         Dim data As DataTable = execute_query(query, -1, True, "", "", "", "")
         '
