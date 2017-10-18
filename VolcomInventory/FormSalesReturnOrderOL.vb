e7f15a1770d2f8d61f7eba17e2009678a805afe9
Satrya Anas <catur.satrya@gmail.com>
2017-10-18 14:43:54 +0800

    New Menu : Return Order OL Store

---
+++ VolcomInventory/FormSalesReturnOrderOL.vb
@@ -0,0 +1,7 @@
+﻿Public Class FormSalesReturnOrderOL
+
+
+    Sub viewSalesReturnOrder()
+
+    End Sub
+End Class
---
+++ VolcomInventory/FormSalesReturnOrderOLDet.vb
@@ -0,0 +1,8 @@
+﻿Public Class FormSalesReturnOrderOLDet
+    Public id_sales_return_order As String = ""
+    Public action As String = "-1"
+
+    Private Sub FormSalesReturnOrderOLDet_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
