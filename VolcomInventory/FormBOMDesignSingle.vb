e77a41d5314bff74d9e5fa0c095e833e6e6ff434
Putu Septian Primadewa <iansaviour@gmail.com>
2017-10-30 10:50:26 +0800

    BOM sync

--- VolcomInventory/FormBOMDesignSingle.vb
+++ VolcomInventory/FormBOMDesignSingle.vb
@@ -313,11 +313,60 @@
 
         For i As Integer = 0 To data.Rows.Count - 1
             Dim id_po As String = data.Rows(i)("id_prod_order").ToString
-            'then look the wo
-            Dim query_wo As String = "SELECT * FROM tb_prod_order_wo"
-            'delete first
             'update
+            Dim query_upd As String = "UPDATE tb_prod_order_wo wo
+                                        INNER JOIN 
+                                        (
+	                                        SELECT bom.id_bom,bomd.id_ovh_price,ovh.overhead,ovhp.id_comp_contact,bomd.kurs,ovhp.id_currency,bomd.is_ovh_main,SUM(bomd.bom_price*pod.prod_order_qty) AS amount FROM tb_prod_order_det pod
+	                                        INNER JOIN tb_prod_demand_product pdp ON pdp.`id_prod_demand_product`=pod.`id_prod_demand_product`
+	                                        INNER JOIN tb_bom bom ON bom.`id_product`=pdp.`id_product` AND bom.is_default='1'
+	                                        INNER JOIN tb_bom_det bomd ON bomd.`id_bom`=bom.`id_bom` 
+	                                        INNER JOIN tb_m_ovh_price ovhp ON ovhp.id_ovh_price=bomd.`id_ovh_price`
+	                                        INNER JOIN tb_m_ovh ovh ON ovh.id_ovh=ovhp.`id_ovh`
+	                                        WHERE pod.`id_prod_order`='" & id_po & "' AND bomd.`id_component_category`='2'
+	                                        GROUP BY bomd.id_ovh_price
+                                        )bom ON bom.id_ovh_price=wo.`id_ovh_price`
+                                        SET wo.id_comp_contact_ship_to=bom.id_comp_contact,id_currency,prod_order_wo_kurs=bom.`kurs`,is_main_vendor=bom.is_ovh_main,prod_order_wo_amount=bom.amount
+                                        WHERE wo.`id_prod_order`='" & id_po & "' AND NOT ISNULL(bom.`id_bom`);
+
+                                        UPDATE tb_prod_order_wo_det wod
+                                        INNER JOIN tb_prod_order_wo wo ON wod.id_prod_order_wo=wo.`id_prod_order_wo`
+                                        INNER JOIN
+                                        (
+	                                        SELECT bom.id_bom,bomd.id_ovh_price,ovh.overhead,ovhp.id_comp_contact,bomd.kurs,ovhp.id_currency,bomd.is_ovh_main,bomd.bom_price AS price FROM tb_prod_order_det pod
+	                                        INNER JOIN tb_prod_demand_product pdp ON pdp.`id_prod_demand_product`=pod.`id_prod_demand_product`
+	                                        INNER JOIN tb_bom bom ON bom.`id_product`=pdp.`id_product` AND bom.is_default='1'
+	                                        INNER JOIN tb_bom_det bomd ON bomd.`id_bom`=bom.`id_bom` 
+	                                        INNER JOIN tb_m_ovh_price ovhp ON ovhp.id_ovh_price=bomd.`id_ovh_price`
+	                                        INNER JOIN tb_m_ovh ovh ON ovh.id_ovh=ovhp.`id_ovh`
+	                                        WHERE pod.`id_prod_order`='" & id_po & "' AND bomd.`id_component_category`='2'
+	                                        GROUP BY bomd.id_ovh_price
+                                        )bom ON bom.id_ovh_price=wo.`id_ovh_price`
+                                        SET wod.prod_order_wo_det_price=bom.price
+                                        WHERE wo.`id_prod_order`='" & id_po & "' AND NOT ISNULL(bom.`id_bom`);"
+            execute_non_query(query, True, "", "", "", "")
             'insert
+            Dim query_ins As String = "SELECT po.id_report_status,bom.id_bom,bomd.id_ovh_price,ovh.overhead,ovhp.id_comp_contact,bomd.kurs,ovhp.id_currency,bomd.is_ovh_main,pod.prod_order_qty,bomd.bom_price,SUM(bomd.bom_price*pod.prod_order_qty) AS amount,
+                                        wo.`id_prod_order_wo`,wo.`prod_order_wo_kurs`,wo.`id_currency`,wo.`is_main_vendor`,wo.`prod_order_wo_amount` FROM tb_prod_order_det pod
+                                        INNER JOIN tb_prod_order po ON po.id_prod_order=pod.id_prod_order
+                                        INNER JOIN tb_prod_demand_product pdp ON pdp.`id_prod_demand_product`=pod.`id_prod_demand_product`
+                                        INNER JOIN tb_bom bom ON bom.`id_product`=pdp.`id_product` AND bom.is_default='1'
+                                        INNER JOIN tb_bom_det bomd ON bomd.`id_bom`=bom.`id_bom` 
+                                        INNER JOIN tb_m_ovh_price ovhp ON ovhp.id_ovh_price=bomd.`id_ovh_price`
+                                        INNER JOIN tb_m_ovh ovh ON ovh.id_ovh=ovhp.`id_ovh`
+                                        LEFT JOIN tb_prod_order_wo wo ON bomd.id_ovh_price=wo.`id_ovh_price` AND wo.`id_prod_order`=pod.id_prod_order
+                                        WHERE pod.`id_prod_order`='" & id_po & "' AND bomd.`id_component_category`='2' AND ISNULL(wo.`id_prod_order_wo`)
+                                        GROUP BY bomd.id_ovh_price"
+            Dim data_ins As DataTable = execute_query(query_ins, -1, True, "", "", "", "")
+            For j As Integer = 0 To data_ins.Rows.Count - 1
+                Dim wo_number As String = header_number_prod(2)
+                Dim query_ins_wo As String = "INSERT INTO tb_prod_order_wo(id_prod_order,prod_order_wo_number,id_ovh_price,id_comp_contact_ship_to,id_payment,prod_order_wo_del_date,prod_order_wo_date,prod_order_wo_amount,id_currency,kurs,is_main_vendor,id_report_status)
+                                                VALUES('" & id_po & "','" & wo_number & "','" & data_ins.Rows(j)("id_ovh_price").ToString & "','" & data_ins.Rows(j)("id_comp_contact").ToString & "','1',NOW(),NOW(),'" & data_ins.Rows(j)("amount").ToString & "','" & data_ins.Rows(j)("id_currency").ToString & "','" & data_ins.Rows(j)("kurs").ToString & "','" & data_ins.Rows(j)("is_ovh_main").ToString & "','" & data_ins.Rows(j)("id_report_status").ToString & "'); SELECT LAST_INSERT_ID()"
+                Dim id_wo_new As String = execute_query(query, 0, True, "", "", "", "")
+                increase_inc_prod("2")
+
+            Next
+            'delete
 
         Next
     End Sub
@@ -359,7 +408,6 @@
             GCBomDetOvh.DataSource = data
             GVBomDetOvh.BestFitColumns()
             calculate_unit_price()
-
         Catch ex As Exception
             errorConnection()
         End Try
@@ -424,7 +472,6 @@
     End Sub
 
     Private Sub Bprint_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Bprint.Click
-        '
         ReportBOM.qty_order = TEQtyPD.Text
         ReportBOM.id_pd = id_prod_demand_design
         ReportBOM.product_name = FormBOM.GVDesign.GetFocusedRowCellDisplayText("design_name").ToString
