efe0b7bb9e80bc8d46104d64501d4b88fdcdb5b2
Satrya Anas <catur.satrya@gmail.com>
2017-11-08 10:08:20 +0800

    New Menu : Aging report

--- VolcomInventory/FormMain.vb
+++ VolcomInventory/FormMain.vb
@@ -10854,4 +10854,8 @@ Public Class FormMain
         End Try
         Cursor = Cursors.Default
     End Sub
+
+    Private Sub NBAgingFGReport_LinkClicked(sender As Object, e As DevExpress.XtraNavBar.NavBarLinkEventArgs) Handles NBAgingFGReport.LinkClicked
+
+    End Sub
 End Class
