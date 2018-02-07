26c78d993dcadb127dbae5d2eacc0bb88a883359
Putu Septian Primadewa <iansaviour@gmail.com>
2018-02-07 11:00:54 +0800

    Fix Report Mark

--- VolcomInventory/FormEmpPayrollAdjustmentDet.vb
+++ VolcomInventory/FormEmpPayrollAdjustmentDet.vb
@@ -1,3 +1,8 @@
 ﻿Public Class FormEmpPayrollAdjustmentDet
+    Public id_payroll As String = "-1"
+    Public id_payroll_adj As String = "-1"
 
+    Private Sub TEEmployeeCode_KeyDown(sender As Object, e As KeyEventArgs) Handles TEEmployeeCode.KeyDown
+
+    End Sub
 End Class
