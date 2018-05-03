5e26ac734a7f0686ab6d8bf69cddb8b7c6f02a81
Putu Septian Primadewa <iansaviour@gmail.com>
2018-05-03 10:22:19 +0800

    tes lagi

--- VolcomInventory/FormEmpPayrollReportSummary.vb
+++ VolcomInventory/FormEmpPayrollReportSummary.vb
@@ -5,7 +5,7 @@
         load_sum()
     End Sub
     Sub load_sum()
-        Dim query As String = "CALL"
+        Dim query As String = ""
         Dim data As DataTable = execute_query(query, -1, True, "", "", "", "")
     End Sub
 End Class
