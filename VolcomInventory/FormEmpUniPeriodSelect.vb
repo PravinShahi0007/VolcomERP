edadd1003ccfe319ac0ed3013ced7404e62a62aa
Satrya Anas <catur.satrya@gmail.com>
2018-05-15 09:45:00 +0800

    period jika ada >1 active : public menu :100:

--- VolcomInventory/FormEmpUniPeriodSelect.vb
+++ VolcomInventory/FormEmpUniPeriodSelect.vb
@@ -1,5 +1,6 @@
 ﻿Public Class FormEmpUniPeriodSelect
     Public data As DataTable
+    Public id_pop_up As String = "-1"
 
     Private Sub FormEmpUniPeriodSelect_FormClosed(sender As Object, e As FormClosedEventArgs) Handles MyBase.FormClosed
         Dispose()
@@ -12,15 +13,20 @@
     Sub choose()
         Cursor = Cursors.WaitCursor
         If GVUni.RowCount > 0 And GVUni.FocusedRowHandle >= 0 Then
-            Dim id_periode As String = GVUni.GetFocusedRowCellValue("id_emp_uni_period").ToString
-            FormEmpUniPeriodDet.MdiParent = FormMain
-            FormEmpUniPeriodDet.action = "upd"
-            FormEmpUniPeriodDet.id_emp_uni_period = id_periode
-            Close()
-            FormEmpUniPeriodDet.is_public_form = True
-            FormEmpUniPeriodDet.Show()
-            FormEmpUniPeriodDet.WindowState = FormWindowState.Maximized
-            FormEmpUniPeriodDet.Focus()
+            If id_pop_up = "-1" Then 'admin
+                Dim id_periode As String = GVUni.GetFocusedRowCellValue("id_emp_uni_period").ToString
+                FormEmpUniPeriodDet.MdiParent = FormMain
+                FormEmpUniPeriodDet.action = "upd"
+                FormEmpUniPeriodDet.id_emp_uni_period = id_periode
+                Close()
+                FormEmpUniPeriodDet.is_public_form = True
+                FormEmpUniPeriodDet.Show()
+                FormEmpUniPeriodDet.WindowState = FormWindowState.Maximized
+                FormEmpUniPeriodDet.Focus()
+            ElseIf id_pop_up = "1" Then
+                FormMain.id_period_uniform_sel = GVUni.GetFocusedRowCellValue("id_emp_uni_period").ToString
+                Close()
+            End If
         End If
         Cursor = Cursors.Default
     End Sub
