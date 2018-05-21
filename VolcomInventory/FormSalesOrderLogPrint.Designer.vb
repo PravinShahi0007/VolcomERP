468a50e8803a00a60d3ff3bebd138bebfa865311
Satrya Anas <catur.satrya@gmail.com>
2018-05-21 16:21:32 +0800

    first print SO

---
+++ VolcomInventory/FormSalesOrderLogPrint.Designer.vb
@@ -0,0 +1,128 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FormSalesOrderLogPrint
+    Inherits DevExpress.XtraEditors.XtraForm
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        If disposing AndAlso components IsNot Nothing Then
+            components.Dispose()
+        End If
+        MyBase.Dispose(disposing)
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FormSalesOrderLogPrint))
+        Me.PanelControl1 = New DevExpress.XtraEditors.PanelControl()
+        Me.GCLog = New DevExpress.XtraGrid.GridControl()
+        Me.GVLog = New DevExpress.XtraGrid.Views.Grid.GridView()
+        Me.GridColumnPrintedBy = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GridColumnPrintedDate = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.BtnPrint = New DevExpress.XtraEditors.SimpleButton()
+        Me.BtnClose = New DevExpress.XtraEditors.SimpleButton()
+        CType(Me.PanelControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.PanelControl1.SuspendLayout()
+        CType(Me.GCLog, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.GVLog, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PanelControl1
+        '
+        Me.PanelControl1.Controls.Add(Me.BtnClose)
+        Me.PanelControl1.Controls.Add(Me.BtnPrint)
+        Me.PanelControl1.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.PanelControl1.Location = New System.Drawing.Point(0, 372)
+        Me.PanelControl1.Name = "PanelControl1"
+        Me.PanelControl1.Size = New System.Drawing.Size(657, 46)
+        Me.PanelControl1.TabIndex = 0
+        '
+        'GCLog
+        '
+        Me.GCLog.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.GCLog.Location = New System.Drawing.Point(0, 0)
+        Me.GCLog.MainView = Me.GVLog
+        Me.GCLog.Name = "GCLog"
+        Me.GCLog.Size = New System.Drawing.Size(657, 372)
+        Me.GCLog.TabIndex = 1
+        Me.GCLog.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() {Me.GVLog})
+        '
+        'GVLog
+        '
+        Me.GVLog.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() {Me.GridColumnPrintedBy, Me.GridColumnPrintedDate})
+        Me.GVLog.GridControl = Me.GCLog
+        Me.GVLog.Name = "GVLog"
+        Me.GVLog.OptionsBehavior.Editable = False
+        Me.GVLog.OptionsView.ShowGroupPanel = False
+        '
+        'GridColumnPrintedBy
+        '
+        Me.GridColumnPrintedBy.Caption = "Printed By"
+        Me.GridColumnPrintedBy.FieldName = "printed_by"
+        Me.GridColumnPrintedBy.Name = "GridColumnPrintedBy"
+        Me.GridColumnPrintedBy.Visible = True
+        Me.GridColumnPrintedBy.VisibleIndex = 0
+        '
+        'GridColumnPrintedDate
+        '
+        Me.GridColumnPrintedDate.Caption = "Printed Date"
+        Me.GridColumnPrintedDate.DisplayFormat.FormatString = "dd MMMM yyyy'/'hh:mm tt"
+        Me.GridColumnPrintedDate.DisplayFormat.FormatType = DevExpress.Utils.FormatType.DateTime
+        Me.GridColumnPrintedDate.FieldName = "printed_date"
+        Me.GridColumnPrintedDate.Name = "GridColumnPrintedDate"
+        Me.GridColumnPrintedDate.Visible = True
+        Me.GridColumnPrintedDate.VisibleIndex = 1
+        '
+        'BtnPrint
+        '
+        Me.BtnPrint.Dock = System.Windows.Forms.DockStyle.Right
+        Me.BtnPrint.Image = CType(resources.GetObject("BtnPrint.Image"), System.Drawing.Image)
+        Me.BtnPrint.Location = New System.Drawing.Point(569, 2)
+        Me.BtnPrint.Name = "BtnPrint"
+        Me.BtnPrint.Size = New System.Drawing.Size(86, 42)
+        Me.BtnPrint.TabIndex = 0
+        Me.BtnPrint.Text = "Print"
+        '
+        'BtnClose
+        '
+        Me.BtnClose.Dock = System.Windows.Forms.DockStyle.Right
+        Me.BtnClose.Image = CType(resources.GetObject("BtnClose.Image"), System.Drawing.Image)
+        Me.BtnClose.Location = New System.Drawing.Point(482, 2)
+        Me.BtnClose.Name = "BtnClose"
+        Me.BtnClose.Size = New System.Drawing.Size(87, 42)
+        Me.BtnClose.TabIndex = 1
+        Me.BtnClose.Text = "Close"
+        '
+        'FormSalesOrderLogPrint
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(657, 418)
+        Me.Controls.Add(Me.GCLog)
+        Me.Controls.Add(Me.PanelControl1)
+        Me.MinimizeBox = False
+        Me.Name = "FormSalesOrderLogPrint"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Order Log Print"
+        CType(Me.PanelControl1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.PanelControl1.ResumeLayout(False)
+        CType(Me.GCLog, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.GVLog, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents PanelControl1 As DevExpress.XtraEditors.PanelControl
+    Friend WithEvents GCLog As DevExpress.XtraGrid.GridControl
+    Friend WithEvents GVLog As DevExpress.XtraGrid.Views.Grid.GridView
+    Friend WithEvents GridColumnPrintedBy As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GridColumnPrintedDate As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents BtnClose As DevExpress.XtraEditors.SimpleButton
+    Friend WithEvents BtnPrint As DevExpress.XtraEditors.SimpleButton
+End Class
---
+++ VolcomInventory/FormSalesOrderLogPrint.vb
@@ -0,0 +1,26 @@
+﻿Public Class FormSalesOrderLogPrint
+    Public id As String = ""
+
+    Private Sub FormSalesOrderLogPrint_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim query As String = "SELECT a.id_sales_order,a.log_date AS `printed_date`, e.employee_name AS `printed_by` 
+        FROM tb_sales_order_log_print a
+        INNER JOIN tb_m_user u ON u.id_user = a.id_user
+        INNER JOIN tb_m_employee e ON e.id_employee = u.id_employee
+        WHERE a.id_sales_order=" + id + "
+        ORDER BY a.log_date ASC "
+        Dim data As DataTable = execute_query(query, -1, True, "", "", "", "")
+        GCLog.DataSource = data
+    End Sub
+
+    Private Sub FormSalesOrderLogPrint_FormClosed(sender As Object, e As FormClosedEventArgs) Handles MyBase.FormClosed
+        Dispose()
+    End Sub
+
+    Private Sub BtnClose_Click(sender As Object, e As EventArgs) Handles BtnClose.Click
+        Close()
+    End Sub
+
+    Private Sub BtnPrint_Click(sender As Object, e As EventArgs) Handles BtnPrint.Click
+        print_raw(GCLog, "")
+    End Sub
+End Class
