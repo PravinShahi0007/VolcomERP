034e269fff46befc8bb0be902c651895523d4444
Satrya Anas <catur.satrya@gmail.com>
2018-07-16 13:43:46 +0800

    optimize invoice & show period uniform expense

--- VolcomInventory/ClassEmpUniExpense.vb
+++ VolcomInventory/ClassEmpUniExpense.vb
@@ -15,7 +15,7 @@
         Dim query As String = "SELECT e.id_emp_uni_ex, e.id_comp_contact, c.comp_number, c.comp_name, CONCAT(c.comp_number,' - ', c.comp_name) AS `comp`, 
         e.id_pl_sales_order_del, del.pl_sales_order_del_number, 
         e.emp_uni_ex_number, e.emp_uni_ex_date, e.emp_uni_ex_note, e.id_report_status , rs.report_status,
-        so.id_so_status, so.id_emp_uni_budget
+        so.id_so_status, so.id_emp_uni_budget, e.period_from, e.period_until
         FROM tb_emp_uni_ex e
         INNER JOIN tb_m_comp_contact cc ON cc.id_comp_contact = e.id_comp_contact
         INNER JOIN tb_m_comp c ON c.id_comp = cc.id_comp
