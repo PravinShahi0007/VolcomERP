c67c6314edccc85586db6c7cad1eee97ce73b3bf
Putu Septian Primadewa <iansaviour@gmail.com>
2018-07-23 14:50:21 +0800

    Lock Employee

--- VolcomInventory/FormPurcItemDet.vb
+++ VolcomInventory/FormPurcItemDet.vb
@@ -23,4 +23,28 @@
     Private Sub FormPurcItemDet_FormClosed(sender As Object, e As FormClosedEventArgs) Handles MyBase.FormClosed
         Dispose()
     End Sub
+
+    Private Sub BSave_Click(sender As Object, e As EventArgs) Handles BSave.Click
+        Dim is_check As String = ""
+        '
+        If CETrackStock.Checked = True Then
+            is_check = "1"
+        Else
+            is_check = "2"
+        End If
+        '
+        If id_item = "-1" Then 'new
+            Dim query As String = "INSERT INTO tb_item(item_desc,id_item_cat,id_uom,is_stock,date_created,id_user_created,is_active) VALUES('" & TEDesc.Text & "','" & SLECat.EditValue.ToString & "','" & SLEUOM.EditValue.ToString & "','" & is_check & "',NOW(),'" & id_user & "','1')"
+            id_item = execute_query(query, 0, True, "", "", "", "")
+            FormPurcItem.load_item()
+            FormPurcItem.GVItem.FocusedRowHandle = find_row(FormPurcItem.GVItem, "id_item", id_item)
+            Close()
+        Else 'edit
+            Dim query As String = "UPDATE tb_item SET item_desc='" & TEDesc.Text & "',id_item_cat='" & SLECat.EditValue.ToString & "',id_uom='" & SLEUOM.EditValue.ToString & "',is_stock='" & is_check & "',is_active='',date_updated=NOW(),id_user_created='" & id_user & "' WHERE id_item='" & id_item & "'"
+            id_item = execute_query(query, 0, True, "", "", "", "")
+            FormPurcItem.load_item()
+            FormPurcItem.GVItem.FocusedRowHandle = find_row(FormPurcItem.GVItem, "id_item", id_item)
+            Close()
+        End If
+    End Sub
 End Class
