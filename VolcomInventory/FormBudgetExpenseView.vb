a0696da65c9178986274463e2e0d68c7b0b5e3e3
Satrya Anas <catur.satrya@gmail.com>
2018-08-06 16:40:00 +0800

    prepare view

--- VolcomInventory/FormBudgetExpenseView.vb
+++ VolcomInventory/FormBudgetExpenseView.vb
@@ -1,3 +1,40 @@
 ﻿Public Class FormBudgetExpenseView
+    Private Sub FormBudgetExpenseView_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        viewDept()
+        viewCat()
+        viewYear()
+    End Sub
 
+    Sub viewDept()
+        Cursor = Cursors.WaitCursor
+        Dim query As String = ""
+        query += "(SELECT id_departement,departement FROM tb_m_departement a ORDER BY a.departement ASC) "
+        viewLookupQuery(LEDeptSum, query, 0, "departement", "id_departement")
+        Cursor = Cursors.Default
+    End Sub
+
+    Sub viewCat()
+        Cursor = Cursors.WaitCursor
+        Dim query As String = "SELECT 0 AS `id_item_cat`, 'All Category' AS `item_cat`
+        UNION ALL
+        SELECT c.id_item_cat, c.item_cat FROM tb_item_cat c ORDER BY id_item_cat ASC"
+        viewLookupQuery(LECat, query, 0, "item_cat", "id_item_cat")
+        Cursor = Cursors.Default
+    End Sub
+
+    Sub viewYear()
+        Cursor = Cursors.WaitCursor
+        Dim query As String = ""
+        Dim j As Integer = 0
+        Dim dym As DataTable = execute_query("SELECT YEAR(DATE_ADD(NOW(),INTERVAL 10 YEAR)) AS `ym`", -1, True, "", "", "", "")
+        For i As Integer = 2018 To dym.Rows(0)("ym")
+            If j > 0 Then
+                query += "UNION ALL "
+            End If
+            query += "SELECT " + i.ToString + " AS `year` "
+            j += 1
+        Next
+        viewLookupQuery(LEYear, query, 0, "year", "year")
+        Cursor = Cursors.Default
+    End Sub
 End Class
