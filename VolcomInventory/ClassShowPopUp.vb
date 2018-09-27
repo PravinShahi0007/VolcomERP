35aa551af5860a422090bacdd8cf77e9b4d0da0b
Putu Septian Primadewa <iansaviour@gmail.com>
2018-09-27 14:35:06 +0800

    Cancel List

--- VolcomInventory/ClassShowPopUp.vb
+++ VolcomInventory/ClassShowPopUp.vb
@@ -1879,7 +1879,7 @@
             If report_mark_type = "x" Then
 
             ElseIf report_mark_type = "22" Then
-                query_view = "SELECT 'no' AS is_check, tb.id_prod_order AS id_report,tb.prod_order_number AS number,tb.prod_order_date AS date_created,SUM(det.prod_order_qty) AS qty,wo.amount,ovh.comp_name,dsg.`design_display_name`,dsg.`design_code_import` FROM tb_prod_order tb
+                query_view = "SELECT 'no' AS is_check, tb.id_prod_order AS id_report,tb.prod_order_number AS number,tb.prod_order_date AS date_created,ovh.comp_name,dsg.`design_display_name`,dsg.`design_code_import`,dsg.design_code,SUM(det.prod_order_qty) AS qty,wo.amount,ovh.comp_name,dsg.`design_display_name`,dsg.`design_code_import`,dsg.design_code FROM tb_prod_order tb
                                 INNER JOIN tb_prod_order_det det ON det.id_prod_order=tb.id_prod_order
                                 INNER JOIN (
 	                                SELECT wo.`id_prod_order_wo`,wo.`id_prod_order`,SUM(wod.`prod_order_wo_det_qty`*wod.`prod_order_wo_det_price`*IF(wo.`id_currency`=1,1,wo.`prod_order_wo_kurs`)) AS amount FROM tb_prod_order_wo wo
@@ -1900,7 +1900,7 @@
                 End If
                 query_view += " GROUP BY tb.id_prod_order"
                 '
-                query_view_blank = "SELECT tb.id_prod_order AS id_report,dsg.`design_display_name`,tb.prod_order_number AS number,tb.prod_order_date AS date_created,0 AS qty,0.00 AS amount,ovh.comp_name,dsg.`design_display_name`,dsg.`design_code_import` FROM tb_prod_order tb
+                query_view_blank = "SELECT tb.id_prod_order AS id_report,tb.prod_order_number AS number,tb.prod_order_date AS date_created,ovh.comp_name,dsg.`design_display_name`,dsg.`design_code_import`,dsg.design_code,0 AS qty,0.00 AS amount,ovh.comp_name,dsg.`design_display_name`,dsg.`design_code_import`,dsg.design_code FROM tb_prod_order tb
                                     INNER JOIN tb_prod_order_det det ON det.id_prod_order=tb.id_prod_order
                                     INNER JOIN tb_prod_demand_design pdd ON pdd.`id_prod_demand_design`=tb.`id_prod_demand_design`
                                     INNER JOIN tb_m_design dsg ON dsg.`id_design`=pdd.`id_design`
@@ -1912,7 +1912,7 @@
                                     )ovh ON ovh.id_prod_order=tb.id_prod_order
                                     WHERE tb.id_prod_order='-1'"
                 '
-                query_view_edit = "SELECT tb.id_prod_order AS id_report,tb.prod_order_number AS number,tb.prod_order_date AS date_created,SUM(det.prod_order_qty) AS qty,wo.amount,ovh.comp_name,dsg.`design_display_name`,dsg.`design_code_import` FROM tb_prod_order tb
+                query_view_edit = "SELECT tb.id_prod_order AS id_report,tb.prod_order_number AS number,tb.prod_order_date AS date_created,ovh.comp_name,dsg.`design_display_name`,dsg.`design_code_import`,dsg.design_code,SUM(det.prod_order_qty) AS qty,wo.amount FROM tb_prod_order tb
                                     INNER JOIN tb_prod_order_det det ON det.id_prod_order=tb.id_prod_order
                                     INNER JOIN (
 	                                    SELECT wo.`id_prod_order_wo`,wo.`id_prod_order`,SUM(wod.`prod_order_wo_det_qty`*wod.`prod_order_wo_det_price`*IF(wo.`id_currency`=1,1,wo.`prod_order_wo_kurs`)) AS amount FROM tb_prod_order_wo wo
@@ -1974,7 +1974,8 @@
             gv.Columns("amount").SummaryItem.DisplayFormat = "{0:N2}"
             gv.Columns("comp_name").Caption = "Vendor"
             gv.Columns("design_display_name").Caption = "Style Name"
-            gv.Columns("design_code_import").Caption = "Code"
+            gv.Columns("design_code_import").Caption = "Code Import"
+            gv.Columns("design_code").Caption = "Code Local"
             gv.OptionsBehavior.ReadOnly = True
             gv.OptionsView.ShowFooter = True
 
