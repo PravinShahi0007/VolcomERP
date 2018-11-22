e678c00c1702f84898c3fc2c36fbaa2707a7d1e3
Satrya Anas <catur.satrya@gmail.com>
2018-11-22 17:30:13 +0800

    prepare

--- VolcomInventory/FormItemDelDetail.vb
+++ VolcomInventory/FormItemDelDetail.vb
@@ -118,4 +118,99 @@
             Cursor = Cursors.Default
         End If
     End Sub
+
+    Private Sub BtnPrint_Click(sender As Object, e As EventArgs) Handles BtnPrint.Click
+        Cursor = Cursors.WaitCursor
+        If id_report_status = "6" Then
+            'Dim gcx As DevExpress.XtraGrid.GridControl = Nothing
+            'Dim gvx As DevExpress.XtraGrid.Views.Grid.GridView = Nothing
+            'gcx = GCData
+            'gvx = GVData
+            'ReportItemReq.id = id
+            'ReportItemReq.dt = gcx.DataSource
+            'Dim Report As New ReportItemReq()
+
+            ''' '... 
+            ''' ' creating and saving the view's layout to a new memory stream 
+            'Dim str As System.IO.Stream
+            'str = New System.IO.MemoryStream()
+            'gvx.SaveLayoutToStream(str, DevExpress.Utils.OptionsLayoutBase.FullLayout)
+            'str.Seek(0, System.IO.SeekOrigin.Begin)
+            'Report.GVData.RestoreLayoutFromStream(str, DevExpress.Utils.OptionsLayoutBase.FullLayout)
+            'str.Seek(0, System.IO.SeekOrigin.Begin)
+
+            '''Grid Detail
+            'ReportStyleGridview(Report.GVData)
+
+            '''    'Parse val
+            'Report.LabelNumber.Text = TxtNumber.Text.ToUpper
+            'Report.LabelDept.Text = TxtDept.Text.ToUpper
+            'Report.LabelDate.Text = DECreated.Text.ToString
+            'Report.LNote.Text = MENote.Text.ToString
+
+
+            '''    'Show the report's preview. 
+            'Dim Tool As DevExpress.XtraReports.UI.ReportPrintTool = New DevExpress.XtraReports.UI.ReportPrintTool(Report)
+            'Tool.ShowPreviewDialog()
+        Else
+            print_raw_no_export(GCData)
+        End If
+        Cursor = Cursors.Default
+    End Sub
+
+    Private Sub BtnAttachment_Click(sender As Object, e As EventArgs) Handles BtnAttachment.Click
+        attach
+    End Sub
+
+    Sub attach()
+        Cursor = Cursors.WaitCursor
+        FormDocumentUpload.report_mark_type = "156"
+        FormDocumentUpload.id_report = id
+        If is_view = "1" Or Not check_edit_report_status(id_report_status, "156", id) Then
+            FormDocumentUpload.is_view = "1"
+        End If
+        FormDocumentUpload.ShowDialog()
+        Cursor = Cursors.Default
+    End Sub
+
+    Private Sub BtnMark_Click(sender As Object, e As EventArgs) Handles BtnMark.Click
+        Cursor = Cursors.WaitCursor
+        FormReportMark.report_mark_type = "156"
+        FormReportMark.id_report = id
+        FormReportMark.is_view = is_view
+        FormReportMark.form_origin = Name
+        FormReportMark.ShowDialog()
+        Cursor = Cursors.Default
+    End Sub
+
+    Private Sub GVData_CustomColumnDisplayText(sender As Object, e As DevExpress.XtraGrid.Views.Base.CustomColumnDisplayTextEventArgs) Handles GVData.CustomColumnDisplayText
+        If e.Column.FieldName = "no" Then
+            e.DisplayText = (e.ListSourceRowIndex + 1).ToString()
+        End If
+    End Sub
+
+    Private Sub BtnSave_Click(sender As Object, e As EventArgs) Handles BtnSave.Click
+
+    End Sub
+
+    Private Sub GVData_CellValueChanged(sender As Object, e As DevExpress.XtraGrid.Views.Base.CellValueChangedEventArgs) Handles GVData.CellValueChanged
+        Cursor = Cursors.WaitCursor
+        Dim rh As Integer = e.RowHandle
+        Dim id_item_req_det As String = GVData.GetRowCellValue(rh, "id_item_req_det").ToString
+        If e.Column.FieldName = "qty" Then
+            If e.Value >= 0 Then
+                Dim old_value As Decimal = GVData.ActiveEditor.OldEditValue
+                Dim dcek As DataTable = getRmg("AND rd.id_item_req_det='" + id_item_req_det + "' ")
+                If e.Value > dcek.Rows(0)("qty") Then
+                    warningCustom("Qty can't exceed " + dcek.Rows(0)("qty").ToString)
+                    GVData.SetRowCellValue(rh, "qty", old_value)
+                End If
+                GVData.BestFitColumns()
+            Else
+                GVData.SetRowCellValue(rh, "qty", 0)
+                GVData.BestFitColumns()
+            End If
+        End If
+        Cursor = Cursors.Default
+    End Sub
 End Class
