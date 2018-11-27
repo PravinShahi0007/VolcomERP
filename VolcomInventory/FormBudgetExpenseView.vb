e3fe3ce7bf114fb5fd28fedb2db81ae3ed492227
Satrya Anas <catur.satrya@gmail.com>
2018-11-27 11:51:41 +0800

    summary custom pemakain budget

--- VolcomInventory/FormBudgetExpenseView.vb
+++ VolcomInventory/FormBudgetExpenseView.vb
@@ -260,4 +260,417 @@
             Cursor = Cursors.Default
         End If
     End Sub
+
+    Dim tot_1_budget As Decimal
+    Dim tot_2_budget As Decimal
+    Dim tot_3_budget As Decimal
+    Dim tot_4_budget As Decimal
+    Dim tot_5_budget As Decimal
+    Dim tot_6_budget As Decimal
+    Dim tot_7_budget As Decimal
+    Dim tot_8_budget As Decimal
+    Dim tot_9_budget As Decimal
+    Dim tot_10_budget As Decimal
+    Dim tot_11_budget As Decimal
+    Dim tot_12_budget As Decimal
+    Dim tot_1_budget_grp As Decimal
+    Dim tot_2_budget_grp As Decimal
+    Dim tot_3_budget_grp As Decimal
+    Dim tot_4_budget_grp As Decimal
+    Dim tot_5_budget_grp As Decimal
+    Dim tot_6_budget_grp As Decimal
+    Dim tot_7_budget_grp As Decimal
+    Dim tot_8_budget_grp As Decimal
+    Dim tot_9_budget_grp As Decimal
+    Dim tot_10_budget_grp As Decimal
+    Dim tot_11_budget_grp As Decimal
+    Dim tot_12_budget_grp As Decimal
+    Dim tot_1_actual As Decimal
+    Dim tot_2_actual As Decimal
+    Dim tot_3_actual As Decimal
+    Dim tot_4_actual As Decimal
+    Dim tot_5_actual As Decimal
+    Dim tot_6_actual As Decimal
+    Dim tot_7_actual As Decimal
+    Dim tot_8_actual As Decimal
+    Dim tot_9_actual As Decimal
+    Dim tot_10_actual As Decimal
+    Dim tot_11_actual As Decimal
+    Dim tot_12_actual As Decimal
+    Dim tot_1_actual_grp As Decimal
+    Dim tot_2_actual_grp As Decimal
+    Dim tot_3_actual_grp As Decimal
+    Dim tot_4_actual_grp As Decimal
+    Dim tot_5_actual_grp As Decimal
+    Dim tot_6_actual_grp As Decimal
+    Dim tot_7_actual_grp As Decimal
+    Dim tot_8_actual_grp As Decimal
+    Dim tot_9_actual_grp As Decimal
+    Dim tot_10_actual_grp As Decimal
+    Dim tot_11_actual_grp As Decimal
+    Dim tot_12_actual_grp As Decimal
+    Dim tot_budget As Decimal
+    Dim tot_actual As Decimal
+    Dim tot_budget_grp As Decimal
+    Dim tot_actual_grp As Decimal
+    Private Sub GVData_CustomSummaryCalculate(sender As Object, e As DevExpress.Data.CustomSummaryEventArgs) Handles GVData.CustomSummaryCalculate
+        Dim summaryID As String = Convert.ToString(CType(e.Item, DevExpress.XtraGrid.GridSummaryItem).Tag)
+        Dim View As DevExpress.XtraGrid.Views.Grid.GridView = CType(sender, DevExpress.XtraGrid.Views.Grid.GridView)
+
+        ' Initialization 
+        If e.SummaryProcess = DevExpress.Data.CustomSummaryProcess.Start Then
+            tot_1_budget = 0.0
+            tot_2_budget = 0.0
+            tot_3_budget = 0.0
+            tot_4_budget = 0.0
+            tot_5_budget = 0.0
+            tot_6_budget = 0.0
+            tot_7_budget = 0.0
+            tot_8_budget = 0.0
+            tot_9_budget = 0.0
+            tot_10_budget = 0.0
+            tot_11_budget = 0.0
+            tot_12_budget = 0.0
+            tot_1_actual = 0.0
+            tot_2_actual = 0.0
+            tot_3_actual = 0.0
+            tot_4_actual = 0.0
+            tot_5_actual = 0.0
+            tot_6_actual = 0.0
+            tot_7_actual = 0.0
+            tot_8_actual = 0.0
+            tot_9_actual = 0.0
+            tot_10_actual = 0.0
+            tot_11_actual = 0.0
+            tot_12_actual = 0.0
+
+            tot_1_budget_grp = 0.0
+            tot_2_budget_grp = 0.0
+            tot_3_budget_grp = 0.0
+            tot_4_budget_grp = 0.0
+            tot_5_budget_grp = 0.0
+            tot_6_budget_grp = 0.0
+            tot_7_budget_grp = 0.0
+            tot_8_budget_grp = 0.0
+            tot_9_budget_grp = 0.0
+            tot_10_budget_grp = 0.0
+            tot_11_budget_grp = 0.0
+            tot_12_budget_grp = 0.0
+            tot_1_actual_grp = 0.0
+            tot_2_actual_grp = 0.0
+            tot_3_actual_grp = 0.0
+            tot_4_actual_grp = 0.0
+            tot_5_actual_grp = 0.0
+            tot_6_actual_grp = 0.0
+            tot_7_actual_grp = 0.0
+            tot_8_actual_grp = 0.0
+            tot_9_actual_grp = 0.0
+            tot_10_actual_grp = 0.0
+            tot_11_actual_grp = 0.0
+            tot_12_actual_grp = 0.0
+
+            tot_budget = 0.0
+            tot_actual = 0.0
+            tot_budget_grp = 0.0
+            tot_actual_grp = 0.0
+        End If
+
+        ' Calculation 
+        If e.SummaryProcess = DevExpress.Data.CustomSummaryProcess.Calculate Then
+            Dim _1_budget As Decimal = CDec(myCoalesce(View.GetRowCellValue(e.RowHandle, "1_budget").ToString, "0.00"))
+            Dim _2_budget As Decimal = CDec(myCoalesce(View.GetRowCellValue(e.RowHandle, "2_budget").ToString, "0.00"))
+            Dim _3_budget As Decimal = CDec(myCoalesce(View.GetRowCellValue(e.RowHandle, "3_budget").ToString, "0.00"))
+            Dim _4_budget As Decimal = CDec(myCoalesce(View.GetRowCellValue(e.RowHandle, "4_budget").ToString, "0.00"))
+            Dim _5_budget As Decimal = CDec(myCoalesce(View.GetRowCellValue(e.RowHandle, "5_budget").ToString, "0.00"))
+            Dim _6_budget As Decimal = CDec(myCoalesce(View.GetRowCellValue(e.RowHandle, "6_budget").ToString, "0.00"))
+            Dim _7_budget As Decimal = CDec(myCoalesce(View.GetRowCellValue(e.RowHandle, "7_budget").ToString, "0.00"))
+            Dim _8_budget As Decimal = CDec(myCoalesce(View.GetRowCellValue(e.RowHandle, "8_budget").ToString, "0.00"))
+            Dim _9_budget As Decimal = CDec(myCoalesce(View.GetRowCellValue(e.RowHandle, "9_budget").ToString, "0.00"))
+            Dim _10_budget As Decimal = CDec(myCoalesce(View.GetRowCellValue(e.RowHandle, "10_budget").ToString, "0.00"))
+            Dim _11_budget As Decimal = CDec(myCoalesce(View.GetRowCellValue(e.RowHandle, "11_budget").ToString, "0.00"))
+            Dim _12_budget As Decimal = CDec(myCoalesce(View.GetRowCellValue(e.RowHandle, "12_budget").ToString, "0.00"))
+            Dim _1_actual As Decimal = CDec(myCoalesce(View.GetRowCellValue(e.RowHandle, "1_actual").ToString, "0.00"))
+            Dim _2_actual As Decimal = CDec(myCoalesce(View.GetRowCellValue(e.RowHandle, "2_actual").ToString, "0.00"))
+            Dim _3_actual As Decimal = CDec(myCoalesce(View.GetRowCellValue(e.RowHandle, "3_actual").ToString, "0.00"))
+            Dim _4_actual As Decimal = CDec(myCoalesce(View.GetRowCellValue(e.RowHandle, "4_actual").ToString, "0.00"))
+            Dim _5_actual As Decimal = CDec(myCoalesce(View.GetRowCellValue(e.RowHandle, "5_actual").ToString, "0.00"))
+            Dim _6_actual As Decimal = CDec(myCoalesce(View.GetRowCellValue(e.RowHandle, "6_actual").ToString, "0.00"))
+            Dim _7_actual As Decimal = CDec(myCoalesce(View.GetRowCellValue(e.RowHandle, "7_actual").ToString, "0.00"))
+            Dim _8_actual As Decimal = CDec(myCoalesce(View.GetRowCellValue(e.RowHandle, "8_actual").ToString, "0.00"))
+            Dim _9_actual As Decimal = CDec(myCoalesce(View.GetRowCellValue(e.RowHandle, "9_actual").ToString, "0.00"))
+            Dim _10_actual As Decimal = CDec(myCoalesce(View.GetRowCellValue(e.RowHandle, "10_actual").ToString, "0.00"))
+            Dim _11_actual As Decimal = CDec(myCoalesce(View.GetRowCellValue(e.RowHandle, "11_actual").ToString, "0.00"))
+            Dim _12_actual As Decimal = CDec(myCoalesce(View.GetRowCellValue(e.RowHandle, "12_actual").ToString, "0.00"))
+            Dim budget As Decimal = CDec(myCoalesce(View.GetRowCellValue(e.RowHandle, "total_budget").ToString, "0.00"))
+            Dim actual As Decimal = CDec(myCoalesce(View.GetRowCellValue(e.RowHandle, "total_actual"), "0.00"))
+            Select Case summaryID
+                Case "1_pros_sum"
+                    tot_1_budget += _1_budget
+                    tot_1_actual += _1_actual
+                Case "1_pros_gsum"
+                    tot_1_budget_grp += _1_budget
+                    tot_1_actual_grp += _1_actual
+                Case "2_pros_sum"
+                    tot_2_budget += _2_budget
+                    tot_2_actual += _2_actual
+                Case "2_pros_gsum"
+                    tot_2_budget_grp += _1_budget
+                    tot_2_actual_grp += _1_actual
+                Case "3_pros_sum"
+                    tot_3_budget += _3_budget
+                    tot_3_actual += _3_actual
+                Case "3_pros_gsum"
+                    tot_3_budget_grp += _3_budget
+                    tot_3_actual_grp += _3_actual
+                Case "4_pros_sum"
+                    tot_4_budget += _4_budget
+                    tot_4_actual += _4_actual
+                Case "4_pros_gsum"
+                    tot_4_budget_grp += _4_budget
+                    tot_4_actual_grp += _4_actual
+                Case "5_pros_sum"
+                    tot_5_budget += _5_budget
+                    tot_5_actual += _5_actual
+                Case "5_pros_gsum"
+                    tot_5_budget_grp += _5_budget
+                    tot_5_actual_grp += _5_actual
+                Case "6_pros_sum"
+                    tot_6_budget += _6_budget
+                    tot_6_actual += _6_actual
+                Case "6_pros_gsum"
+                    tot_6_budget_grp += _6_budget
+                    tot_6_actual_grp += _6_actual
+                Case "7_pros_sum"
+                    tot_7_budget += _7_budget
+                    tot_7_actual += _7_actual
+                Case "7_pros_gsum"
+                    tot_7_budget_grp += _7_budget
+                    tot_7_actual_grp += _7_actual
+                Case "8_pros_sum"
+                    tot_8_budget += _8_budget
+                    tot_8_actual += _8_actual
+                Case "8_pros_gsum"
+                    tot_8_budget_grp += _8_budget
+                    tot_8_actual_grp += _8_actual
+                Case "9_pros_sum"
+                    tot_9_budget += _9_budget
+                    tot_9_actual += _9_actual
+                Case "9_pros_gsum"
+                    tot_9_budget_grp += _9_budget
+                    tot_9_actual_grp += _9_actual
+                Case "10_pros_sum"
+                    tot_10_budget += _10_budget
+                    tot_10_actual += _10_actual
+                Case "10_pros_gsum"
+                    tot_10_budget_grp += _10_budget
+                    tot_10_actual_grp += _10_actual
+                Case "11_pros_sum"
+                    tot_11_budget += _11_budget
+                    tot_11_actual += _11_actual
+                Case "11_pros_gsum"
+                    tot_11_budget_grp += _11_budget
+                    tot_11_actual_grp += _11_actual
+                Case "12_pros_sum"
+                    tot_12_budget += _12_budget
+                    tot_12_actual += _12_actual
+                Case "12_pros_gsum"
+                    tot_12_budget_grp += _12_budget
+                    tot_12_actual_grp += _12_actual
+                Case "total_pros_sum"
+                    tot_budget += budget
+                    tot_actual += actual
+                Case "total_pros_gsum"
+                    tot_budget_grp += budget
+                    tot_actual_grp += actual
+            End Select
+        End If
+
+        ' Finalization 
+        If e.SummaryProcess = DevExpress.Data.CustomSummaryProcess.Finalize Then
+            Select Case summaryID
+                Case "1_pros_sum"
+                    Dim sum_res As Decimal = 0.0
+                    Try
+                        sum_res = (tot_1_actual / tot_1_budget) * 100
+                    Catch ex As Exception
+                    End Try
+                    e.TotalValue = sum_res
+                Case "1_pros_gsum"
+                    Dim sum_res As Decimal = 0.0
+                    Try
+                        sum_res = (tot_1_actual_grp / tot_1_budget_grp) * 100
+                    Catch ex As Exception
+                    End Try
+                    e.TotalValue = sum_res
+                Case "2_pros_sum"
+                    Dim sum_res As Decimal = 0.0
+                    Try
+                        sum_res = (tot_2_actual / tot_2_budget) * 100
+                    Catch ex As Exception
+                    End Try
+                    e.TotalValue = sum_res
+                Case "2_pros_gsum"
+                    Dim sum_res As Decimal = 0.0
+                    Try
+                        sum_res = (tot_2_actual_grp / tot_2_budget_grp) * 100
+                    Catch ex As Exception
+                    End Try
+                    e.TotalValue = sum_res
+                Case "3_pros_sum"
+                    Dim sum_res As Decimal = 0.0
+                    Try
+                        sum_res = (tot_3_actual / tot_3_budget) * 100
+                    Catch ex As Exception
+                    End Try
+                    e.TotalValue = sum_res
+                Case "3_pros_gsum"
+                    Dim sum_res As Decimal = 0.0
+                    Try
+                        sum_res = (tot_3_actual_grp / tot_3_budget_grp) * 100
+                    Catch ex As Exception
+                    End Try
+                    e.TotalValue = sum_res
+                Case "4_pros_sum"
+                    Dim sum_res As Decimal = 0.0
+                    Try
+                        sum_res = (tot_4_actual / tot_4_budget) * 100
+                    Catch ex As Exception
+                    End Try
+                    e.TotalValue = sum_res
+                Case "4_pros_gsum"
+                    Dim sum_res As Decimal = 0.0
+                    Try
+                        sum_res = (tot_4_actual_grp / tot_4_budget_grp) * 100
+                    Catch ex As Exception
+                    End Try
+                    e.TotalValue = sum_res
+                Case "5_pros_sum"
+                    Dim sum_res As Decimal = 0.0
+                    Try
+                        sum_res = (tot_5_actual / tot_5_budget) * 100
+                    Catch ex As Exception
+                    End Try
+                    e.TotalValue = sum_res
+                Case "5_pros_gsum"
+                    Dim sum_res As Decimal = 0.0
+                    Try
+                        sum_res = (tot_5_actual_grp / tot_5_budget_grp) * 100
+                    Catch ex As Exception
+                    End Try
+                    e.TotalValue = sum_res
+                Case "6_pros_sum"
+                    Dim sum_res As Decimal = 0.0
+                    Try
+                        sum_res = (tot_6_actual / tot_6_budget) * 100
+                    Catch ex As Exception
+                    End Try
+                    e.TotalValue = sum_res
+                Case "6_pros_gsum"
+                    Dim sum_res As Decimal = 0.0
+                    Try
+                        sum_res = (tot_6_actual_grp / tot_6_budget_grp) * 100
+                    Catch ex As Exception
+                    End Try
+                    e.TotalValue = sum_res
+                Case "7_pros_sum"
+                    Dim sum_res As Decimal = 0.0
+                    Try
+                        sum_res = (tot_7_actual / tot_7_budget) * 100
+                    Catch ex As Exception
+                    End Try
+                    e.TotalValue = sum_res
+                Case "7_pros_gsum"
+                    Dim sum_res As Decimal = 0.0
+                    Try
+                        sum_res = (tot_7_actual_grp / tot_7_budget_grp) * 100
+                    Catch ex As Exception
+                    End Try
+                    e.TotalValue = sum_res
+                Case "8_pros_sum"
+                    Dim sum_res As Decimal = 0.0
+                    Try
+                        sum_res = (tot_8_actual / tot_8_budget) * 100
+                    Catch ex As Exception
+                    End Try
+                    e.TotalValue = sum_res
+                Case "8_pros_gsum"
+                    Dim sum_res As Decimal = 0.0
+                    Try
+                        sum_res = (tot_8_actual_grp / tot_8_budget_grp) * 100
+                    Catch ex As Exception
+                    End Try
+                    e.TotalValue = sum_res
+                Case "9_pros_sum"
+                    Dim sum_res As Decimal = 0.0
+                    Try
+                        sum_res = (tot_9_actual / tot_9_budget) * 100
+                    Catch ex As Exception
+                    End Try
+                    e.TotalValue = sum_res
+                Case "9_pros_gsum"
+                    Dim sum_res As Decimal = 0.0
+                    Try
+                        sum_res = (tot_9_actual_grp / tot_9_budget_grp) * 100
+                    Catch ex As Exception
+                    End Try
+                    e.TotalValue = sum_res
+                Case "10_pros_sum"
+                    Dim sum_res As Decimal = 0.0
+                    Try
+                        sum_res = (tot_10_actual / tot_10_budget) * 100
+                    Catch ex As Exception
+                    End Try
+                    e.TotalValue = sum_res
+                Case "10_pros_gsum"
+                    Dim sum_res As Decimal = 0.0
+                    Try
+                        sum_res = (tot_10_actual_grp / tot_10_budget_grp) * 100
+                    Catch ex As Exception
+                    End Try
+                    e.TotalValue = sum_res
+                Case "11_pros_sum"
+                    Dim sum_res As Decimal = 0.0
+                    Try
+                        sum_res = (tot_11_actual / tot_11_budget) * 100
+                    Catch ex As Exception
+                    End Try
+                    e.TotalValue = sum_res
+                Case "11_pros_gsum"
+                    Dim sum_res As Decimal = 0.0
+                    Try
+                        sum_res = (tot_11_actual_grp / tot_11_budget_grp) * 100
+                    Catch ex As Exception
+                    End Try
+                    e.TotalValue = sum_res
+                Case "12_pros_sum"
+                    Dim sum_res As Decimal = 0.0
+                    Try
+                        sum_res = (tot_12_actual / tot_12_budget) * 100
+                    Catch ex As Exception
+                    End Try
+                    e.TotalValue = sum_res
+                Case "12_pros_gsum"
+                    Dim sum_res As Decimal = 0.0
+                    Try
+                        sum_res = (tot_12_actual_grp / tot_12_budget_grp) * 100
+                    Catch ex As Exception
+                    End Try
+                    e.TotalValue = sum_res
+                Case "total_pros_sum"
+                    Dim sum_res As Decimal = 0.0
+                    Try
+                        sum_res = (tot_actual / tot_budget) * 100
+                    Catch ex As Exception
+                    End Try
+                    e.TotalValue = sum_res
+                Case "total_pros_gsum"
+                    Dim sum_res As Decimal = 0.0
+                    Try
+                        sum_res = (tot_actual_grp / tot_budget_grp) * 100
+                    Catch ex As Exception
+                    End Try
+                    e.TotalValue = sum_res
+            End Select
+        End If
+    End Sub
 End Class
