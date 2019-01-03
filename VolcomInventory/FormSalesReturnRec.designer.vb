c67293f43e9dbabadf60a65f98c891a4c6e8deca
Agus Friastana <friastana@gmail.com>
2019-01-03 11:12:10 +0800

    Add receive return

--- VolcomInventory/FormSalesReturnRec.designer.vb
+++ VolcomInventory/FormSalesReturnRec.designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class FormSalesReturnRec
     Inherits DevExpress.XtraEditors.XtraForm
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         If disposing AndAlso components IsNot Nothing Then
             components.Dispose()
@@ -17,7 +17,7 @@ Partial Class FormSalesReturnRec
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.PanelControl1 = New DevExpress.XtraEditors.PanelControl()
         Me.GCList = New DevExpress.XtraGrid.GridControl()
@@ -25,21 +25,34 @@ Partial Class FormSalesReturnRec
         Me.GCId = New DevExpress.XtraGrid.Columns.GridColumn()
         Me.GCNumber = New DevExpress.XtraGrid.Columns.GridColumn()
         Me.GCDONumber = New DevExpress.XtraGrid.Columns.GridColumn()
-        Me.GCCreatedDate = New DevExpress.XtraGrid.Columns.GridColumn()
         Me.GCReportStatus = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GCCreatedDate = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GCTotal = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GroupControl1 = New DevExpress.XtraEditors.GroupControl()
+        Me.DEFrom = New DevExpress.XtraEditors.DateEdit()
+        Me.LabelControl1 = New DevExpress.XtraEditors.LabelControl()
+        Me.LabelControl2 = New DevExpress.XtraEditors.LabelControl()
+        Me.DEUntil = New DevExpress.XtraEditors.DateEdit()
+        Me.SBView = New DevExpress.XtraEditors.SimpleButton()
         CType(Me.PanelControl1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.PanelControl1.SuspendLayout()
         CType(Me.GCList, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.GVList, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.GroupControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupControl1.SuspendLayout()
+        CType(Me.DEFrom.Properties.CalendarTimeProperties, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DEFrom.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DEUntil.Properties.CalendarTimeProperties, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DEUntil.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'PanelControl1
         '
         Me.PanelControl1.Controls.Add(Me.GCList)
         Me.PanelControl1.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.PanelControl1.Location = New System.Drawing.Point(0, 0)
+        Me.PanelControl1.Location = New System.Drawing.Point(0, 53)
         Me.PanelControl1.Name = "PanelControl1"
-        Me.PanelControl1.Size = New System.Drawing.Size(1176, 565)
+        Me.PanelControl1.Size = New System.Drawing.Size(1176, 512)
         Me.PanelControl1.TabIndex = 1
         '
         'GCList
@@ -48,16 +61,18 @@ Partial Class FormSalesReturnRec
         Me.GCList.Location = New System.Drawing.Point(2, 2)
         Me.GCList.MainView = Me.GVList
         Me.GCList.Name = "GCList"
-        Me.GCList.Size = New System.Drawing.Size(1172, 561)
+        Me.GCList.Size = New System.Drawing.Size(1172, 508)
         Me.GCList.TabIndex = 0
         Me.GCList.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() {Me.GVList})
         '
         'GVList
         '
-        Me.GVList.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() {Me.GCId, Me.GCNumber, Me.GCDONumber, Me.GCCreatedDate, Me.GCReportStatus})
+        Me.GVList.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() {Me.GCId, Me.GCNumber, Me.GCDONumber, Me.GCTotal, Me.GCCreatedDate, Me.GCReportStatus})
         Me.GVList.GridControl = Me.GCList
+        Me.GVList.GroupSummary.AddRange(New DevExpress.XtraGrid.GridSummaryItem() {New DevExpress.XtraGrid.GridGroupSummaryItem(DevExpress.Data.SummaryItemType.Sum, "total", Me.GCNumber, "{0:N0}")})
         Me.GVList.Name = "GVList"
         Me.GVList.OptionsBehavior.Editable = False
+        Me.GVList.OptionsView.ShowFooter = True
         Me.GVList.OptionsView.ShowGroupPanel = False
         '
         'GCId
@@ -82,21 +97,93 @@ Partial Class FormSalesReturnRec
         Me.GCDONumber.Visible = True
         Me.GCDONumber.VisibleIndex = 1
         '
-        'GCCreatedDate
-        '
-        Me.GCCreatedDate.Caption = "Created Date"
-        Me.GCCreatedDate.FieldName = "created_date"
-        Me.GCCreatedDate.Name = "GCCreatedDate"
-        Me.GCCreatedDate.Visible = True
-        Me.GCCreatedDate.VisibleIndex = 2
-        '
         'GCReportStatus
         '
         Me.GCReportStatus.Caption = "Status"
         Me.GCReportStatus.FieldName = "report_status"
         Me.GCReportStatus.Name = "GCReportStatus"
         Me.GCReportStatus.Visible = True
-        Me.GCReportStatus.VisibleIndex = 3
+        Me.GCReportStatus.VisibleIndex = 4
+        '
+        'GCCreatedDate
+        '
+        Me.GCCreatedDate.Caption = "Created Date"
+        Me.GCCreatedDate.FieldName = "created_date"
+        Me.GCCreatedDate.Name = "GCCreatedDate"
+        Me.GCCreatedDate.Visible = True
+        Me.GCCreatedDate.VisibleIndex = 3
+        '
+        'GCTotal
+        '
+        Me.GCTotal.Caption = "Total"
+        Me.GCTotal.DisplayFormat.FormatType = DevExpress.Utils.FormatType.Numeric
+        Me.GCTotal.FieldName = "total"
+        Me.GCTotal.Name = "GCTotal"
+        Me.GCTotal.Summary.AddRange(New DevExpress.XtraGrid.GridSummaryItem() {New DevExpress.XtraGrid.GridColumnSummaryItem(DevExpress.Data.SummaryItemType.Sum, "total", "{0:N0}")})
+        Me.GCTotal.Visible = True
+        Me.GCTotal.VisibleIndex = 2
+        '
+        'GroupControl1
+        '
+        Me.GroupControl1.CaptionLocation = DevExpress.Utils.Locations.Left
+        Me.GroupControl1.Controls.Add(Me.SBView)
+        Me.GroupControl1.Controls.Add(Me.DEUntil)
+        Me.GroupControl1.Controls.Add(Me.LabelControl2)
+        Me.GroupControl1.Controls.Add(Me.LabelControl1)
+        Me.GroupControl1.Controls.Add(Me.DEFrom)
+        Me.GroupControl1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.GroupControl1.Location = New System.Drawing.Point(0, 0)
+        Me.GroupControl1.Name = "GroupControl1"
+        Me.GroupControl1.Size = New System.Drawing.Size(1176, 53)
+        Me.GroupControl1.TabIndex = 1
+        '
+        'DEFrom
+        '
+        Me.DEFrom.EditValue = Nothing
+        Me.DEFrom.Location = New System.Drawing.Point(65, 17)
+        Me.DEFrom.Name = "DEFrom"
+        Me.DEFrom.Properties.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() {New DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)})
+        Me.DEFrom.Properties.CalendarTimeProperties.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() {New DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)})
+        Me.DEFrom.Properties.DisplayFormat.FormatString = "dd MMMM yyyy"
+        Me.DEFrom.Properties.DisplayFormat.FormatType = DevExpress.Utils.FormatType.DateTime
+        Me.DEFrom.Size = New System.Drawing.Size(183, 20)
+        Me.DEFrom.TabIndex = 0
+        '
+        'LabelControl1
+        '
+        Me.LabelControl1.Location = New System.Drawing.Point(35, 20)
+        Me.LabelControl1.Name = "LabelControl1"
+        Me.LabelControl1.Size = New System.Drawing.Size(24, 13)
+        Me.LabelControl1.TabIndex = 1
+        Me.LabelControl1.Text = "From"
+        '
+        'LabelControl2
+        '
+        Me.LabelControl2.Location = New System.Drawing.Point(266, 20)
+        Me.LabelControl2.Name = "LabelControl2"
+        Me.LabelControl2.Size = New System.Drawing.Size(21, 13)
+        Me.LabelControl2.TabIndex = 2
+        Me.LabelControl2.Text = "Until"
+        '
+        'DEUntil
+        '
+        Me.DEUntil.EditValue = Nothing
+        Me.DEUntil.Location = New System.Drawing.Point(293, 17)
+        Me.DEUntil.Name = "DEUntil"
+        Me.DEUntil.Properties.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() {New DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)})
+        Me.DEUntil.Properties.CalendarTimeProperties.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() {New DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)})
+        Me.DEUntil.Properties.DisplayFormat.FormatString = "dd MMMM yyyy"
+        Me.DEUntil.Properties.DisplayFormat.FormatType = DevExpress.Utils.FormatType.DateTime
+        Me.DEUntil.Size = New System.Drawing.Size(183, 20)
+        Me.DEUntil.TabIndex = 3
+        '
+        'SBView
+        '
+        Me.SBView.Location = New System.Drawing.Point(494, 15)
+        Me.SBView.Name = "SBView"
+        Me.SBView.Size = New System.Drawing.Size(78, 23)
+        Me.SBView.TabIndex = 4
+        Me.SBView.Text = "View"
         '
         'FormSalesReturnRec
         '
@@ -104,12 +191,20 @@ Partial Class FormSalesReturnRec
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(1176, 565)
         Me.Controls.Add(Me.PanelControl1)
+        Me.Controls.Add(Me.GroupControl1)
         Me.Name = "FormSalesReturnRec"
         Me.Text = "Receive Return"
         CType(Me.PanelControl1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.PanelControl1.ResumeLayout(False)
         CType(Me.GCList, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.GVList, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.GroupControl1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupControl1.ResumeLayout(False)
+        Me.GroupControl1.PerformLayout()
+        CType(Me.DEFrom.Properties.CalendarTimeProperties, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DEFrom.Properties, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DEUntil.Properties.CalendarTimeProperties, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DEUntil.Properties, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
@@ -119,6 +214,13 @@ Partial Class FormSalesReturnRec
     Friend WithEvents GCId As DevExpress.XtraGrid.Columns.GridColumn
     Friend WithEvents GCNumber As DevExpress.XtraGrid.Columns.GridColumn
     Friend WithEvents GCDONumber As DevExpress.XtraGrid.Columns.GridColumn
-    Friend WithEvents GCCreatedDate As DevExpress.XtraGrid.Columns.GridColumn
     Friend WithEvents GCReportStatus As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GCTotal As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GCCreatedDate As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GroupControl1 As DevExpress.XtraEditors.GroupControl
+    Friend WithEvents LabelControl1 As DevExpress.XtraEditors.LabelControl
+    Friend WithEvents DEFrom As DevExpress.XtraEditors.DateEdit
+    Friend WithEvents DEUntil As DevExpress.XtraEditors.DateEdit
+    Friend WithEvents LabelControl2 As DevExpress.XtraEditors.LabelControl
+    Friend WithEvents SBView As DevExpress.XtraEditors.SimpleButton
 End Class
