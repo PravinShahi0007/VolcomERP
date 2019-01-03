a8078066c656a0e96afaecb38121eabf46bc2e3d
Putu Septian Primadewa <iansaviour@gmail.com>
2019-01-03 17:16:59 +0800

    Operation workdays

--- VolcomInventory/FormCashAdvance.vb
+++ VolcomInventory/FormCashAdvance.vb
@@ -1,5 +1,30 @@
 ﻿Public Class FormCashAdvance
+    Dim bnew_active As String = "1"
+    Dim bedit_active As String = "1"
+    Dim bdel_active As String = "1"
+
+    Private Sub FormCashAdvance_Deactivate(sender As Object, e As EventArgs) Handles MyBase.Deactivate
+        FormMain.hide_rb()
+    End Sub
+
+    Private Sub FormCashAdvance_Activated(sender As Object, e As EventArgs) Handles MyBase.Activated
+        FormMain.show_rb(Name)
+        check_menu()
+    End Sub
+
+    Sub check_menu()
+        bnew_active = "1"
+        bedit_active = "1"
+        bdel_active = "1"
+        checkFormAccess(Name)
+        button_main(bnew_active, bedit_active, bdel_active)
+    End Sub
+
     Private Sub FormCashAdvance_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub BView_Click(sender As Object, e As EventArgs) Handles BView.Click
 
     End Sub
 End Class
--- VolcomInventory/FormCashAdvanceDet.vb
+++ VolcomInventory/FormCashAdvanceDet.vb
@@ -1,5 +1,7 @@
 ﻿Public Class FormCashAdvanceDet
     Public id_ca As String = "-1"
+    Public is_no_schedule As Boolean = False
+    Public is_load As Boolean = False
 
     Private Sub FormCashAdvanceDet_FormClosed(sender As Object, e As FormClosedEventArgs) Handles MyBase.FormClosed
         Dispose()
@@ -20,7 +22,7 @@
 
         DEDateCreated.EditValue = Now
         DEAdvanceEnd.EditValue = Now
-        DEReportDue.EditValue = Now
+        DEDueDate.EditValue = Now
         '
         TENumber.Text = "[auto generate]"
         '
@@ -29,6 +31,8 @@
         Else 'edit
 
         End If
+
+        is_load = True
     End Sub
 
     Sub load_type()
@@ -59,5 +63,53 @@
     Sub load_report_status()
         Dim query As String = "SELECT id_report_status,report_status FROM tb_lookup_report_status"
         viewLookupQuery(LEReportStatus, query, 0, "report_status", "id_report_status")
+    End Sub
+
+    Private Sub SLEType_EditValueChanged(sender As Object, e As EventArgs) Handles SLEType.EditValueChanged
+        calculate_report_day()
+    End Sub
+
+    Sub calculate_report_day()
+        If is_load = True Then
+            Try
+                Dim date_start As String = Date.Parse(DEAdvanceEnd.EditValue.ToString).ToString("yyyy-MM-dd")
+                Dim query As String = ""
+
+                If SLEType.EditValue.ToString = "1" Then 'dinas H+3
+                    query = "CALL work_days(3,'" & date_start & "','" & SLEEmployee.EditValue.ToString & "')"
+                ElseIf SLEType.EditValue.ToString = "2" Then 'operation H+1
+                    query = "CALL work_days(1,'" & date_start & "','" & SLEEmployee.EditValue.ToString & "')"
+                End If
+
+                Dim data As DataTable = execute_query(query, -1, True, "", "", "", "")
+                If data.Rows.Count > 0 Then
+                    DEDueDate.EditValue = data.Rows(0)("date")
+                    is_no_schedule = False
+                Else
+                    warningCustom("Please register schedule for this employee first, contact HRD for further detail.")
+                    is_no_schedule = True
+                End If
+                '
+                Console.WriteLine(query)
+            Catch ex As Exception
+                Console.WriteLine(ex.ToString)
+            End Try
+        End If
+    End Sub
+
+    Private Sub DEAdvanceEnd_EditValueChanged(sender As Object, e As EventArgs) Handles DEAdvanceEnd.EditValueChanged
+        calculate_report_day()
+    End Sub
+
+    Private Sub SLEEmployee_EditValueChanged(sender As Object, e As EventArgs) Handles SLEEmployee.EditValueChanged
+        calculate_report_day()
+    End Sub
+
+    Private Sub BSave_Click(sender As Object, e As EventArgs) Handles BSave.Click
+        If is_no_schedule = True Then
+            warningCustom("Please register schedule for this employee first, contact HRD for further detail.")
+        Else
+
+        End If
     End Sub
 End Class
