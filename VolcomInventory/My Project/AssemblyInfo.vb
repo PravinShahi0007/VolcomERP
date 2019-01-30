4c61cb8ce14e30c394d12bd181b8647efcd3d424
Putu Septian Primadewa <iansaviour@gmail.com>
2019-01-30 10:41:42 +0800

    Fix Db

--- VolcomInventory/My Project/AssemblyInfo.vb
+++ VolcomInventory/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("0.0.3.87")>
-<Assembly: AssemblyFileVersion("0.0.3.87")>
+<Assembly: AssemblyVersion("0.0.3.88")>
+<Assembly: AssemblyFileVersion("0.0.3.88")>
