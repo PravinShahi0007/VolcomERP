a099ff7389ecc7be028fb99538bc6b623b37e244
Agus Friastana <friastana@gmail.com>
2019-02-11 17:12:58 +0800

    add scanner device check

--- VolcomInventory/FormNtwainCoba.vb
+++ VolcomInventory/FormNtwainCoba.vb
@@ -4,21 +4,21 @@
     End Sub
 
     Private Sub BSave_Click(sender As Object, e As EventArgs) Handles BSave.Click
-        If PictureEdit1.Image Is Nothing Then
-            PictureEdit1.Image = Scanner.Scan
+        If Not PictureEdit1.Image Is Nothing Then
+            save_image_ori(PictureEdit1, Application.StartupPath + "\imagestemp\", "wiascanner.jpg")
+
+            Dim command As String = "/K net use \\192.168.1.30\dataapp\wia /user:Catur bangcat48 & COPY """ + Application.StartupPath + "\imagestemp\wiascanner.jpg"" ""\\192.168.1.30\dataapp\wia\wiascanner.jpg"" /Y & exit"
+
+            Dim p As New ProcessStartInfo("CMD.EXE")
+
+            p.WindowStyle = ProcessWindowStyle.Hidden
+            p.CreateNoWindow = True
+            p.UseShellExecute = False
+            p.Arguments = command
+
+            Process.Start(p)
+        Else
+            errorCustom("Please scan image first.")
         End If
-
-        save_image_ori(PictureEdit1, Application.StartupPath + "\imagestemp\", "wiascanner.jpg")
-
-        Dim command As String = "/K net use \\192.168.1.30\dataapp\wia /user:Catur bangcat48 & COPY """ + Application.StartupPath + "\imagestemp\wiascanner.jpg"" ""\\192.168.1.30\dataapp\wia\wiascanner.jpg"" /Y & exit"
-
-        Dim p As New ProcessStartInfo("CMD.EXE")
-
-        p.WindowStyle = ProcessWindowStyle.Hidden
-        p.CreateNoWindow = True
-        p.UseShellExecute = False
-        p.Arguments = command
-
-        Process.Start(p)
     End Sub
 End Class
--- VolcomInventory/Scanner.vb
+++ VolcomInventory/Scanner.vb
@@ -2,65 +2,69 @@
     Shared Function Scan() As Image
         Dim cdd As WIA.CommonDialog = New WIA.CommonDialog
 
-        Dim device As WIA.Device = cdd.ShowSelectDevice(WIA.WiaDeviceType.UnspecifiedDeviceType, True, False)
+        Dim device As WIA.Device
 
-        Dim deviceId = ""
+        Try
+            device = cdd.ShowSelectDevice(WIA.WiaDeviceType.UnspecifiedDeviceType, True, False)
+        Catch ex As Exception
+            errorCustom("No scanners were detected.")
+        End Try
 
         If Not device Is Nothing Then
-            deviceId = device.DeviceID
-        End If
+            Dim manage As WIA.DeviceManager = New WIA.DeviceManager()
+            Dim wiaDev As WIA.Device = Nothing
 
-        Dim manage As WIA.DeviceManager = New WIA.DeviceManager()
-        Dim wiaDev As WIA.Device = Nothing
+            For Each info As WIA.DeviceInfo In manage.DeviceInfos
+                If info.DeviceID = device.DeviceID Then
+                    Dim infoprop As WIA.Properties = Nothing
+                    infoprop = info.Properties
 
-        For Each info As WIA.DeviceInfo In manage.DeviceInfos
-            If info.DeviceID = deviceId Then
-                Dim infoprop As WIA.Properties = Nothing
-                infoprop = info.Properties
+                    wiaDev = info.Connect()
 
-                wiaDev = info.Connect()
+                    Exit For
+                End If
+            Next
 
-                Exit For
+            Dim item As WIA.Item = wiaDev.Items(1)
+
+            Dim cds As WIA.CommonDialog = New WIA.CommonDialog
+
+            Dim file As WIA.ImageFile = cds.ShowTransfer(item, WIA.FormatID.wiaFormatBMP, False)
+
+            Dim imageStream As IO.MemoryStream = New IO.MemoryStream(CType(file.FileData.BinaryData, Byte()))
+
+            Dim imageOri As New Bitmap(imageStream)
+
+            'resize
+            Dim width As Integer = 0
+            Dim height As Integer = 0
+            Dim maxSize As Integer = 1024
+
+            If imageOri.Width > maxSize Or imageOri.Height > maxSize Then
+                If imageOri.Width > imageOri.Height Then
+                    width = maxSize
+                    height = (maxSize / imageOri.Width) * imageOri.Height
+                Else
+                    width = (maxSize / imageOri.Height) * imageOri.Width
+                    height = maxSize
+                End If
             End If
-        Next
 
-        Dim item As WIA.Item = wiaDev.Items(1)
+            Dim imageRes As New Bitmap(width, height)
 
-        Dim cds As WIA.CommonDialog = New WIA.CommonDialog
+            Using gfx As Graphics = Graphics.FromImage(imageRes)
+                gfx.CompositingQuality = Drawing2D.CompositingQuality.HighQuality
+                gfx.InterpolationMode = Drawing2D.InterpolationMode.HighQualityBicubic
+                gfx.PixelOffsetMode = Drawing2D.PixelOffsetMode.HighQuality
+                gfx.SmoothingMode = Drawing2D.SmoothingMode.AntiAlias
+                gfx.CompositingMode = Drawing2D.CompositingMode.SourceOver
 
-        Dim file As WIA.ImageFile = cds.ShowTransfer(item, WIA.FormatID.wiaFormatBMP, False)
+                gfx.DrawImage(imageOri, 0, 0, imageRes.Width, imageRes.Height)
+            End Using
 
-        Dim imageStream As IO.MemoryStream = New IO.MemoryStream(CType(file.FileData.BinaryData, Byte()))
-
-        Dim imageOri As New Bitmap(imageStream)
-
-        'resize
-        Dim width As Integer = 0
-        Dim height As Integer = 0
-        Dim maxSize As Integer = 1024
-
-        If imageOri.Width > maxSize Or imageOri.Height > maxSize Then
-            If imageOri.Width > imageOri.Height Then
-                width = maxSize
-                height = (maxSize / imageOri.Width) * imageOri.Height
-            Else
-                width = (maxSize / imageOri.Height) * imageOri.Width
-                height = maxSize
-            End If
+            Return CType(imageRes, Image)
+        Else
+            Return CType(Nothing, Image)
         End If
-
-        Dim imageRes As New Bitmap(width, height)
-
-        Using gfx As Graphics = Graphics.FromImage(imageRes)
-            gfx.CompositingQuality = Drawing2D.CompositingQuality.HighQuality
-            gfx.InterpolationMode = Drawing2D.InterpolationMode.HighQualityBicubic
-            gfx.PixelOffsetMode = Drawing2D.PixelOffsetMode.HighQuality
-            gfx.SmoothingMode = Drawing2D.SmoothingMode.AntiAlias
-            gfx.CompositingMode = Drawing2D.CompositingMode.SourceOver
-
-            gfx.DrawImage(imageOri, 0, 0, imageRes.Width, imageRes.Height)
-        End Using
-
-        Return CType(imageRes, Image)
     End Function
 End Class
