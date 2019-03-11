2e1eadfe0bd9c13bddc1ac0d7aa1c89542f8ebc8
Putu Septian Primadewa <iansaviour@gmail.com>
2019-03-11 11:11:27 +0800

    Employee Propose

---
+++ VolcomInventory/FormEmloyeePps.Designer.vb
@@ -0,0 +1,324 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FormEmloyeePps
+    Inherits DevExpress.XtraEditors.XtraForm
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        If disposing AndAlso components IsNot Nothing Then
+            components.Dispose()
+        End If
+        MyBase.Dispose(disposing)
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FormEmloyeePps))
+        Me.PanelControl2 = New DevExpress.XtraEditors.PanelControl()
+        Me.SLUEEmployee = New DevExpress.XtraEditors.SearchLookUpEdit()
+        Me.SearchLookUpEditEmp = New DevExpress.XtraGrid.Views.Grid.GridView()
+        Me.GridColumn94 = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GridColumn97 = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GridColumn95 = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GridColumn98 = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GridColumn96 = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.LabelControl3 = New DevExpress.XtraEditors.LabelControl()
+        Me.LEDeptSum = New DevExpress.XtraEditors.LookUpEdit()
+        Me.LabelControl1 = New DevExpress.XtraEditors.LabelControl()
+        Me.BViewSum = New DevExpress.XtraEditors.SimpleButton()
+        Me.GCEmployeePps = New DevExpress.XtraGrid.GridControl()
+        Me.GVEmployeePps = New DevExpress.XtraGrid.Views.Grid.GridView()
+        Me.GridColumnID = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GridColumnNumber = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GridColumnName = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GridColumnNote = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GridColumnStatus = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.LargeImageCollection = New DevExpress.Utils.ImageCollection(Me.components)
+        Me.BNew = New DevExpress.XtraEditors.SimpleButton()
+        Me.BEdit = New DevExpress.XtraEditors.SimpleButton()
+        Me.GridColumnType = New DevExpress.XtraGrid.Columns.GridColumn()
+        CType(Me.PanelControl2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.PanelControl2.SuspendLayout()
+        CType(Me.SLUEEmployee.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.SearchLookUpEditEmp, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.LEDeptSum.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.GCEmployeePps, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.GVEmployeePps, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.LargeImageCollection, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PanelControl2
+        '
+        Me.PanelControl2.Controls.Add(Me.BEdit)
+        Me.PanelControl2.Controls.Add(Me.BNew)
+        Me.PanelControl2.Controls.Add(Me.SLUEEmployee)
+        Me.PanelControl2.Controls.Add(Me.LabelControl3)
+        Me.PanelControl2.Controls.Add(Me.LEDeptSum)
+        Me.PanelControl2.Controls.Add(Me.LabelControl1)
+        Me.PanelControl2.Controls.Add(Me.BViewSum)
+        Me.PanelControl2.Dock = System.Windows.Forms.DockStyle.Top
+        Me.PanelControl2.Location = New System.Drawing.Point(0, 0)
+        Me.PanelControl2.Name = "PanelControl2"
+        Me.PanelControl2.Size = New System.Drawing.Size(876, 38)
+        Me.PanelControl2.TabIndex = 4
+        '
+        'SLUEEmployee
+        '
+        Me.SLUEEmployee.Location = New System.Drawing.Point(266, 9)
+        Me.SLUEEmployee.Name = "SLUEEmployee"
+        Me.SLUEEmployee.Properties.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() {New DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)})
+        Me.SLUEEmployee.Properties.ShowClearButton = False
+        Me.SLUEEmployee.Properties.View = Me.SearchLookUpEditEmp
+        Me.SLUEEmployee.Size = New System.Drawing.Size(209, 20)
+        Me.SLUEEmployee.TabIndex = 19
+        '
+        'SearchLookUpEditEmp
+        '
+        Me.SearchLookUpEditEmp.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() {Me.GridColumn94, Me.GridColumn97, Me.GridColumn95, Me.GridColumn98, Me.GridColumn96})
+        Me.SearchLookUpEditEmp.FocusRectStyle = DevExpress.XtraGrid.Views.Grid.DrawFocusRectStyle.RowFocus
+        Me.SearchLookUpEditEmp.Name = "SearchLookUpEditEmp"
+        Me.SearchLookUpEditEmp.OptionsSelection.EnableAppearanceFocusedCell = False
+        Me.SearchLookUpEditEmp.OptionsView.ShowFilterPanelMode = DevExpress.XtraGrid.Views.Base.ShowFilterPanelMode.Never
+        Me.SearchLookUpEditEmp.OptionsView.ShowGroupPanel = False
+        '
+        'GridColumn94
+        '
+        Me.GridColumn94.FieldName = "id_employee"
+        Me.GridColumn94.Name = "GridColumn94"
+        '
+        'GridColumn97
+        '
+        Me.GridColumn97.Caption = "Code"
+        Me.GridColumn97.FieldName = "employee_code"
+        Me.GridColumn97.Name = "GridColumn97"
+        Me.GridColumn97.Visible = True
+        Me.GridColumn97.VisibleIndex = 0
+        '
+        'GridColumn95
+        '
+        Me.GridColumn95.Caption = "Employee"
+        Me.GridColumn95.FieldName = "employee_name"
+        Me.GridColumn95.Name = "GridColumn95"
+        Me.GridColumn95.Visible = True
+        Me.GridColumn95.VisibleIndex = 1
+        '
+        'GridColumn98
+        '
+        Me.GridColumn98.FieldName = "id_departement"
+        Me.GridColumn98.Name = "GridColumn98"
+        '
+        'GridColumn96
+        '
+        Me.GridColumn96.FieldName = "id_employee_active"
+        Me.GridColumn96.Name = "GridColumn96"
+        '
+        'LabelControl3
+        '
+        Me.LabelControl3.Location = New System.Drawing.Point(217, 12)
+        Me.LabelControl3.Name = "LabelControl3"
+        Me.LabelControl3.Size = New System.Drawing.Size(46, 13)
+        Me.LabelControl3.TabIndex = 18
+        Me.LabelControl3.Text = "Employee"
+        '
+        'LEDeptSum
+        '
+        Me.LEDeptSum.Location = New System.Drawing.Point(81, 9)
+        Me.LEDeptSum.Name = "LEDeptSum"
+        Me.LEDeptSum.Properties.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() {New DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)})
+        Me.LEDeptSum.Properties.Columns.AddRange(New DevExpress.XtraEditors.Controls.LookUpColumnInfo() {New DevExpress.XtraEditors.Controls.LookUpColumnInfo("id_departement", "Id Departement", 20, DevExpress.Utils.FormatType.None, "", False, DevExpress.Utils.HorzAlignment.[Default]), New DevExpress.XtraEditors.Controls.LookUpColumnInfo("departement", "Departemen")})
+        Me.LEDeptSum.Size = New System.Drawing.Size(130, 20)
+        Me.LEDeptSum.TabIndex = 14
+        '
+        'LabelControl1
+        '
+        Me.LabelControl1.Location = New System.Drawing.Point(12, 12)
+        Me.LabelControl1.Name = "LabelControl1"
+        Me.LabelControl1.Size = New System.Drawing.Size(63, 13)
+        Me.LabelControl1.TabIndex = 13
+        Me.LabelControl1.Text = "Departement"
+        '
+        'BViewSum
+        '
+        Me.BViewSum.Location = New System.Drawing.Point(481, 6)
+        Me.BViewSum.Name = "BViewSum"
+        Me.BViewSum.Size = New System.Drawing.Size(59, 25)
+        Me.BViewSum.TabIndex = 1
+        Me.BViewSum.Text = "View"
+        '
+        'GCEmployeePps
+        '
+        Me.GCEmployeePps.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.GCEmployeePps.Location = New System.Drawing.Point(0, 38)
+        Me.GCEmployeePps.MainView = Me.GVEmployeePps
+        Me.GCEmployeePps.Name = "GCEmployeePps"
+        Me.GCEmployeePps.Size = New System.Drawing.Size(876, 525)
+        Me.GCEmployeePps.TabIndex = 5
+        Me.GCEmployeePps.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() {Me.GVEmployeePps})
+        '
+        'GVEmployeePps
+        '
+        Me.GVEmployeePps.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() {Me.GridColumnID, Me.GridColumnType, Me.GridColumnNumber, Me.GridColumnName, Me.GridColumnNote, Me.GridColumnStatus})
+        Me.GVEmployeePps.GridControl = Me.GCEmployeePps
+        Me.GVEmployeePps.Name = "GVEmployeePps"
+        Me.GVEmployeePps.OptionsView.ShowGroupPanel = False
+        '
+        'GridColumnID
+        '
+        Me.GridColumnID.Caption = "ID"
+        Me.GridColumnID.FieldName = "id_employee_pps"
+        Me.GridColumnID.Name = "GridColumnID"
+        '
+        'GridColumnNumber
+        '
+        Me.GridColumnNumber.Caption = "Number"
+        Me.GridColumnNumber.FieldName = "number"
+        Me.GridColumnNumber.Name = "GridColumnNumber"
+        Me.GridColumnNumber.Visible = True
+        Me.GridColumnNumber.VisibleIndex = 0
+        Me.GridColumnNumber.Width = 186
+        '
+        'GridColumnName
+        '
+        Me.GridColumnName.Caption = "Name"
+        Me.GridColumnName.FieldName = "employee_name"
+        Me.GridColumnName.Name = "GridColumnName"
+        Me.GridColumnName.Visible = True
+        Me.GridColumnName.VisibleIndex = 2
+        Me.GridColumnName.Width = 245
+        '
+        'GridColumnNote
+        '
+        Me.GridColumnNote.Caption = "Note"
+        Me.GridColumnNote.FieldName = "note"
+        Me.GridColumnNote.Name = "GridColumnNote"
+        Me.GridColumnNote.Visible = True
+        Me.GridColumnNote.VisibleIndex = 3
+        Me.GridColumnNote.Width = 882
+        '
+        'GridColumnStatus
+        '
+        Me.GridColumnStatus.AppearanceCell.Options.UseTextOptions = True
+        Me.GridColumnStatus.AppearanceCell.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Center
+        Me.GridColumnStatus.AppearanceHeader.Options.UseTextOptions = True
+        Me.GridColumnStatus.AppearanceHeader.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Center
+        Me.GridColumnStatus.Caption = "Status"
+        Me.GridColumnStatus.FieldName = "report_status"
+        Me.GridColumnStatus.Name = "GridColumnStatus"
+        Me.GridColumnStatus.Visible = True
+        Me.GridColumnStatus.VisibleIndex = 4
+        Me.GridColumnStatus.Width = 189
+        '
+        'LargeImageCollection
+        '
+        Me.LargeImageCollection.ImageSize = New System.Drawing.Size(24, 24)
+        Me.LargeImageCollection.ImageStream = CType(resources.GetObject("LargeImageCollection.ImageStream"), DevExpress.Utils.ImageCollectionStreamer)
+        Me.LargeImageCollection.Images.SetKeyName(0, "20_24x24.png")
+        Me.LargeImageCollection.Images.SetKeyName(1, "8_24x24.png")
+        Me.LargeImageCollection.Images.SetKeyName(2, "23_24x24.png")
+        Me.LargeImageCollection.Images.SetKeyName(3, "arrow_refresh.png")
+        Me.LargeImageCollection.Images.SetKeyName(4, "check_mark.png")
+        Me.LargeImageCollection.Images.SetKeyName(5, "gnome_application_exit (1).png")
+        Me.LargeImageCollection.Images.SetKeyName(6, "printer_3.png")
+        Me.LargeImageCollection.Images.SetKeyName(7, "save.png")
+        Me.LargeImageCollection.Images.SetKeyName(8, "31_24x24.png")
+        Me.LargeImageCollection.Images.SetKeyName(9, "18_24x24.png")
+        Me.LargeImageCollection.Images.SetKeyName(10, "1415351112474759854-32.png")
+        Me.LargeImageCollection.Images.SetKeyName(11, "icon_merchandise_clothes32.png")
+        Me.LargeImageCollection.Images.SetKeyName(12, "t_shirtgreen.png")
+        Me.LargeImageCollection.Images.SetKeyName(13, "lock red.png")
+        Me.LargeImageCollection.Images.SetKeyName(14, "ordering32.png")
+        Me.LargeImageCollection.Images.SetKeyName(15, "kghostview.png")
+        Me.LargeImageCollection.Images.SetKeyName(16, "MetroUI-Folder-OS-Configure-icon.png")
+        Me.LargeImageCollection.Images.SetKeyName(17, "Setting(32).png")
+        Me.LargeImageCollection.Images.SetKeyName(18, "estimate_icon32.png")
+        Me.LargeImageCollection.Images.SetKeyName(19, "copy_icon.png")
+        '
+        'BNew
+        '
+        Me.BNew.Dock = System.Windows.Forms.DockStyle.Right
+        Me.BNew.ImageIndex = 0
+        Me.BNew.ImageList = Me.LargeImageCollection
+        Me.BNew.Location = New System.Drawing.Point(761, 2)
+        Me.BNew.Name = "BNew"
+        Me.BNew.Size = New System.Drawing.Size(113, 34)
+        Me.BNew.TabIndex = 20
+        Me.BNew.Text = "New Employee"
+        '
+        'BEdit
+        '
+        Me.BEdit.Dock = System.Windows.Forms.DockStyle.Right
+        Me.BEdit.ImageIndex = 14
+        Me.BEdit.ImageList = Me.LargeImageCollection
+        Me.BEdit.Location = New System.Drawing.Point(605, 2)
+        Me.BEdit.Name = "BEdit"
+        Me.BEdit.Size = New System.Drawing.Size(156, 34)
+        Me.BEdit.TabIndex = 21
+        Me.BEdit.Text = "Change Data Employee"
+        '
+        'GridColumnType
+        '
+        Me.GridColumnType.Caption = "Type"
+        Me.GridColumnType.FieldName = "pps_type"
+        Me.GridColumnType.Name = "GridColumnType"
+        Me.GridColumnType.Visible = True
+        Me.GridColumnType.VisibleIndex = 1
+        Me.GridColumnType.Width = 130
+        '
+        'FormEmloyeePps
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(876, 563)
+        Me.Controls.Add(Me.GCEmployeePps)
+        Me.Controls.Add(Me.PanelControl2)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "FormEmloyeePps"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Employee Proposal"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+        CType(Me.PanelControl2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.PanelControl2.ResumeLayout(False)
+        Me.PanelControl2.PerformLayout()
+        CType(Me.SLUEEmployee.Properties, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.SearchLookUpEditEmp, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.LEDeptSum.Properties, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.GCEmployeePps, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.GVEmployeePps, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.LargeImageCollection, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents PanelControl2 As DevExpress.XtraEditors.PanelControl
+    Friend WithEvents SLUEEmployee As DevExpress.XtraEditors.SearchLookUpEdit
+    Friend WithEvents SearchLookUpEditEmp As DevExpress.XtraGrid.Views.Grid.GridView
+    Friend WithEvents GridColumn94 As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GridColumn97 As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GridColumn95 As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GridColumn98 As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GridColumn96 As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents LabelControl3 As DevExpress.XtraEditors.LabelControl
+    Friend WithEvents LEDeptSum As DevExpress.XtraEditors.LookUpEdit
+    Friend WithEvents LabelControl1 As DevExpress.XtraEditors.LabelControl
+    Friend WithEvents BViewSum As DevExpress.XtraEditors.SimpleButton
+    Friend WithEvents GCEmployeePps As DevExpress.XtraGrid.GridControl
+    Friend WithEvents GVEmployeePps As DevExpress.XtraGrid.Views.Grid.GridView
+    Friend WithEvents GridColumnID As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GridColumnNumber As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GridColumnName As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GridColumnNote As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GridColumnStatus As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents BEdit As DevExpress.XtraEditors.SimpleButton
+    Friend WithEvents BNew As DevExpress.XtraEditors.SimpleButton
+    Public WithEvents LargeImageCollection As DevExpress.Utils.ImageCollection
+    Friend WithEvents GridColumnType As DevExpress.XtraGrid.Columns.GridColumn
+End Class
---
+++ VolcomInventory/FormEmloyeePps.vb
@@ -0,0 +1,53 @@
+﻿Public Class FormEmloyeePps
+    Dim bnew_active As String = "0"
+    Dim bedit_active As String = "0"
+    Dim bdel_active As String = "0"
+    '
+    Public is_hrd As String = "-1"
+
+    Private Sub FormEmployeePps_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        viewDept()
+        viewEmployee()
+    End Sub
+
+    Sub viewDept()
+        Dim query As String = "SELECT 0 as id_departement, 'All departement' as departement UNION  "
+        query += "(SELECT id_departement,departement FROM tb_m_departement a ORDER BY a.departement ASC) "
+        viewLookupQuery(LEDeptSum, query, 0, "departement", "id_departement")
+    End Sub
+
+    Sub viewEmployee()
+        Dim query As String = "SELECT 0 AS id_employee, '' AS employee_code, 'All employee' AS employee_name, 0 AS id_departement, 0 AS id_employee_active UNION (SELECT e.id_employee, e.employee_code, e.employee_name, e.id_departement, e.id_employee_active FROM tb_m_employee e LEFT JOIN tb_m_departement d ON e.id_departement = d.id_departement ORDER BY e.employee_name)"
+        viewSearchLookupQuery(SLUEEmployee, query, "id_employee", "employee_name", "id_employee")
+    End Sub
+
+    Private Sub FormEmployeePps_Activated(sender As Object, e As EventArgs) Handles MyBase.Activated
+        FormMain.show_rb(Name)
+        checkFormAccess(Name)
+        button_main(bnew_active, bedit_active, bdel_active)
+    End Sub
+
+    Private Sub FormEmployeePps_Deactivate(sender As Object, e As EventArgs) Handles MyBase.Deactivate
+        FormMain.hide_rb()
+    End Sub
+
+    Private Sub FormEmpLeave_FormClosed(sender As Object, e As FormClosedEventArgs) Handles MyBase.FormClosed
+        Dispose()
+    End Sub
+
+    Private Sub SLUEEmployee_Click(sender As Object, e As EventArgs) Handles SLUEEmployee.Click
+        SearchLookUpEditEmp.ActiveFilterString = ""
+
+        If LEDeptSum.EditValue.ToString <> "0" Then
+            SearchLookUpEditEmp.ActiveFilterString += "[id_departement] = " + LEDeptSum.EditValue.ToString
+        End If
+
+        If SearchLookUpEditEmp.ActiveFilterString <> "" Then
+            SearchLookUpEditEmp.ActiveFilterString += "OR [id_employee] = 0"
+        End If
+    End Sub
+
+    Private Sub LEDeptSum_EditValueChanged(sender As Object, e As EventArgs) Handles LEDeptSum.EditValueChanged
+        SLUEEmployee.EditValue = 0
+    End Sub
+End Class
