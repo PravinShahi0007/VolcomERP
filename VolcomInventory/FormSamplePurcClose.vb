acecfed123773b9817326576c274d59a6728125c
Putu Septian Primadewa <iansaviour@gmail.com>
2019-03-15 12:00:59 +0800

    Add sample purc close form + menu

---
+++ VolcomInventory/FormSamplePurcClose.vb
@@ -0,0 +1,9 @@
+﻿Public Class FormSamplePurcClose
+    Private Sub FormSamplePurcClose_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Sub load_close()
+
+    End Sub
+End Class
---
+++ VolcomInventory/FormSamplePurcCloseDet.vb
@@ -0,0 +1,7 @@
+﻿Public Class FormSamplePurcCloseDet
+    Public id_close As String = "-1"
+
+    Private Sub FormSamplePurcCloseDet_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
