c919325b7c208b90f547590561e7bc28b29f6d8b
Satrya Anas <catur.satrya@gmail.com>
2019-04-01 09:57:16 +0800

    prepare compare

--- VolcomInventory/FormFGProposePrice.Designer.vb
+++ VolcomInventory/FormFGProposePrice.Designer.vb
@@ -40,6 +40,21 @@ Partial Class FormFGProposePrice
         Me.DEFromList = New DevExpress.XtraEditors.DateEdit()
         Me.LabelControl3 = New DevExpress.XtraEditors.LabelControl()
         Me.LabelControl5 = New DevExpress.XtraEditors.LabelControl()
+        Me.XTCPropose = New DevExpress.XtraTab.XtraTabControl()
+        Me.XTPPropose = New DevExpress.XtraTab.XtraTabPage()
+        Me.XtraTabPage2 = New DevExpress.XtraTab.XtraTabPage()
+        Me.PanelControl1 = New DevExpress.XtraEditors.PanelControl()
+        Me.SLESeason = New DevExpress.XtraEditors.SearchLookUpEdit()
+        Me.SearchLookUpEdit1View = New DevExpress.XtraGrid.Views.Grid.GridView()
+        Me.GridColumn6 = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GridColumn8 = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GridColumn3 = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.LabelControl1 = New DevExpress.XtraEditors.LabelControl()
+        Me.LESource = New DevExpress.XtraEditors.LookUpEdit()
+        Me.LabelControl2 = New DevExpress.XtraEditors.LabelControl()
+        Me.LEDivision = New DevExpress.XtraEditors.LookUpEdit()
+        Me.LabelControl4 = New DevExpress.XtraEditors.LabelControl()
+        Me.SimpleButton1 = New DevExpress.XtraEditors.SimpleButton()
         CType(Me.GCFGPropose, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.GVFGPropose, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.GCFilter, System.ComponentModel.ISupportInitialize).BeginInit()
@@ -48,15 +63,25 @@ Partial Class FormFGProposePrice
         CType(Me.DEUntilList.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.DEFromList.Properties.CalendarTimeProperties, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.DEFromList.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.XTCPropose, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.XTCPropose.SuspendLayout()
+        Me.XTPPropose.SuspendLayout()
+        Me.XtraTabPage2.SuspendLayout()
+        CType(Me.PanelControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.PanelControl1.SuspendLayout()
+        CType(Me.SLESeason.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.SearchLookUpEdit1View, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.LESource.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.LEDivision.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'GCFGPropose
         '
         Me.GCFGPropose.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.GCFGPropose.Location = New System.Drawing.Point(0, 47)
+        Me.GCFGPropose.Location = New System.Drawing.Point(0, 0)
         Me.GCFGPropose.MainView = Me.GVFGPropose
         Me.GCFGPropose.Name = "GCFGPropose"
-        Me.GCFGPropose.Size = New System.Drawing.Size(769, 433)
+        Me.GCFGPropose.Size = New System.Drawing.Size(939, 494)
         Me.GCFGPropose.TabIndex = 0
         Me.GCFGPropose.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() {Me.GVFGPropose})
         '
@@ -161,7 +186,7 @@ Partial Class FormFGProposePrice
         Me.GCFilter.Dock = System.Windows.Forms.DockStyle.Top
         Me.GCFilter.Location = New System.Drawing.Point(0, 0)
         Me.GCFilter.Name = "GCFilter"
-        Me.GCFilter.Size = New System.Drawing.Size(769, 47)
+        Me.GCFilter.Size = New System.Drawing.Size(939, 45)
         Me.GCFilter.TabIndex = 7
         '
         'BtnListCOP
@@ -248,13 +273,152 @@ Partial Class FormFGProposePrice
         Me.LabelControl5.TabIndex = 8892
         Me.LabelControl5.Text = "From"
         '
+        'XTCPropose
+        '
+        Me.XTCPropose.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.XTCPropose.HeaderLocation = DevExpress.XtraTab.TabHeaderLocation.Bottom
+        Me.XTCPropose.Location = New System.Drawing.Point(0, 0)
+        Me.XTCPropose.Name = "XTCPropose"
+        Me.XTCPropose.SelectedTabPage = Me.XTPPropose
+        Me.XTCPropose.Size = New System.Drawing.Size(945, 522)
+        Me.XTCPropose.TabIndex = 8
+        Me.XTCPropose.TabPages.AddRange(New DevExpress.XtraTab.XtraTabPage() {Me.XTPPropose, Me.XtraTabPage2})
+        '
+        'XTPPropose
+        '
+        Me.XTPPropose.Controls.Add(Me.GCFilter)
+        Me.XTPPropose.Controls.Add(Me.GCFGPropose)
+        Me.XTPPropose.Name = "XTPPropose"
+        Me.XTPPropose.Size = New System.Drawing.Size(939, 494)
+        Me.XTPPropose.Text = "Propose"
+        '
+        'XtraTabPage2
+        '
+        Me.XtraTabPage2.Controls.Add(Me.PanelControl1)
+        Me.XtraTabPage2.Name = "XtraTabPage2"
+        Me.XtraTabPage2.Size = New System.Drawing.Size(939, 494)
+        Me.XtraTabPage2.Text = "Compare Mark Up (Final Cost)"
+        '
+        'PanelControl1
+        '
+        Me.PanelControl1.Controls.Add(Me.SimpleButton1)
+        Me.PanelControl1.Controls.Add(Me.LEDivision)
+        Me.PanelControl1.Controls.Add(Me.LESource)
+        Me.PanelControl1.Controls.Add(Me.LabelControl4)
+        Me.PanelControl1.Controls.Add(Me.SLESeason)
+        Me.PanelControl1.Controls.Add(Me.LabelControl2)
+        Me.PanelControl1.Controls.Add(Me.LabelControl1)
+        Me.PanelControl1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.PanelControl1.Location = New System.Drawing.Point(0, 0)
+        Me.PanelControl1.Name = "PanelControl1"
+        Me.PanelControl1.Size = New System.Drawing.Size(939, 46)
+        Me.PanelControl1.TabIndex = 0
+        '
+        'SLESeason
+        '
+        Me.SLESeason.Location = New System.Drawing.Point(54, 12)
+        Me.SLESeason.Name = "SLESeason"
+        Me.SLESeason.Properties.Appearance.Font = New System.Drawing.Font("Tahoma", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.SLESeason.Properties.Appearance.Options.UseFont = True
+        Me.SLESeason.Properties.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() {New DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)})
+        Me.SLESeason.Properties.ShowClearButton = False
+        Me.SLESeason.Properties.View = Me.SearchLookUpEdit1View
+        Me.SLESeason.Size = New System.Drawing.Size(225, 20)
+        Me.SLESeason.TabIndex = 8910
+        '
+        'SearchLookUpEdit1View
+        '
+        Me.SearchLookUpEdit1View.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() {Me.GridColumn6, Me.GridColumn8, Me.GridColumn3})
+        Me.SearchLookUpEdit1View.FocusRectStyle = DevExpress.XtraGrid.Views.Grid.DrawFocusRectStyle.RowFocus
+        Me.SearchLookUpEdit1View.Name = "SearchLookUpEdit1View"
+        Me.SearchLookUpEdit1View.OptionsSelection.EnableAppearanceFocusedCell = False
+        Me.SearchLookUpEdit1View.OptionsView.ShowGroupPanel = False
+        '
+        'GridColumn6
+        '
+        Me.GridColumn6.Caption = "Id Season"
+        Me.GridColumn6.FieldName = "id_season"
+        Me.GridColumn6.Name = "GridColumn6"
+        '
+        'GridColumn8
+        '
+        Me.GridColumn8.Caption = "Season"
+        Me.GridColumn8.FieldName = "season"
+        Me.GridColumn8.Name = "GridColumn8"
+        Me.GridColumn8.Visible = True
+        Me.GridColumn8.VisibleIndex = 1
+        '
+        'GridColumn3
+        '
+        Me.GridColumn3.Caption = "Range"
+        Me.GridColumn3.FieldName = "range"
+        Me.GridColumn3.Name = "GridColumn3"
+        Me.GridColumn3.Visible = True
+        Me.GridColumn3.VisibleIndex = 0
+        '
+        'LabelControl1
+        '
+        Me.LabelControl1.Location = New System.Drawing.Point(13, 15)
+        Me.LabelControl1.Name = "LabelControl1"
+        Me.LabelControl1.Size = New System.Drawing.Size(35, 13)
+        Me.LabelControl1.TabIndex = 8909
+        Me.LabelControl1.Text = "Season"
+        '
+        'LESource
+        '
+        Me.LESource.Location = New System.Drawing.Point(324, 12)
+        Me.LESource.Name = "LESource"
+        Me.LESource.Properties.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() {New DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)})
+        Me.LESource.Properties.Columns.AddRange(New DevExpress.XtraEditors.Controls.LookUpColumnInfo() {New DevExpress.XtraEditors.Controls.LookUpColumnInfo("id_source", "Id Code Detail", 20, DevExpress.Utils.FormatType.None, "", False, DevExpress.Utils.HorzAlignment.[Default]), New DevExpress.XtraEditors.Controls.LookUpColumnInfo("display_name", "Display Name"), New DevExpress.XtraEditors.Controls.LookUpColumnInfo("source", "Source")})
+        Me.LESource.Properties.NullText = "- Select Product Source -"
+        Me.LESource.Size = New System.Drawing.Size(153, 20)
+        Me.LESource.TabIndex = 8912
+        '
+        'LabelControl2
+        '
+        Me.LabelControl2.Location = New System.Drawing.Point(285, 15)
+        Me.LabelControl2.Name = "LabelControl2"
+        Me.LabelControl2.Size = New System.Drawing.Size(33, 13)
+        Me.LabelControl2.TabIndex = 8911
+        Me.LabelControl2.Text = "Source"
+        '
+        'LEDivision
+        '
+        Me.LEDivision.Location = New System.Drawing.Point(525, 12)
+        Me.LEDivision.Name = "LEDivision"
+        Me.LEDivision.Properties.Appearance.Options.UseTextOptions = True
+        Me.LEDivision.Properties.Appearance.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Near
+        Me.LEDivision.Properties.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() {New DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)})
+        Me.LEDivision.Properties.Columns.AddRange(New DevExpress.XtraEditors.Controls.LookUpColumnInfo() {New DevExpress.XtraEditors.Controls.LookUpColumnInfo("id_code_detail", "ID Division", 20, DevExpress.Utils.FormatType.None, "", False, DevExpress.Utils.HorzAlignment.[Default]), New DevExpress.XtraEditors.Controls.LookUpColumnInfo("display_name", "Display Name"), New DevExpress.XtraEditors.Controls.LookUpColumnInfo("code_detail_name", "Division")})
+        Me.LEDivision.Properties.NullText = ""
+        Me.LEDivision.Properties.ShowFooter = False
+        Me.LEDivision.Size = New System.Drawing.Size(103, 20)
+        Me.LEDivision.TabIndex = 8911
+        '
+        'LabelControl4
+        '
+        Me.LabelControl4.Location = New System.Drawing.Point(483, 15)
+        Me.LabelControl4.Name = "LabelControl4"
+        Me.LabelControl4.Size = New System.Drawing.Size(36, 13)
+        Me.LabelControl4.TabIndex = 8910
+        Me.LabelControl4.Text = "Division"
+        '
+        'SimpleButton1
+        '
+        Me.SimpleButton1.Image = CType(resources.GetObject("SimpleButton1.Image"), System.Drawing.Image)
+        Me.SimpleButton1.Location = New System.Drawing.Point(634, 11)
+        Me.SimpleButton1.LookAndFeel.SkinName = "Blue"
+        Me.SimpleButton1.Name = "SimpleButton1"
+        Me.SimpleButton1.Size = New System.Drawing.Size(75, 20)
+        Me.SimpleButton1.TabIndex = 8913
+        Me.SimpleButton1.Text = "View"
+        '
         'FormFGProposePrice
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(769, 480)
-        Me.Controls.Add(Me.GCFGPropose)
-        Me.Controls.Add(Me.GCFilter)
+        Me.ClientSize = New System.Drawing.Size(945, 522)
+        Me.Controls.Add(Me.XTCPropose)
         Me.Name = "FormFGProposePrice"
         Me.ShowInTaskbar = False
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
@@ -268,6 +432,17 @@ Partial Class FormFGProposePrice
         CType(Me.DEUntilList.Properties, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.DEFromList.Properties.CalendarTimeProperties, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.DEFromList.Properties, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.XTCPropose, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.XTCPropose.ResumeLayout(False)
+        Me.XTPPropose.ResumeLayout(False)
+        Me.XtraTabPage2.ResumeLayout(False)
+        CType(Me.PanelControl1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.PanelControl1.ResumeLayout(False)
+        Me.PanelControl1.PerformLayout()
+        CType(Me.SLESeason.Properties, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.SearchLookUpEdit1View, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.LESource.Properties, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.LEDivision.Properties, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
@@ -291,4 +466,19 @@ Partial Class FormFGProposePrice
     Friend WithEvents DEFromList As DevExpress.XtraEditors.DateEdit
     Friend WithEvents LabelControl3 As DevExpress.XtraEditors.LabelControl
     Friend WithEvents LabelControl5 As DevExpress.XtraEditors.LabelControl
+    Friend WithEvents XTCPropose As DevExpress.XtraTab.XtraTabControl
+    Friend WithEvents XTPPropose As DevExpress.XtraTab.XtraTabPage
+    Friend WithEvents XtraTabPage2 As DevExpress.XtraTab.XtraTabPage
+    Friend WithEvents PanelControl1 As DevExpress.XtraEditors.PanelControl
+    Friend WithEvents SLESeason As DevExpress.XtraEditors.SearchLookUpEdit
+    Friend WithEvents SearchLookUpEdit1View As DevExpress.XtraGrid.Views.Grid.GridView
+    Friend WithEvents GridColumn6 As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GridColumn8 As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GridColumn3 As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents LabelControl1 As DevExpress.XtraEditors.LabelControl
+    Friend WithEvents LESource As DevExpress.XtraEditors.LookUpEdit
+    Friend WithEvents LabelControl2 As DevExpress.XtraEditors.LabelControl
+    Friend WithEvents LEDivision As DevExpress.XtraEditors.LookUpEdit
+    Friend WithEvents LabelControl4 As DevExpress.XtraEditors.LabelControl
+    Friend WithEvents SimpleButton1 As DevExpress.XtraEditors.SimpleButton
 End Class
--- VolcomInventory/FormFGProposePrice.vb
+++ VolcomInventory/FormFGProposePrice.vb
@@ -2,6 +2,7 @@
     Dim bnew_active As String = "1"
     Dim bedit_active As String = "1"
     Dim bdel_active As String = "1"
+    Dim is_first_load_compare As Boolean = True
 
     Private Sub FormFGProposePrice_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
         Dim dt_now As DataTable = execute_query("SELECT DATE(NOW()) as tgl", -1, True, "", "", "", "")
@@ -94,5 +95,35 @@
 
     Private Sub BtnViewList_Click(sender As Object, e As EventArgs) Handles BtnViewList.Click
         viewPropose()
+    End Sub
+
+    Sub viewSeason()
+        Dim query As String = "(SELECT a.id_season, a.season, b.range AS `range` FROM tb_season a 
+                                INNER JOIN tb_range b ON a.id_range = b.id_range 
+                                ORDER BY b.range ASC)"
+        viewSearchLookupQuery(SLESeason, query, "id_season", "season", "id_season")
+    End Sub
+
+    Sub view_division_fg()
+        Dim id_code As String = get_setup_field("id_code_fg_division")
+        Dim query As String = "SELECT id_code_detail,code_detail_name,display_name 
+        FROM tb_m_code_detail a WHERE a.id_code='" + id_code + "' ORDER BY id_code_detail ASC "
+        Dim data As DataTable = execute_query(query, -1, True, "", "", "", "")
+        viewLookupQuery(LEDivision, query, 0, "display_name", "id_code_detail")
+    End Sub
+
+    Sub view_source_fg()
+        Dim id_code As String = get_setup_field("id_code_fg_source")
+        Dim query As String = "SELECT (id_code_detail) AS id_source, (code_detail_name) AS source, display_name FROM tb_m_code_detail a WHERE a.id_code='" + id_code + "' HAVING source<>'-' ORDER BY a.id_code_detail "
+        Dim data As DataTable = execute_query(query, -1, True, "", "", "", "")
+        viewLookupQuery(LESource, query, 0, "display_name", "id_source")
+    End Sub
+
+    Private Sub XTCPropose_SelectedPageChanged(sender As Object, e As DevExpress.XtraTab.TabPageChangedEventArgs) Handles XTCPropose.SelectedPageChanged
+        If XTCPropose.SelectedTabPageIndex = 0 Then
+
+        ElseIf XTCPropose.SelectedTabPageIndex = 1 Then
+
+        End If
     End Sub
 End Class
