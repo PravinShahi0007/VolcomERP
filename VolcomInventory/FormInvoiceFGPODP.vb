4fca0c703d4b3885cf3f3d2dd2f50719e210df08
Putu Septian Primadewa <iansaviour@gmail.com>
2019-04-08 11:11:40 +0800

    add invoice dp

--- VolcomInventory/FormInvoiceFGPODP.vb
+++ VolcomInventory/FormInvoiceFGPODP.vb
@@ -1,3 +1,9 @@
 ﻿Public Class FormInvoiceFGPODP
+    Private Sub BtnCancel_Click(sender As Object, e As EventArgs) Handles BtnCancel.Click
+        Close()
+    End Sub
 
+    Private Sub FormInvoiceFGPODP_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
 End Class
