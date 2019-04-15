186a69da7f3d1c6d13e6e6cf93a6592836a26b75
Putu Septian Primadewa <iansaviour@gmail.com>
2019-04-15 17:30:57 +0800

    add form detail wo

--- VolcomInventory/FormWorkOrder.vb
+++ VolcomInventory/FormWorkOrder.vb
@@ -1,9 +1,47 @@
 ﻿Public Class FormWorkOrder
-    Private Sub FormWorkOrder_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    Dim bnew_active As String = "1"
+    Dim bedit_active As String = "1"
+    Dim bdel_active As String = "1"
 
+    Private Sub FormWorkOrder_FormClosed(sender As Object, e As FormClosedEventArgs) Handles MyBase.FormClosed
+        Dispose()
+    End Sub
+
+    Private Sub FormWorkOrder_Deactivate(sender As Object, e As EventArgs) Handles MyBase.Deactivate
+        FormMain.hide_rb()
+    End Sub
+
+    Private Sub FormWorkOrder_Activated(sender As Object, e As EventArgs) Handles MyBase.Activated
+        FormMain.show_rb(Name)
+        check_menu()
+    End Sub
+
+    Sub check_menu()
+        If GVWorkOrder.RowCount < 1 Then
+            'hide all except new
+            bnew_active = "1"
+            bedit_active = "0"
+            bdel_active = "0"
+            checkFormAccess(Name)
+            button_main(bnew_active, bedit_active, bdel_active)
+            'noManipulating()
+        Else
+            'show all
+            bnew_active = "1"
+            bedit_active = "1"
+            bdel_active = "1"
+            checkFormAccess(Name)
+            button_main(bnew_active, bedit_active, bdel_active)
+            'noManipulating()
+        End If
+    End Sub
+
+    Private Sub FormWorkOrder_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        load_wo()
     End Sub
 
     Sub load_wo()
 
     End Sub
+
 End Class
--- VolcomInventory/FormWorkOrderDet.vb
+++ VolcomInventory/FormWorkOrderDet.vb
@@ -1,6 +1,23 @@
 ﻿Public Class FormWorkOrderDet
     Public id_wo As String = "-1"
     Private Sub FormWorkOrderDet_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        load_work_order_type()
+        TEReqNUmber.Text = "[auto_generate]"
+        TEReqBy.Text = get_emp(id_user, "4")
+        TEDep.Text = get_emp(id_user, "5")
+        DEDateCreated.EditValue = Now
+    End Sub
 
+    Sub load_work_order_type()
+        Dim query As String = "SELECT wot.`id_work_order_type`,wot.`work_order_type`,dep.`departement` FROM tb_lookup_work_order_type wot
+INNER JOIN tb_m_departement_sub sub ON sub.`id_departement_sub`=wot.`id_sub_departement`
+INNER JOIN tb_m_departement dep ON dep.`id_departement`=sub.`id_departement`"
+        viewSearchLookupQuery(SLEType, query, "id_work_order_type", "work_order_type", "id_work_order_type")
+    End Sub
+
+    Private Sub BtnSave_Click(sender As Object, e As EventArgs) Handles BtnSave.Click
+        If id_wo = "-1" Then
+            Dim query As String = ""
+        End If
     End Sub
 End Class
