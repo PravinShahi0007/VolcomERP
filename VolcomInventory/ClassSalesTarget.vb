83a43f4ed8290359df946f8fc7b5fbc4047304e0
Satrya Anas <catur.satrya@gmail.com>
2019-04-22 11:39:41 +0800

    prepare detail

--- VolcomInventory/ClassSalesTarget.vb
+++ VolcomInventory/ClassSalesTarget.vb
@@ -13,11 +13,12 @@
         End If
 
         Dim query As String = "SELECT t.id_sales_trg_propose, t.year, t.number, 
-        t.created_date, t.updated_date, 
+        t.created_date, t.updated_date, t.updated_by, emp.employee_name AS `updated_by_name`,
         t.note, t.id_report_status, rs.report_status, t.is_confirm
         FROM tb_sales_trg_propose t
-        INNER JOIN tb_sales_trg_propose_det td ON td.id_sales_trg_propose = t.id_sales_trg_propose
         INNER JOIN tb_lookup_report_status rs ON rs.id_report_status = t.id_report_status
+        INNER JOIN tb_m_user u ON u.id_user = t.updated_by
+        INNER JOIN tb_m_employee emp ON emp.id_employee = u.id_employee
         WHERE t.id_sales_trg_propose>0 "
         query += condition + " "
         query += "ORDER BY t.id_sales_trg_propose " + order_type
--- VolcomInventory/FormSalesTargetProposeNew.Designer.vb
+++ VolcomInventory/FormSalesTargetProposeNew.Designer.vb
@@ -22,12 +22,12 @@ Partial Class FormSalesTargetProposeNew
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FormSalesTargetProposeNew))
         Me.LabelControl1 = New DevExpress.XtraEditors.LabelControl()
         Me.LabelControl2 = New DevExpress.XtraEditors.LabelControl()
-        Me.MemoEdit1 = New DevExpress.XtraEditors.MemoEdit()
+        Me.MENote = New DevExpress.XtraEditors.MemoEdit()
         Me.PanelControl1 = New DevExpress.XtraEditors.PanelControl()
         Me.BtnCancel = New DevExpress.XtraEditors.SimpleButton()
         Me.BtnCreateNew = New DevExpress.XtraEditors.SimpleButton()
         Me.TxtYear = New DevExpress.XtraEditors.TextEdit()
-        CType(Me.MemoEdit1.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.MENote.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PanelControl1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.PanelControl1.SuspendLayout()
         CType(Me.TxtYear.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
@@ -49,22 +49,22 @@ Partial Class FormSalesTargetProposeNew
         Me.LabelControl2.TabIndex = 2
         Me.LabelControl2.Text = "Note"
         '
-        'MemoEdit1
+        'MENote
         '
-        Me.MemoEdit1.Location = New System.Drawing.Point(64, 44)
-        Me.MemoEdit1.Name = "MemoEdit1"
-        Me.MemoEdit1.Size = New System.Drawing.Size(289, 69)
-        Me.MemoEdit1.TabIndex = 3
+        Me.MENote.Location = New System.Drawing.Point(64, 44)
+        Me.MENote.Name = "MENote"
+        Me.MENote.Size = New System.Drawing.Size(289, 53)
+        Me.MENote.TabIndex = 4
         '
         'PanelControl1
         '
         Me.PanelControl1.Controls.Add(Me.BtnCancel)
         Me.PanelControl1.Controls.Add(Me.BtnCreateNew)
         Me.PanelControl1.Dock = System.Windows.Forms.DockStyle.Bottom
-        Me.PanelControl1.Location = New System.Drawing.Point(0, 141)
+        Me.PanelControl1.Location = New System.Drawing.Point(0, 112)
         Me.PanelControl1.Name = "PanelControl1"
         Me.PanelControl1.Size = New System.Drawing.Size(379, 43)
-        Me.PanelControl1.TabIndex = 4
+        Me.PanelControl1.TabIndex = 100
         '
         'BtnCancel
         '
@@ -73,7 +73,7 @@ Partial Class FormSalesTargetProposeNew
         Me.BtnCancel.Location = New System.Drawing.Point(185, 2)
         Me.BtnCancel.Name = "BtnCancel"
         Me.BtnCancel.Size = New System.Drawing.Size(87, 39)
-        Me.BtnCancel.TabIndex = 1
+        Me.BtnCancel.TabIndex = 6
         Me.BtnCancel.Text = "Cancel"
         '
         'BtnCreateNew
@@ -83,7 +83,7 @@ Partial Class FormSalesTargetProposeNew
         Me.BtnCreateNew.Location = New System.Drawing.Point(272, 2)
         Me.BtnCreateNew.Name = "BtnCreateNew"
         Me.BtnCreateNew.Size = New System.Drawing.Size(105, 39)
-        Me.BtnCreateNew.TabIndex = 0
+        Me.BtnCreateNew.TabIndex = 5
         Me.BtnCreateNew.Text = "Create New"
         '
         'TxtYear
@@ -94,23 +94,23 @@ Partial Class FormSalesTargetProposeNew
         Me.TxtYear.Properties.Mask.MaskType = DevExpress.XtraEditors.Mask.MaskType.Numeric
         Me.TxtYear.Properties.MaxLength = 4
         Me.TxtYear.Size = New System.Drawing.Size(173, 20)
-        Me.TxtYear.TabIndex = 5
+        Me.TxtYear.TabIndex = 3
         '
         'FormSalesTargetProposeNew
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(379, 184)
+        Me.ClientSize = New System.Drawing.Size(379, 155)
         Me.Controls.Add(Me.TxtYear)
         Me.Controls.Add(Me.PanelControl1)
-        Me.Controls.Add(Me.MemoEdit1)
+        Me.Controls.Add(Me.MENote)
         Me.Controls.Add(Me.LabelControl2)
         Me.Controls.Add(Me.LabelControl1)
         Me.MinimizeBox = False
         Me.Name = "FormSalesTargetProposeNew"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Propose Sales Target - New"
-        CType(Me.MemoEdit1.Properties, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.MENote.Properties, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.PanelControl1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.PanelControl1.ResumeLayout(False)
         CType(Me.TxtYear.Properties, System.ComponentModel.ISupportInitialize).EndInit()
@@ -121,7 +121,7 @@ Partial Class FormSalesTargetProposeNew
 
     Friend WithEvents LabelControl1 As DevExpress.XtraEditors.LabelControl
     Friend WithEvents LabelControl2 As DevExpress.XtraEditors.LabelControl
-    Friend WithEvents MemoEdit1 As DevExpress.XtraEditors.MemoEdit
+    Friend WithEvents MENote As DevExpress.XtraEditors.MemoEdit
     Friend WithEvents PanelControl1 As DevExpress.XtraEditors.PanelControl
     Friend WithEvents BtnCancel As DevExpress.XtraEditors.SimpleButton
     Friend WithEvents BtnCreateNew As DevExpress.XtraEditors.SimpleButton
