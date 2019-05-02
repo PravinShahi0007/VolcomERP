5d99974c067ccedd962e6c91eb7039f3e8d7487a
Satrya Anas <catur.satrya@gmail.com>
2019-05-02 10:49:42 +0800

    is_use_size_profile

--- VolcomInventory/ClassEmpUni.vb
+++ VolcomInventory/ClassEmpUni.vb
@@ -38,7 +38,7 @@
         End If
 
         Dim query As String = "SELECT so.id_sales_order, so.sales_order_number, so.sales_order_date, so.sales_order_note, SUM(IFNULL(sod.sales_order_det_qty,0)) AS `total_order`, SUM(IFNULL(udd.point,0)) AS `amount`,
-        so.id_emp_uni_budget, b.id_emp_uni_period, p.period_name, (b.budget/p.budget_point)*100 AS `budget`, so.tolerance, so.discount,
+        so.id_emp_uni_budget, b.id_emp_uni_period, p.period_name, p.is_use_size_profile, (b.budget/p.budget_point)*100 AS `budget`, so.tolerance, so.discount,
         b.id_employee, e.employee_code, e.employee_name, e.employee_position, e.id_employee_level, lvl.employee_level, e.id_departement, d.departement, e.id_sex,
         so.id_report_status, rs.report_status, so.sales_order_note, so.sales_order_date, eu.employee_name AS `prepared_by`, c.id_drawer_def, IFNULL(so.is_selected,2) AS is_selected, b.is_dept_head
         FROM tb_sales_order so
