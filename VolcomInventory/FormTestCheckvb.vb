73267a65de02f6d5b3837d023c707bc4ed3dd9d6
Satrya Anas <catur.satrya@gmail.com>
2019-05-02 17:23:10 +0800

    fix PL view

---
+++ VolcomInventory/FormTestCheckvb.vb
@@ -0,0 +1,9 @@
+﻿Public Class FormTestCheckvb
+    Private Sub FormTestCheckvb_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim query As String = ""
+    End Sub
+
+    Private Sub FormTestCheckvb_FormClosed(sender As Object, e As FormClosedEventArgs) Handles MyBase.FormClosed
+        Dispose()
+    End Sub
+End Class
