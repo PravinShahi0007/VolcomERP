d9a7dab07ebc6787bf3f64aa7a380470c1e5f0ce
Satrya Anas <catur.satrya@gmail.com>
2019-05-21 15:45:54 +0800

    cek perubahan harus cancell ato gk

--- VolcomInventory/FormProdDemandRevSingle.vb
+++ VolcomInventory/FormProdDemandRevSingle.vb
@@ -13,15 +13,20 @@
         Dim query As String = "SELECT  dp.id_prod_demand_design_rev, pdd.id_prod_demand_design, pdd.id_design,d.design_code AS `code`, d.design_display_name AS `name`, po.prod_order_number,
         IFNULL(po.ordered,0) AS `ordered`,
         IFNULL(rec.received,0) AS `received`,
-        pd_det.current_qty, pd_det.current_cost, pd_det.current_add_cost,'No' AS `is_select`
+        pd_det.current_qty, pd_det.current_cost, pd_det.current_add_cost, pd_det.current_size_chart,
+        'No' AS `is_select`
         FROM tb_prod_demand_design pdd
         INNER JOIN tb_m_design d ON d.id_design = pdd.id_design
         LEFT JOIN (
 	        SELECT pdd.id_prod_demand_design, pdd.id_design, SUM(pdp.prod_demand_product_qty) AS `current_qty`, pdd.prod_demand_design_total_cost AS `current_cost`, 
-	        pdd.additional_cost AS `current_add_cost`
+	        pdd.additional_cost AS `current_add_cost`,
+            GROUP_CONCAT(DISTINCT cd.`code_detail_name` ORDER BY cd.id_code_detail ASC SEPARATOR ',') AS `current_size_chart`
 	        FROM tb_prod_demand_product pdp
 	        INNER JOIN tb_prod_demand_design pdd ON pdd.id_prod_demand_design = pdp.id_prod_demand_design
-	        WHERE pdd.id_prod_demand=" + FormProdDemandRevDet.id_prod_demand + " 
+            INNER JOIN tb_m_product p ON p.id_product = pdp.id_product
+            INNER JOIN tb_m_product_code pc ON pc.id_product = p.id_product
+            INNER JOIN tb_m_code_detail cd ON cd.id_code_detail = pc.id_code_detail
+	        WHERE pdd.id_prod_demand=" + FormProdDemandRevDet.id_prod_demand + " AND pdp.prod_demand_product_qty>0
 	        GROUP BY pdd.id_prod_demand_design
         ) pd_det ON pd_det.id_prod_demand_design = pdd.id_prod_demand_design
         LEFT JOIN (
@@ -57,29 +62,63 @@
     End Sub
 
     Private Sub BtnRevise_Click(sender As Object, e As EventArgs) Handles BtnRevise.Click
-        ok(False)
-    End Sub
-
-    Private Sub BtnDrop_Click(sender As Object, e As EventArgs) Handles BtnDrop.Click
-        ok(True)
-    End Sub
-
-    Sub ok(ByVal is_drop As Boolean)
         Cursor = Cursors.WaitCursor
         If GVDesign.RowCount > 0 And GVDesign.FocusedRowHandle >= 0 Then
-            If GVDesign.GetFocusedRowCellValue("received") > 0 Then
-                stopCustom("Can't revise because " + GVDesign.GetFocusedRowCellValue("name").ToString + " already received in QC")
-            Else
-                If Not is_drop Then
-                    'insert detail
-                    Dim query_det_new As String = "CALL generate_pd_rev_line_list('" + FormProdDemandRevDet.id + "','" + GVDesign.GetFocusedRowCellValue("id_prod_demand_design").ToString + "', '" + SLETypeLineList.EditValue.ToString + "', '" + GVDesign.GetFocusedRowCellValue("id_design").ToString + "')"
-                    execute_non_query(query_det_new, True, "", "", "", "")
-                Else
-                    'drop - data pake existing pd
-                    'insert detail
-                    Dim query_det_new As String = "CALL generate_pd_drop_line_list('" + FormProdDemandRevDet.id + "','" + GVDesign.GetFocusedRowCellValue("id_prod_demand_design").ToString + "')"
-                    execute_non_query(query_det_new, True, "", "", "", "")
+            'cek
+            Dim is_cancel_po As String = "2"
+            Dim id_design As String = GVDesign.GetFocusedRowCellValue("id_design").ToString
+            Dim qcek As String = "SELECT d.id_design, d.design_code AS `code`, d.design_display_name AS `name`,  
+            GROUP_CONCAT(DISTINCT cd.`code_detail_name` ORDER BY cd.id_code_detail ASC SEPARATOR ',') AS `size_chart`,
+            pdd.prod_demand_design_total_cost AS `cost`,  pdd.additional_cost AS `add_cost`,
+            SUM(pdp.prod_demand_product_qty) AS `total_qty`
+            FROM tb_m_design d 
+            INNER JOIN tb_prod_demand_design pdd ON pdd.id_prod_demand_design = d.id_prod_demand_design_line
+            INNER JOIN tb_prod_demand_product pdp ON pdp.id_prod_demand_design = pdd.id_prod_demand_design
+            INNER JOIN tb_m_product p ON p.id_product = pdp.id_product
+            INNER JOIN tb_m_product_code pc ON pc.id_product = p.id_product
+            INNER JOIN tb_m_code_detail cd ON cd.id_code_detail = pc.id_code_detail
+            WHERE d.id_design=" + id_design + " AND pdp.prod_demand_product_qty>0
+            GROUP BY pdd.id_design "
+            Dim dcek As DataTable = execute_query(qcek, -1, True, "", "", "", "")
+            If dcek.Rows.Count > 0 Then
+                'total qty
+                If dcek.Rows(0)("total_qty") <> GVDesign.GetFocusedRowCellValue("current_qty") Then
+                    is_cancel_po = "1"
                 End If
+
+                'size chart
+                If dcek.Rows(0)("size_chart") <> GVDesign.GetFocusedRowCellValue("current_size_chart") Then
+                    is_cancel_po = "1"
+                End If
+
+                'cop
+                If dcek.Rows(0)("cost") <> GVDesign.GetFocusedRowCellValue("current_cost") Then
+                    is_cancel_po = "1"
+                End If
+
+                'add cop
+                If dcek.Rows(0)("add_cost") <> GVDesign.GetFocusedRowCellValue("current_add_cost") Then
+                    is_cancel_po = "1"
+                End If
+            Else
+                stopCustom("This product not found on line list")
+                Exit Sub
+            End If
+
+            If is_cancel_po = "1" Then
+                If GVDesign.GetFocusedRowCellValue("received") > 0 Then
+                    stopCustom("Can't revise because " + GVDesign.GetFocusedRowCellValue("name").ToString + " already received in QC")
+                Else
+                    'insert detail
+                    Dim query_det_new As String = "CALL generate_pd_rev_line_list('" + FormProdDemandRevDet.id + "','" + GVDesign.GetFocusedRowCellValue("id_prod_demand_design").ToString + "', '" + SLETypeLineList.EditValue.ToString + "', '" + GVDesign.GetFocusedRowCellValue("id_design").ToString + "', is_cancel_po)"
+                    execute_non_query(query_det_new, True, "", "", "", "")
+                    FormProdDemandRevDet.viewDetail()
+                    actionLoad()
+                End If
+            ElseIf is_cancel_po = "2" Then
+                'insert detail
+                Dim query_det_new As String = "CALL generate_pd_rev_line_list('" + FormProdDemandRevDet.id + "','" + GVDesign.GetFocusedRowCellValue("id_prod_demand_design").ToString + "', '" + SLETypeLineList.EditValue.ToString + "', '" + GVDesign.GetFocusedRowCellValue("id_design").ToString + "', is_cancel_po)"
+                execute_non_query(query_det_new, True, "", "", "", "")
                 FormProdDemandRevDet.viewDetail()
                 actionLoad()
             End If
@@ -89,7 +128,22 @@
         Cursor = Cursors.Default
     End Sub
 
-
-
-
+    Private Sub BtnDrop_Click(sender As Object, e As EventArgs) Handles BtnDrop.Click
+        Cursor = Cursors.WaitCursor
+        If GVDesign.RowCount > 0 And GVDesign.FocusedRowHandle >= 0 Then
+            If GVDesign.GetFocusedRowCellValue("received") > 0 Then
+                stopCustom("Can't revise because " + GVDesign.GetFocusedRowCellValue("name").ToString + " already received in QC")
+            Else
+                'drop - data pake existing pd
+                'insert detail
+                Dim query_det_new As String = "CALL generate_pd_drop_line_list('" + FormProdDemandRevDet.id + "','" + GVDesign.GetFocusedRowCellValue("id_prod_demand_design").ToString + "')"
+                execute_non_query(query_det_new, True, "", "", "", "")
+                FormProdDemandRevDet.viewDetail()
+                actionLoad()
+            End If
+        Else
+            stopCustom("No item selected")
+        End If
+        Cursor = Cursors.Default
+    End Sub
 End Class
