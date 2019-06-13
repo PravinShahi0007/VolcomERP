1a9783beda9380339e94a9329e09e6c542b8bbdc
Putu Septian Primadewa <iansaviour@gmail.com>
2019-06-13 11:08:36 +0800

    print

--- VolcomInventory/FormMatPurchasePD.vb
+++ VolcomInventory/FormMatPurchasePD.vb
@@ -29,10 +29,8 @@ WHERE p.id_mat_purc_list='" & id_list & "'"
                 TEToleransi.Enabled = False
                 '
                 BSave.Visible = False
-                BPrint.Visible = False
             End If
         Else
-            BPrint.Visible = True
             BSave.Visible = True
         End If
     End Sub
