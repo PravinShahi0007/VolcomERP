5d557ad7dc57376a266e394a618539cf29cded31
Putu Septian Primadewa <iansaviour@gmail.com>
2019-07-11 14:23:35 +0800

    COP PD limit

--- VolcomInventory/FormMasterDesignCOPPD.vb
+++ VolcomInventory/FormMasterDesignCOPPD.vb
@@ -56,6 +56,7 @@ WHERE dsg.id_design='" & id_design & "'"
                     If plan_dt.Rows(0)("target_cost") = 0 Then
                         target_ok = False
                     Else
+                        TETargetCost.EditValue = plan_dt.Rows(0)("target_cost")
                         target_ok = True
                     End If
                 Else
@@ -67,7 +68,6 @@ WHERE dsg.id_design='" & id_design & "'"
                 BUpdateVendor.Visible = False
                 BtnSave.Visible = True
             Else
-                'warningCustom("Estimasti Cost melewati batas target cost. " & vbNewLine & "Untuk melanjutkan, silahkan negosiasi dengan vendor atau koordinasikan dengan Merchandise dan Design")
                 warningCustom("Target cost belum terdaftar, silahkan hubungi merchandise.")
                 '
                 BUpdateVendor.Visible = False
@@ -136,28 +136,47 @@ WHERE dsg.id_design='" & id_design & "'"
         'If id_comp = "-1" Or id_comp = "" Then
         'stopCustom("Please select vendor first")
         'Else
-        Dim query As String = ""
-        'check 
-        query = "SELECT * FROM "
-        query = "SELECT pdd.`id_prod_demand`,pd.`id_report_status`,pdd.`id_design` FROM tb_prod_demand_design pdd
+        Dim id_season As Integer = FormMasterDesignCOP.BGVDesign.GetFocusedRowCellValue("id_season")
+        Dim is_more_than_limit As Boolean = False
+
+        'check limit
+        If id_season >= get_opt_prod_field("ecop_limit_start") Then
+            Dim limit As Decimal = 0.00
+            Dim target_cost As Decimal = TETargetCost.EditValue
+            Dim ecop As Decimal = TEEcop.EditValue
+            '
+            limit = Decimal.Parse(get_opt_prod_field("ecop_limit_upper_percent"))
+
+            If ecop > (target_cost * ((100 + limit) / 100)) Then
+                is_more_than_limit = True
+            End If
+        End If
+
+        If is_more_than_limit = True Then
+            warningCustom("Estimasti Cost melewati batas target cost. " & vbNewLine & "Untuk melanjutkan, silahkan negosiasi dengan vendor atau koordinasikan dengan Merchandise dan Design")
+        Else
+            Dim query As String = ""
+            'check 
+            query = "SELECT pdd.`id_prod_demand`,pd.`id_report_status`,pdd.`id_design` FROM tb_prod_demand_design pdd
 INNER JOIN tb_prod_demand pd ON pd.`id_prod_demand`=pdd.`id_prod_demand`
 WHERE pd.`id_report_status` != '5' AND pdd.`id_design`='" & id_design & "' AND pd.is_pd='1'"
-        Dim data As DataTable = execute_query(query, -1, True, "", "", "", "")
-        If data.Rows.Count > 0 Then
-            warningCustom("PD already created, COP already locked.")
-        Else
-            Dim id_c As String = ""
-            If id_comp = "-1" Or id_comp = "" Then
-                id_c = "NULL"
+            Dim data As DataTable = execute_query(query, -1, True, "", "", "", "")
+            If data.Rows.Count > 0 Then
+                warningCustom("PD already created, COP already locked.")
             Else
-                id_c = "'" & id_comp_contact & "'"
+                Dim id_c As String = ""
+                If id_comp = "-1" Or id_comp = "" Then
+                    id_c = "NULL"
+                Else
+                    id_c = "'" & id_comp_contact & "'"
+                End If
+                query = String.Format("UPDATE tb_m_design SET prod_order_cop_pd='{1}',prod_order_cop_pd_addcost='{5}',prod_order_cop_kurs_pd='{2}',prod_order_cop_pd_vendor={3},prod_order_cop_pd_curr='{4}' WHERE id_design='{0}'", id_design, decimalSQL((TEEcop.EditValue + TEAdditionalCost.EditValue).ToString), decimalSQL(TEKurs.EditValue.ToString), id_c, LECurrency.EditValue.ToString, decimalSQL(TEAdditionalCost.EditValue.ToString))
+                execute_non_query(query, True, "", "", "", "")
+                infoCustom("ECOP entry success.")
+                FormMasterDesignCOP.view_design()
+                FormMasterDesignCOP.BGVDesign.FocusedRowHandle = find_row_as_is(FormMasterDesignCOP.BGVDesign, "id_design", id_design)
+                Close()
             End If
-            query = String.Format("UPDATE tb_m_design SET prod_order_cop_pd='{1}',prod_order_cop_pd_addcost='{5}',prod_order_cop_kurs_pd='{2}',prod_order_cop_pd_vendor={3},prod_order_cop_pd_curr='{4}' WHERE id_design='{0}'", id_design, decimalSQL((TEEcop.EditValue + TEAdditionalCost.EditValue).ToString), decimalSQL(TEKurs.EditValue.ToString), id_c, LECurrency.EditValue.ToString, decimalSQL(TEAdditionalCost.EditValue.ToString))
-            execute_non_query(query, True, "", "", "", "")
-            infoCustom("ECOP entry success.")
-            FormMasterDesignCOP.view_design()
-            FormMasterDesignCOP.BGVDesign.FocusedRowHandle = find_row_as_is(FormMasterDesignCOP.BGVDesign, "id_design", id_design)
-            Close()
         End If
         'End If
     End Sub
