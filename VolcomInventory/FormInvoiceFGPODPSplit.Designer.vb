1452421f0f499cc7927fe9949ed381eb586c5cb8
Putu Septian Primadewa <iansaviour@gmail.com>
2019-07-18 17:32:33 +0800

    Finish split value

--- VolcomInventory/FormInvoiceFGPODPSplit.Designer.vb
+++ VolcomInventory/FormInvoiceFGPODPSplit.Designer.vb
@@ -20,39 +20,42 @@ Partial Class FormInvoiceFGPODPSplit
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.PanelControl1 = New DevExpress.XtraEditors.PanelControl()
-        Me.SimpleButton1 = New DevExpress.XtraEditors.SimpleButton()
+        Me.BSplit = New DevExpress.XtraEditors.SimpleButton()
         Me.LabelControl1 = New DevExpress.XtraEditors.LabelControl()
         Me.TEAmount = New DevExpress.XtraEditors.TextEdit()
+        Me.TEVAT = New DevExpress.XtraEditors.TextEdit()
+        Me.LabelControl2 = New DevExpress.XtraEditors.LabelControl()
         CType(Me.PanelControl1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.PanelControl1.SuspendLayout()
         CType(Me.TEAmount.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TEVAT.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'PanelControl1
         '
-        Me.PanelControl1.Controls.Add(Me.SimpleButton1)
+        Me.PanelControl1.Controls.Add(Me.BSplit)
         Me.PanelControl1.Dock = System.Windows.Forms.DockStyle.Bottom
-        Me.PanelControl1.Location = New System.Drawing.Point(0, 58)
+        Me.PanelControl1.Location = New System.Drawing.Point(0, 87)
         Me.PanelControl1.Name = "PanelControl1"
         Me.PanelControl1.Size = New System.Drawing.Size(372, 34)
         Me.PanelControl1.TabIndex = 0
         '
-        'SimpleButton1
+        'BSplit
         '
-        Me.SimpleButton1.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.SimpleButton1.Location = New System.Drawing.Point(2, 2)
-        Me.SimpleButton1.Name = "SimpleButton1"
-        Me.SimpleButton1.Size = New System.Drawing.Size(368, 30)
-        Me.SimpleButton1.TabIndex = 0
-        Me.SimpleButton1.Text = "Split"
+        Me.BSplit.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.BSplit.Location = New System.Drawing.Point(2, 2)
+        Me.BSplit.Name = "BSplit"
+        Me.BSplit.Size = New System.Drawing.Size(368, 30)
+        Me.BSplit.TabIndex = 0
+        Me.BSplit.Text = "Split"
         '
         'LabelControl1
         '
         Me.LabelControl1.Location = New System.Drawing.Point(12, 24)
         Me.LabelControl1.Name = "LabelControl1"
-        Me.LabelControl1.Size = New System.Drawing.Size(75, 13)
+        Me.LabelControl1.Size = New System.Drawing.Size(37, 13)
         Me.LabelControl1.TabIndex = 1
-        Me.LabelControl1.Text = "Amount Invoice"
+        Me.LabelControl1.Text = "Amount"
         '
         'TEAmount
         '
@@ -61,11 +64,28 @@ Partial Class FormInvoiceFGPODPSplit
         Me.TEAmount.Size = New System.Drawing.Size(267, 20)
         Me.TEAmount.TabIndex = 2
         '
+        'TEVAT
+        '
+        Me.TEVAT.Location = New System.Drawing.Point(93, 47)
+        Me.TEVAT.Name = "TEVAT"
+        Me.TEVAT.Size = New System.Drawing.Size(267, 20)
+        Me.TEVAT.TabIndex = 4
+        '
+        'LabelControl2
+        '
+        Me.LabelControl2.Location = New System.Drawing.Point(12, 50)
+        Me.LabelControl2.Name = "LabelControl2"
+        Me.LabelControl2.Size = New System.Drawing.Size(19, 13)
+        Me.LabelControl2.TabIndex = 3
+        Me.LabelControl2.Text = "VAT"
+        '
         'FormInvoiceFGPODPSplit
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(372, 92)
+        Me.ClientSize = New System.Drawing.Size(372, 121)
+        Me.Controls.Add(Me.TEVAT)
+        Me.Controls.Add(Me.LabelControl2)
         Me.Controls.Add(Me.TEAmount)
         Me.Controls.Add(Me.LabelControl1)
         Me.Controls.Add(Me.PanelControl1)
@@ -78,13 +98,16 @@ Partial Class FormInvoiceFGPODPSplit
         CType(Me.PanelControl1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.PanelControl1.ResumeLayout(False)
         CType(Me.TEAmount.Properties, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.TEVAT.Properties, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents PanelControl1 As DevExpress.XtraEditors.PanelControl
-    Friend WithEvents SimpleButton1 As DevExpress.XtraEditors.SimpleButton
+    Friend WithEvents BSplit As DevExpress.XtraEditors.SimpleButton
     Friend WithEvents LabelControl1 As DevExpress.XtraEditors.LabelControl
     Friend WithEvents TEAmount As DevExpress.XtraEditors.TextEdit
+    Friend WithEvents TEVAT As DevExpress.XtraEditors.TextEdit
+    Friend WithEvents LabelControl2 As DevExpress.XtraEditors.LabelControl
 End Class
