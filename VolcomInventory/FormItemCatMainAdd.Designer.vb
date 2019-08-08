62b5e9a6d083e95dbefe75eace0a6fe7e2e0cc31
Putu Septian Primadewa <iansaviour@gmail.com>
2019-08-08 10:50:31 +0800

    pop item cat main

--- VolcomInventory/FormItemCatMainAdd.Designer.vb
+++ VolcomInventory/FormItemCatMainAdd.Designer.vb
@@ -20,83 +20,20 @@ Partial Class FormItemCatMainAdd
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FormItemCatMainAdd))
-        Me.SLEMainCategory = New DevExpress.XtraEditors.SearchLookUpEdit()
-        Me.SearchLookUpEdit1View = New DevExpress.XtraGrid.Views.Grid.GridView()
-        Me.GridColumn1 = New DevExpress.XtraGrid.Columns.GridColumn()
-        Me.GridColumn2 = New DevExpress.XtraGrid.Columns.GridColumn()
-        Me.GridColumn4 = New DevExpress.XtraGrid.Columns.GridColumn()
-        Me.GridColumn3 = New DevExpress.XtraGrid.Columns.GridColumn()
-        Me.Label4 = New System.Windows.Forms.Label()
         Me.BtnCancel = New DevExpress.XtraEditors.SimpleButton()
         Me.BTnOK = New DevExpress.XtraEditors.SimpleButton()
         Me.TxtCat = New DevExpress.XtraEditors.TextEdit()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.LEExpenseType = New DevExpress.XtraEditors.LookUpEdit()
         Me.Label1 = New System.Windows.Forms.Label()
-        CType(Me.SLEMainCategory.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.SearchLookUpEdit1View, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.TxtCat.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.LEExpenseType.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
-        'SLEMainCategory
-        '
-        Me.SLEMainCategory.Location = New System.Drawing.Point(103, 16)
-        Me.SLEMainCategory.Name = "SLEMainCategory"
-        Me.SLEMainCategory.Properties.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() {New DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)})
-        Me.SLEMainCategory.Properties.View = Me.SearchLookUpEdit1View
-        Me.SLEMainCategory.Size = New System.Drawing.Size(260, 20)
-        Me.SLEMainCategory.TabIndex = 17
-        '
-        'SearchLookUpEdit1View
-        '
-        Me.SearchLookUpEdit1View.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() {Me.GridColumn1, Me.GridColumn2, Me.GridColumn4, Me.GridColumn3})
-        Me.SearchLookUpEdit1View.FocusRectStyle = DevExpress.XtraGrid.Views.Grid.DrawFocusRectStyle.RowFocus
-        Me.SearchLookUpEdit1View.Name = "SearchLookUpEdit1View"
-        Me.SearchLookUpEdit1View.OptionsSelection.EnableAppearanceFocusedCell = False
-        Me.SearchLookUpEdit1View.OptionsView.ShowGroupPanel = False
-        '
-        'GridColumn1
-        '
-        Me.GridColumn1.Caption = "ID"
-        Me.GridColumn1.FieldName = "id_item_cat_main"
-        Me.GridColumn1.Name = "GridColumn1"
-        '
-        'GridColumn2
-        '
-        Me.GridColumn2.Caption = "Main Category"
-        Me.GridColumn2.FieldName = "item_cat_main"
-        Me.GridColumn2.Name = "GridColumn2"
-        Me.GridColumn2.Visible = True
-        Me.GridColumn2.VisibleIndex = 0
-        '
-        'GridColumn4
-        '
-        Me.GridColumn4.Caption = "ID Expense Type"
-        Me.GridColumn4.FieldName = "id_expense_type"
-        Me.GridColumn4.Name = "GridColumn4"
-        '
-        'GridColumn3
-        '
-        Me.GridColumn3.Caption = "Expense Type"
-        Me.GridColumn3.FieldName = "expense_type"
-        Me.GridColumn3.Name = "GridColumn3"
-        Me.GridColumn3.Visible = True
-        Me.GridColumn3.VisibleIndex = 1
-        '
-        'Label4
-        '
-        Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(12, 19)
-        Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(77, 13)
-        Me.Label4.TabIndex = 16
-        Me.Label4.Text = "Main Category"
-        '
         'BtnCancel
         '
         Me.BtnCancel.Image = CType(resources.GetObject("BtnCancel.Image"), System.Drawing.Image)
-        Me.BtnCancel.Location = New System.Drawing.Point(194, 94)
+        Me.BtnCancel.Location = New System.Drawing.Point(194, 64)
         Me.BtnCancel.Name = "BtnCancel"
         Me.BtnCancel.Size = New System.Drawing.Size(88, 36)
         Me.BtnCancel.TabIndex = 15
@@ -105,7 +42,7 @@ Partial Class FormItemCatMainAdd
         'BTnOK
         '
         Me.BTnOK.Image = CType(resources.GetObject("BTnOK.Image"), System.Drawing.Image)
-        Me.BTnOK.Location = New System.Drawing.Point(288, 94)
+        Me.BTnOK.Location = New System.Drawing.Point(288, 64)
         Me.BTnOK.Name = "BTnOK"
         Me.BTnOK.Size = New System.Drawing.Size(75, 36)
         Me.BTnOK.TabIndex = 14
@@ -113,7 +50,7 @@ Partial Class FormItemCatMainAdd
         '
         'TxtCat
         '
-        Me.TxtCat.Location = New System.Drawing.Point(103, 68)
+        Me.TxtCat.Location = New System.Drawing.Point(103, 38)
         Me.TxtCat.Name = "TxtCat"
         Me.TxtCat.Size = New System.Drawing.Size(260, 20)
         Me.TxtCat.TabIndex = 13
@@ -121,7 +58,7 @@ Partial Class FormItemCatMainAdd
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(12, 71)
+        Me.Label2.Location = New System.Drawing.Point(12, 41)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(52, 13)
         Me.Label2.TabIndex = 12
@@ -129,8 +66,7 @@ Partial Class FormItemCatMainAdd
         '
         'LEExpenseType
         '
-        Me.LEExpenseType.Enabled = False
-        Me.LEExpenseType.Location = New System.Drawing.Point(103, 42)
+        Me.LEExpenseType.Location = New System.Drawing.Point(103, 12)
         Me.LEExpenseType.Name = "LEExpenseType"
         Me.LEExpenseType.Properties.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() {New DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)})
         Me.LEExpenseType.Properties.Columns.AddRange(New DevExpress.XtraEditors.Controls.LookUpColumnInfo() {New DevExpress.XtraEditors.Controls.LookUpColumnInfo("id_expense_type", "Id", 20, DevExpress.Utils.FormatType.None, "", False, DevExpress.Utils.HorzAlignment.[Default]), New DevExpress.XtraEditors.Controls.LookUpColumnInfo("expense_type", "Type")})
@@ -140,7 +76,7 @@ Partial Class FormItemCatMainAdd
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(12, 45)
+        Me.Label1.Location = New System.Drawing.Point(12, 15)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(75, 13)
         Me.Label1.TabIndex = 10
@@ -150,9 +86,7 @@ Partial Class FormItemCatMainAdd
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(373, 144)
-        Me.Controls.Add(Me.SLEMainCategory)
-        Me.Controls.Add(Me.Label4)
+        Me.ClientSize = New System.Drawing.Size(373, 109)
         Me.Controls.Add(Me.BtnCancel)
         Me.Controls.Add(Me.BTnOK)
         Me.Controls.Add(Me.TxtCat)
@@ -165,22 +99,12 @@ Partial Class FormItemCatMainAdd
         Me.Name = "FormItemCatMainAdd"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Add Main Category"
-        CType(Me.SLEMainCategory.Properties, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.SearchLookUpEdit1View, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.TxtCat.Properties, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.LEExpenseType.Properties, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
-
-    Friend WithEvents SLEMainCategory As DevExpress.XtraEditors.SearchLookUpEdit
-    Friend WithEvents SearchLookUpEdit1View As DevExpress.XtraGrid.Views.Grid.GridView
-    Friend WithEvents GridColumn1 As DevExpress.XtraGrid.Columns.GridColumn
-    Friend WithEvents GridColumn2 As DevExpress.XtraGrid.Columns.GridColumn
-    Friend WithEvents GridColumn4 As DevExpress.XtraGrid.Columns.GridColumn
-    Friend WithEvents GridColumn3 As DevExpress.XtraGrid.Columns.GridColumn
-    Friend WithEvents Label4 As Label
     Friend WithEvents BtnCancel As DevExpress.XtraEditors.SimpleButton
     Friend WithEvents BTnOK As DevExpress.XtraEditors.SimpleButton
     Friend WithEvents TxtCat As DevExpress.XtraEditors.TextEdit
--- VolcomInventory/FormItemCatMainAdd.vb
+++ VolcomInventory/FormItemCatMainAdd.vb
@@ -2,4 +2,82 @@
     Private Sub FormItemCatMainAdd_FormClosed(sender As Object, e As FormClosedEventArgs) Handles MyBase.FormClosed
         Dispose()
     End Sub
+
+    Private Sub FormItemCatMainAdd_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        actionLoad()
+    End Sub
+
+    Sub actionLoad()
+        viewType()
+        TxtCat.Text = ""
+    End Sub
+
+    Sub viewType()
+        Dim query As String = "SELECT * FROM tb_lookup_expense_type t "
+        viewLookupQuery(LEExpenseType, query, 0, "expense_type", "id_expense_type")
+    End Sub
+
+    Private Sub SimpleButton2_Click(sender As Object, e As EventArgs) Handles BtnCancel.Click
+        Close()
+    End Sub
+
+    Private Sub SimpleButton1_Click(sender As Object, e As EventArgs) Handles BTnOK.Click
+        save()
+    End Sub
+
+    Sub save()
+        If TxtCat.Text = "" Then
+            stopCustom("Category can't blank")
+        Else
+            Dim item_cat_main As String = addSlashes(TxtCat.Text).Trim()
+            Dim id_expense_type As String = LEExpenseType.EditValue.ToString
+
+            'cek kondisi master
+            Dim cm As Boolean = False
+            Dim qm As String = "SELECT * FROM tb_item_cat_main WHERE item_cat_main='" + item_cat_main + "' "
+            Dim dm As DataTable = execute_query(qm, -1, True, "", "", "", "")
+            If dm.Rows.Count > 0 Then
+                cm = True
+            End If
+
+            'cel kondisi propose
+            Dim cp As Boolean = False
+            Dim qp As String = "SELECT * FROM tb_item_cat_main_pps_det d 
+            INNER JOIN tb_item_cat_main_pps m ON m.id_item_cat_main_pps = d.id_item_cat_main_pps
+            WHERE d.item_cat_main='" + item_cat_main + "' AND m.id_report_status!=5 "
+            Dim dp As DataTable = execute_query(qp, -1, True, "", "", "", "")
+            If dp.Rows.Count > 0 Then
+                cp = True
+            End If
+
+            If cm Or cp Then
+                stopCustom("Category already exist")
+                TxtCat.Focus()
+            Else
+                Dim query As String = "INSERT INTO tb_item_cat_main_pps_det(id_item_cat_main_pps,  id_expense_type, item_cat_main)
+            VALUES('" + FormItemCatMaindet.id_propose + "', " + id_expense_type + ", '" + item_cat_main + "'); "
+                execute_non_query(query, True, "", "", "", "")
+                FormItemCatMainDet.viewDetail()
+                actionLoad()
+            End If
+        End If
+
+    End Sub
+
+    Private Sub LookUpEdit1_KeyDown(sender As Object, e As KeyEventArgs) Handles LEExpenseType.KeyDown
+        If e.KeyCode = Keys.Enter Then
+            TxtCat.Focus()
+        End If
+    End Sub
+
+    Private Sub TxtCat_KeyDown(sender As Object, e As KeyEventArgs) Handles TxtCat.KeyDown
+        If e.KeyCode = Keys.Enter Then
+            If TxtCat.Text <> "" Then
+                save()
+            Else
+                stopCustom("Can't blank.")
+                TxtCat.Focus()
+            End If
+        End If
+    End Sub
 End Class
