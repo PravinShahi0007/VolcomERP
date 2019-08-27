4b4b116d9cb3113c5d5f04422f3b94ecc66f4176
Satrya Anas <catur.satrya@gmail.com>
2019-08-27 11:35:26 +0800

    report own store for master

---
+++ VolcomInventory/ReportSalesDelOrderOwnStore.vb
@@ -0,0 +1,9 @@
+﻿Public Class ReportSalesDelOrderOwnStore
+    Public Shared id_pl_sales_order_del As String
+    Public Shared dt As DataTable
+    Public Shared rmt As String = "43"
+
+    Private Sub ReportSalesDelOrderOwnStore_BeforePrint(sender As Object, e As Printing.PrintEventArgs) Handles MyBase.BeforePrint
+
+    End Sub
+End Class
