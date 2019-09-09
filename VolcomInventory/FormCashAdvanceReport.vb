8fcb0522215eb55088f8330b3bf2f8cdf97f57dc
Putu Septian Primadewa <iansaviour@gmail.com>
2019-09-09 15:31:28 +0800

    Add expense + advance report

--- VolcomInventory/FormCashAdvanceReport.vb
+++ VolcomInventory/FormCashAdvanceReport.vb
@@ -33,6 +33,62 @@
     End Sub
 
     Private Sub BPrint_Click(sender As Object, e As EventArgs) Handles BPrint.Click
-        print(Me.GCReport, "Cash Advance (" & DateTime.Parse(DateFrom.EditValue.ToString).ToString("dd MMMM yyyy") & " - " & DateTime.Parse(DateTo.EditValue.ToString).ToString("dd MMMM yyyy") & ")")
+        GridColumnDateCreated.Visible = False
+        GridColumnStatus.Visible = False
+        GridColumnReconcileDueDate.Visible = False
+        GridColumnCashInAdvancePeriode.Visible = False
+        GridColumnCashOnHandOut.Visible = False
+        GridColumnAdvance.Visible = False
+        GridColumnOverdue.Visible = False
+        '
+        GVReport.ActiveFilterString = "[expense]>0"
+        '
+        print(Me.GCReport, "Expense Report (" & DateTime.Parse(DateFrom.EditValue.ToString).ToString("dd MMMM yyyy") & " - " & DateTime.Parse(DateTo.EditValue.ToString).ToString("dd MMMM yyyy") & ")")
+        '
+        GVReport.ActiveFilterString = ""
+        '
+        GridColumnNumber.VisibleIndex = 0
+        GridColumnDateCreated.VisibleIndex = 1
+        GridColumnEmployee.VisibleIndex = 2
+        GridColumnPurpose.VisibleIndex = 3
+        GridColumnStatus.VisibleIndex = 4
+        GridColumnReconcileDueDate.VisibleIndex = 5
+        GridColumnReconcileActualDate.VisibleIndex = 6
+        GridColumnCashInAdvancePeriode.VisibleIndex = 7
+        GridColumnCashInAdvance.VisibleIndex = 8
+        GridColumnExpense.VisibleIndex = 9
+        GridColumnAdvance.VisibleIndex = 10
+        GridColumnCashOnHandOut.VisibleIndex = 11
+        GridColumnOverdue.VisibleIndex = 12
+    End Sub
+
+    Private Sub BPrintAdvance_Click(sender As Object, e As EventArgs) Handles BPrintAdvance.Click
+        GridColumnDateCreated.Visible = False
+        GridColumnStatus.Visible = False
+        GridColumnReconcileActualDate.Visible = False
+        GridColumnCashInAdvancePeriode.Visible = False
+        GridColumnCashInAdvance.Visible = False
+        GridColumnExpense.Visible = False
+        GridColumnCashOnHandOut.Visible = False
+        '
+        GVReport.ActiveFilterString = "[advance]>0"
+        '
+        print(Me.GCReport, "Advance Report (" & DateTime.Parse(DateFrom.EditValue.ToString).ToString("dd MMMM yyyy") & " - " & DateTime.Parse(DateTo.EditValue.ToString).ToString("dd MMMM yyyy") & ")")
+        '
+        GVReport.ActiveFilterString = ""
+        '
+        GridColumnNumber.VisibleIndex = 0
+        GridColumnDateCreated.VisibleIndex = 1
+        GridColumnEmployee.VisibleIndex = 2
+        GridColumnPurpose.VisibleIndex = 3
+        GridColumnStatus.VisibleIndex = 4
+        GridColumnReconcileDueDate.VisibleIndex = 5
+        GridColumnReconcileActualDate.VisibleIndex = 6
+        GridColumnCashInAdvancePeriode.VisibleIndex = 7
+        GridColumnCashInAdvance.VisibleIndex = 8
+        GridColumnExpense.VisibleIndex = 9
+        GridColumnAdvance.VisibleIndex = 10
+        GridColumnCashOnHandOut.VisibleIndex = 11
+        GridColumnOverdue.VisibleIndex = 12
     End Sub
 End Class
