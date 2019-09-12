d9c555c61db04cb6232967ca8999290fd7621340
Satrya Anas <catur.satrya@gmail.com>
2019-09-12 11:53:11 +0800

    fix bbug return combine : hanya user login yg combine return

--- VolcomInventory/FormSalesReturnDet.vb
+++ VolcomInventory/FormSalesReturnDet.vb
@@ -448,7 +448,7 @@ Public Class FormSalesReturnDet
         WHERE r.id_sales_return>0 AND r.sales_return_store_number='" + addSlashes(TxtStoreReturnNumber.Text) + "' 
         AND r.id_store_contact_from=" + id_store_contact_from + " "
         If BtnCombineReturn.Visible = True Then
-            query += "AND r.id_report_status=1 AND r.combine_number='' "
+            query += "AND r.id_report_status=1 AND r.combine_number='' AND r.last_update_by='" + id_user + "' "
         Else
             query += "AND r.combine_number='" + addSlashes(TxtCombineNumber.Text) + "' "
         End If
@@ -460,8 +460,11 @@ Public Class FormSalesReturnDet
         'member number
         Dim qm As String = "SELECT GROUP_CONCAT(DISTINCT r.sales_return_number ORDER BY r.id_sales_return ASC SEPARATOR ', ') AS `number`
                 FROM tb_sales_return r
-                WHERE r.combine_number='" + addSlashes(TxtCombineNumber.Text) + "' AND r.sales_return_store_number='" + addSlashes(TxtStoreReturnNumber.Text) + "'
-                GROUP BY r.combine_number "
+                WHERE r.combine_number='" + addSlashes(TxtCombineNumber.Text) + "' AND r.sales_return_store_number='" + addSlashes(TxtStoreReturnNumber.Text) + "' "
+        If BtnCombineReturn.Visible = True Then
+            qm += "AND r.last_update_by='" + id_user + "' "
+        End If
+        qm += "GROUP BY r.combine_number "
         Dim dm As DataTable = execute_query(qm, -1, True, "", "", "", "")
         If dm.Rows.Count > 0 Then
             TxtCombineFrom.Text = dm.Rows(0)("number").ToString
