79f983eb5dd2c4dfce79c5208be5b742b09e48b7
Satrya Anas <catur.satrya@gmail.com>
2019-10-03 17:16:42 +0800

    prepare form promo rules

--- VolcomInventory/FormPromoRules.vb
+++ VolcomInventory/FormPromoRules.vb
@@ -1,3 +1,51 @@
 ﻿Public Class FormPromoRules
+    Private Sub FormPromoRules_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+    End Sub
+
+    Sub viewRules()
+        Cursor = Cursors.WaitCursor
+        Dim query As String = "SELECT r.id_rules, r.id_design_cat, dc.design_cat, r.limit_value, r.id_product, p.product_full_code AS `code`, p.product_display_name AS `name`
+        FROM tb_promo_rules r 
+        INNER JOIN tb_lookup_design_cat dc ON dc.id_design_cat = r.id_design_cat
+        INNER JOIN tb_m_product p ON p.id_product = r.id_product "
+        Dim data As DataTable = execute_query(query, -1, True, "", "", "", "")
+        GCRules.DataSource = data
+        GVRules.BestFitColumns()
+        Cursor = Cursors.Default
+    End Sub
+
+    Sub viewStore()
+        Cursor = Cursors.WaitCursor
+        Dim id_rules As String = "-1"
+        Try
+            id_rules = GVRules.GetFocusedRowCellValue("id_rules").ToString
+        Catch ex As Exception
+            id_rules = "-1"
+        End Try
+        Dim query As String = "SELECT c.id_outlet, c.outlet_name, IF(!ISNULL(rd.id_rules_det), 'yes', 'no') AS `is_select`
+        FROM tb_store_conn c
+        LEFT JOIN tb_promo_rules_det rd ON rd.id_outlet = c.id_outlet AND rd.id_rules=" + id_rules + " "
+        Dim data As DataTable = execute_query(query, -1, True, "", "", "", "")
+        GCStore.DataSource = data
+        GVStore.BestFitColumns()
+        Cursor = Cursors.Default
+    End Sub
+
+    Private Sub FormPromoRules_Activated(sender As Object, e As EventArgs) Handles MyBase.Activated
+        FormMain.show_rb(Name)
+        checkFormAccess(Name)
+    End Sub
+
+    Private Sub FormPromoRules_Deactivate(sender As Object, e As EventArgs) Handles MyBase.Deactivate
+        FormMain.hide_rb()
+    End Sub
+
+    Private Sub GVRules_FocusedRowChanged(sender As Object, e As DevExpress.XtraGrid.Views.Base.FocusedRowChangedEventArgs) Handles GVRules.FocusedRowChanged
+        viewStore()
+    End Sub
+
+    Private Sub GVRules_ColumnFilterChanged(sender As Object, e As EventArgs) Handles GVRules.ColumnFilterChanged
+        viewStore()
+    End Sub
 End Class
---
+++ VolcomInventory/FormPromoRulesDet.vb
@@ -0,0 +1,48 @@
+﻿Public Class FormPromoRulesDet
+    Public id As String = "-1"
+    Public action As String = "-1"
+
+    Sub viewDesignCat()
+        Cursor = Cursors.WaitCursor
+        Dim query As String = "SELECT * FROM tb_lookup_design_cat c"
+        viewLookupQuery(LEProductStatus, query, 0, "design_cat", "id_design_cat")
+        Cursor = Cursors.Default
+    End Sub
+
+    Private Sub FormPromoRulesDet_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        viewDesignCat()
+        actionLoad
+    End Sub
+
+    Sub actionLoad()
+        Cursor = Cursors.WaitCursor
+        If action = "ins" Then
+            TxtLimitValue.EditValue = 0
+        End If
+        Cursor = Cursors.Default
+    End Sub
+
+    Private Sub BtnClose_Click(sender As Object, e As EventArgs) Handles BtnClose.Click
+        Close()
+    End Sub
+
+    Private Sub FormPromoRulesDet_FormClosed(sender As Object, e As FormClosedEventArgs) Handles MyBase.FormClosed
+        Dispose()
+    End Sub
+
+    Private Sub TxtCode_KeyDown(sender As Object, e As KeyEventArgs) Handles TxtCode.KeyDown
+        If e.KeyCode = Keys.Add Then
+            Cursor = Cursors.WaitCursor
+            Dim code As String = addSlashes(TxtCode.Text)
+            Dim query As String = "SELECT p.id_product, p.product_full_code AS `code`, p.product_display_name AS `name` 
+            FROM tb_m_product p
+            INNER JOIN tb_m_design d ON d.id_design = p.id_design
+            WHERE d.id_lookup_status_order!=2 AND d.id_active=1 AND p.product_full_code='" + code + "' "
+            Dim data As DataTable = execute_query(query, -1, True, "", "", "", "")
+            If data.Rows.Count > 0 Then
+
+            End If
+            Cursor = Cursors.Default
+        End If
+    End Sub
+End Class
