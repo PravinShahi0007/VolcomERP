fee1c9c59efd08ccdfac6918ef23088b381b4212
Agus Friastana <friastana@gmail.com>
2019-10-22 17:11:36 +0800

    bukti pickup load

--- VolcomInventory/FormBuktiPickupDet.vb
+++ VolcomInventory/FormBuktiPickupDet.vb
@@ -2,10 +2,46 @@
     Public id_pickup As String = "0"
 
     Private Sub FormBuktiPickupDet_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
+        view_comp()
+        load_form()
     End Sub
 
     Sub load_form()
+        If id_pickup = "0" Then
+            TECreatedBy.EditValue = get_emp(id_employee_user, "2")
+            DECreatedDate.EditValue = Now
+        Else
+        End If
+    End Sub
+
+    Sub view_comp()
+        Dim query As String = "SELECT id_comp, CONCAT(comp_number, ' - ', comp_name) AS comp_name FROM tb_m_comp WHERE id_comp_cat = 7"
+
+        viewSearchLookupQuery(SLUECompany, query, "id_comp", "comp_name", "id_comp")
+    End Sub
+
+    Private Sub SBAdd_Click(sender As Object, e As EventArgs) Handles SBAdd.Click
+        FormBuktiPickupPick.id_comp = SLUECompany.EditValue.ToString
+        FormBuktiPickupPick.ShowDialog()
+    End Sub
+
+    Private Sub SBRemove_Click(sender As Object, e As EventArgs) Handles SBRemove.Click
+        GVList.DeleteSelectedRows()
+    End Sub
+
+    Private Sub FormBuktiPickupDet_FormClosed(sender As Object, e As FormClosedEventArgs) Handles MyBase.FormClosed
+        Dispose()
+    End Sub
+
+    Private Sub SBComplete_Click(sender As Object, e As EventArgs) Handles SBComplete.Click
+
+    End Sub
+
+    Private Sub SBSave_Click(sender As Object, e As EventArgs) Handles SBSave.Click
+
+    End Sub
+
+    Private Sub SBAttachement_Click(sender As Object, e As EventArgs) Handles SBAttachement.Click
 
     End Sub
 End Class
--- VolcomInventory/FormBuktiPickupPick.Designer.vb
+++ VolcomInventory/FormBuktiPickupPick.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class FormBuktiPickupPick
     Inherits DevExpress.XtraEditors.XtraForm
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         If disposing AndAlso components IsNot Nothing Then
             components.Dispose()
@@ -17,31 +17,39 @@ Partial Class FormBuktiPickupPick
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FormBuktiPickupPick))
         Me.PanelControl1 = New DevExpress.XtraEditors.PanelControl()
         Me.Label6 = New System.Windows.Forms.Label()
-        Me.SLUECompany = New DevExpress.XtraEditors.SearchLookUpEdit()
-        Me.SearchLookUpEdit1View = New DevExpress.XtraGrid.Views.Grid.GridView()
         Me.GCList = New DevExpress.XtraGrid.GridControl()
         Me.GVList = New DevExpress.XtraGrid.Views.Grid.GridView()
         Me.PanelControl2 = New DevExpress.XtraEditors.PanelControl()
         Me.SBClose = New DevExpress.XtraEditors.SimpleButton()
         Me.SBAdd = New DevExpress.XtraEditors.SimpleButton()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.DEFrom = New DevExpress.XtraEditors.DateEdit()
+        Me.DEUntil = New DevExpress.XtraEditors.DateEdit()
+        Me.SBView = New DevExpress.XtraEditors.SimpleButton()
         CType(Me.PanelControl1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.PanelControl1.SuspendLayout()
-        CType(Me.SLUECompany.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.SearchLookUpEdit1View, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.GCList, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.GVList, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PanelControl2, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.PanelControl2.SuspendLayout()
+        CType(Me.DEFrom.Properties.CalendarTimeProperties, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DEFrom.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DEUntil.Properties.CalendarTimeProperties, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DEUntil.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'PanelControl1
         '
+        Me.PanelControl1.Controls.Add(Me.SBView)
+        Me.PanelControl1.Controls.Add(Me.DEUntil)
+        Me.PanelControl1.Controls.Add(Me.DEFrom)
+        Me.PanelControl1.Controls.Add(Me.Label1)
         Me.PanelControl1.Controls.Add(Me.Label6)
-        Me.PanelControl1.Controls.Add(Me.SLUECompany)
         Me.PanelControl1.Dock = System.Windows.Forms.DockStyle.Top
         Me.PanelControl1.Location = New System.Drawing.Point(0, 0)
         Me.PanelControl1.Name = "PanelControl1"
@@ -53,25 +61,9 @@ Partial Class FormBuktiPickupPick
         Me.Label6.AutoSize = True
         Me.Label6.Location = New System.Drawing.Point(13, 15)
         Me.Label6.Name = "Label6"
-        Me.Label6.Size = New System.Drawing.Size(52, 13)
+        Me.Label6.Size = New System.Drawing.Size(31, 13)
         Me.Label6.TabIndex = 11
-        Me.Label6.Text = "Company"
-        '
-        'SLUECompany
-        '
-        Me.SLUECompany.Location = New System.Drawing.Point(76, 12)
-        Me.SLUECompany.Name = "SLUECompany"
-        Me.SLUECompany.Properties.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() {New DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)})
-        Me.SLUECompany.Properties.View = Me.SearchLookUpEdit1View
-        Me.SLUECompany.Size = New System.Drawing.Size(200, 20)
-        Me.SLUECompany.TabIndex = 10
-        '
-        'SearchLookUpEdit1View
-        '
-        Me.SearchLookUpEdit1View.FocusRectStyle = DevExpress.XtraGrid.Views.Grid.DrawFocusRectStyle.RowFocus
-        Me.SearchLookUpEdit1View.Name = "SearchLookUpEdit1View"
-        Me.SearchLookUpEdit1View.OptionsSelection.EnableAppearanceFocusedCell = False
-        Me.SearchLookUpEdit1View.OptionsView.ShowGroupPanel = False
+        Me.Label6.Text = "From"
         '
         'GCList
         '
@@ -101,7 +93,8 @@ Partial Class FormBuktiPickupPick
         'SBClose
         '
         Me.SBClose.Dock = System.Windows.Forms.DockStyle.Right
-        Me.SBClose.Location = New System.Drawing.Point(613, 2)
+        Me.SBClose.Image = CType(resources.GetObject("SBClose.Image"), System.Drawing.Image)
+        Me.SBClose.Location = New System.Drawing.Point(608, 2)
         Me.SBClose.Name = "SBClose"
         Me.SBClose.Size = New System.Drawing.Size(84, 45)
         Me.SBClose.TabIndex = 4
@@ -110,13 +103,65 @@ Partial Class FormBuktiPickupPick
         'SBAdd
         '
         Me.SBAdd.Dock = System.Windows.Forms.DockStyle.Right
-        Me.SBAdd.Location = New System.Drawing.Point(697, 2)
+        Me.SBAdd.Image = CType(resources.GetObject("SBAdd.Image"), System.Drawing.Image)
+        Me.SBAdd.Location = New System.Drawing.Point(692, 2)
         Me.SBAdd.Name = "SBAdd"
-        Me.SBAdd.Size = New System.Drawing.Size(85, 45)
+        Me.SBAdd.Size = New System.Drawing.Size(90, 45)
         Me.SBAdd.TabIndex = 3
         Me.SBAdd.Text = "Add"
         '
-        'FormBuktiTransferPick
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(271, 15)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(28, 13)
+        Me.Label1.TabIndex = 12
+        Me.Label1.Text = "Until"
+        '
+        'DEFrom
+        '
+        Me.DEFrom.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.DEFrom.EditValue = Nothing
+        Me.DEFrom.Location = New System.Drawing.Point(55, 12)
+        Me.DEFrom.Name = "DEFrom"
+        Me.DEFrom.Properties.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() {New DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)})
+        Me.DEFrom.Properties.CalendarTimeProperties.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() {New DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)})
+        Me.DEFrom.Properties.DisplayFormat.FormatString = "dd MMMM yyyy"
+        Me.DEFrom.Properties.DisplayFormat.FormatType = DevExpress.Utils.FormatType.DateTime
+        Me.DEFrom.Properties.EditFormat.FormatString = "dd MMMM yyyy"
+        Me.DEFrom.Properties.EditFormat.FormatType = DevExpress.Utils.FormatType.DateTime
+        Me.DEFrom.Properties.Mask.EditMask = "dd MMMM yyyy"
+        Me.DEFrom.Properties.Mask.UseMaskAsDisplayFormat = True
+        Me.DEFrom.Size = New System.Drawing.Size(200, 20)
+        Me.DEFrom.TabIndex = 13
+        '
+        'DEUntil
+        '
+        Me.DEUntil.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.DEUntil.EditValue = Nothing
+        Me.DEUntil.Location = New System.Drawing.Point(310, 12)
+        Me.DEUntil.Name = "DEUntil"
+        Me.DEUntil.Properties.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() {New DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)})
+        Me.DEUntil.Properties.CalendarTimeProperties.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() {New DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)})
+        Me.DEUntil.Properties.DisplayFormat.FormatString = "dd MMMM yyyy"
+        Me.DEUntil.Properties.DisplayFormat.FormatType = DevExpress.Utils.FormatType.DateTime
+        Me.DEUntil.Properties.EditFormat.FormatString = "dd MMMM yyyy"
+        Me.DEUntil.Properties.EditFormat.FormatType = DevExpress.Utils.FormatType.DateTime
+        Me.DEUntil.Properties.Mask.EditMask = "dd MMMM yyyy"
+        Me.DEUntil.Properties.Mask.UseMaskAsDisplayFormat = True
+        Me.DEUntil.Size = New System.Drawing.Size(200, 20)
+        Me.DEUntil.TabIndex = 14
+        '
+        'SBView
+        '
+        Me.SBView.Location = New System.Drawing.Point(521, 10)
+        Me.SBView.Name = "SBView"
+        Me.SBView.Size = New System.Drawing.Size(75, 23)
+        Me.SBView.TabIndex = 15
+        Me.SBView.Text = "View"
+        '
+        'FormBuktiPickupPick
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
@@ -124,28 +169,33 @@ Partial Class FormBuktiPickupPick
         Me.Controls.Add(Me.GCList)
         Me.Controls.Add(Me.PanelControl1)
         Me.Controls.Add(Me.PanelControl2)
-        Me.Name = "FormBuktiTransferPick"
+        Me.Name = "FormBuktiPickupPick"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Bukti Pickup Pick"
         CType(Me.PanelControl1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.PanelControl1.ResumeLayout(False)
         Me.PanelControl1.PerformLayout()
-        CType(Me.SLUECompany.Properties, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.SearchLookUpEdit1View, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.GCList, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.GVList, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.PanelControl2, System.ComponentModel.ISupportInitialize).EndInit()
         Me.PanelControl2.ResumeLayout(False)
+        CType(Me.DEFrom.Properties.CalendarTimeProperties, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DEFrom.Properties, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DEUntil.Properties.CalendarTimeProperties, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DEUntil.Properties, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
 
     Friend WithEvents PanelControl1 As DevExpress.XtraEditors.PanelControl
     Friend WithEvents Label6 As Label
-    Friend WithEvents SLUECompany As DevExpress.XtraEditors.SearchLookUpEdit
-    Friend WithEvents SearchLookUpEdit1View As DevExpress.XtraGrid.Views.Grid.GridView
     Friend WithEvents GCList As DevExpress.XtraGrid.GridControl
     Friend WithEvents GVList As DevExpress.XtraGrid.Views.Grid.GridView
     Friend WithEvents PanelControl2 As DevExpress.XtraEditors.PanelControl
     Friend WithEvents SBClose As DevExpress.XtraEditors.SimpleButton
     Friend WithEvents SBAdd As DevExpress.XtraEditors.SimpleButton
+    Friend WithEvents Label1 As Label
+    Friend WithEvents DEUntil As DevExpress.XtraEditors.DateEdit
+    Friend WithEvents DEFrom As DevExpress.XtraEditors.DateEdit
+    Friend WithEvents SBView As DevExpress.XtraEditors.SimpleButton
 End Class
