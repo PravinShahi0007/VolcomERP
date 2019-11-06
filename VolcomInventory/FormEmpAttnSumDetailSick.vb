c4fb293b2804f736760aa2a365bf13b9de92ba76
Agus Friastana <friastana@gmail.com>
2019-11-06 14:10:13 +0800

    attendance report summary add detail late, print detail sick & late

--- VolcomInventory/FormEmpAttnSumDetailSick.vb
+++ VolcomInventory/FormEmpAttnSumDetailSick.vb
@@ -5,6 +5,8 @@
     Public month As Date = Nothing
 
     Private Sub FormEmpAttnSumDetailSick_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Cursor = Cursors.WaitCursor
+
         Dim data_employee As DataTable = execute_query("SELECT employee_code, employee_name FROM tb_m_employee WHERE id_employee = " + id_employee, -1, True, "", "", "", "")
 
         TEEmployeeCode.EditValue = data_employee.Rows(0)("employee_code").ToString
@@ -29,9 +31,50 @@
         GCList.DataSource = data_list
 
         GVList.BestFitColumns()
+
+        Cursor = Cursors.Default
     End Sub
 
     Private Sub FormEmpAttnSumDetailSick_FormClosed(sender As Object, e As FormClosedEventArgs) Handles MyBase.FormClosed
         Dispose()
+    End Sub
+
+    Private Sub BPrintSum_Click(sender As Object, e As EventArgs) Handles BPrintSum.Click
+        ReportAttnSum.dt = GCList.DataSource
+        ReportAttnSum.id_report_type = "-1"
+        Dim Report As New ReportAttnSum()
+
+        ' '... 
+        ' ' creating and saving the view's layout to a new memory stream 
+        Dim str As System.IO.Stream
+        str = New System.IO.MemoryStream()
+        GVList.SaveLayoutToStream(str, DevExpress.Utils.OptionsLayoutBase.FullLayout)
+        str.Seek(0, System.IO.SeekOrigin.Begin)
+        Report.GVSchedule.RestoreLayoutFromStream(str, DevExpress.Utils.OptionsLayoutBase.FullLayout)
+        str.Seek(0, System.IO.SeekOrigin.Begin)
+
+        'Grid Detail
+        ReportStyleGridview(Report.GVSchedule)
+
+        'Parse val
+        Report.LTitle.Text = "DETAIL SICK REPORT"
+        Report.LType.Text = "Name"
+        Report.LDept.Text = TEEmployeeName.EditValue.ToString
+        Report.LDateRange.Text = Date.Parse(date_from).ToString("dd MMM yyyy") + " - " + Date.Parse(date_to).ToString("dd MMM yyyy")
+
+        'Show the report's preview. 
+        Dim Tool As DevExpress.XtraReports.UI.ReportPrintTool = New DevExpress.XtraReports.UI.ReportPrintTool(Report)
+        Tool.ShowPreview()
+    End Sub
+
+    Private Sub GVList_CustomColumnSort(sender As Object, e As DevExpress.XtraGrid.Views.Base.CustomColumnSortEventArgs) Handles GVList.CustomColumnSort
+        If e.Column.FieldName = "leave_month" Then
+            Dim val1 As DateTime = Convert.ToDateTime(e.Value1)
+            Dim val2 As DateTime = Convert.ToDateTime(e.Value2)
+
+            e.Result = Comparer.Default.Compare(val1, val2)
+
+            e.Handled = True
+        End If
     End Sub
 End Class
