b31236396424b6e03141132307bc83aea35a8c42
Satrya Anas <catur.satrya@gmail.com>
2019-11-25 13:22:35 +0800

    db

--- VolcomInventory/FormMailManageDet.vb
+++ VolcomInventory/FormMailManageDet.vb
@@ -14,7 +14,7 @@
     Sub actionLoad()
         If action = "ins" Then
             If rmt = "225" Then
-                'load member
+                '--- load member
                 Dim qf As String = "SELECT m.id_mail_manage_mapping_intern AS `index`,0 AS `id_mail_manage_member`,0 AS `id_mail_manage`,m.id_mail_member_type, mmt.mail_member_type,m.id_user,0 AS `id_comp_group`, 
                 e.employee_name AS `description`,e.email_external AS `mail_address` 
                 FROM tb_mail_manage_mapping_intern m
@@ -34,7 +34,7 @@
                 GCMember.DataSource = df
                 GVMember.BestFitColumns()
 
-                'load data
+                '--- load data
                 Dim id_sales_pos As String = ""
                 For i As Integer = 0 To (FormMailManage.GVInvoiceList.RowCount - 1)
                     If i > 0 Then
@@ -42,7 +42,31 @@
                     End If
                     id_sales_pos += FormMailManage.GVInvoiceList.GetRowCellValue(i, "id_sales_pos").ToString
                 Next
-
+                Dim qdet As String = "SELECT cg.comp_name AS `group_company`, g.description AS `group_store`,sp.sales_pos_start_period, sp.sales_pos_end_period,prd.period, prd.amount AS `total_amount`,
+                sp.id_sales_pos, sp.sales_pos_number, CONCAT(c.comp_number, ' - ', c.comp_name) AS `store`, sp.sales_pos_total_qty AS `qty_invoice`, 
+                CAST(IF(typ.`is_receive_payment`=2,-1,1) * ((sp.`sales_pos_total`*((100-sp.sales_pos_discount)/100))-sp.`sales_pos_potongan`) AS DECIMAL(15,2)) AS `amount`
+                FROM tb_sales_pos sp 
+                INNER JOIN tb_m_comp_contact cc ON cc.`id_comp_contact`= IF(sp.id_memo_type=8 OR sp.id_memo_type=9, sp.id_comp_contact_bill,sp.`id_store_contact_from`)
+                INNER JOIN tb_m_comp c ON c.`id_comp`=cc.`id_comp`
+                INNER JOIN tb_m_comp_group g ON g.id_comp_group = c.id_comp_group
+                INNER JOIN tb_m_comp cg ON cg.id_comp = g.id_comp
+                INNER JOIN (
+	                SELECT c.id_comp_group,
+	                GROUP_CONCAT(DISTINCT CONCAT(DATE_FORMAT(sp.sales_pos_start_period,'%d %M %Y'),' s/d ', DATE_FORMAT(sp.sales_pos_end_period,'%d %M %Y')) ORDER BY sp.id_sales_pos ASC SEPARATOR ', ') AS `period`,
+	                SUM(CAST(IF(typ.`is_receive_payment`=2,-1,1) * ((sp.`sales_pos_total`*((100-sp.sales_pos_discount)/100))-sp.`sales_pos_potongan`) AS DECIMAL(15,2))) AS `amount`
+	                FROM tb_sales_pos sp
+	                INNER JOIN tb_m_comp_contact cc ON cc.`id_comp_contact`= IF(sp.id_memo_type=8 OR sp.id_memo_type=9, sp.id_comp_contact_bill,sp.`id_store_contact_from`)
+	                INNER JOIN tb_m_comp c ON c.`id_comp`=cc.`id_comp`
+	                INNER JOIN tb_lookup_memo_type typ ON typ.`id_memo_type`=sp.`id_memo_type`
+	                WHERE sp.id_sales_pos IN (" + id_sales_pos + ")
+	                GROUP BY c.id_comp_group
+                ) prd ON prd.id_comp_group = c.id_comp_group
+                INNER JOIN tb_lookup_memo_type typ ON typ.`id_memo_type`=sp.`id_memo_type`
+                WHERE sp.id_sales_pos IN (" + id_sales_pos + ") "
+                Dim ddet As DataTable = execute_query(qdet, -1, True, "", "", "", "")
+                GCDetail.DataSource = ddet
+                GVDetail.BestFitColumns()
+                'custom column
 
 
 
