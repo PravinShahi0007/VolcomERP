d5ae41431f1a27f33c3c6af15077423e1159887d
Satrya Anas <catur.satrya@gmail.com>
2019-11-29 16:37:09 +0800

    aging kalo uda bayar sampe bank received data

--- VolcomInventory/FormInvoiceTracking.vb
+++ VolcomInventory/FormInvoiceTracking.vb
@@ -98,7 +98,7 @@
             IFNULL(pyd.`value`,0.00) - CAST(IF(typ.`is_receive_payment`=2,-1,1) * ((sp.`sales_pos_total`*((100-sp.sales_pos_discount)/100))-sp.`sales_pos_potongan`) AS DECIMAL(15,2)) AS total_due,
             CAST(IF(typ.`is_receive_payment`=2,-1,1) * ((sp.`sales_pos_total`*((100-sp.sales_pos_discount)/100))-sp.`sales_pos_potongan`) AS DECIMAL(15,2)) AS amount
             ,sp.report_mark_type,rmt.report_mark_type_name
-            ,DATEDIFF(NOW(),sp.`sales_pos_due_date`) AS due_days,
+            ,DATEDIFF(IF(sp.is_close_rec_payment=2,NOW(), IF(ISNULL(bbm.bbm_received_date),NOW(),bbm.bbm_received_date)),sp.`sales_pos_due_date`) AS due_days,
             mail_warning_no, mail_warning_date, mail_warning_status,
             mail_notice_no, mail_notice_date, mail_notice_status,
             bbm.`bbm_number`, bbm.`bbm_created_date`, bbm.`bbm_received_date`
