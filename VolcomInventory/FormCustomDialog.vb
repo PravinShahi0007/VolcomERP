ff4bbff1653ffb288f6115f5ae5de79673fb1add
Satrya Anas <catur.satrya@gmail.com>
2019-12-05 16:07:20 +0800

    check existing order

---
+++ VolcomInventory/FormCustomDialog.vb
@@ -0,0 +1,10 @@
+﻿Public Class FormCustomDialog
+    Public rmt As String = "-1"
+    Private Sub FormCustomDialog_FormClosed(sender As Object, e As FormClosedEventArgs) Handles MyBase.FormClosed
+        Dispose()
+    End Sub
+
+    Private Sub BtnOK_Click(sender As Object, e As EventArgs) Handles BtnOK.Click
+        Close()
+    End Sub
+End Class
