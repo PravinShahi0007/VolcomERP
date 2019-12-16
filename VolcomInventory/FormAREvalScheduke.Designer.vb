77e66d6c16bdd1a70880fce555a33c832f36dc52
Satrya Anas <catur.satrya@gmail.com>
2019-12-16 11:42:41 +0800

    AR evaluation schedule form - save - update -refresh- print - close

--- VolcomInventory/FormAREvalScheduke.Designer.vb
+++ VolcomInventory/FormAREvalScheduke.Designer.vb
@@ -19,17 +19,135 @@ Partial Class FormAREvalScheduke
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FormAREvalScheduke))
+        Me.PanelControl1 = New DevExpress.XtraEditors.PanelControl()
+        Me.BtnView = New DevExpress.XtraEditors.SimpleButton()
+        Me.LEYear = New DevExpress.XtraEditors.LookUpEdit()
+        Me.LabelControl1 = New DevExpress.XtraEditors.LabelControl()
+        Me.GCData = New DevExpress.XtraGrid.GridControl()
+        Me.GVData = New DevExpress.XtraGrid.Views.Grid.GridView()
+        Me.GridColumnid_ar_eval_setup_date = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GridColumnmth_name = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GridColumnar_eval_setup_date = New DevExpress.XtraGrid.Columns.GridColumn()
+        CType(Me.PanelControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.PanelControl1.SuspendLayout()
+        CType(Me.LEYear.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.GCData, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.GVData, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
+        '
+        'PanelControl1
+        '
+        Me.PanelControl1.Controls.Add(Me.BtnView)
+        Me.PanelControl1.Controls.Add(Me.LEYear)
+        Me.PanelControl1.Controls.Add(Me.LabelControl1)
+        Me.PanelControl1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.PanelControl1.Location = New System.Drawing.Point(0, 0)
+        Me.PanelControl1.Name = "PanelControl1"
+        Me.PanelControl1.Size = New System.Drawing.Size(743, 42)
+        Me.PanelControl1.TabIndex = 0
+        '
+        'BtnView
+        '
+        Me.BtnView.Image = CType(resources.GetObject("BtnView.Image"), System.Drawing.Image)
+        Me.BtnView.Location = New System.Drawing.Point(179, 10)
+        Me.BtnView.Name = "BtnView"
+        Me.BtnView.Size = New System.Drawing.Size(69, 23)
+        Me.BtnView.TabIndex = 2
+        Me.BtnView.Text = "View"
+        '
+        'LEYear
+        '
+        Me.LEYear.Location = New System.Drawing.Point(43, 12)
+        Me.LEYear.Name = "LEYear"
+        Me.LEYear.Properties.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() {New DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)})
+        Me.LEYear.Properties.Columns.AddRange(New DevExpress.XtraEditors.Controls.LookUpColumnInfo() {New DevExpress.XtraEditors.Controls.LookUpColumnInfo("year", "Year")})
+        Me.LEYear.Size = New System.Drawing.Size(130, 20)
+        Me.LEYear.TabIndex = 1
+        '
+        'LabelControl1
+        '
+        Me.LabelControl1.Location = New System.Drawing.Point(15, 14)
+        Me.LabelControl1.Name = "LabelControl1"
+        Me.LabelControl1.Size = New System.Drawing.Size(22, 13)
+        Me.LabelControl1.TabIndex = 0
+        Me.LabelControl1.Text = "Year"
+        '
+        'GCData
+        '
+        Me.GCData.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.GCData.Location = New System.Drawing.Point(0, 42)
+        Me.GCData.MainView = Me.GVData
+        Me.GCData.Name = "GCData"
+        Me.GCData.Size = New System.Drawing.Size(743, 381)
+        Me.GCData.TabIndex = 1
+        Me.GCData.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() {Me.GVData})
+        '
+        'GVData
+        '
+        Me.GVData.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() {Me.GridColumnid_ar_eval_setup_date, Me.GridColumnmth_name, Me.GridColumnar_eval_setup_date})
+        Me.GVData.GridControl = Me.GCData
+        Me.GVData.GroupCount = 1
+        Me.GVData.Name = "GVData"
+        Me.GVData.OptionsBehavior.AutoExpandAllGroups = True
+        Me.GVData.OptionsBehavior.Editable = False
+        Me.GVData.OptionsView.ColumnAutoWidth = False
+        Me.GVData.OptionsView.ShowGroupedColumns = True
+        Me.GVData.OptionsView.ShowGroupPanel = False
+        Me.GVData.SortInfo.AddRange(New DevExpress.XtraGrid.Columns.GridColumnSortInfo() {New DevExpress.XtraGrid.Columns.GridColumnSortInfo(Me.GridColumnmth_name, DevExpress.Data.ColumnSortOrder.Ascending)})
+        '
+        'GridColumnid_ar_eval_setup_date
+        '
+        Me.GridColumnid_ar_eval_setup_date.Caption = "id_ar_eval_setup_date"
+        Me.GridColumnid_ar_eval_setup_date.FieldName = "id_ar_eval_setup_date"
+        Me.GridColumnid_ar_eval_setup_date.Name = "GridColumnid_ar_eval_setup_date"
+        '
+        'GridColumnmth_name
+        '
+        Me.GridColumnmth_name.Caption = "Month"
+        Me.GridColumnmth_name.FieldName = "mth_name"
+        Me.GridColumnmth_name.FieldNameSortGroup = "mth"
+        Me.GridColumnmth_name.Name = "GridColumnmth_name"
+        Me.GridColumnmth_name.Visible = True
+        Me.GridColumnmth_name.VisibleIndex = 0
+        '
+        'GridColumnar_eval_setup_date
+        '
+        Me.GridColumnar_eval_setup_date.Caption = "Evaluation Date"
+        Me.GridColumnar_eval_setup_date.DisplayFormat.FormatString = "dddd, dd MMMM yyyy"
+        Me.GridColumnar_eval_setup_date.DisplayFormat.FormatType = DevExpress.Utils.FormatType.DateTime
+        Me.GridColumnar_eval_setup_date.FieldName = "ar_eval_setup_date"
+        Me.GridColumnar_eval_setup_date.Name = "GridColumnar_eval_setup_date"
+        Me.GridColumnar_eval_setup_date.Visible = True
+        Me.GridColumnar_eval_setup_date.VisibleIndex = 1
         '
         'FormAREvalScheduke
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(743, 423)
+        Me.Controls.Add(Me.GCData)
+        Me.Controls.Add(Me.PanelControl1)
         Me.Name = "FormAREvalScheduke"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "AR Evaluation Schedule"
+        CType(Me.PanelControl1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.PanelControl1.ResumeLayout(False)
+        Me.PanelControl1.PerformLayout()
+        CType(Me.LEYear.Properties, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.GCData, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.GVData, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
+
+    Friend WithEvents PanelControl1 As DevExpress.XtraEditors.PanelControl
+    Friend WithEvents GCData As DevExpress.XtraGrid.GridControl
+    Friend WithEvents GVData As DevExpress.XtraGrid.Views.Grid.GridView
+    Friend WithEvents LEYear As DevExpress.XtraEditors.LookUpEdit
+    Friend WithEvents LabelControl1 As DevExpress.XtraEditors.LabelControl
+    Friend WithEvents BtnView As DevExpress.XtraEditors.SimpleButton
+    Friend WithEvents GridColumnid_ar_eval_setup_date As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GridColumnmth_name As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GridColumnar_eval_setup_date As DevExpress.XtraGrid.Columns.GridColumn
 End Class
---
+++ VolcomInventory/FormAREvalScheduleDet.vb
@@ -0,0 +1,70 @@
+﻿Public Class FormAREvalScheduleDet
+    Public action As String = "-1"
+    Public id As String = "-1"
+    Dim input_again As Boolean = False
+
+    Private Sub BtnClose_Click(sender As Object, e As EventArgs) Handles BtnClose.Click
+        Close()
+    End Sub
+
+    Private Sub FormAREvalScheduleDet_FormClosed(sender As Object, e As FormClosedEventArgs) Handles MyBase.FormClosed
+        Dispose()
+    End Sub
+
+    Private Sub FormAREvalScheduleDet_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        actionLoad()
+    End Sub
+
+    Sub actionLoad()
+        Cursor = Cursors.WaitCursor
+        If action = "ins" Then
+            If Not input_again Then
+                DESchedule.EditValue = getTimeDB()
+            End If
+        Else
+            Dim query As String = "SELECT * FROM tb_ar_eval_setup_date WHERE id_ar_eval_setup_date='" + id + "' "
+            Dim data As DataTable = execute_query(query, -1, True, "", "", "", "")
+            DESchedule.EditValue = data.Rows(0)("ar_eval_setup_date")
+        End If
+        Cursor = Cursors.Default
+    End Sub
+
+    Private Sub BtnAdd_Click(sender As Object, e As EventArgs) Handles BtnAdd.Click
+        Cursor = Cursors.WaitCursor
+        Dim tgl As String = DateTime.Parse(DESchedule.EditValue.ToString).ToString("yyyy-MM-dd")
+        Dim year As String = DateTime.Parse(DESchedule.EditValue.ToString).Year()
+
+        'cek
+        Dim qcek As String = "SELECT * FROM tb_ar_eval_setup_date WHERE ar_eval_setup_date='" + tgl + "' AND id_ar_eval_setup_date!='" + id + "' "
+        Dim dcek As DataTable = execute_query(qcek, -1, True, "", "", "", "")
+        If dcek.Rows.Count > 0 Then
+            stopCustom("This date already input")
+            Cursor = Cursors.Default
+            Exit Sub
+        End If
+
+        If action = "ins" Then
+            Dim query As String = "INSERT INTO tb_ar_eval_setup_date(ar_eval_setup_date) VALUES('" + tgl + "'); SELECT LAST_INSERT_ID(); "
+            id = execute_query(query, 0, True, "", "", "", "")
+            refreshData(year)
+            action = "ins"
+            id = "-1"
+            input_again = True
+            DESchedule.ShowPopup()
+            actionLoad()
+        Else
+            Dim query As String = "UPDATE tb_ar_eval_setup_date SET ar_eval_setup_date='" + tgl + "' WHERE id_ar_eval_setup_date='" + id + "' "
+            execute_non_query(query, True, "", "", "", "")
+            refreshData(year)
+            Close()
+        End If
+        Cursor = Cursors.Default
+    End Sub
+
+    Sub refreshData(ByVal last_year_input As String)
+        FormAREvalScheduke.viewYear()
+        FormAREvalScheduke.LEYear.ItemIndex = FormAREvalScheduke.LEYear.Properties.GetDataSourceRowIndex("year", last_year_input)
+        FormAREvalScheduke.viewData()
+        FormAREvalScheduke.GVData.FocusedRowHandle = find_row(FormAREvalScheduke.GVData, "id_ar_eval_setup_date", id)
+    End Sub
+End Class
