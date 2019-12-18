65f5e87129c4c3e8613a8a214407e13d81aa6305
Satrya Anas <catur.satrya@gmail.com>
2019-12-18 14:51:31 +0800

    prepare main form

--- VolcomInventory/FormDelManifest.Designer.vb
+++ VolcomInventory/FormDelManifest.Designer.vb
@@ -22,32 +22,53 @@ Partial Class FormDelManifest
         Me.XTCDelManifest = New DevExpress.XtraTab.XtraTabControl()
         Me.XTPList = New DevExpress.XtraTab.XtraTabPage()
         Me.XTPDetail = New DevExpress.XtraTab.XtraTabPage()
+        Me.PanelControl1 = New DevExpress.XtraEditors.PanelControl()
+        Me.LabelControl1 = New DevExpress.XtraEditors.LabelControl()
         CType(Me.XTCDelManifest, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.XTCDelManifest.SuspendLayout()
+        CType(Me.PanelControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.PanelControl1.SuspendLayout()
         Me.SuspendLayout()
         '
         'XTCDelManifest
         '
         Me.XTCDelManifest.Dock = System.Windows.Forms.DockStyle.Fill
         Me.XTCDelManifest.HeaderLocation = DevExpress.XtraTab.TabHeaderLocation.Bottom
-        Me.XTCDelManifest.Location = New System.Drawing.Point(0, 0)
+        Me.XTCDelManifest.Location = New System.Drawing.Point(0, 45)
         Me.XTCDelManifest.Name = "XTCDelManifest"
         Me.XTCDelManifest.SelectedTabPage = Me.XTPList
-        Me.XTCDelManifest.Size = New System.Drawing.Size(766, 502)
+        Me.XTCDelManifest.Size = New System.Drawing.Size(766, 457)
         Me.XTCDelManifest.TabIndex = 0
         Me.XTCDelManifest.TabPages.AddRange(New DevExpress.XtraTab.XtraTabPage() {Me.XTPList, Me.XTPDetail})
         '
         'XTPList
         '
         Me.XTPList.Name = "XTPList"
-        Me.XTPList.Size = New System.Drawing.Size(760, 474)
+        Me.XTPList.Size = New System.Drawing.Size(760, 429)
         Me.XTPList.Text = "Created List"
         '
         'XTPDetail
         '
         Me.XTPDetail.Name = "XTPDetail"
-        Me.XTPDetail.Size = New System.Drawing.Size(294, 272)
+        Me.XTPDetail.Size = New System.Drawing.Size(760, 474)
         Me.XTPDetail.Text = "Detail Manifest"
+        '
+        'PanelControl1
+        '
+        Me.PanelControl1.Controls.Add(Me.LabelControl1)
+        Me.PanelControl1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.PanelControl1.Location = New System.Drawing.Point(0, 0)
+        Me.PanelControl1.Name = "PanelControl1"
+        Me.PanelControl1.Size = New System.Drawing.Size(766, 45)
+        Me.PanelControl1.TabIndex = 1
+        '
+        'LabelControl1
+        '
+        Me.LabelControl1.Location = New System.Drawing.Point(143, 25)
+        Me.LabelControl1.Name = "LabelControl1"
+        Me.LabelControl1.Size = New System.Drawing.Size(17, 13)
+        Me.LabelControl1.TabIndex = 0
+        Me.LabelControl1.Text = "3PL"
         '
         'FormDelManifest
         '
@@ -55,11 +76,15 @@ Partial Class FormDelManifest
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(766, 502)
         Me.Controls.Add(Me.XTCDelManifest)
+        Me.Controls.Add(Me.PanelControl1)
         Me.Name = "FormDelManifest"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Outbound Delivery Manifest"
         CType(Me.XTCDelManifest, System.ComponentModel.ISupportInitialize).EndInit()
         Me.XTCDelManifest.ResumeLayout(False)
+        CType(Me.PanelControl1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.PanelControl1.ResumeLayout(False)
+        Me.PanelControl1.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
@@ -67,4 +92,6 @@ Partial Class FormDelManifest
     Friend WithEvents XTCDelManifest As DevExpress.XtraTab.XtraTabControl
     Friend WithEvents XTPList As DevExpress.XtraTab.XtraTabPage
     Friend WithEvents XTPDetail As DevExpress.XtraTab.XtraTabPage
+    Friend WithEvents PanelControl1 As DevExpress.XtraEditors.PanelControl
+    Friend WithEvents LabelControl1 As DevExpress.XtraEditors.LabelControl
 End Class
