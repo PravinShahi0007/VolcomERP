61ffa8777226ad1e596aec4d33219ee3fac47cec
Satrya Anas <catur.satrya@gmail.com>
2019-12-20 11:37:42 +0800

    - confirm - reset propose - cancel propose

--- VolcomInventory/FormDelayPaymentDet.vb
+++ VolcomInventory/FormDelayPaymentDet.vb
@@ -322,7 +322,7 @@
     End Sub
 
     Private Sub BtnResetPropose_Click(sender As Object, e As EventArgs) Handles BtnResetPropose.Click
-        Dim query As String = "SELECT * FROM tb_report_mark rm WHERE rm.report_mark_type=" + rmt + " AND rm.id_report_status=3
+        Dim query As String = "SELECT * FROM tb_report_mark rm WHERE rm.report_mark_type=" + rmt + " AND rm.id_report_status=2
         AND rm.is_requisite=2 AND rm.id_mark=2 AND rm.id_report=" + id + " "
         Dim data As DataTable = execute_query(query, -1, True, "", "", "", "")
         If data.Rows.Count = 0 Then
