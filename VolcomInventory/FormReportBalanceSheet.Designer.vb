2c563f53aada5f42e0e50c068f9893f7dc1e0d52
Putu Septian Primadewa <iansaviour@gmail.com>
2020-01-20 10:22:12 +0800

    fix PO + profit and loss

--- VolcomInventory/FormReportBalanceSheet.Designer.vb
+++ VolcomInventory/FormReportBalanceSheet.Designer.vb
@@ -45,21 +45,64 @@ Partial Class FormReportBalanceSheet
         Me.TCLIdAccParent = New DevExpress.XtraTreeList.Columns.TreeListColumn()
         Me.TCLedIDAllChild = New DevExpress.XtraTreeList.Columns.TreeListColumn()
         Me.XTPBalanceSheet = New DevExpress.XtraTab.XtraTabPage()
+        Me.DateEdit2 = New DevExpress.XtraEditors.DateEdit()
+        Me.LabelControl1 = New DevExpress.XtraEditors.LabelControl()
+        Me.BView = New DevExpress.XtraEditors.SimpleButton()
+        Me.LabelControl2 = New DevExpress.XtraEditors.LabelControl()
+        Me.SLEUnit = New DevExpress.XtraEditors.SearchLookUpEdit()
+        Me.SearchLookUpEdit1View = New DevExpress.XtraGrid.Views.Grid.GridView()
+        Me.XTPProfitAndLoss = New DevExpress.XtraTab.XtraTabPage()
+        Me.XtraTabControl1 = New DevExpress.XtraTab.XtraTabControl()
+        Me.XTPBSLedger = New DevExpress.XtraTab.XtraTabPage()
+        Me.XTPBSReport = New DevExpress.XtraTab.XtraTabPage()
+        Me.SimpleButton1 = New DevExpress.XtraEditors.SimpleButton()
+        Me.XTCProfitAndLoss = New DevExpress.XtraTab.XtraTabControl()
+        Me.XTPPATreeView = New DevExpress.XtraTab.XtraTabPage()
+        Me.XTPPLReportView = New DevExpress.XtraTab.XtraTabPage()
+        Me.TLProfitAndLoss = New DevExpress.XtraTreeList.TreeList()
+        Me.TCPLIdAcc = New DevExpress.XtraTreeList.Columns.TreeListColumn()
+        Me.TCPLIdAccParent = New DevExpress.XtraTreeList.Columns.TreeListColumn()
+        Me.TCPLAccount = New DevExpress.XtraTreeList.Columns.TreeListColumn()
+        Me.TCPLDescription = New DevExpress.XtraTreeList.Columns.TreeListColumn()
+        Me.TCPLPrevMonth = New DevExpress.XtraTreeList.Columns.TreeListColumn()
+        Me.TCPLThisMonth = New DevExpress.XtraTreeList.Columns.TreeListColumn()
+        Me.TCPLIdAllChild = New DevExpress.XtraTreeList.Columns.TreeListColumn()
+        Me.treeListBand7 = New DevExpress.XtraTreeList.Columns.TreeListBand()
+        Me.treeListBand8 = New DevExpress.XtraTreeList.Columns.TreeListBand()
         CType(Me.PanelControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.PanelControl1.SuspendLayout()
         CType(Me.TLBalanceSheet, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.XTCBalanceSheet, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.XTCBalanceSheet.SuspendLayout()
         Me.XTPGeneralLedger.SuspendLayout()
         CType(Me.TLLedger, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.XTPBalanceSheet.SuspendLayout()
+        CType(Me.DateEdit2.Properties.CalendarTimeProperties, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DateEdit2.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.SLEUnit.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.SearchLookUpEdit1View, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.XTPProfitAndLoss.SuspendLayout()
+        CType(Me.XtraTabControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.XtraTabControl1.SuspendLayout()
+        Me.XTPBSLedger.SuspendLayout()
+        CType(Me.XTCProfitAndLoss, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.XTCProfitAndLoss.SuspendLayout()
+        Me.XTPPATreeView.SuspendLayout()
+        CType(Me.TLProfitAndLoss, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'PanelControl1
         '
+        Me.PanelControl1.Controls.Add(Me.SimpleButton1)
+        Me.PanelControl1.Controls.Add(Me.SLEUnit)
+        Me.PanelControl1.Controls.Add(Me.LabelControl2)
+        Me.PanelControl1.Controls.Add(Me.BView)
+        Me.PanelControl1.Controls.Add(Me.LabelControl1)
+        Me.PanelControl1.Controls.Add(Me.DateEdit2)
         Me.PanelControl1.Dock = System.Windows.Forms.DockStyle.Top
         Me.PanelControl1.Location = New System.Drawing.Point(0, 0)
         Me.PanelControl1.Name = "PanelControl1"
-        Me.PanelControl1.Size = New System.Drawing.Size(900, 54)
+        Me.PanelControl1.Size = New System.Drawing.Size(900, 48)
         Me.PanelControl1.TabIndex = 0
         '
         'TLBalanceSheet
@@ -75,7 +118,7 @@ Partial Class FormReportBalanceSheet
         Me.TLBalanceSheet.OptionsFilter.ShowAllValuesInFilterPopup = True
         Me.TLBalanceSheet.OptionsFind.AllowFindPanel = True
         Me.TLBalanceSheet.OptionsView.ShowBandsMode = DevExpress.Utils.DefaultBoolean.[True]
-        Me.TLBalanceSheet.Size = New System.Drawing.Size(894, 438)
+        Me.TLBalanceSheet.Size = New System.Drawing.Size(888, 416)
         Me.TLBalanceSheet.TabIndex = 1
         '
         'treeListBand3
@@ -166,18 +209,18 @@ Partial Class FormReportBalanceSheet
         '
         Me.XTCBalanceSheet.Dock = System.Windows.Forms.DockStyle.Fill
         Me.XTCBalanceSheet.HeaderLocation = DevExpress.XtraTab.TabHeaderLocation.Bottom
-        Me.XTCBalanceSheet.Location = New System.Drawing.Point(0, 54)
+        Me.XTCBalanceSheet.Location = New System.Drawing.Point(0, 48)
         Me.XTCBalanceSheet.Name = "XTCBalanceSheet"
         Me.XTCBalanceSheet.SelectedTabPage = Me.XTPGeneralLedger
-        Me.XTCBalanceSheet.Size = New System.Drawing.Size(900, 466)
+        Me.XTCBalanceSheet.Size = New System.Drawing.Size(900, 472)
         Me.XTCBalanceSheet.TabIndex = 2
-        Me.XTCBalanceSheet.TabPages.AddRange(New DevExpress.XtraTab.XtraTabPage() {Me.XTPGeneralLedger, Me.XTPBalanceSheet})
+        Me.XTCBalanceSheet.TabPages.AddRange(New DevExpress.XtraTab.XtraTabPage() {Me.XTPGeneralLedger, Me.XTPBalanceSheet, Me.XTPProfitAndLoss})
         '
         'XTPGeneralLedger
         '
         Me.XTPGeneralLedger.Controls.Add(Me.TLLedger)
         Me.XTPGeneralLedger.Name = "XTPGeneralLedger"
-        Me.XTPGeneralLedger.Size = New System.Drawing.Size(894, 438)
+        Me.XTPGeneralLedger.Size = New System.Drawing.Size(894, 444)
         Me.XTPGeneralLedger.Text = "Ledger"
         '
         'TLLedger
@@ -189,7 +232,7 @@ Partial Class FormReportBalanceSheet
         Me.TLLedger.Name = "TLLedger"
         Me.TLLedger.OptionsBehavior.EnableFiltering = True
         Me.TLLedger.OptionsFind.AllowFindPanel = True
-        Me.TLLedger.Size = New System.Drawing.Size(894, 438)
+        Me.TLLedger.Size = New System.Drawing.Size(894, 444)
         Me.TLLedger.TabIndex = 0
         '
         'treeListBand5
@@ -268,10 +311,203 @@ Partial Class FormReportBalanceSheet
         '
         'XTPBalanceSheet
         '
-        Me.XTPBalanceSheet.Controls.Add(Me.TLBalanceSheet)
+        Me.XTPBalanceSheet.Controls.Add(Me.XtraTabControl1)
         Me.XTPBalanceSheet.Name = "XTPBalanceSheet"
-        Me.XTPBalanceSheet.Size = New System.Drawing.Size(894, 438)
+        Me.XTPBalanceSheet.Size = New System.Drawing.Size(894, 444)
         Me.XTPBalanceSheet.Text = "Balance Sheet"
+        '
+        'DateEdit2
+        '
+        Me.DateEdit2.EditValue = Nothing
+        Me.DateEdit2.Location = New System.Drawing.Point(265, 16)
+        Me.DateEdit2.Name = "DateEdit2"
+        Me.DateEdit2.Properties.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() {New DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)})
+        Me.DateEdit2.Properties.CalendarTimeProperties.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() {New DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)})
+        Me.DateEdit2.Size = New System.Drawing.Size(173, 20)
+        Me.DateEdit2.TabIndex = 1
+        '
+        'LabelControl1
+        '
+        Me.LabelControl1.Location = New System.Drawing.Point(236, 19)
+        Me.LabelControl1.Name = "LabelControl1"
+        Me.LabelControl1.Size = New System.Drawing.Size(23, 13)
+        Me.LabelControl1.TabIndex = 2
+        Me.LabelControl1.Text = "Date"
+        '
+        'BView
+        '
+        Me.BView.Location = New System.Drawing.Point(444, 14)
+        Me.BView.Name = "BView"
+        Me.BView.Size = New System.Drawing.Size(50, 23)
+        Me.BView.TabIndex = 3
+        Me.BView.Text = "view"
+        '
+        'LabelControl2
+        '
+        Me.LabelControl2.Location = New System.Drawing.Point(12, 19)
+        Me.LabelControl2.Name = "LabelControl2"
+        Me.LabelControl2.Size = New System.Drawing.Size(19, 13)
+        Me.LabelControl2.TabIndex = 4
+        Me.LabelControl2.Text = "Unit"
+        '
+        'SLEUnit
+        '
+        Me.SLEUnit.Location = New System.Drawing.Point(37, 16)
+        Me.SLEUnit.Name = "SLEUnit"
+        Me.SLEUnit.Properties.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() {New DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)})
+        Me.SLEUnit.Properties.View = Me.SearchLookUpEdit1View
+        Me.SLEUnit.Size = New System.Drawing.Size(193, 20)
+        Me.SLEUnit.TabIndex = 5
+        '
+        'SearchLookUpEdit1View
+        '
+        Me.SearchLookUpEdit1View.FocusRectStyle = DevExpress.XtraGrid.Views.Grid.DrawFocusRectStyle.RowFocus
+        Me.SearchLookUpEdit1View.Name = "SearchLookUpEdit1View"
+        Me.SearchLookUpEdit1View.OptionsSelection.EnableAppearanceFocusedCell = False
+        Me.SearchLookUpEdit1View.OptionsView.ShowGroupPanel = False
+        '
+        'XTPProfitAndLoss
+        '
+        Me.XTPProfitAndLoss.Controls.Add(Me.XTCProfitAndLoss)
+        Me.XTPProfitAndLoss.Name = "XTPProfitAndLoss"
+        Me.XTPProfitAndLoss.Size = New System.Drawing.Size(894, 444)
+        Me.XTPProfitAndLoss.Text = "Profit And Loss"
+        '
+        'XtraTabControl1
+        '
+        Me.XtraTabControl1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.XtraTabControl1.HeaderLocation = DevExpress.XtraTab.TabHeaderLocation.Bottom
+        Me.XtraTabControl1.Location = New System.Drawing.Point(0, 0)
+        Me.XtraTabControl1.Name = "XtraTabControl1"
+        Me.XtraTabControl1.SelectedTabPage = Me.XTPBSLedger
+        Me.XtraTabControl1.Size = New System.Drawing.Size(894, 444)
+        Me.XtraTabControl1.TabIndex = 2
+        Me.XtraTabControl1.TabPages.AddRange(New DevExpress.XtraTab.XtraTabPage() {Me.XTPBSLedger, Me.XTPBSReport})
+        '
+        'XTPBSLedger
+        '
+        Me.XTPBSLedger.Controls.Add(Me.TLBalanceSheet)
+        Me.XTPBSLedger.Name = "XTPBSLedger"
+        Me.XTPBSLedger.Size = New System.Drawing.Size(888, 416)
+        Me.XTPBSLedger.Text = "Tree View"
+        '
+        'XTPBSReport
+        '
+        Me.XTPBSReport.Name = "XTPBSReport"
+        Me.XTPBSReport.Size = New System.Drawing.Size(888, 416)
+        Me.XTPBSReport.Text = "Report View"
+        '
+        'SimpleButton1
+        '
+        Me.SimpleButton1.Location = New System.Drawing.Point(500, 14)
+        Me.SimpleButton1.Name = "SimpleButton1"
+        Me.SimpleButton1.Size = New System.Drawing.Size(50, 23)
+        Me.SimpleButton1.TabIndex = 6
+        Me.SimpleButton1.Text = "print"
+        '
+        'XTCProfitAndLoss
+        '
+        Me.XTCProfitAndLoss.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.XTCProfitAndLoss.HeaderLocation = DevExpress.XtraTab.TabHeaderLocation.Bottom
+        Me.XTCProfitAndLoss.Location = New System.Drawing.Point(0, 0)
+        Me.XTCProfitAndLoss.Name = "XTCProfitAndLoss"
+        Me.XTCProfitAndLoss.SelectedTabPage = Me.XTPPATreeView
+        Me.XTCProfitAndLoss.Size = New System.Drawing.Size(894, 444)
+        Me.XTCProfitAndLoss.TabIndex = 0
+        Me.XTCProfitAndLoss.TabPages.AddRange(New DevExpress.XtraTab.XtraTabPage() {Me.XTPPATreeView, Me.XTPPLReportView})
+        '
+        'XTPPATreeView
+        '
+        Me.XTPPATreeView.Controls.Add(Me.TLProfitAndLoss)
+        Me.XTPPATreeView.Name = "XTPPATreeView"
+        Me.XTPPATreeView.Size = New System.Drawing.Size(888, 416)
+        Me.XTPPATreeView.Text = "Tree View"
+        '
+        'XTPPLReportView
+        '
+        Me.XTPPLReportView.Name = "XTPPLReportView"
+        Me.XTPPLReportView.Size = New System.Drawing.Size(294, 272)
+        Me.XTPPLReportView.Text = "Report View"
+        '
+        'TLProfitAndLoss
+        '
+        Me.TLProfitAndLoss.Bands.AddRange(New DevExpress.XtraTreeList.Columns.TreeListBand() {Me.treeListBand7, Me.treeListBand8})
+        Me.TLProfitAndLoss.Columns.AddRange(New DevExpress.XtraTreeList.Columns.TreeListColumn() {Me.TCPLIdAcc, Me.TCPLIdAccParent, Me.TCPLAccount, Me.TCPLDescription, Me.TCPLPrevMonth, Me.TCPLThisMonth, Me.TCPLIdAllChild})
+        Me.TLProfitAndLoss.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TLProfitAndLoss.Location = New System.Drawing.Point(0, 0)
+        Me.TLProfitAndLoss.Name = "TLProfitAndLoss"
+        Me.TLProfitAndLoss.Size = New System.Drawing.Size(888, 416)
+        Me.TLProfitAndLoss.TabIndex = 0
+        '
+        'TCPLIdAcc
+        '
+        Me.TCPLIdAcc.Caption = "ID Acc"
+        Me.TCPLIdAcc.FieldName = "id_acc"
+        Me.TCPLIdAcc.Name = "TCPLIdAcc"
+        Me.TCPLIdAcc.Visible = True
+        Me.TCPLIdAcc.VisibleIndex = 0
+        '
+        'TCPLIdAccParent
+        '
+        Me.TCPLIdAccParent.Caption = "ID Acc Parent"
+        Me.TCPLIdAccParent.FieldName = "id_acc_parent"
+        Me.TCPLIdAccParent.Name = "TCPLIdAccParent"
+        Me.TCPLIdAccParent.Visible = True
+        Me.TCPLIdAccParent.VisibleIndex = 1
+        '
+        'TCPLAccount
+        '
+        Me.TCPLAccount.Caption = "Account"
+        Me.TCPLAccount.FieldName = "coa_name"
+        Me.TCPLAccount.Name = "TCPLAccount"
+        Me.TCPLAccount.Visible = True
+        Me.TCPLAccount.VisibleIndex = 0
+        '
+        'TCPLDescription
+        '
+        Me.TCPLDescription.Caption = "Description"
+        Me.TCPLDescription.FieldName = "coa_description"
+        Me.TCPLDescription.Name = "TCPLDescription"
+        Me.TCPLDescription.Visible = True
+        Me.TCPLDescription.VisibleIndex = 1
+        '
+        'TCPLPrevMonth
+        '
+        Me.TCPLPrevMonth.Caption = "Previous Month"
+        Me.TCPLPrevMonth.FieldName = "prev_month"
+        Me.TCPLPrevMonth.Name = "TCPLPrevMonth"
+        Me.TCPLPrevMonth.Visible = True
+        Me.TCPLPrevMonth.VisibleIndex = 2
+        '
+        'TCPLThisMonth
+        '
+        Me.TCPLThisMonth.Caption = "This Month"
+        Me.TCPLThisMonth.FieldName = "this_month"
+        Me.TCPLThisMonth.Name = "TCPLThisMonth"
+        Me.TCPLThisMonth.Visible = True
+        Me.TCPLThisMonth.VisibleIndex = 3
+        '
+        'TCPLIdAllChild
+        '
+        Me.TCPLIdAllChild.Caption = "ID All Child"
+        Me.TCPLIdAllChild.FieldName = "id_all_child"
+        Me.TCPLIdAllChild.Name = "TCPLIdAllChild"
+        Me.TCPLIdAllChild.Visible = True
+        Me.TCPLIdAllChild.VisibleIndex = 6
+        '
+        'treeListBand7
+        '
+        Me.treeListBand7.Caption = "Account"
+        Me.treeListBand7.Columns.Add(Me.TCPLAccount)
+        Me.treeListBand7.Columns.Add(Me.TCPLDescription)
+        Me.treeListBand7.Name = "treeListBand7"
+        '
+        'treeListBand8
+        '
+        Me.treeListBand8.Caption = "Amount"
+        Me.treeListBand8.Columns.Add(Me.TCPLPrevMonth)
+        Me.treeListBand8.Columns.Add(Me.TCPLThisMonth)
+        Me.treeListBand8.Name = "treeListBand8"
         '
         'FormReportBalanceSheet
         '
@@ -285,14 +521,28 @@ Partial Class FormReportBalanceSheet
         Me.MinimizeBox = False
         Me.Name = "FormReportBalanceSheet"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
-        Me.Text = "Balance Sheet"
+        Me.Text = "Accounting Report"
         CType(Me.PanelControl1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.PanelControl1.ResumeLayout(False)
+        Me.PanelControl1.PerformLayout()
         CType(Me.TLBalanceSheet, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.XTCBalanceSheet, System.ComponentModel.ISupportInitialize).EndInit()
         Me.XTCBalanceSheet.ResumeLayout(False)
         Me.XTPGeneralLedger.ResumeLayout(False)
         CType(Me.TLLedger, System.ComponentModel.ISupportInitialize).EndInit()
         Me.XTPBalanceSheet.ResumeLayout(False)
+        CType(Me.DateEdit2.Properties.CalendarTimeProperties, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DateEdit2.Properties, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.SLEUnit.Properties, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.SearchLookUpEdit1View, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.XTPProfitAndLoss.ResumeLayout(False)
+        CType(Me.XtraTabControl1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.XtraTabControl1.ResumeLayout(False)
+        Me.XTPBSLedger.ResumeLayout(False)
+        CType(Me.XTCProfitAndLoss, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.XTCProfitAndLoss.ResumeLayout(False)
+        Me.XTPPATreeView.ResumeLayout(False)
+        CType(Me.TLProfitAndLoss, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
@@ -323,4 +573,28 @@ Partial Class FormReportBalanceSheet
     Friend WithEvents TCLedIDAllChild As DevExpress.XtraTreeList.Columns.TreeListColumn
     Friend WithEvents treeListBand3 As DevExpress.XtraTreeList.Columns.TreeListBand
     Friend WithEvents treeListBand4 As DevExpress.XtraTreeList.Columns.TreeListBand
+    Friend WithEvents BView As DevExpress.XtraEditors.SimpleButton
+    Friend WithEvents LabelControl1 As DevExpress.XtraEditors.LabelControl
+    Friend WithEvents DateEdit2 As DevExpress.XtraEditors.DateEdit
+    Friend WithEvents LabelControl2 As DevExpress.XtraEditors.LabelControl
+    Friend WithEvents SLEUnit As DevExpress.XtraEditors.SearchLookUpEdit
+    Friend WithEvents SearchLookUpEdit1View As DevExpress.XtraGrid.Views.Grid.GridView
+    Friend WithEvents XTPProfitAndLoss As DevExpress.XtraTab.XtraTabPage
+    Friend WithEvents XtraTabControl1 As DevExpress.XtraTab.XtraTabControl
+    Friend WithEvents XTPBSLedger As DevExpress.XtraTab.XtraTabPage
+    Friend WithEvents XTPBSReport As DevExpress.XtraTab.XtraTabPage
+    Friend WithEvents SimpleButton1 As DevExpress.XtraEditors.SimpleButton
+    Friend WithEvents XTCProfitAndLoss As DevExpress.XtraTab.XtraTabControl
+    Friend WithEvents XTPPATreeView As DevExpress.XtraTab.XtraTabPage
+    Friend WithEvents XTPPLReportView As DevExpress.XtraTab.XtraTabPage
+    Friend WithEvents TLProfitAndLoss As DevExpress.XtraTreeList.TreeList
+    Friend WithEvents treeListBand7 As DevExpress.XtraTreeList.Columns.TreeListBand
+    Friend WithEvents TCPLAccount As DevExpress.XtraTreeList.Columns.TreeListColumn
+    Friend WithEvents TCPLDescription As DevExpress.XtraTreeList.Columns.TreeListColumn
+    Friend WithEvents treeListBand8 As DevExpress.XtraTreeList.Columns.TreeListBand
+    Friend WithEvents TCPLPrevMonth As DevExpress.XtraTreeList.Columns.TreeListColumn
+    Friend WithEvents TCPLThisMonth As DevExpress.XtraTreeList.Columns.TreeListColumn
+    Friend WithEvents TCPLIdAcc As DevExpress.XtraTreeList.Columns.TreeListColumn
+    Friend WithEvents TCPLIdAccParent As DevExpress.XtraTreeList.Columns.TreeListColumn
+    Friend WithEvents TCPLIdAllChild As DevExpress.XtraTreeList.Columns.TreeListColumn
 End Class
