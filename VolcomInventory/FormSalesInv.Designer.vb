daec99edfe92f8a169c57ff4cdb9d06bcd3d24a9
Satrya Anas <catur.satrya@gmail.com>
2020-01-29 15:33:51 +0800

    band general :1--

--- VolcomInventory/FormSalesInv.Designer.vb
+++ VolcomInventory/FormSalesInv.Designer.vb
@@ -24,7 +24,6 @@ Partial Class FormSalesInv
         Me.XTPProduct = New DevExpress.XtraTab.XtraTabPage()
         Me.GCByProduct = New DevExpress.XtraGrid.GridControl()
         Me.GVByProduct = New DevExpress.XtraGrid.Views.BandedGrid.BandedGridView()
-        Me.GridBand1 = New DevExpress.XtraGrid.Views.BandedGrid.GridBand()
         Me.PanelControl1 = New DevExpress.XtraEditors.PanelControl()
         Me.BtnExportToXLSRec = New DevExpress.XtraEditors.SimpleButton()
         Me.DEUntil = New DevExpress.XtraEditors.DateEdit()
@@ -33,6 +32,21 @@ Partial Class FormSalesInv
         Me.LabelControl2 = New DevExpress.XtraEditors.LabelControl()
         Me.LabelControl3 = New DevExpress.XtraEditors.LabelControl()
         Me.XTPOutlet = New DevExpress.XtraTab.XtraTabPage()
+        Me.BandedGridColumnid_design = New DevExpress.XtraGrid.Views.BandedGrid.BandedGridColumn()
+        Me.BandedGridColumncode = New DevExpress.XtraGrid.Views.BandedGrid.BandedGridColumn()
+        Me.BandedGridColumnname = New DevExpress.XtraGrid.Views.BandedGrid.BandedGridColumn()
+        Me.BandedGridColumnid_design_cat = New DevExpress.XtraGrid.Views.BandedGrid.BandedGridColumn()
+        Me.BandedGridColumndesign_cat = New DevExpress.XtraGrid.Views.BandedGrid.BandedGridColumn()
+        Me.BandedGridColumnstt = New DevExpress.XtraGrid.Views.BandedGrid.BandedGridColumn()
+        Me.BandedGridColumnid_class = New DevExpress.XtraGrid.Views.BandedGrid.BandedGridColumn()
+        Me.BandedGridColumnclass = New DevExpress.XtraGrid.Views.BandedGrid.BandedGridColumn()
+        Me.BandedGridColumnid_subkat = New DevExpress.XtraGrid.Views.BandedGrid.BandedGridColumn()
+        Me.BandedGridColumnsubkat = New DevExpress.XtraGrid.Views.BandedGrid.BandedGridColumn()
+        Me.BandedGridColumn1 = New DevExpress.XtraGrid.Views.BandedGrid.BandedGridColumn()
+        Me.BandedGridColumnkat = New DevExpress.XtraGrid.Views.BandedGrid.BandedGridColumn()
+        Me.BandedGridColumnsize_type = New DevExpress.XtraGrid.Views.BandedGrid.BandedGridColumn()
+        Me.BandedGridColumnage = New DevExpress.XtraGrid.Views.BandedGrid.BandedGridColumn()
+        Me.GridBand1 = New DevExpress.XtraGrid.Views.BandedGrid.GridBand()
         CType(Me.XTCSalesInv, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.XTCSalesInv.SuspendLayout()
         Me.XTPProduct.SuspendLayout()
@@ -78,21 +92,18 @@ Partial Class FormSalesInv
         'GVByProduct
         '
         Me.GVByProduct.Bands.AddRange(New DevExpress.XtraGrid.Views.BandedGrid.GridBand() {Me.GridBand1})
+        Me.GVByProduct.Columns.AddRange(New DevExpress.XtraGrid.Views.BandedGrid.BandedGridColumn() {Me.BandedGridColumnid_design, Me.BandedGridColumncode, Me.BandedGridColumnname, Me.BandedGridColumnid_design_cat, Me.BandedGridColumndesign_cat, Me.BandedGridColumnstt, Me.BandedGridColumnid_class, Me.BandedGridColumnclass, Me.BandedGridColumnid_subkat, Me.BandedGridColumnsubkat, Me.BandedGridColumn1, Me.BandedGridColumnkat, Me.BandedGridColumnsize_type, Me.BandedGridColumnage})
         Me.GVByProduct.GridControl = Me.GCByProduct
         Me.GVByProduct.Name = "GVByProduct"
         Me.GVByProduct.OptionsBehavior.AutoExpandAllGroups = True
         Me.GVByProduct.OptionsBehavior.ReadOnly = True
         Me.GVByProduct.OptionsFind.AlwaysVisible = True
+        Me.GVByProduct.OptionsPrint.AllowMultilineHeaders = True
         Me.GVByProduct.OptionsView.ColumnAutoWidth = False
+        Me.GVByProduct.OptionsView.ColumnHeaderAutoHeight = DevExpress.Utils.DefaultBoolean.[True]
         Me.GVByProduct.OptionsView.GroupFooterShowMode = DevExpress.XtraGrid.Views.Grid.GroupFooterShowMode.VisibleAlways
         Me.GVByProduct.OptionsView.ShowFooter = True
         Me.GVByProduct.OptionsView.ShowGroupPanel = False
-        '
-        'GridBand1
-        '
-        Me.GridBand1.Caption = "GridBand1"
-        Me.GridBand1.Name = "GridBand1"
-        Me.GridBand1.VisibleIndex = 0
         '
         'PanelControl1
         '
@@ -178,6 +189,124 @@ Partial Class FormSalesInv
         Me.XTPOutlet.Size = New System.Drawing.Size(781, 457)
         Me.XTPOutlet.Text = "View By Product && Account"
         '
+        'BandedGridColumnid_design
+        '
+        Me.BandedGridColumnid_design.Caption = "id_design"
+        Me.BandedGridColumnid_design.FieldName = "id_design"
+        Me.BandedGridColumnid_design.Name = "BandedGridColumnid_design"
+        '
+        'BandedGridColumncode
+        '
+        Me.BandedGridColumncode.Caption = "Code"
+        Me.BandedGridColumncode.FieldName = "code"
+        Me.BandedGridColumncode.Name = "BandedGridColumncode"
+        Me.BandedGridColumncode.Visible = True
+        Me.BandedGridColumncode.Width = 67
+        '
+        'BandedGridColumnname
+        '
+        Me.BandedGridColumnname.Caption = "Description"
+        Me.BandedGridColumnname.FieldName = "name"
+        Me.BandedGridColumnname.Name = "BandedGridColumnname"
+        Me.BandedGridColumnname.Visible = True
+        Me.BandedGridColumnname.Width = 134
+        '
+        'BandedGridColumnid_design_cat
+        '
+        Me.BandedGridColumnid_design_cat.Caption = "id_design_cat"
+        Me.BandedGridColumnid_design_cat.FieldName = "id_design_cat"
+        Me.BandedGridColumnid_design_cat.Name = "BandedGridColumnid_design_cat"
+        '
+        'BandedGridColumndesign_cat
+        '
+        Me.BandedGridColumndesign_cat.Caption = "design_cat"
+        Me.BandedGridColumndesign_cat.FieldName = "design_cat"
+        Me.BandedGridColumndesign_cat.Name = "BandedGridColumndesign_cat"
+        '
+        'BandedGridColumnstt
+        '
+        Me.BandedGridColumnstt.Caption = "Status"
+        Me.BandedGridColumnstt.FieldName = "stt"
+        Me.BandedGridColumnstt.Name = "BandedGridColumnstt"
+        Me.BandedGridColumnstt.Visible = True
+        Me.BandedGridColumnstt.Width = 44
+        '
+        'BandedGridColumnid_class
+        '
+        Me.BandedGridColumnid_class.Caption = "id_class"
+        Me.BandedGridColumnid_class.FieldName = "id_class"
+        Me.BandedGridColumnid_class.Name = "BandedGridColumnid_class"
+        '
+        'BandedGridColumnclass
+        '
+        Me.BandedGridColumnclass.Caption = "Class"
+        Me.BandedGridColumnclass.FieldName = "class"
+        Me.BandedGridColumnclass.Name = "BandedGridColumnclass"
+        Me.BandedGridColumnclass.Visible = True
+        Me.BandedGridColumnclass.Width = 42
+        '
+        'BandedGridColumnid_subkat
+        '
+        Me.BandedGridColumnid_subkat.Caption = "id_subkat"
+        Me.BandedGridColumnid_subkat.FieldName = "id_subkat"
+        Me.BandedGridColumnid_subkat.Name = "BandedGridColumnid_subkat"
+        '
+        'BandedGridColumnsubkat
+        '
+        Me.BandedGridColumnsubkat.Caption = "Subcategory"
+        Me.BandedGridColumnsubkat.FieldName = "subkat"
+        Me.BandedGridColumnsubkat.Name = "BandedGridColumnsubkat"
+        '
+        'BandedGridColumn1
+        '
+        Me.BandedGridColumn1.Caption = "id_kat"
+        Me.BandedGridColumn1.FieldName = "id_kat"
+        Me.BandedGridColumn1.Name = "BandedGridColumn1"
+        '
+        'BandedGridColumnkat
+        '
+        Me.BandedGridColumnkat.Caption = "Category"
+        Me.BandedGridColumnkat.FieldName = "kat"
+        Me.BandedGridColumnkat.Name = "BandedGridColumnkat"
+        '
+        'BandedGridColumnsize_type
+        '
+        Me.BandedGridColumnsize_type.Caption = "Sizetype"
+        Me.BandedGridColumnsize_type.FieldName = "size_type"
+        Me.BandedGridColumnsize_type.Name = "BandedGridColumnsize_type"
+        Me.BandedGridColumnsize_type.Visible = True
+        Me.BandedGridColumnsize_type.Width = 73
+        '
+        'BandedGridColumnage
+        '
+        Me.BandedGridColumnage.Caption = "Age"
+        Me.BandedGridColumnage.DisplayFormat.FormatString = "N0"
+        Me.BandedGridColumnage.DisplayFormat.FormatType = DevExpress.Utils.FormatType.Numeric
+        Me.BandedGridColumnage.FieldName = "age"
+        Me.BandedGridColumnage.Name = "BandedGridColumnage"
+        Me.BandedGridColumnage.Visible = True
+        Me.BandedGridColumnage.Width = 41
+        '
+        'GridBand1
+        '
+        Me.GridBand1.Columns.Add(Me.BandedGridColumnid_design)
+        Me.GridBand1.Columns.Add(Me.BandedGridColumncode)
+        Me.GridBand1.Columns.Add(Me.BandedGridColumnname)
+        Me.GridBand1.Columns.Add(Me.BandedGridColumnid_class)
+        Me.GridBand1.Columns.Add(Me.BandedGridColumnclass)
+        Me.GridBand1.Columns.Add(Me.BandedGridColumn1)
+        Me.GridBand1.Columns.Add(Me.BandedGridColumnkat)
+        Me.GridBand1.Columns.Add(Me.BandedGridColumnid_subkat)
+        Me.GridBand1.Columns.Add(Me.BandedGridColumnsubkat)
+        Me.GridBand1.Columns.Add(Me.BandedGridColumnid_design_cat)
+        Me.GridBand1.Columns.Add(Me.BandedGridColumndesign_cat)
+        Me.GridBand1.Columns.Add(Me.BandedGridColumnstt)
+        Me.GridBand1.Columns.Add(Me.BandedGridColumnage)
+        Me.GridBand1.Columns.Add(Me.BandedGridColumnsize_type)
+        Me.GridBand1.Name = "GridBand1"
+        Me.GridBand1.VisibleIndex = 0
+        Me.GridBand1.Width = 401
+        '
         'FormSalesInv
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
@@ -215,4 +344,18 @@ Partial Class FormSalesInv
     Friend WithEvents GCByProduct As DevExpress.XtraGrid.GridControl
     Friend WithEvents GVByProduct As DevExpress.XtraGrid.Views.BandedGrid.BandedGridView
     Friend WithEvents GridBand1 As DevExpress.XtraGrid.Views.BandedGrid.GridBand
+    Friend WithEvents BandedGridColumnid_design As DevExpress.XtraGrid.Views.BandedGrid.BandedGridColumn
+    Friend WithEvents BandedGridColumncode As DevExpress.XtraGrid.Views.BandedGrid.BandedGridColumn
+    Friend WithEvents BandedGridColumnname As DevExpress.XtraGrid.Views.BandedGrid.BandedGridColumn
+    Friend WithEvents BandedGridColumnid_class As DevExpress.XtraGrid.Views.BandedGrid.BandedGridColumn
+    Friend WithEvents BandedGridColumnclass As DevExpress.XtraGrid.Views.BandedGrid.BandedGridColumn
+    Friend WithEvents BandedGridColumn1 As DevExpress.XtraGrid.Views.BandedGrid.BandedGridColumn
+    Friend WithEvents BandedGridColumnkat As DevExpress.XtraGrid.Views.BandedGrid.BandedGridColumn
+    Friend WithEvents BandedGridColumnid_subkat As DevExpress.XtraGrid.Views.BandedGrid.BandedGridColumn
+    Friend WithEvents BandedGridColumnsubkat As DevExpress.XtraGrid.Views.BandedGrid.BandedGridColumn
+    Friend WithEvents BandedGridColumnid_design_cat As DevExpress.XtraGrid.Views.BandedGrid.BandedGridColumn
+    Friend WithEvents BandedGridColumndesign_cat As DevExpress.XtraGrid.Views.BandedGrid.BandedGridColumn
+    Friend WithEvents BandedGridColumnstt As DevExpress.XtraGrid.Views.BandedGrid.BandedGridColumn
+    Friend WithEvents BandedGridColumnage As DevExpress.XtraGrid.Views.BandedGrid.BandedGridColumn
+    Friend WithEvents BandedGridColumnsize_type As DevExpress.XtraGrid.Views.BandedGrid.BandedGridColumn
 End Class
