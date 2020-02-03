e6c98c860377c362d5079798b51fc36432485455
Agus Friastana <friastana@gmail.com>
2020-02-03 10:48:14 +0800

    uniform credit note store minus qty

--- VolcomInventory/ClassEmpUniExpense.vb
+++ VolcomInventory/ClassEmpUniExpense.vb
@@ -58,28 +58,34 @@
 
     Public Sub completedStock(ByVal id_report_param As String, ByVal report_mark_type_param As String)
         'completedReservedStock 
+        Dim qty As String = "pd.qty"
+
+        If report_mark_type_param = "236" Then
+            qty = "ABS(pd.qty)"
+        End If
+
         Dim query_expense As String = ""
 
         If report_mark_type_param = "132" Then
             query_expense = "
-                SELECT getCompByContact(p.id_comp_contact, 4), 1, pd.id_product, IFNULL(pd.design_cop,0), " + report_mark_type_param + ", " + id_report_param + ", pd.qty, NOW(), '', 2
+                SELECT getCompByContact(p.id_comp_contact, 4), 1, pd.id_product, IFNULL(pd.design_cop,0), " + report_mark_type_param + ", " + id_report_param + ", " + qty + " AS qty, NOW(), '', 2
                 FROM tb_emp_uni_ex p
                 INNER JOIN tb_emp_uni_ex_det pd ON pd.id_emp_uni_ex = p.id_emp_uni_ex
                 INNER JOIN tb_m_product prod ON prod.id_product = pd.id_product
                 INNER JOIN tb_m_design dsg ON dsg.id_design = prod.id_design
-                WHERE p.id_emp_uni_ex=" + id_report_param + " AND pd.qty>0 
+                WHERE p.id_emp_uni_ex=" + id_report_param + " AND " + qty + ">0 
                 UNION ALL
             "
         End If
 
         Dim query As String = "INSERT INTO tb_storage_fg(id_wh_drawer, id_storage_category, id_product, bom_unit_price, report_mark_type, id_report, storage_product_qty, storage_product_datetime, storage_product_notes, id_stock_status)
         " + query_expense + "
-        SELECT getCompByContact(p.id_comp_contact, 4), " + If(report_mark_type_param = "132", "2", "1") + ", pd.id_product, IFNULL(pd.design_cop,0), " + report_mark_type_param + ", " + id_report_param + ", pd.qty, NOW(), '', 1
+        SELECT getCompByContact(p.id_comp_contact, 4), " + If(report_mark_type_param = "132", "2", "1") + ", pd.id_product, IFNULL(pd.design_cop,0), " + report_mark_type_param + ", " + id_report_param + ", " + qty + ", NOW(), '', 1
         FROM tb_emp_uni_ex p
         INNER JOIN tb_emp_uni_ex_det pd ON pd.id_emp_uni_ex = p.id_emp_uni_ex
         INNER JOIN tb_m_product prod ON prod.id_product = pd.id_product
         INNER JOIN tb_m_design dsg ON dsg.id_design = prod.id_design
-        WHERE p.id_emp_uni_ex=" + id_report_param + " AND pd.qty>0 "
+        WHERE p.id_emp_uni_ex=" + id_report_param + " AND " + qty + ">0 "
 
         execute_non_query(query, True, "", "", "", "")
 
@@ -100,7 +106,7 @@
         Dim id_user_prepared As String = du.Rows(0)("id_user").ToString
         Dim report_number As String = du.Rows(0)("report_number").ToString
 
-        Dim check_draft As String = execute_query("SELECT IFNULL((SELECT id_report FROM tb_a_acc_trans_draft WHERE report_mark_type = " + report_mark_type_param + " AND id_report = " + id_report_param + "), 0) AS id_report", 0, True, "", "", "", "")
+        Dim check_draft As String = execute_query("SELECT IFNULL((SELECT id_report FROM tb_a_acc_trans_draft WHERE report_mark_type = " + report_mark_type_param + " AND id_report = " + id_report_param + " LIMIT 1), 0) AS id_report", 0, True, "", "", "", "")
 
         If Not check_draft = "0" Then
             'main journal
--- VolcomInventory/FormEmpUniCreditNoteDet.vb
+++ VolcomInventory/FormEmpUniCreditNoteDet.vb
@@ -170,7 +170,7 @@
             SELECT d.id_emp_uni_ex_det, (d.qty - IFNULL(t_used.used, 0)) AS max_qty
             FROM tb_emp_uni_ex_det AS d
             LEFT JOIN (
-	            SELECT d.id_emp_uni_ex_det_ref, SUM(d.qty) AS used
+	            SELECT d.id_emp_uni_ex_det_ref, SUM(ABS(d.qty)) AS used
 	            FROM tb_emp_uni_ex_det AS d
 	            LEFT JOIN tb_emp_uni_ex AS a ON d.id_emp_uni_ex = a.id_emp_uni_ex
 	            WHERE a.id_report_status <> 5 AND d.id_emp_uni_ex_det_ref IN (" + q_in + ")
@@ -261,7 +261,7 @@
 
                 For i As Integer = 0 To ((GVData.RowCount - 1) - GetGroupRowCount(GVData))
                     If GVData.GetRowCellValue(i, "qty") > 0 Then
-                        qd += "('" + id_emp_uni_ex + "', '" + GVData.GetRowCellValue(i, "id_emp_uni_ex_det").ToString + "', '" + GVData.GetRowCellValue(i, "id_pl_sales_order_del_det").ToString + "', '" + GVData.GetRowCellValue(i, "id_product").ToString + "', '" + decimalSQL(GVData.GetRowCellValue(i, "qty").ToString) + "', '" + decimalSQL(GVData.GetRowCellValue(i, "design_cop").ToString) + "', '" + GVData.GetRowCellValue(i, "remark").ToString + "'), "
+                        qd += "('" + id_emp_uni_ex + "', '" + GVData.GetRowCellValue(i, "id_emp_uni_ex_det").ToString + "', '" + GVData.GetRowCellValue(i, "id_pl_sales_order_del_det").ToString + "', '" + GVData.GetRowCellValue(i, "id_product").ToString + "', '-" + decimalSQL(GVData.GetRowCellValue(i, "qty").ToString) + "', '" + decimalSQL(GVData.GetRowCellValue(i, "design_cop").ToString) + "', '" + GVData.GetRowCellValue(i, "remark").ToString + "'), "
                     End If
                 Next
 
