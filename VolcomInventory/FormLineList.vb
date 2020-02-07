abb34234a9ef449386678d998770b4a2bcf21751
Satrya Anas <catur.satrya@gmail.com>
2020-02-07 10:27:03 +0800

    line list view basic :100:

--- VolcomInventory/FormLineList.vb
+++ VolcomInventory/FormLineList.vb
@@ -21,4 +21,86 @@
         query += "ORDER BY b.range DESC"
         viewSearchLookupQuery(SLESeason, query, "id_season", "season", "id_season")
     End Sub
+
+    Private Sub BtnView_Click(sender As Object, e As EventArgs) Handles BtnView.Click
+        viewData()
+    End Sub
+
+    Sub viewData()
+        FormMain.SplashScreenManager1.ShowWaitForm()
+        Dim id_ss As String = SLESeason.EditValue.ToString
+        Dim query As String = "CALL view_line_list_all_new(" + id_ss + ")"
+        Dim data As DataTable = execute_query(query, -1, True, "", "", "", "")
+        GCData.DataSource = data
+        FormMain.SplashScreenManager1.CloseWaitForm()
+    End Sub
+
+    Private Sub GVData_CustomColumnDisplayText(sender As Object, e As DevExpress.XtraGrid.Views.Base.CustomColumnDisplayTextEventArgs) Handles GVData.CustomColumnDisplayText
+        If e.Column.FieldName = "no" Then
+            Dim view As DevExpress.XtraGrid.Views.Grid.GridView = TryCast(sender, DevExpress.XtraGrid.Views.Grid.GridView)
+            If view.GroupedColumns.Count <> 0 AndAlso Not e.IsForGroupRow Then
+                Dim rowHandle As Integer = view.GetRowHandle(e.ListSourceRowIndex)
+                e.DisplayText = (view.GetRowGroupIndexByRowHandle(rowHandle) + 1).ToString()
+            Else
+                e.DisplayText = (e.ListSourceRowIndex + 1).ToString()
+            End If
+        End If
+    End Sub
+
+    Private ImageDir As String = product_image_path
+    Private Images As Hashtable = New Hashtable()
+    Private Sub GVData_CustomUnboundColumnData(sender As Object, e As DevExpress.XtraGrid.Views.Base.CustomColumnDataEventArgs) Handles GVData.CustomUnboundColumnData
+        If e.Column.FieldName = "img" AndAlso e.IsGetData And CheckImg.EditValue.ToString = "True" Then
+            Images = Nothing
+            Images = New Hashtable()
+            Dim view As DevExpress.XtraGrid.Views.Grid.GridView = TryCast(sender, DevExpress.XtraGrid.Views.Grid.GridView)
+            Dim id As String = CStr(view.GetListSourceRowCellValue(e.ListSourceRowIndex, "id_design"))
+
+            Dim fileName As String = id & ".jpg".ToLower
+
+            If (Not Images.ContainsKey(fileName)) Then
+                Dim img As Image = Nothing
+                Dim resizeImg As Image = Nothing
+
+                Try
+
+                    Dim filePath As String = DevExpress.Utils.FilesHelper.FindingFileName(ImageDir, fileName, False)
+                    img = Image.FromFile(filePath)
+                    resizeImg = img.GetThumbnailImage(100, 100, Nothing, Nothing)
+                Catch
+
+                End Try
+
+                Images.Add(fileName, resizeImg)
+
+            End If
+
+            e.Value = Images(fileName)
+        End If
+    End Sub
+
+    Private Sub CheckImg_CheckedChanged(sender As Object, e As EventArgs) Handles CheckImg.CheckedChanged
+        Cursor = Cursors.WaitCursor
+        Dim val As String = CheckImg.EditValue.ToString
+        If val = "True" Then
+            BandedGridColumnImg.Visible = True
+            BandedGridColumnImg.VisibleIndex = 1
+        Else
+            BandedGridColumnImg.Visible = False
+        End If
+        GCData.RefreshDataSource()
+        GVData.RefreshData()
+        Cursor = Cursors.Default
+    End Sub
+
+    Private Sub CheckEditFreeze_CheckedChanged(sender As Object, e As EventArgs) Handles CheckEditFreeze.CheckedChanged
+        Cursor = Cursors.WaitCursor
+        Dim val As String = CheckEditFreeze.EditValue.ToString
+        If val = "True" Then
+            GridBandFreeze.Fixed = DevExpress.XtraGrid.Columns.FixedStyle.Left
+        Else
+            GridBandFreeze.Fixed = DevExpress.XtraGrid.Columns.FixedStyle.None
+        End If
+        Cursor = Cursors.Default
+    End Sub
 End Class
