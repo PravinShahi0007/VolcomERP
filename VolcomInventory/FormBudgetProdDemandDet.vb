64ef234e710786d0419cc34b48137c6d75e77477
Satrya Anas <catur.satrya@gmail.com>
2020-02-20 14:23:15 +0800

    form detauk

---
+++ VolcomInventory/FormBudgetProdDemandDet.vb
@@ -0,0 +1,8 @@
+﻿Public Class FormBudgetProdDemandDet
+    Public id As String = "-1"
+    Public action As String = "-1"
+
+    Private Sub FormBudgetProdDemandDet_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
