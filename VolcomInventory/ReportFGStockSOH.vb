99074b50edcdf63cb52d1251772420bbcdb59600
Agus Friastana <friastana@gmail.com>
2020-02-24 10:56:36 +0800

    stock soh (by size & code), reserved stock print

---
+++ VolcomInventory/ReportFGStockSOH.vb
@@ -0,0 +1,8 @@
+﻿Public Class ReportFGStockSOH
+    Public Shared dt As DataTable
+
+    Private Sub ReportFGStockQC_BeforePrint(ByVal sender As System.Object, ByVal e As System.Drawing.Printing.PrintEventArgs) Handles MyBase.BeforePrint
+        GCSOH.DataSource = dt
+        GCSOHCode.DataSource = dt
+    End Sub
+End Class
