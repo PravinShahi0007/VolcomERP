09523463d37bb31340d51ed08584f2114437e2c0
Putu Septian Primadewa <iansaviour@gmail.com>
2020-03-04 14:34:48 +0800

    set font print

--- VolcomInventory/FormMasterCargoRateDet.Designer.vb
+++ VolcomInventory/FormMasterCargoRateDet.Designer.vb
@@ -39,14 +39,14 @@ Partial Class FormMasterCargoRateDet
         Me.XTPOutbound = New DevExpress.XtraTab.XtraTabPage()
         Me.GCOutbound = New DevExpress.XtraGrid.GridControl()
         Me.GVOutbound = New DevExpress.XtraGrid.Views.Grid.GridView()
-        Me.PanelControl1 = New DevExpress.XtraEditors.PanelControl()
-        Me.BAdd = New DevExpress.XtraEditors.SimpleButton()
-        Me.LargeImageCollection = New DevExpress.Utils.ImageCollection(Me.components)
         Me.GridColumnIDCargoDetOut = New DevExpress.XtraGrid.Columns.GridColumn()
         Me.GridColumnCargoRateOut = New DevExpress.XtraGrid.Columns.GridColumn()
         Me.GridColumnMinWeightOut = New DevExpress.XtraGrid.Columns.GridColumn()
         Me.GridColumnLeadTimeOut = New DevExpress.XtraGrid.Columns.GridColumn()
         Me.GridColumnDateInputOut = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.PanelControl1 = New DevExpress.XtraEditors.PanelControl()
+        Me.BAdd = New DevExpress.XtraEditors.SimpleButton()
+        Me.LargeImageCollection = New DevExpress.Utils.ImageCollection(Me.components)
         CType(Me.SPC, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SPC.SuspendLayout()
         CType(Me.GCCargo, System.ComponentModel.ISupportInitialize).BeginInit()
@@ -75,7 +75,7 @@ Partial Class FormMasterCargoRateDet
         Me.SPC.Panel2.Controls.Add(Me.XTCInOut)
         Me.SPC.Panel2.Controls.Add(Me.PanelControl1)
         Me.SPC.Panel2.Text = "Panel2"
-        Me.SPC.Size = New System.Drawing.Size(791, 397)
+        Me.SPC.Size = New System.Drawing.Size(791, 499)
         Me.SPC.SplitterPosition = 148
         Me.SPC.TabIndex = 0
         Me.SPC.Text = "SplitContainerControl1"
@@ -127,7 +127,7 @@ Partial Class FormMasterCargoRateDet
         Me.XTCInOut.Location = New System.Drawing.Point(0, 40)
         Me.XTCInOut.Name = "XTCInOut"
         Me.XTCInOut.SelectedTabPage = Me.XTPInbound
-        Me.XTCInOut.Size = New System.Drawing.Size(791, 204)
+        Me.XTCInOut.Size = New System.Drawing.Size(791, 306)
         Me.XTCInOut.TabIndex = 2
         Me.XTCInOut.TabPages.AddRange(New DevExpress.XtraTab.XtraTabPage() {Me.XTPInbound, Me.XTPOutbound})
         '
@@ -135,7 +135,7 @@ Partial Class FormMasterCargoRateDet
         '
         Me.XTPInbound.Controls.Add(Me.GCInbound)
         Me.XTPInbound.Name = "XTPInbound"
-        Me.XTPInbound.Size = New System.Drawing.Size(785, 176)
+        Me.XTPInbound.Size = New System.Drawing.Size(785, 278)
         Me.XTPInbound.Text = "Inbound"
         '
         'GCInbound
@@ -144,7 +144,7 @@ Partial Class FormMasterCargoRateDet
         Me.GCInbound.Location = New System.Drawing.Point(0, 0)
         Me.GCInbound.MainView = Me.GVInbound
         Me.GCInbound.Name = "GCInbound"
-        Me.GCInbound.Size = New System.Drawing.Size(785, 176)
+        Me.GCInbound.Size = New System.Drawing.Size(785, 278)
         Me.GCInbound.TabIndex = 1
         Me.GCInbound.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() {Me.GVInbound})
         '
@@ -197,7 +197,7 @@ Partial Class FormMasterCargoRateDet
         '
         Me.XTPOutbound.Controls.Add(Me.GCOutbound)
         Me.XTPOutbound.Name = "XTPOutbound"
-        Me.XTPOutbound.Size = New System.Drawing.Size(785, 176)
+        Me.XTPOutbound.Size = New System.Drawing.Size(785, 278)
         Me.XTPOutbound.Text = "Outbound"
         '
         'GCOutbound
@@ -206,7 +206,7 @@ Partial Class FormMasterCargoRateDet
         Me.GCOutbound.Location = New System.Drawing.Point(0, 0)
         Me.GCOutbound.MainView = Me.GVOutbound
         Me.GCOutbound.Name = "GCOutbound"
-        Me.GCOutbound.Size = New System.Drawing.Size(785, 176)
+        Me.GCOutbound.Size = New System.Drawing.Size(785, 278)
         Me.GCOutbound.TabIndex = 1
         Me.GCOutbound.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() {Me.GVOutbound})
         '
@@ -216,43 +216,6 @@ Partial Class FormMasterCargoRateDet
         Me.GVOutbound.GridControl = Me.GCOutbound
         Me.GVOutbound.Name = "GVOutbound"
         Me.GVOutbound.OptionsView.ShowGroupPanel = False
-        '
-        'PanelControl1
-        '
-        Me.PanelControl1.Controls.Add(Me.BAdd)
-        Me.PanelControl1.Dock = System.Windows.Forms.DockStyle.Top
-        Me.PanelControl1.Location = New System.Drawing.Point(0, 0)
-        Me.PanelControl1.Name = "PanelControl1"
-        Me.PanelControl1.Size = New System.Drawing.Size(791, 40)
-        Me.PanelControl1.TabIndex = 1
-        '
-        'BAdd
-        '
-        Me.BAdd.Dock = System.Windows.Forms.DockStyle.Right
-        Me.BAdd.ImageIndex = 0
-        Me.BAdd.ImageList = Me.LargeImageCollection
-        Me.BAdd.Location = New System.Drawing.Point(673, 2)
-        Me.BAdd.Name = "BAdd"
-        Me.BAdd.Size = New System.Drawing.Size(116, 36)
-        Me.BAdd.TabIndex = 0
-        Me.BAdd.Text = "Update Rate"
-        '
-        'LargeImageCollection
-        '
-        Me.LargeImageCollection.ImageSize = New System.Drawing.Size(24, 24)
-        Me.LargeImageCollection.ImageStream = CType(resources.GetObject("LargeImageCollection.ImageStream"), DevExpress.Utils.ImageCollectionStreamer)
-        Me.LargeImageCollection.Images.SetKeyName(0, "20_24x24.png")
-        Me.LargeImageCollection.Images.SetKeyName(1, "8_24x24.png")
-        Me.LargeImageCollection.Images.SetKeyName(2, "23_24x24.png")
-        Me.LargeImageCollection.Images.SetKeyName(3, "arrow_refresh.png")
-        Me.LargeImageCollection.Images.SetKeyName(4, "check_mark.png")
-        Me.LargeImageCollection.Images.SetKeyName(5, "gnome_application_exit (1).png")
-        Me.LargeImageCollection.Images.SetKeyName(6, "printer_3.png")
-        Me.LargeImageCollection.Images.SetKeyName(7, "save.png")
-        Me.LargeImageCollection.Images.SetKeyName(8, "31_24x24.png")
-        Me.LargeImageCollection.Images.SetKeyName(9, "18_24x24.png")
-        Me.LargeImageCollection.Images.SetKeyName(10, "attachment-icon.png")
-        Me.LargeImageCollection.Images.SetKeyName(11, "30-Text_32x32.png")
         '
         'GridColumnIDCargoDetOut
         '
@@ -292,11 +255,48 @@ Partial Class FormMasterCargoRateDet
         Me.GridColumnDateInputOut.Visible = True
         Me.GridColumnDateInputOut.VisibleIndex = 3
         '
+        'PanelControl1
+        '
+        Me.PanelControl1.Controls.Add(Me.BAdd)
+        Me.PanelControl1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.PanelControl1.Location = New System.Drawing.Point(0, 0)
+        Me.PanelControl1.Name = "PanelControl1"
+        Me.PanelControl1.Size = New System.Drawing.Size(791, 40)
+        Me.PanelControl1.TabIndex = 1
+        '
+        'BAdd
+        '
+        Me.BAdd.Dock = System.Windows.Forms.DockStyle.Right
+        Me.BAdd.ImageIndex = 0
+        Me.BAdd.ImageList = Me.LargeImageCollection
+        Me.BAdd.Location = New System.Drawing.Point(673, 2)
+        Me.BAdd.Name = "BAdd"
+        Me.BAdd.Size = New System.Drawing.Size(116, 36)
+        Me.BAdd.TabIndex = 0
+        Me.BAdd.Text = "Update Rate"
+        '
+        'LargeImageCollection
+        '
+        Me.LargeImageCollection.ImageSize = New System.Drawing.Size(24, 24)
+        Me.LargeImageCollection.ImageStream = CType(resources.GetObject("LargeImageCollection.ImageStream"), DevExpress.Utils.ImageCollectionStreamer)
+        Me.LargeImageCollection.Images.SetKeyName(0, "20_24x24.png")
+        Me.LargeImageCollection.Images.SetKeyName(1, "8_24x24.png")
+        Me.LargeImageCollection.Images.SetKeyName(2, "23_24x24.png")
+        Me.LargeImageCollection.Images.SetKeyName(3, "arrow_refresh.png")
+        Me.LargeImageCollection.Images.SetKeyName(4, "check_mark.png")
+        Me.LargeImageCollection.Images.SetKeyName(5, "gnome_application_exit (1).png")
+        Me.LargeImageCollection.Images.SetKeyName(6, "printer_3.png")
+        Me.LargeImageCollection.Images.SetKeyName(7, "save.png")
+        Me.LargeImageCollection.Images.SetKeyName(8, "31_24x24.png")
+        Me.LargeImageCollection.Images.SetKeyName(9, "18_24x24.png")
+        Me.LargeImageCollection.Images.SetKeyName(10, "attachment-icon.png")
+        Me.LargeImageCollection.Images.SetKeyName(11, "30-Text_32x32.png")
+        '
         'FormMasterCargoRateDet
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(791, 397)
+        Me.ClientSize = New System.Drawing.Size(791, 499)
         Me.Controls.Add(Me.SPC)
         Me.MaximizeBox = False
         Me.MinimizeBox = False
