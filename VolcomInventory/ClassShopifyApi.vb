327a688a03fda04d86f5f05f50cf0323dcb79111
Satrya Anas <catur.satrya@gmail.com>
2020-04-21 09:07:21 +0800

    save price

--- VolcomInventory/ClassShopifyApi.vb
+++ VolcomInventory/ClassShopifyApi.vb
@@ -217,23 +217,25 @@
 
                     'detail line item
                     Dim qins As String = "INSERT tb_ol_store_order(id, sales_order_ol_shop_number, sales_order_ol_shop_date, customer_name, shipping_name, shipping_address, shipping_phone, 
-                    shipping_city, shipping_post_code, shipping_region, payment_method, tracking_code, ol_store_sku, ol_store_id, sku, sales_order_det_qty) VALUES "
+                    shipping_city, shipping_post_code, shipping_region, payment_method, tracking_code, ol_store_sku, ol_store_id, sku, design_price, sales_order_det_qty) VALUES "
                     Dim ol_store_sku As String = ""
                     Dim ol_store_id As String = ""
                     Dim sku As String = ""
+                    Dim design_price As String = ""
                     Dim sales_order_det_qty As String = ""
                     Dim i As Integer = 0
                     For Each row_item In row("line_items").ToList
-                        ol_store_sku = row_item("variant_id").ToString
+                        ol_store_sku = row_item("sku").ToString
                         ol_store_id = row_item("id").ToString
                         sku = row_item("sku").ToString
-                        sales_order_det_qty = row_item("quantity").ToString
+                        design_price = decimalSQL(row_item("price").ToString)
+                        sales_order_det_qty = decimalSQL(row_item("quantity").ToString)
 
                         If i > 0 Then
                             qins += ","
                         End If
                         qins += "('" + id + "', '" + sales_order_ol_shop_number + "', '" + sales_order_ol_shop_date + "', '" + customer_name + "', '" + shipping_name + "', '" + shipping_address + "', '" + shipping_phone + "', 
-                        '" + shipping_city + "', '" + shipping_post_code + "', '" + shipping_region + "', '" + payment_method + "', '" + tracking_code + "', '" + ol_store_sku + "', '" + ol_store_id + "', '" + sku + "', '" + sales_order_det_qty + "') "
+                        '" + shipping_city + "', '" + shipping_post_code + "', '" + shipping_region + "', '" + payment_method + "', '" + tracking_code + "', '" + ol_store_sku + "', '" + ol_store_id + "', '" + sku + "', '" + design_price + "', '" + sales_order_det_qty + "') "
                         i += 1
                     Next
 
