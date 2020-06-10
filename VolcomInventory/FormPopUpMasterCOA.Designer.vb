44c4cca84cf88381d0b18de8faaf6c93500d6e03
Putu Septian Primadewa <iansaviour@gmail.com>
2020-06-10 10:40:30 +0800

    add popup coa

---
+++ VolcomInventory/FormPopUpMasterCOA.Designer.vb
@@ -0,0 +1,215 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FormPopUpMasterCOA
+    Inherits DevExpress.XtraEditors.XtraForm
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        If disposing AndAlso components IsNot Nothing Then
+            components.Dispose()
+        End If
+        MyBase.Dispose(disposing)
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FormPopUpMasterCOA))
+        Me.GCAcc = New DevExpress.XtraGrid.GridControl()
+        Me.GVAcc = New DevExpress.XtraGrid.Views.Grid.GridView()
+        Me.id_company = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.company = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.address_primary = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.is_active = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.RepositoryItemCheckEdit1 = New DevExpress.XtraEditors.Repository.RepositoryItemCheckEdit()
+        Me.GridColumn1 = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GridColumn2 = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GridColumn3 = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GridColumn11 = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.is_active_company = New DevExpress.XtraEditors.Repository.RepositoryItemCheckEdit()
+        Me.PanelControl1 = New DevExpress.XtraEditors.PanelControl()
+        Me.BRefresh = New DevExpress.XtraEditors.SimpleButton()
+        Me.BAddNew = New DevExpress.XtraEditors.SimpleButton()
+        CType(Me.GCAcc, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.GVAcc, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.RepositoryItemCheckEdit1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.is_active_company, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PanelControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.PanelControl1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GCAcc
+        '
+        Me.GCAcc.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.GCAcc.Location = New System.Drawing.Point(0, 40)
+        Me.GCAcc.MainView = Me.GVAcc
+        Me.GCAcc.Name = "GCAcc"
+        Me.GCAcc.RepositoryItems.AddRange(New DevExpress.XtraEditors.Repository.RepositoryItem() {Me.is_active_company, Me.RepositoryItemCheckEdit1})
+        Me.GCAcc.Size = New System.Drawing.Size(872, 425)
+        Me.GCAcc.TabIndex = 5
+        Me.GCAcc.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() {Me.GVAcc})
+        '
+        'GVAcc
+        '
+        Me.GVAcc.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() {Me.id_company, Me.company, Me.address_primary, Me.is_active, Me.GridColumn1, Me.GridColumn2, Me.GridColumn3, Me.GridColumn11})
+        Me.GVAcc.GridControl = Me.GCAcc
+        Me.GVAcc.Name = "GVAcc"
+        Me.GVAcc.OptionsBehavior.Editable = False
+        Me.GVAcc.OptionsFind.AlwaysVisible = True
+        Me.GVAcc.OptionsView.ShowGroupPanel = False
+        Me.GVAcc.SortInfo.AddRange(New DevExpress.XtraGrid.Columns.GridColumnSortInfo() {New DevExpress.XtraGrid.Columns.GridColumnSortInfo(Me.company, DevExpress.Data.ColumnSortOrder.Ascending)})
+        '
+        'id_company
+        '
+        Me.id_company.Caption = "ID"
+        Me.id_company.FieldName = "id_acc"
+        Me.id_company.Name = "id_company"
+        '
+        'company
+        '
+        Me.company.Caption = "Account"
+        Me.company.FieldName = "acc_name"
+        Me.company.Name = "company"
+        Me.company.Visible = True
+        Me.company.VisibleIndex = 0
+        Me.company.Width = 237
+        '
+        'address_primary
+        '
+        Me.address_primary.Caption = "Description"
+        Me.address_primary.FieldName = "acc_description"
+        Me.address_primary.Name = "address_primary"
+        Me.address_primary.Visible = True
+        Me.address_primary.VisibleIndex = 1
+        Me.address_primary.Width = 500
+        '
+        'is_active
+        '
+        Me.is_active.AppearanceHeader.Options.UseTextOptions = True
+        Me.is_active.AppearanceHeader.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Center
+        Me.is_active.Caption = "Active"
+        Me.is_active.ColumnEdit = Me.RepositoryItemCheckEdit1
+        Me.is_active.FieldName = "id_status"
+        Me.is_active.Name = "is_active"
+        Me.is_active.Visible = True
+        Me.is_active.VisibleIndex = 4
+        Me.is_active.Width = 110
+        '
+        'RepositoryItemCheckEdit1
+        '
+        Me.RepositoryItemCheckEdit1.AutoHeight = False
+        Me.RepositoryItemCheckEdit1.Name = "RepositoryItemCheckEdit1"
+        Me.RepositoryItemCheckEdit1.ValueChecked = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.RepositoryItemCheckEdit1.ValueGrayed = "0"
+        Me.RepositoryItemCheckEdit1.ValueUnchecked = New Decimal(New Integer() {2, 0, 0, 0})
+        '
+        'GridColumn1
+        '
+        Me.GridColumn1.Caption = "Cost Center"
+        Me.GridColumn1.FieldName = "comp_name"
+        Me.GridColumn1.Name = "GridColumn1"
+        Me.GridColumn1.Visible = True
+        Me.GridColumn1.VisibleIndex = 2
+        Me.GridColumn1.Width = 333
+        '
+        'GridColumn2
+        '
+        Me.GridColumn2.Caption = "CC Number"
+        Me.GridColumn2.FieldName = "comp_number"
+        Me.GridColumn2.Name = "GridColumn2"
+        '
+        'GridColumn3
+        '
+        Me.GridColumn3.Caption = "Id Comp"
+        Me.GridColumn3.FieldName = "id_comp"
+        Me.GridColumn3.Name = "GridColumn3"
+        '
+        'GridColumn11
+        '
+        Me.GridColumn11.Caption = "Debit / Credit"
+        Me.GridColumn11.FieldName = "dc"
+        Me.GridColumn11.Name = "GridColumn11"
+        Me.GridColumn11.Visible = True
+        Me.GridColumn11.VisibleIndex = 3
+        '
+        'is_active_company
+        '
+        Me.is_active_company.AutoHeight = False
+        Me.is_active_company.DisplayValueChecked = "1"
+        Me.is_active_company.DisplayValueUnchecked = "0"
+        Me.is_active_company.Name = "is_active_company"
+        '
+        'PanelControl1
+        '
+        Me.PanelControl1.Controls.Add(Me.BAddNew)
+        Me.PanelControl1.Controls.Add(Me.BRefresh)
+        Me.PanelControl1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.PanelControl1.Location = New System.Drawing.Point(0, 0)
+        Me.PanelControl1.Name = "PanelControl1"
+        Me.PanelControl1.Size = New System.Drawing.Size(872, 40)
+        Me.PanelControl1.TabIndex = 6
+        '
+        'BRefresh
+        '
+        Me.BRefresh.Dock = System.Windows.Forms.DockStyle.Right
+        Me.BRefresh.Image = CType(resources.GetObject("BRefresh.Image"), System.Drawing.Image)
+        Me.BRefresh.Location = New System.Drawing.Point(771, 2)
+        Me.BRefresh.Name = "BRefresh"
+        Me.BRefresh.Size = New System.Drawing.Size(99, 36)
+        Me.BRefresh.TabIndex = 0
+        Me.BRefresh.Text = "Refresh"
+        '
+        'BAddNew
+        '
+        Me.BAddNew.Dock = System.Windows.Forms.DockStyle.Right
+        Me.BAddNew.Image = CType(resources.GetObject("BAddNew.Image"), System.Drawing.Image)
+        Me.BAddNew.Location = New System.Drawing.Point(649, 2)
+        Me.BAddNew.Name = "BAddNew"
+        Me.BAddNew.Size = New System.Drawing.Size(122, 36)
+        Me.BAddNew.TabIndex = 1
+        Me.BAddNew.Text = "Create COA"
+        '
+        'FormPopUpMasterCOA
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(872, 465)
+        Me.Controls.Add(Me.GCAcc)
+        Me.Controls.Add(Me.PanelControl1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.MinimizeBox = False
+        Me.Name = "FormPopUpMasterCOA"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Master Chart Of Account"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+        CType(Me.GCAcc, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.GVAcc, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.RepositoryItemCheckEdit1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.is_active_company, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PanelControl1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.PanelControl1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GCAcc As DevExpress.XtraGrid.GridControl
+    Friend WithEvents GVAcc As DevExpress.XtraGrid.Views.Grid.GridView
+    Friend WithEvents id_company As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents company As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents address_primary As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents is_active As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents RepositoryItemCheckEdit1 As DevExpress.XtraEditors.Repository.RepositoryItemCheckEdit
+    Friend WithEvents GridColumn1 As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GridColumn2 As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GridColumn3 As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GridColumn11 As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents is_active_company As DevExpress.XtraEditors.Repository.RepositoryItemCheckEdit
+    Friend WithEvents PanelControl1 As DevExpress.XtraEditors.PanelControl
+    Friend WithEvents BAddNew As DevExpress.XtraEditors.SimpleButton
+    Friend WithEvents BRefresh As DevExpress.XtraEditors.SimpleButton
+End Class
---
+++ VolcomInventory/FormPopUpMasterCOA.vb
@@ -0,0 +1,18 @@
+﻿Public Class FormPopUpMasterCOA
+    Private Sub FormPopUpMasterCOA_FormClosed(sender As Object, e As FormClosedEventArgs) Handles MyBase.FormClosed
+        Dispose()
+    End Sub
+
+    Private Sub FormPopUpMasterCOA_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        view_acc()
+    End Sub
+
+    Sub view_acc()
+        Dim query As String = "SELECT a.id_acc,acc_name,a.acc_description,a.id_acc_cat,b.acc_cat,a.id_status,c.status,a.id_is_det,d.is_det,comp.id_comp,comp.comp_name,comp.comp_number,e.dc FROM tb_a_acc a "
+        query += "INNER JOIN tb_lookup_acc_cat b ON a.id_acc_cat=b.id_acc_cat INNER JOIN tb_lookup_status c ON a.id_status=c.id_status INNER JOIN tb_lookup_is_det d ON a.id_is_det=d.id_is_det INNER JOIN tb_lookup_dc AS e ON a.id_dc = e.id_dc "
+        query += "LEFT JOIN tb_m_comp comp ON comp.id_comp=a.id_comp "
+        Dim data As DataTable = execute_query(query, -1, True, "", "", "", "")
+        GCAcc.DataSource = data
+        GVAcc.BestFitColumns()
+    End Sub
+End Class
