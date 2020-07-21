16100c5e1b8c1845712107bd153c5849a990314e
Agus Friastana <friastana@gmail.com>
2020-07-21 11:15:04 +0800

    add menu design column, add type all

--- VolcomInventory/FormMain.vb
+++ VolcomInventory/FormMain.vb
@@ -9172,6 +9172,9 @@ WHERE pddr.id_prod_demand_design='" & FormProduction.GVDesign.GetFocusedRowCellV
         ElseIf formName = "FormMappingStore" Then
             FormMappingStore.Close()
             FormMappingStore.Dispose()
+        ElseIf formName = "FormDesignColumn" Then
+            FormDesignColumn.Close()
+            FormDesignColumn.Dispose()
         Else
             RPSubMenu.Visible = False
         End If
@@ -10101,6 +10104,8 @@ WHERE pddr.id_prod_demand_design='" & FormProduction.GVDesign.GetFocusedRowCellV
             FormMasterStore.form_load()
         ElseIf formName = "FormMappingStore" Then
             FormMappingStore.form_load()
+        ElseIf formName = "FormDesignColumn" Then
+            FormDesignColumn.form_load()
         End If
     End Sub
     'Switch
@@ -15041,6 +15046,19 @@ WHERE pddr.id_prod_demand_design='" & FormProduction.GVDesign.GetFocusedRowCellV
             FormMasterStore.Show()
             FormMasterStore.WindowState = FormWindowState.Maximized
             FormMasterStore.Focus()
+        Catch ex As Exception
+            errorProcess()
+        End Try
+        Cursor = Cursors.Default
+    End Sub
+
+    Private Sub NBDesignColumn_LinkClicked(sender As Object, e As DevExpress.XtraNavBar.NavBarLinkEventArgs) Handles NBDesignColumn.LinkClicked
+        Cursor = Cursors.WaitCursor
+        Try
+            FormDesignColumn.MdiParent = Me
+            FormDesignColumn.Show()
+            FormDesignColumn.WindowState = FormWindowState.Maximized
+            FormDesignColumn.Focus()
         Catch ex As Exception
             errorProcess()
         End Try
