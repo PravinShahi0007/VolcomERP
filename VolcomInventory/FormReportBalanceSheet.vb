3387d33a46b857cf2d38ee11be36a84bd62fc420
Putu Septian Primadewa <iansaviour@gmail.com>
2020-07-28 17:15:59 +0800

    BUM

--- VolcomInventory/FormReportBalanceSheet.vb
+++ VolcomInventory/FormReportBalanceSheet.vb
@@ -285,7 +285,15 @@ LEFT JOIN
 ) atx ON atx.id_report=recd.`id_purc_rec`
 WHERE DATE(atx.`date_reference`)>='" + Date.Parse(DETaxFrom.EditValue.ToString).ToString("yyyy-MM-dd") + "' AND DATE(atx.`date_reference`)<='" + Date.Parse(DETaxUntil.EditValue.ToString).ToString("yyyy-MM-dd") + "' " + q_where + "
 GROUP BY ied.id_purc_order_det
-HAVING NOT ISNULL(jurnal_no)"
+HAVING NOT ISNULL(jurnal_no)
+UNION ALL
+-- BUM
+SELECT atx.`report_number_ref` AS inv_number,tr.acc_trans_number AS jurnal_no,atx.`id_acc_trans` AS id_report,atx.`vendor` AS `comp_number`,atx.`vendor` AS `comp_name`,'' AS `npwp_name`,'' AS `npwp`,'' AS `npwp_address`
+,tr.acc_trans_number AS `number`,tr.`date_reference`,atx.`acc_trans_det_note` AS description,atx.`id_acc`,tr.`date_reference` AS `due_date`,acc_pph.`acc_name`,acc_pph.`acc_description`,100 AS pph_percent,IF(acc_pph.`id_dc`=1,atx.`debit`,atx.`credit`) AS dpp,IF(acc_pph.`id_dc`=1,atx.`debit`,atx.`credit`) AS pph 
+FROM tb_a_acc_trans_det atx
+INNER JOIN tb_a_acc_trans tr ON tr.`id_acc_trans`=atx.`id_acc_trans`
+INNER JOIN tb_a_acc acc_pph ON acc_pph.id_acc=atx.id_acc AND acc_pph.is_tax_report=1 AND tr.`id_bill_type`=25
+WHERE DATE(tr.`date_reference`)>='" + Date.Parse(DETaxFrom.EditValue.ToString).ToString("yyyy-MM-dd") + "' AND DATE(tr.`date_reference`)<='" + Date.Parse(DETaxUntil.EditValue.ToString).ToString("yyyy-MM-dd") + "' " + q_where + ""
         Dim dt As DataTable = execute_query(q, -1, True, "", "", "", "")
         GCTaxReport.DataSource = dt
         GVTaxReport.BestFitColumns()
