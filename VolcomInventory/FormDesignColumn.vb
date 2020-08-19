d9e68738f1bb56bb2f8a591638f3c998bf9279ee
Agus Friastana <friastana@gmail.com>
2020-08-19 16:27:07 +0800

    design column add category

--- VolcomInventory/FormDesignColumn.vb
+++ VolcomInventory/FormDesignColumn.vb
@@ -1,11 +1,21 @@
 ﻿Public Class FormDesignColumn
-    Private column_type_front As String = ""
-    Private column_type_end As String = ""
+    Private id_design_column_category As String = "-1"
+    Private column_type_front As String = "-1"
+    Private column_type_end As String = "-1"
+
+    Private loaded As Boolean = False
 
     Private Sub FormDesignColumn_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        view_category()
         view_type()
 
         form_load()
+
+        loaded = True
+    End Sub
+
+    Sub view_category()
+        viewSearchLookupQuery(SLUECategory, "SELECT id_design_column_category, category FROM tb_design_column_category", "id_design_column_category", "category", "id_design_column_category")
     End Sub
 
     Sub view_type()
@@ -37,15 +47,19 @@
             Nothing
         )
 
-        For i = 0 To data_division.Rows.Count - 1
-            For j = 0 To data_subcategory.Rows.Count - 1
-                data.Rows.Add(
-                    data_division.Rows(i)("code").ToString + " " + data_subcategory.Rows(j)("code").ToString,
-                    data_division.Rows(i)("id_code_detail").ToString,
-                    data_subcategory.Rows(j)("id_code_detail").ToString()
-                )
+        Dim view_all As String = execute_query("SELECT view_all FROM tb_design_column_category WHERE id_design_column_category = " + id_design_column_category, 0, True, "", "", "", "")
+
+        If view_all = "1" Then
+            For i = 0 To data_division.Rows.Count - 1
+                For j = 0 To data_subcategory.Rows.Count - 1
+                    data.Rows.Add(
+                        data_division.Rows(i)("code").ToString + " " + data_subcategory.Rows(j)("code").ToString,
+                        data_division.Rows(i)("id_code_detail").ToString,
+                        data_subcategory.Rows(j)("id_code_detail").ToString()
+                    )
+                Next
             Next
-        Next
+        End If
 
         'load to lookup
         SLUEType.Properties.DataSource = Nothing
@@ -73,6 +87,7 @@
         Dim data_column As DataTable = execute_query("
             SELECT tb_design_column.id_design_column, tb_design_column.column_name
             FROM tb_design_column
+            WHERE id_design_column_category = " + id_design_column_category + "
         ", -1, True, "", "", "", "")
 
         'get row
@@ -80,7 +95,7 @@
             SELECT tb_design_column_value.id_design_column, tb_design_column.column_name, tb_design_column_value.id_design_column_value, tb_design_column_value.value
             FROM tb_design_column_value
             LEFT JOIN tb_design_column ON tb_design_column_value.id_design_column = tb_design_column.id_design_column
-            WHERE tb_design_column_value.column_type_front " + If(column_type_front = "", "IS NULL", "= " + column_type_front) + " AND tb_design_column_value.column_type_end " + If(column_type_end = "", "IS NULL", "= " + column_type_end) + "
+            WHERE tb_design_column.id_design_column_category = " + id_design_column_category + " AND tb_design_column_value.column_type_front " + If(column_type_front = "", "IS NULL", "= " + column_type_front) + " AND tb_design_column_value.column_type_end " + If(column_type_end = "", "IS NULL", "= " + column_type_end) + "
         ", -1, True, "", "", "", "")
 
         If data_row.Rows.Count = 0 Then
@@ -167,6 +182,16 @@
         GVDesignColumn.BestFitColumns()
     End Sub
 
+    Private Sub SLUECategory_EditValueChanged(sender As Object, e As EventArgs) Handles SLUECategory.EditValueChanged
+        id_design_column_category = SLUECategory.EditValue.ToString
+
+        If loaded Then
+            view_type()
+        End If
+
+        form_load()
+    End Sub
+
     Private Sub SLUEType_EditValueChanged(sender As Object, e As EventArgs) Handles SLUEType.EditValueChanged
         form_load()
     End Sub
@@ -179,6 +204,7 @@
         End Try
 
         Try
+            FormDesignColumnValue.id_design_column_category = id_design_column_category
             FormDesignColumnValue.id_design_column = GVDesignColumn.FocusedColumn.Tag.ToString.Split(";")(0)
             FormDesignColumnValue.column_type_front = column_type_front
             FormDesignColumnValue.column_type_end = column_type_end
@@ -196,6 +222,7 @@
 
         Try
             If Not GVDesignColumn.GetFocusedRowCellValue(GVDesignColumn.FocusedColumn.FieldName).ToString = "" Then
+                FormDesignColumnValue.id_design_column_category = id_design_column_category
                 FormDesignColumnValue.id_design_column_value = GVDesignColumn.FocusedColumn.Tag.ToString.Split(";")(GVDesignColumn.FocusedRowHandle + 1)
                 FormDesignColumnValue.id_design_column = GVDesignColumn.FocusedColumn.Tag.ToString.Split(";")(0)
                 FormDesignColumnValue.column_type_front = column_type_front
@@ -208,6 +235,7 @@
 
     Private Sub BarButtonItemAddColumn_ItemClick(sender As Object, e As DevExpress.XtraBars.ItemClickEventArgs) Handles BarButtonItemAddColumn.ItemClick
         Try
+            FormDesignColumnDet.id_design_column_category = id_design_column_category
             FormDesignColumnDet.ShowDialog()
         Catch ex As Exception
         End Try
@@ -215,6 +243,7 @@
 
     Private Sub BarButtonItemEditColumn_ItemClick(sender As Object, e As DevExpress.XtraBars.ItemClickEventArgs) Handles BarButtonItemEditColumn.ItemClick
         Try
+            FormDesignColumnDet.id_design_column_category = id_design_column_category
             FormDesignColumnDet.id_design_column = GVDesignColumn.FocusedColumn.Tag.ToString.Split(";")(0)
             FormDesignColumnDet.ShowDialog()
         Catch ex As Exception
