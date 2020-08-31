c0e9cee396dd0c6d944e8d818ce0e765617cb0ef
Putu Septian Primadewa <iansaviour@gmail.com>
2020-08-31 09:31:51 +0800

    N4 ketinggalan di report

--- VolcomInventory/ReportMatAdjOut.vb
+++ VolcomInventory/ReportMatAdjOut.vb
@@ -27,7 +27,6 @@
         query += "CALL view_mat_adj_out('" + id_adj_out_mat + "')"
         Dim data As DataTable = execute_query(query, -1, True, "", "", "", "")
         GCDetail.DataSource = data
-        GVDetail.Columns("adj_out_mat_det_amount").SummaryItem.DisplayFormat = currency + " {0:n2} "
     End Sub
 
     Private Sub GVDetail_CustomColumnDisplayText(ByVal sender As System.Object, ByVal e As DevExpress.XtraGrid.Views.Base.CustomColumnDisplayTextEventArgs) Handles GVDetail.CustomColumnDisplayText
