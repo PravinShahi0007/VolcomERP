69950ec95595cc0a754c11365249cb86471234bc
Putu Septian Primadewa <iansaviour@gmail.com>
2020-09-11 09:24:13 +0800

    already verified

--- VolcomInventory/FormSampleDevelopmentVerifyCPS2.vb
+++ VolcomInventory/FormSampleDevelopmentVerifyCPS2.vb
@@ -11,11 +11,16 @@
     Private Sub BUpdate_Click(sender As Object, e As EventArgs) Handles BUpdate.Click
         If Not MENote.Text = "" Then
             'check already verified
-
+            Dim qc As String = "SELECT cps2_verify FROM tb_prod_order WHERE id_prod_order='" & id_po & "' AND cps2_verify='2'"
+            Dim dtc As DataTable = execute_query(qc, -1, True, "", "", "", "")
             '
-            Dim q As String = "UPDATE tb_prod_order SET cps2_verify='1',cps2_verify_by='" & id_user & "',cps2_verify_note='" & addSlashes(MENote.Text) & "',cps2_verify_date=NOW() WHERE id_prod_order='" & id_po & "'"
-            execute_non_query(q, True, "", "", "", "")
-            Close()
+            If dtc.Rows.Count > 0 Then
+                Dim q As String = "UPDATE tb_prod_order SET cps2_verify='1',cps2_verify_by='" & id_user & "',cps2_verify_note='" & addSlashes(MENote.Text) & "',cps2_verify_date=NOW() WHERE id_prod_order='" & id_po & "'"
+                execute_non_query(q, True, "", "", "", "")
+                Close()
+            Else
+                warningCustom("Already verified.")
+            End If
         Else
             warningCustom("Please put some note !")
         End If
