2fc23113d94d731bd47f240aae5c9e8fe59c7a2d
Putu Septian Primadewa <iansaviour@gmail.com>
2020-09-24 13:52:54 +0800

    tax pending

--- VolcomInventory/FormReportBalanceSheetTax.vb
+++ VolcomInventory/FormReportBalanceSheetTax.vb
@@ -2,4 +2,12 @@
     Private Sub FormReportBalanceSheetTax_FormClosed(sender As Object, e As FormClosedEventArgs) Handles MyBase.FormClosed
         Dispose()
     End Sub
+
+    Private Sub BClose_Click(sender As Object, e As EventArgs) Handles BClose.Click
+        Close()
+    End Sub
+
+    Private Sub BSave_Click(sender As Object, e As EventArgs) Handles BSave.Click
+
+    End Sub
 End Class
