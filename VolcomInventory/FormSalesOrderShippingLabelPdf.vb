043f30e539e1e2519673a6ae1b8e86fd8fb4a03f
Agus Friastana <friastana@gmail.com>
2020-10-06 17:40:53 +0800

    zalora shipping label

--- VolcomInventory/FormSalesOrderShippingLabelPdf.vb
+++ VolcomInventory/FormSalesOrderShippingLabelPdf.vb
@@ -45,6 +45,10 @@
             If Not out_shippinglabel = "" Then
                 bytes_shippinglabel = Convert.FromBase64String(out_shippinglabel)
             End If
+
+            'replace https
+            bytes_invoice = System.Text.Encoding.UTF8.GetBytes(System.Text.Encoding.UTF8.GetString(bytes_invoice).Replace("https", "http"))
+            bytes_shippinglabel = System.Text.Encoding.UTF8.GetBytes(System.Text.Encoding.UTF8.GetString(bytes_shippinglabel).Replace("https", "http"))
         End If
 
         If ol_store = "blibli" Then
@@ -55,8 +59,6 @@
                 writer.Write(bytes_shippinglabel, 0, bytes_shippinglabel.Length)
 
                 writer.Close()
-
-                'Dim browser As CefSharp.WinForms.ChromiumWebBrowser = New CefSharp.WinForms.ChromiumWebBrowser(full_path_shippinglabel)
 
                 Dim browser As WebBrowser = New WebBrowser
 
@@ -70,50 +72,85 @@
             End If
         ElseIf ol_store = "zalora" Then
             If bytes_invoice IsNot Nothing And bytes_shippinglabel IsNot Nothing Then
-                is_not_found = True
-                ''invoice
-                'Dim stream_invoice As IO.FileStream = New IO.FileStream(full_path_invoice, IO.FileMode.CreateNew)
-                'Dim writer_invoice As IO.BinaryWriter = New IO.BinaryWriter(stream_invoice)
+                'invoice
+                Dim stream_invoice As IO.FileStream = New IO.FileStream(full_path_invoice, IO.FileMode.CreateNew)
+                Dim writer_invoice As IO.BinaryWriter = New IO.BinaryWriter(stream_invoice)
 
-                'writer_invoice.Write(bytes_invoice, 0, bytes_invoice.Length)
+                writer_invoice.Write(bytes_invoice, 0, bytes_invoice.Length)
 
-                'writer_invoice.Close()
+                writer_invoice.Close()
 
-                ''shipping label
-                'Dim stream_shippinglabel As IO.FileStream = New IO.FileStream(full_path_shippinglabel, IO.FileMode.CreateNew)
-                'Dim writer_shippinglabel As IO.BinaryWriter = New IO.BinaryWriter(stream_shippinglabel)
+                'shipping label
+                Dim stream_shippinglabel As IO.FileStream = New IO.FileStream(full_path_shippinglabel, IO.FileMode.CreateNew)
+                Dim writer_shippinglabel As IO.BinaryWriter = New IO.BinaryWriter(stream_shippinglabel)
 
-                'writer_shippinglabel.Write(bytes_shippinglabel, 0, bytes_shippinglabel.Length)
+                writer_shippinglabel.Write(bytes_shippinglabel, 0, bytes_shippinglabel.Length)
 
-                'writer_shippinglabel.Close()
+                writer_shippinglabel.Close()
 
-                ''tab
-                'Dim tab As DevExpress.XtraTab.XtraTabControl = New DevExpress.XtraTab.XtraTabControl
+                'tab
+                Dim tab As DevExpress.XtraTab.XtraTabControl = New DevExpress.XtraTab.XtraTabControl
 
-                'tab.Dock = DockStyle.Fill
+                tab.Dock = DockStyle.Fill
 
-                'tab.TabPages.Add("Invoice")
-                'tab.TabPages.Add("Shipping Label")
+                tab.TabPages.Add("Invoice")
+                tab.TabPages.Add("Shipping Label")
 
-                ''Dim browser_invoice As CefSharp.WinForms.ChromiumWebBrowser = New CefSharp.WinForms.ChromiumWebBrowser(full_path_invoice)
-                ''Dim browser_shippinglabel As CefSharp.WinForms.ChromiumWebBrowser = New CefSharp.WinForms.ChromiumWebBrowser(full_path_shippinglabel)
+                'pdf
+                Dim pdf_invoice As Pechkin.GlobalConfig = New Pechkin.GlobalConfig
 
-                'Dim browser_invoice As WebBrowser = New WebBrowser
+                pdf_invoice.SetPaperOrientation(True)
+                pdf_invoice.SetPaperSize(Printing.PaperKind.A4)
 
-                'browser_invoice.Dock = DockStyle.Fill
+                Dim obj_invoice As Pechkin.ObjectConfig = New Pechkin.ObjectConfig
 
-                'browser_invoice.Url = New Uri(full_path_invoice)
+                obj_invoice.SetPageUri(full_path_invoice)
+                obj_invoice.SetPrintBackground(True)
+                obj_invoice.SetScreenMediaType(True)
+                obj_invoice.SetLoadImages(True)
 
-                'Dim browser_shippinglabel As WebBrowser = New WebBrowser
+                Dim converter_invoice As Pechkin.Synchronized.SynchronizedPechkin = New Pechkin.Synchronized.SynchronizedPechkin(pdf_invoice)
 
-                'browser_shippinglabel.Dock = DockStyle.Fill
+                Dim result_invoice() As Byte = converter_invoice.Convert(obj_invoice)
 
-                'browser_shippinglabel.Url = New Uri(full_path_shippinglabel)
+                IO.File.WriteAllBytes(full_path_invoice.Replace(".html", ".pdf"), result_invoice)
 
-                'tab.TabPages.Item(0).Controls.Add(browser_invoice)
-                'tab.TabPages.Item(1).Controls.Add(browser_shippinglabel)
+                Dim pdf_shippinglabel As TuesPechkin.HtmlToPdfDocument = New TuesPechkin.HtmlToPdfDocument
 
-                'Controls.Add(tab)
+                pdf_shippinglabel.GlobalSettings.PaperSize = Printing.PaperKind.A4Rotated
+
+                Dim obj_shippinglabel As TuesPechkin.ObjectSettings = New TuesPechkin.ObjectSettings
+
+                obj_shippinglabel.PageUrl = full_path_shippinglabel
+                obj_shippinglabel.WebSettings.PrintBackground = True
+                obj_shippinglabel.WebSettings.PrintMediaType = True
+                obj_shippinglabel.WebSettings.LoadImages = True
+
+                pdf_shippinglabel.Objects.Add(obj_shippinglabel)
+
+                Dim converter_shippinglabel As TuesPechkin.IConverter = New TuesPechkin.StandardConverter(New TuesPechkin.PdfToolset(New TuesPechkin.Win32EmbeddedDeployment(New TuesPechkin.TempFolderDeployment())))
+
+                Dim result_shippinglabel() As Byte = converter_shippinglabel.Convert(pdf_shippinglabel)
+
+                IO.File.WriteAllBytes(full_path_shippinglabel.Replace(".html", ".pdf"), result_shippinglabel)
+
+                'browser
+                Dim browser_invoice As WebBrowser = New WebBrowser
+
+                browser_invoice.Dock = DockStyle.Fill
+
+                browser_invoice.Url = New Uri(full_path_invoice.Replace(".html", ".pdf"))
+
+                Dim browser_shippinglabel As WebBrowser = New WebBrowser
+
+                browser_shippinglabel.Dock = DockStyle.Fill
+
+                browser_shippinglabel.Url = New Uri(full_path_shippinglabel.Replace(".html", ".pdf"))
+
+                tab.TabPages.Item(0).Controls.Add(browser_invoice)
+                tab.TabPages.Item(1).Controls.Add(browser_shippinglabel)
+
+                Controls.Add(tab)
             Else
                 is_not_found = True
             End If
