392045879eee566346d3500aaec64c51bc411248
Agus Friastana <friastana@gmail.com>
2020-11-02 17:32:34 +0800

    design images upload images, view images, validation store code, validation design code

--- VolcomInventory/FormDesignImages.Designer.vb
+++ VolcomInventory/FormDesignImages.Designer.vb
@@ -19,16 +19,148 @@ Partial Class FormDesignImages
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FormDesignImages))
+        Me.PanelControl1 = New DevExpress.XtraEditors.PanelControl()
+        Me.GCImageList = New DevExpress.XtraGrid.GridControl()
+        Me.GVImageList = New DevExpress.XtraGrid.Views.Grid.GridView()
+        Me.SBView = New DevExpress.XtraEditors.SimpleButton()
+        Me.GridColumn1 = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GridColumn2 = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GridColumn3 = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GridColumn4 = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GridColumn5 = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GridColumn6 = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.RepositoryItemCheckEdit = New DevExpress.XtraEditors.Repository.RepositoryItemCheckEdit()
+        CType(Me.PanelControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.PanelControl1.SuspendLayout()
+        CType(Me.GCImageList, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.GVImageList, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.RepositoryItemCheckEdit, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
+        '
+        'PanelControl1
+        '
+        Me.PanelControl1.Controls.Add(Me.SBView)
+        Me.PanelControl1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.PanelControl1.Location = New System.Drawing.Point(0, 0)
+        Me.PanelControl1.Name = "PanelControl1"
+        Me.PanelControl1.Size = New System.Drawing.Size(1008, 49)
+        Me.PanelControl1.TabIndex = 0
+        '
+        'GCImageList
+        '
+        Me.GCImageList.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.GCImageList.Location = New System.Drawing.Point(0, 49)
+        Me.GCImageList.MainView = Me.GVImageList
+        Me.GCImageList.Name = "GCImageList"
+        Me.GCImageList.RepositoryItems.AddRange(New DevExpress.XtraEditors.Repository.RepositoryItem() {Me.RepositoryItemCheckEdit})
+        Me.GCImageList.Size = New System.Drawing.Size(1008, 680)
+        Me.GCImageList.TabIndex = 1
+        Me.GCImageList.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() {Me.GVImageList})
+        '
+        'GVImageList
+        '
+        Me.GVImageList.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() {Me.GridColumn1, Me.GridColumn2, Me.GridColumn3, Me.GridColumn4, Me.GridColumn5, Me.GridColumn6})
+        Me.GVImageList.GridControl = Me.GCImageList
+        Me.GVImageList.Name = "GVImageList"
+        Me.GVImageList.OptionsBehavior.ReadOnly = True
+        Me.GVImageList.OptionsView.ShowGroupPanel = False
+        '
+        'SBView
+        '
+        Me.SBView.Location = New System.Drawing.Point(334, 12)
+        Me.SBView.Name = "SBView"
+        Me.SBView.Size = New System.Drawing.Size(75, 23)
+        Me.SBView.TabIndex = 0
+        Me.SBView.Text = "View"
+        '
+        'GridColumn1
+        '
+        Me.GridColumn1.Caption = "Code"
+        Me.GridColumn1.FieldName = "design_code"
+        Me.GridColumn1.Name = "GridColumn1"
+        Me.GridColumn1.Visible = True
+        Me.GridColumn1.VisibleIndex = 0
+        '
+        'GridColumn2
+        '
+        Me.GridColumn2.Caption = "Description"
+        Me.GridColumn2.FieldName = "design_display_name"
+        Me.GridColumn2.Name = "GridColumn2"
+        Me.GridColumn2.Visible = True
+        Me.GridColumn2.VisibleIndex = 1
+        '
+        'GridColumn3
+        '
+        Me.GridColumn3.Caption = "File Name"
+        Me.GridColumn3.FieldName = "file_name"
+        Me.GridColumn3.Name = "GridColumn3"
+        Me.GridColumn3.Visible = True
+        Me.GridColumn3.VisibleIndex = 2
+        '
+        'GridColumn4
+        '
+        Me.GridColumn4.Caption = "Uploaded At"
+        Me.GridColumn4.DisplayFormat.FormatString = "dd MMMM yyyy HH:mm:ss"
+        Me.GridColumn4.DisplayFormat.FormatType = DevExpress.Utils.FormatType.DateTime
+        Me.GridColumn4.FieldName = "created_at"
+        Me.GridColumn4.Name = "GridColumn4"
+        Me.GridColumn4.Visible = True
+        Me.GridColumn4.VisibleIndex = 3
+        '
+        'GridColumn5
+        '
+        Me.GridColumn5.Caption = "Uploaded By"
+        Me.GridColumn5.FieldName = "created_by"
+        Me.GridColumn5.Name = "GridColumn5"
+        Me.GridColumn5.Visible = True
+        Me.GridColumn5.VisibleIndex = 4
+        '
+        'GridColumn6
+        '
+        Me.GridColumn6.Caption = "Image"
+        Me.GridColumn6.ColumnEdit = Me.RepositoryItemCheckEdit
+        Me.GridColumn6.FieldName = "image"
+        Me.GridColumn6.Name = "GridColumn6"
+        Me.GridColumn6.Visible = True
+        Me.GridColumn6.VisibleIndex = 5
+        '
+        'RepositoryItemCheckEdit
+        '
+        Me.RepositoryItemCheckEdit.AutoHeight = False
+        Me.RepositoryItemCheckEdit.CheckStyle = DevExpress.XtraEditors.Controls.CheckStyles.UserDefined
+        Me.RepositoryItemCheckEdit.Name = "RepositoryItemCheckEdit"
+        Me.RepositoryItemCheckEdit.PictureChecked = CType(resources.GetObject("RepositoryItemCheckEdit.PictureChecked"), System.Drawing.Image)
+        Me.RepositoryItemCheckEdit.PictureGrayed = CType(resources.GetObject("RepositoryItemCheckEdit.PictureGrayed"), System.Drawing.Image)
+        Me.RepositoryItemCheckEdit.PictureUnchecked = CType(resources.GetObject("RepositoryItemCheckEdit.PictureUnchecked"), System.Drawing.Image)
         '
         'FormDesignImages
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(1008, 729)
+        Me.Controls.Add(Me.GCImageList)
+        Me.Controls.Add(Me.PanelControl1)
         Me.Name = "FormDesignImages"
         Me.Text = "Design Images"
+        CType(Me.PanelControl1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.PanelControl1.ResumeLayout(False)
+        CType(Me.GCImageList, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.GVImageList, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.RepositoryItemCheckEdit, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
+
+    Friend WithEvents PanelControl1 As DevExpress.XtraEditors.PanelControl
+    Friend WithEvents GCImageList As DevExpress.XtraGrid.GridControl
+    Friend WithEvents GVImageList As DevExpress.XtraGrid.Views.Grid.GridView
+    Friend WithEvents SBView As DevExpress.XtraEditors.SimpleButton
+    Friend WithEvents GridColumn1 As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GridColumn2 As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GridColumn3 As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GridColumn4 As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GridColumn5 As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GridColumn6 As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents RepositoryItemCheckEdit As DevExpress.XtraEditors.Repository.RepositoryItemCheckEdit
 End Class
--- VolcomInventory/FormDesignImages.vb
+++ VolcomInventory/FormDesignImages.vb
@@ -1,6 +1,6 @@
 ﻿Public Class FormDesignImages
     Private Sub FormDesignImages_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
+        view_images()
     End Sub
 
     Private Sub FormDesignImages_FormClosed(sender As Object, e As FormClosedEventArgs) Handles MyBase.FormClosed
@@ -17,11 +17,126 @@
     End Sub
 
     Sub view_images()
+        Dim query As String = "
+            SELECT d.design_code, d.design_display_name, i.file_name, i.created_at, e.employee_name AS created_by, '' AS image
+            FROM tb_design_images AS i
+            LEFT JOIN tb_m_design AS d ON d.id_design = i.id_design
+            LEFT JOIN tb_m_user AS u ON i.created_by = u.id_user
+            LEFT JOIN tb_m_employee AS e ON u.id_employee = e.id_employee
+            ORDER BY i.id_design_images DESC
+        "
 
+        Dim data As DataTable = execute_query(query, -1, True, "", "", "", "")
+
+        GCImageList.DataSource = data
+
+        GVImageList.BestFitColumns()
     End Sub
 
     Sub browse_images()
+        Dim browse As FolderBrowserDialog = New FolderBrowserDialog
 
+        browse.ShowDialog()
+
+        If Not browse.SelectedPath = "" Then
+            FormMain.SplashScreenManager1.ShowWaitForm()
+
+            Dim file() As String = IO.Directory.GetFiles(browse.SelectedPath)
+
+            Dim is_valid As String = ""
+
+            Dim uploaded_image As DataTable = execute_query("SELECT SUBSTRING(file_name, 1, (POSITION('.' IN file_name) - 1)) AS file_name FROM tb_design_images", -1, True, "", "", "", "")
+
+            Dim store_code As DataTable = execute_query("SELECT id_comp_group, design_images_code FROM tb_m_comp_group WHERE design_images_code IS NOT NULL", -1, True, "", "", "", "")
+
+            Dim design_code As DataTable = execute_query("SELECT id_design, design_code FROM tb_m_design WHERE design_code <> '' ORDER BY id_design DESC", -1, True, "", "", "", "")
+
+            For i = 0 To file.Count - 1
+                FormMain.SplashScreenManager1.SetWaitFormDescription("Checking file " + (i + 1).ToString + " of " + file.Count.ToString)
+
+                'check already uploaded
+                Dim dv_uploaded As DataView = New DataView(uploaded_image)
+
+                dv_uploaded.RowFilter = "file_name = '" + IO.Path.GetFileNameWithoutExtension(file(i)) + "'"
+
+                If dv_uploaded.Count = 0 Then
+                    Dim array_name() As String = IO.Path.GetFileNameWithoutExtension(file(i)).Split("_")
+
+                    If array_name.Length = 3 Then
+                        'check store code
+                        Dim dv_store As DataView = New DataView(store_code)
+
+                        dv_store.RowFilter = "design_images_code = '" + array_name(0) + "'"
+
+                        If dv_store.Count < 1 Then
+                            is_valid += IO.Path.GetFileName(file(i)) + " store code not found." + Environment.NewLine
+                        End If
+
+                        'check design code
+                        Dim dv_design As DataView = New DataView(design_code)
+
+                        dv_design.RowFilter = "design_code = '" + array_name(1) + "'"
+
+                        If dv_design.Count < 1 Then
+                            is_valid += IO.Path.GetFileName(file(i)) + " design code not found." + Environment.NewLine
+                        End If
+                    Else
+                        is_valid += IO.Path.GetFileName(file(i)) + " is not valid." + Environment.NewLine
+                    End If
+                Else
+                    is_valid += IO.Path.GetFileName(file(i)) + " already uploaded." + Environment.NewLine
+                End If
+            Next
+
+            If is_valid = "" Then
+                'upload
+                Dim cloud_host As String = get_setup_field("cloud_host").ToString
+                Dim cloud_username As String = get_setup_field("cloud_username").ToString
+                Dim cloud_password As String = get_setup_field("cloud_password").ToString
+                Dim cloud_port As String = get_setup_field("cloud_port").ToString
+
+                Dim cloud_image_path As String = get_setup_field("cloud_image_path").ToString
+
+                Dim sftp As Renci.SshNet.SftpClient = New Renci.SshNet.SftpClient(cloud_host, cloud_username, cloud_password)
+
+                sftp.Connect()
+
+                If sftp.IsConnected Then
+                    For i = 0 To file.Count - 1
+                        FormMain.SplashScreenManager1.SetWaitFormDescription("Uploading file " + (i + 1).ToString + " of " + file.Count.ToString)
+
+                        Dim file_stream As IO.FileStream = New IO.FileStream(file(i), IO.FileMode.Open)
+
+                        sftp.UploadFile(file_stream, cloud_image_path + "/" + IO.Path.GetFileName(file(i)))
+
+                        Dim id_design As String = ""
+
+                        'check design code
+                        Dim array_name() As String = IO.Path.GetFileNameWithoutExtension(file(i)).Split("_")
+
+                        Dim dv_design As DataView = New DataView(design_code)
+
+                        dv_design.RowFilter = "design_code = '" + array_name(1) + "'"
+
+                        'store database
+                        execute_non_query("INSERT INTO tb_design_images (id_design, file_name, created_at, created_by) VALUES (" + dv_design(0)("id_design").ToString + ", '" + IO.Path.GetFileName(file(i)) + "', NOW(), " + id_user + ")", True, "", "", "", "")
+                    Next
+                End If
+
+                sftp.Disconnect()
+                sftp.Dispose()
+
+                view_images()
+            Else
+                FormMain.SplashScreenManager1.CloseWaitForm()
+
+                stopCustom("Some files could not be uploaded: " + Environment.NewLine + is_valid)
+            End If
+
+            If FormMain.SplashScreenManager1.IsSplashFormVisible Then
+                FormMain.SplashScreenManager1.CloseWaitForm()
+            End If
+        End If
     End Sub
 
     Sub delete_images()
@@ -29,6 +144,46 @@
     End Sub
 
     Sub print_images()
+        print(GCImageList, "Design Images")
+    End Sub
 
+    Private Sub SBView_Click(sender As Object, e As EventArgs) Handles SBView.Click
+        view_images()
+    End Sub
+
+    Private Sub RepositoryItemCheckEdit_Click(sender As Object, e As EventArgs) Handles RepositoryItemCheckEdit.Click
+        Cursor = Cursors.WaitCursor
+
+        'download file
+        Dim client As Net.WebClient = New Net.WebClient
+
+        Dim file As String = GVImageList.GetFocusedRowCellValue("file_name").ToString
+
+        Dim path As String = Application.StartupPath + "\download\"
+
+        If Not IO.Directory.Exists(path) Then
+            System.IO.Directory.CreateDirectory(path)
+        End If
+
+        client.DownloadFile("http://103.58.101.112/" + file, path + file)
+
+        'open file
+        Dim process_info As ProcessStartInfo = New ProcessStartInfo()
+
+        process_info.FileName = path + file
+
+        process_info.WorkingDirectory = path
+
+        Process.Start(process_info)
+
+        Cursor = Cursors.Default
+    End Sub
+
+    Private Sub RepositoryItemCheckEdit_MouseHover(sender As Object, e As EventArgs) Handles RepositoryItemCheckEdit.MouseHover
+        Cursor = Cursors.Hand
+    End Sub
+
+    Private Sub RepositoryItemCheckEdit_MouseLeave(sender As Object, e As EventArgs) Handles RepositoryItemCheckEdit.MouseLeave
+        Cursor = Cursors.Default
     End Sub
 End Class
