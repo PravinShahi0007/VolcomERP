0f3457ba699d480356ed2a6b166653ca614db614
Putu Septian Primadewa <iansaviour@gmail.com>
2020-11-19 17:37:06 +0800

    add form scan return BAP

--- VolcomInventory/FormScanReturn.Designer.vb
+++ VolcomInventory/FormScanReturn.Designer.vb
@@ -38,6 +38,26 @@ Partial Class FormScanReturn
         Me.GridColumn10 = New DevExpress.XtraGrid.Columns.GridColumn()
         Me.GridColumn4 = New DevExpress.XtraGrid.Columns.GridColumn()
         Me.GridColumn5 = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.XTCScanReturn = New DevExpress.XtraTab.XtraTabControl()
+        Me.XTPScanReturn = New DevExpress.XtraTab.XtraTabPage()
+        Me.XTPBAP = New DevExpress.XtraTab.XtraTabPage()
+        Me.PanelControl2 = New DevExpress.XtraEditors.PanelControl()
+        Me.BViewBAP = New DevExpress.XtraEditors.SimpleButton()
+        Me.DEUntilBAP = New DevExpress.XtraEditors.DateEdit()
+        Me.LabelControl1 = New DevExpress.XtraEditors.LabelControl()
+        Me.LabelControl2 = New DevExpress.XtraEditors.LabelControl()
+        Me.DEStartBAP = New DevExpress.XtraEditors.DateEdit()
+        Me.GCBAP = New DevExpress.XtraGrid.GridControl()
+        Me.GVBAP = New DevExpress.XtraGrid.Views.Grid.GridView()
+        Me.GridColumn11 = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GridColumn12 = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GridColumn13 = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GridColumn14 = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.RepositoryItemMemoEdit2 = New DevExpress.XtraEditors.Repository.RepositoryItemMemoEdit()
+        Me.GridColumn19 = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GridColumn20 = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GridColumn15 = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GridColumn16 = New DevExpress.XtraGrid.Columns.GridColumn()
         CType(Me.PanelControl1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.PanelControl1.SuspendLayout()
         CType(Me.DEUntil.Properties.CalendarTimeProperties, System.ComponentModel.ISupportInitialize).BeginInit()
@@ -47,6 +67,19 @@ Partial Class FormScanReturn
         CType(Me.GCAwb, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.GVAwb, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.RepositoryItemMemoEdit1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.XTCScanReturn, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.XTCScanReturn.SuspendLayout()
+        Me.XTPScanReturn.SuspendLayout()
+        Me.XTPBAP.SuspendLayout()
+        CType(Me.PanelControl2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.PanelControl2.SuspendLayout()
+        CType(Me.DEUntilBAP.Properties.CalendarTimeProperties, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DEUntilBAP.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DEStartBAP.Properties.CalendarTimeProperties, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DEStartBAP.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.GCBAP, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.GVBAP, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.RepositoryItemMemoEdit2, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'PanelControl1
@@ -59,7 +92,7 @@ Partial Class FormScanReturn
         Me.PanelControl1.Dock = System.Windows.Forms.DockStyle.Top
         Me.PanelControl1.Location = New System.Drawing.Point(0, 0)
         Me.PanelControl1.Name = "PanelControl1"
-        Me.PanelControl1.Size = New System.Drawing.Size(903, 45)
+        Me.PanelControl1.Size = New System.Drawing.Size(897, 45)
         Me.PanelControl1.TabIndex = 0
         '
         'BView
@@ -123,7 +156,7 @@ Partial Class FormScanReturn
         Me.GCAwb.MainView = Me.GVAwb
         Me.GCAwb.Name = "GCAwb"
         Me.GCAwb.RepositoryItems.AddRange(New DevExpress.XtraEditors.Repository.RepositoryItem() {Me.RepositoryItemMemoEdit1})
-        Me.GCAwb.Size = New System.Drawing.Size(903, 468)
+        Me.GCAwb.Size = New System.Drawing.Size(897, 440)
         Me.GCAwb.TabIndex = 2
         Me.GCAwb.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() {Me.GVAwb})
         '
@@ -227,13 +260,198 @@ Partial Class FormScanReturn
         Me.GridColumn5.VisibleIndex = 8
         Me.GridColumn5.Width = 222
         '
+        'XTCScanReturn
+        '
+        Me.XTCScanReturn.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.XTCScanReturn.HeaderLocation = DevExpress.XtraTab.TabHeaderLocation.Bottom
+        Me.XTCScanReturn.Location = New System.Drawing.Point(0, 0)
+        Me.XTCScanReturn.Name = "XTCScanReturn"
+        Me.XTCScanReturn.SelectedTabPage = Me.XTPScanReturn
+        Me.XTCScanReturn.Size = New System.Drawing.Size(903, 513)
+        Me.XTCScanReturn.TabIndex = 3
+        Me.XTCScanReturn.TabPages.AddRange(New DevExpress.XtraTab.XtraTabPage() {Me.XTPScanReturn, Me.XTPBAP})
+        '
+        'XTPScanReturn
+        '
+        Me.XTPScanReturn.Controls.Add(Me.GCAwb)
+        Me.XTPScanReturn.Controls.Add(Me.PanelControl1)
+        Me.XTPScanReturn.Name = "XTPScanReturn"
+        Me.XTPScanReturn.Size = New System.Drawing.Size(897, 485)
+        Me.XTPScanReturn.Text = "Scan Return"
+        '
+        'XTPBAP
+        '
+        Me.XTPBAP.Controls.Add(Me.GCBAP)
+        Me.XTPBAP.Controls.Add(Me.PanelControl2)
+        Me.XTPBAP.Name = "XTPBAP"
+        Me.XTPBAP.Size = New System.Drawing.Size(897, 485)
+        Me.XTPBAP.Text = "BAP"
+        '
+        'PanelControl2
+        '
+        Me.PanelControl2.Controls.Add(Me.BViewBAP)
+        Me.PanelControl2.Controls.Add(Me.DEUntilBAP)
+        Me.PanelControl2.Controls.Add(Me.LabelControl1)
+        Me.PanelControl2.Controls.Add(Me.LabelControl2)
+        Me.PanelControl2.Controls.Add(Me.DEStartBAP)
+        Me.PanelControl2.Dock = System.Windows.Forms.DockStyle.Top
+        Me.PanelControl2.Location = New System.Drawing.Point(0, 0)
+        Me.PanelControl2.Name = "PanelControl2"
+        Me.PanelControl2.Size = New System.Drawing.Size(897, 45)
+        Me.PanelControl2.TabIndex = 1
+        '
+        'BViewBAP
+        '
+        Me.BViewBAP.Location = New System.Drawing.Point(365, 10)
+        Me.BViewBAP.Name = "BViewBAP"
+        Me.BViewBAP.Size = New System.Drawing.Size(49, 23)
+        Me.BViewBAP.TabIndex = 8924
+        Me.BViewBAP.Text = "view"
+        '
+        'DEUntilBAP
+        '
+        Me.DEUntilBAP.EditValue = Nothing
+        Me.DEUntilBAP.Location = New System.Drawing.Point(211, 12)
+        Me.DEUntilBAP.Name = "DEUntilBAP"
+        Me.DEUntilBAP.Properties.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() {New DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)})
+        Me.DEUntilBAP.Properties.CalendarTimeProperties.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() {New DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)})
+        Me.DEUntilBAP.Properties.DisplayFormat.FormatString = "dd MMMM yyyy"
+        Me.DEUntilBAP.Properties.DisplayFormat.FormatType = DevExpress.Utils.FormatType.DateTime
+        Me.DEUntilBAP.Properties.EditFormat.FormatString = "dd MMMM yyyy"
+        Me.DEUntilBAP.Properties.EditFormat.FormatType = DevExpress.Utils.FormatType.DateTime
+        Me.DEUntilBAP.Properties.Mask.EditMask = "dd MMMM yyyy"
+        Me.DEUntilBAP.Size = New System.Drawing.Size(148, 20)
+        Me.DEUntilBAP.TabIndex = 8923
+        '
+        'LabelControl1
+        '
+        Me.LabelControl1.Location = New System.Drawing.Point(184, 15)
+        Me.LabelControl1.Name = "LabelControl1"
+        Me.LabelControl1.Size = New System.Drawing.Size(21, 13)
+        Me.LabelControl1.TabIndex = 8922
+        Me.LabelControl1.Text = "Until"
+        '
+        'LabelControl2
+        '
+        Me.LabelControl2.Location = New System.Drawing.Point(12, 15)
+        Me.LabelControl2.Name = "LabelControl2"
+        Me.LabelControl2.Size = New System.Drawing.Size(24, 13)
+        Me.LabelControl2.TabIndex = 8921
+        Me.LabelControl2.Text = "From"
+        '
+        'DEStartBAP
+        '
+        Me.DEStartBAP.EditValue = Nothing
+        Me.DEStartBAP.Location = New System.Drawing.Point(42, 12)
+        Me.DEStartBAP.Name = "DEStartBAP"
+        Me.DEStartBAP.Properties.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() {New DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)})
+        Me.DEStartBAP.Properties.CalendarTimeProperties.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() {New DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)})
+        Me.DEStartBAP.Properties.DisplayFormat.FormatString = "dd MMMM yyyy"
+        Me.DEStartBAP.Properties.DisplayFormat.FormatType = DevExpress.Utils.FormatType.DateTime
+        Me.DEStartBAP.Properties.EditFormat.FormatString = "dd MMMM yyyy"
+        Me.DEStartBAP.Properties.EditFormat.FormatType = DevExpress.Utils.FormatType.DateTime
+        Me.DEStartBAP.Properties.Mask.EditMask = "dd MMMM yyyy"
+        Me.DEStartBAP.Size = New System.Drawing.Size(136, 20)
+        Me.DEStartBAP.TabIndex = 8920
+        '
+        'GCBAP
+        '
+        Me.GCBAP.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.GCBAP.Location = New System.Drawing.Point(0, 45)
+        Me.GCBAP.MainView = Me.GVBAP
+        Me.GCBAP.Name = "GCBAP"
+        Me.GCBAP.RepositoryItems.AddRange(New DevExpress.XtraEditors.Repository.RepositoryItem() {Me.RepositoryItemMemoEdit2})
+        Me.GCBAP.Size = New System.Drawing.Size(897, 440)
+        Me.GCBAP.TabIndex = 3
+        Me.GCBAP.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() {Me.GVBAP})
+        '
+        'GVBAP
+        '
+        Me.GVBAP.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() {Me.GridColumn11, Me.GridColumn15, Me.GridColumn16, Me.GridColumn12, Me.GridColumn13, Me.GridColumn14, Me.GridColumn19, Me.GridColumn20})
+        Me.GVBAP.GridControl = Me.GCBAP
+        Me.GVBAP.Name = "GVBAP"
+        Me.GVBAP.OptionsBehavior.Editable = False
+        Me.GVBAP.OptionsBehavior.ReadOnly = True
+        Me.GVBAP.OptionsView.RowAutoHeight = True
+        Me.GVBAP.OptionsView.ShowGroupPanel = False
+        '
+        'GridColumn11
+        '
+        Me.GridColumn11.Caption = "ID"
+        Me.GridColumn11.FieldName = "id_scan_return_bap"
+        Me.GridColumn11.Name = "GridColumn11"
+        '
+        'GridColumn12
+        '
+        Me.GridColumn12.Caption = "BAP Number"
+        Me.GridColumn12.FieldName = "bap_number"
+        Me.GridColumn12.Name = "GridColumn12"
+        Me.GridColumn12.Visible = True
+        Me.GridColumn12.VisibleIndex = 1
+        Me.GridColumn12.Width = 236
+        '
+        'GridColumn13
+        '
+        Me.GridColumn13.Caption = "BAP Date"
+        Me.GridColumn13.FieldName = "bap_date"
+        Me.GridColumn13.Name = "GridColumn13"
+        Me.GridColumn13.Visible = True
+        Me.GridColumn13.VisibleIndex = 0
+        Me.GridColumn13.Width = 137
+        '
+        'GridColumn14
+        '
+        Me.GridColumn14.Caption = "Condition"
+        Me.GridColumn14.FieldName = "package_condition"
+        Me.GridColumn14.Name = "GridColumn14"
+        Me.GridColumn14.Visible = True
+        Me.GridColumn14.VisibleIndex = 3
+        Me.GridColumn14.Width = 214
+        '
+        'RepositoryItemMemoEdit2
+        '
+        Me.RepositoryItemMemoEdit2.Name = "RepositoryItemMemoEdit2"
+        '
+        'GridColumn19
+        '
+        Me.GridColumn19.Caption = "Created By"
+        Me.GridColumn19.FieldName = "employee_name"
+        Me.GridColumn19.Name = "GridColumn19"
+        Me.GridColumn19.Visible = True
+        Me.GridColumn19.VisibleIndex = 4
+        Me.GridColumn19.Width = 214
+        '
+        'GridColumn20
+        '
+        Me.GridColumn20.Caption = "Created Date"
+        Me.GridColumn20.DisplayFormat.FormatString = "dd MMMM yyyy"
+        Me.GridColumn20.DisplayFormat.FormatType = DevExpress.Utils.FormatType.DateTime
+        Me.GridColumn20.FieldName = "created_date"
+        Me.GridColumn20.Name = "GridColumn20"
+        Me.GridColumn20.Visible = True
+        Me.GridColumn20.VisibleIndex = 5
+        Me.GridColumn20.Width = 222
+        '
+        'GridColumn15
+        '
+        Me.GridColumn15.Caption = "ID Comp"
+        Me.GridColumn15.FieldName = "id_comp"
+        Me.GridColumn15.Name = "GridColumn15"
+        '
+        'GridColumn16
+        '
+        Me.GridColumn16.Caption = "3PL"
+        Me.GridColumn16.FieldName = "comp_name"
+        Me.GridColumn16.Name = "GridColumn16"
+        Me.GridColumn16.Visible = True
+        Me.GridColumn16.VisibleIndex = 2
+        '
         'FormScanReturn
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(903, 513)
-        Me.Controls.Add(Me.GCAwb)
-        Me.Controls.Add(Me.PanelControl1)
+        Me.Controls.Add(Me.XTCScanReturn)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
         Me.MaximizeBox = False
         Me.MinimizeBox = False
@@ -250,6 +468,20 @@ Partial Class FormScanReturn
         CType(Me.GCAwb, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.GVAwb, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.RepositoryItemMemoEdit1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.XTCScanReturn, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.XTCScanReturn.ResumeLayout(False)
+        Me.XTPScanReturn.ResumeLayout(False)
+        Me.XTPBAP.ResumeLayout(False)
+        CType(Me.PanelControl2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.PanelControl2.ResumeLayout(False)
+        Me.PanelControl2.PerformLayout()
+        CType(Me.DEUntilBAP.Properties.CalendarTimeProperties, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DEUntilBAP.Properties, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DEStartBAP.Properties.CalendarTimeProperties, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DEStartBAP.Properties, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.GCBAP, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.GVBAP, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.RepositoryItemMemoEdit2, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
@@ -273,4 +505,24 @@ Partial Class FormScanReturn
     Friend WithEvents GridColumn10 As DevExpress.XtraGrid.Columns.GridColumn
     Friend WithEvents GridColumn4 As DevExpress.XtraGrid.Columns.GridColumn
     Friend WithEvents GridColumn5 As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents XTCScanReturn As DevExpress.XtraTab.XtraTabControl
+    Friend WithEvents XTPScanReturn As DevExpress.XtraTab.XtraTabPage
+    Friend WithEvents XTPBAP As DevExpress.XtraTab.XtraTabPage
+    Friend WithEvents GCBAP As DevExpress.XtraGrid.GridControl
+    Friend WithEvents GVBAP As DevExpress.XtraGrid.Views.Grid.GridView
+    Friend WithEvents GridColumn11 As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GridColumn12 As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GridColumn13 As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GridColumn14 As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents RepositoryItemMemoEdit2 As DevExpress.XtraEditors.Repository.RepositoryItemMemoEdit
+    Friend WithEvents GridColumn19 As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GridColumn20 As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents PanelControl2 As DevExpress.XtraEditors.PanelControl
+    Friend WithEvents BViewBAP As DevExpress.XtraEditors.SimpleButton
+    Friend WithEvents DEUntilBAP As DevExpress.XtraEditors.DateEdit
+    Friend WithEvents LabelControl1 As DevExpress.XtraEditors.LabelControl
+    Friend WithEvents LabelControl2 As DevExpress.XtraEditors.LabelControl
+    Friend WithEvents DEStartBAP As DevExpress.XtraEditors.DateEdit
+    Friend WithEvents GridColumn15 As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GridColumn16 As DevExpress.XtraGrid.Columns.GridColumn
 End Class
