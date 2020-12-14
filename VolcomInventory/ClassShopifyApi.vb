4b8980c93abff8dbcd843b3074918ee47aa112a1
Satrya Anas <catur.satrya@gmail.com>
2020-12-14 10:29:44 +0800

    VIOS ol_order_qty

--- VolcomInventory/ClassShopifyApi.vb
+++ VolcomInventory/ClassShopifyApi.vb
@@ -338,11 +338,12 @@
 
                             'detail line item
                             Dim qins As String = "INSERT tb_ol_store_order(id, sales_order_ol_shop_number, sales_order_ol_shop_date, customer_name, shipping_name, shipping_address,shipping_address1,shipping_address2, shipping_phone, 
-                    shipping_city, shipping_post_code, shipping_region, payment_method, tracking_code, ol_store_sku, ol_store_id, sku, design_price, sales_order_det_qty, grams, financial_status, total_disc_order, discount_allocations_amo,checkout_id, shipping_price, discount_code, id_comp_group) VALUES "
+                    shipping_city, shipping_post_code, shipping_region, payment_method, tracking_code, ol_store_sku, ol_store_id, sku, design_price, ol_order_qty, sales_order_det_qty, grams, financial_status, total_disc_order, discount_allocations_amo,checkout_id, shipping_price, discount_code, id_comp_group) VALUES "
                             Dim ol_store_sku As String = ""
                             Dim ol_store_id As String = ""
                             Dim sku As String = ""
                             Dim design_price As String = ""
+                            Dim ol_order_qty As String = ""
                             Dim sales_order_det_qty As String = ""
                             Dim grams As String = ""
                             Dim discount_allocations_amo As String = "0"
@@ -352,6 +353,7 @@
                                 ol_store_id = row_item("id").ToString
                                 sku = row_item("sku").ToString.Replace("-GWP", "").Trim
                                 design_price = decimalSQL(row_item("price").ToString)
+                                ol_order_qty = decimalSQL(row_item("quantity").ToString)
                                 sales_order_det_qty = decimalSQL(row_item("quantity").ToString)
                                 grams = decimalSQL(row_item("grams").ToString)
 
@@ -369,7 +371,7 @@
                                     qins += ","
                                 End If
                                 qins += "('" + id + "', '" + sales_order_ol_shop_number + "', '" + sales_order_ol_shop_date + "', '" + addSlashes(customer_name) + "', '" + addSlashes(shipping_name) + "', '" + addSlashes(shipping_address) + "','" + addSlashes(shipping_address1) + "','" + addSlashes(shipping_address2) + "', '" + addSlashes(shipping_phone) + "', 
-                        '" + addSlashes(shipping_city) + "', '" + addSlashes(shipping_post_code) + "', '" + addSlashes(shipping_region) + "', '" + payment_method + "', '" + tracking_code + "', '" + ol_store_sku + "', '" + ol_store_id + "', '" + sku + "', '" + design_price + "', '" + sales_order_det_qty + "','" + grams + "', '" + addSlashes(financial_status) + "', '" + total_discounts + "', '" + discount_allocations_amo + "','" + addSlashes(checkout_id) + "', '" + shipping_price + "', '" + discount_code + "', '" + id_comp_group + "') "
+                        '" + addSlashes(shipping_city) + "', '" + addSlashes(shipping_post_code) + "', '" + addSlashes(shipping_region) + "', '" + payment_method + "', '" + tracking_code + "', '" + ol_store_sku + "', '" + ol_store_id + "', '" + sku + "', '" + design_price + "', '" + ol_order_qty + "', '" + sales_order_det_qty + "','" + grams + "', '" + addSlashes(financial_status) + "', '" + total_discounts + "', '" + discount_allocations_amo + "','" + addSlashes(checkout_id) + "', '" + shipping_price + "', '" + discount_code + "', '" + id_comp_group + "') "
                                 i += 1
                             Next
 
