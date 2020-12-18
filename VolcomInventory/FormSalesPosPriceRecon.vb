28664d5ecd4ebb4c354d987c11efada5ba1f777b
Satrya Anas <catur.satrya@gmail.com>
2020-12-18 16:30:12 +0800

    prepare form price reckom

---
+++ VolcomInventory/FormSalesPosPriceRecon.vb
@@ -0,0 +1,6 @@
+﻿Public Class FormSalesPosPriceRecon
+    Public id As String = "-1"
+    Private Sub FormSalesPosPriceRecon_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
