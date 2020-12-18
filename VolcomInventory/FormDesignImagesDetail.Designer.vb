67deea54c5e6d40e88667b933fe0a1a057152149
Agus Friastana <friastana@gmail.com>
2020-12-18 16:48:53 +0800

    add form images detail

---
+++ VolcomInventory/FormDesignImagesDetail.Designer.vb
@@ -0,0 +1,398 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FormDesignImagesDetail
+    Inherits DevExpress.XtraEditors.XtraForm
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        If disposing AndAlso components IsNot Nothing Then
+            components.Dispose()
+        End If
+        MyBase.Dispose(disposing)
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.XtraTabControl1 = New DevExpress.XtraTab.XtraTabControl()
+        Me.XtraTabPageZ = New DevExpress.XtraTab.XtraTabPage()
+        Me.GCZ = New DevExpress.XtraGrid.GridControl()
+        Me.GVZ = New DevExpress.XtraGrid.Views.Grid.GridView()
+        Me.GridColumn1 = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.RepositoryItemPictureEdit = New DevExpress.XtraEditors.Repository.RepositoryItemPictureEdit()
+        Me.GridColumn2 = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GridColumn3 = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GridColumn4 = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.XtraTabPageV = New DevExpress.XtraTab.XtraTabPage()
+        Me.GCV = New DevExpress.XtraGrid.GridControl()
+        Me.GVV = New DevExpress.XtraGrid.Views.Grid.GridView()
+        Me.GridColumn5 = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.RepositoryItemPictureEdit1 = New DevExpress.XtraEditors.Repository.RepositoryItemPictureEdit()
+        Me.GridColumn6 = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GridColumn7 = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GridColumn8 = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.XtraTabPageSQ = New DevExpress.XtraTab.XtraTabPage()
+        Me.GCSQ = New DevExpress.XtraGrid.GridControl()
+        Me.GVSQ = New DevExpress.XtraGrid.Views.Grid.GridView()
+        Me.GridColumn9 = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.RepositoryItemPictureEdit2 = New DevExpress.XtraEditors.Repository.RepositoryItemPictureEdit()
+        Me.GridColumn10 = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GridColumn11 = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GridColumn12 = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.XtraTabPageTH = New DevExpress.XtraTab.XtraTabPage()
+        Me.GCTH = New DevExpress.XtraGrid.GridControl()
+        Me.GVTH = New DevExpress.XtraGrid.Views.Grid.GridView()
+        Me.GridColumn13 = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.RepositoryItemPictureEdit3 = New DevExpress.XtraEditors.Repository.RepositoryItemPictureEdit()
+        Me.GridColumn14 = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GridColumn15 = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GridColumn16 = New DevExpress.XtraGrid.Columns.GridColumn()
+        CType(Me.XtraTabControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.XtraTabControl1.SuspendLayout()
+        Me.XtraTabPageZ.SuspendLayout()
+        CType(Me.GCZ, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.GVZ, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.RepositoryItemPictureEdit, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.XtraTabPageV.SuspendLayout()
+        CType(Me.GCV, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.GVV, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.RepositoryItemPictureEdit1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.XtraTabPageSQ.SuspendLayout()
+        CType(Me.GCSQ, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.GVSQ, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.RepositoryItemPictureEdit2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.XtraTabPageTH.SuspendLayout()
+        CType(Me.GCTH, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.GVTH, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.RepositoryItemPictureEdit3, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'XtraTabControl1
+        '
+        Me.XtraTabControl1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.XtraTabControl1.Location = New System.Drawing.Point(0, 0)
+        Me.XtraTabControl1.Name = "XtraTabControl1"
+        Me.XtraTabControl1.SelectedTabPage = Me.XtraTabPageZ
+        Me.XtraTabControl1.Size = New System.Drawing.Size(784, 528)
+        Me.XtraTabControl1.TabIndex = 0
+        Me.XtraTabControl1.TabPages.AddRange(New DevExpress.XtraTab.XtraTabPage() {Me.XtraTabPageZ, Me.XtraTabPageV, Me.XtraTabPageSQ, Me.XtraTabPageTH})
+        '
+        'XtraTabPageZ
+        '
+        Me.XtraTabPageZ.Controls.Add(Me.GCZ)
+        Me.XtraTabPageZ.Name = "XtraTabPageZ"
+        Me.XtraTabPageZ.Size = New System.Drawing.Size(778, 500)
+        Me.XtraTabPageZ.Text = "Zalora"
+        '
+        'GCZ
+        '
+        Me.GCZ.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.GCZ.Location = New System.Drawing.Point(0, 0)
+        Me.GCZ.MainView = Me.GVZ
+        Me.GCZ.Name = "GCZ"
+        Me.GCZ.RepositoryItems.AddRange(New DevExpress.XtraEditors.Repository.RepositoryItem() {Me.RepositoryItemPictureEdit})
+        Me.GCZ.Size = New System.Drawing.Size(778, 500)
+        Me.GCZ.TabIndex = 0
+        Me.GCZ.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() {Me.GVZ})
+        '
+        'GVZ
+        '
+        Me.GVZ.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() {Me.GridColumn1, Me.GridColumn2, Me.GridColumn3, Me.GridColumn4})
+        Me.GVZ.GridControl = Me.GCZ
+        Me.GVZ.Name = "GVZ"
+        Me.GVZ.OptionsBehavior.ReadOnly = True
+        Me.GVZ.OptionsView.ShowGroupPanel = False
+        Me.GVZ.RowHeight = 200
+        '
+        'GridColumn1
+        '
+        Me.GridColumn1.Caption = "Photo"
+        Me.GridColumn1.ColumnEdit = Me.RepositoryItemPictureEdit
+        Me.GridColumn1.FieldName = "image"
+        Me.GridColumn1.MaxWidth = 138
+        Me.GridColumn1.MinWidth = 138
+        Me.GridColumn1.Name = "GridColumn1"
+        Me.GridColumn1.UnboundType = DevExpress.Data.UnboundColumnType.[Object]
+        Me.GridColumn1.Width = 138
+        '
+        'RepositoryItemPictureEdit
+        '
+        Me.RepositoryItemPictureEdit.Name = "RepositoryItemPictureEdit"
+        '
+        'GridColumn2
+        '
+        Me.GridColumn2.Caption = "File Name"
+        Me.GridColumn2.FieldName = "file_name"
+        Me.GridColumn2.Name = "GridColumn2"
+        Me.GridColumn2.Visible = True
+        Me.GridColumn2.VisibleIndex = 0
+        '
+        'GridColumn3
+        '
+        Me.GridColumn3.Caption = "URL"
+        Me.GridColumn3.FieldName = "url"
+        Me.GridColumn3.Name = "GridColumn3"
+        '
+        'GridColumn4
+        '
+        Me.GridColumn4.Caption = "Sort"
+        Me.GridColumn4.FieldName = "sort"
+        Me.GridColumn4.Name = "GridColumn4"
+        '
+        'XtraTabPageV
+        '
+        Me.XtraTabPageV.Controls.Add(Me.GCV)
+        Me.XtraTabPageV.Name = "XtraTabPageV"
+        Me.XtraTabPageV.Size = New System.Drawing.Size(778, 500)
+        Me.XtraTabPageV.Text = "Volcom Online Store"
+        '
+        'GCV
+        '
+        Me.GCV.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.GCV.Location = New System.Drawing.Point(0, 0)
+        Me.GCV.MainView = Me.GVV
+        Me.GCV.Name = "GCV"
+        Me.GCV.RepositoryItems.AddRange(New DevExpress.XtraEditors.Repository.RepositoryItem() {Me.RepositoryItemPictureEdit1})
+        Me.GCV.Size = New System.Drawing.Size(778, 500)
+        Me.GCV.TabIndex = 1
+        Me.GCV.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() {Me.GVV})
+        '
+        'GVV
+        '
+        Me.GVV.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() {Me.GridColumn5, Me.GridColumn6, Me.GridColumn7, Me.GridColumn8})
+        Me.GVV.GridControl = Me.GCV
+        Me.GVV.Name = "GVV"
+        Me.GVV.OptionsBehavior.ReadOnly = True
+        Me.GVV.OptionsView.ShowGroupPanel = False
+        Me.GVV.RowHeight = 200
+        '
+        'GridColumn5
+        '
+        Me.GridColumn5.Caption = "Photo"
+        Me.GridColumn5.ColumnEdit = Me.RepositoryItemPictureEdit1
+        Me.GridColumn5.FieldName = "image"
+        Me.GridColumn5.MaxWidth = 150
+        Me.GridColumn5.MinWidth = 150
+        Me.GridColumn5.Name = "GridColumn5"
+        Me.GridColumn5.UnboundType = DevExpress.Data.UnboundColumnType.[Object]
+        Me.GridColumn5.Width = 150
+        '
+        'RepositoryItemPictureEdit1
+        '
+        Me.RepositoryItemPictureEdit1.Name = "RepositoryItemPictureEdit1"
+        '
+        'GridColumn6
+        '
+        Me.GridColumn6.Caption = "File Name"
+        Me.GridColumn6.FieldName = "file_name"
+        Me.GridColumn6.Name = "GridColumn6"
+        Me.GridColumn6.Visible = True
+        Me.GridColumn6.VisibleIndex = 0
+        '
+        'GridColumn7
+        '
+        Me.GridColumn7.Caption = "URL"
+        Me.GridColumn7.FieldName = "url"
+        Me.GridColumn7.Name = "GridColumn7"
+        '
+        'GridColumn8
+        '
+        Me.GridColumn8.Caption = "Sort"
+        Me.GridColumn8.FieldName = "sort"
+        Me.GridColumn8.Name = "GridColumn8"
+        '
+        'XtraTabPageSQ
+        '
+        Me.XtraTabPageSQ.Controls.Add(Me.GCSQ)
+        Me.XtraTabPageSQ.Name = "XtraTabPageSQ"
+        Me.XtraTabPageSQ.Size = New System.Drawing.Size(778, 500)
+        Me.XtraTabPageSQ.Text = "Shopee && BliBli"
+        '
+        'GCSQ
+        '
+        Me.GCSQ.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.GCSQ.Location = New System.Drawing.Point(0, 0)
+        Me.GCSQ.MainView = Me.GVSQ
+        Me.GCSQ.Name = "GCSQ"
+        Me.GCSQ.RepositoryItems.AddRange(New DevExpress.XtraEditors.Repository.RepositoryItem() {Me.RepositoryItemPictureEdit2})
+        Me.GCSQ.Size = New System.Drawing.Size(778, 500)
+        Me.GCSQ.TabIndex = 2
+        Me.GCSQ.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() {Me.GVSQ})
+        '
+        'GVSQ
+        '
+        Me.GVSQ.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() {Me.GridColumn9, Me.GridColumn10, Me.GridColumn11, Me.GridColumn12})
+        Me.GVSQ.GridControl = Me.GCSQ
+        Me.GVSQ.Name = "GVSQ"
+        Me.GVSQ.OptionsBehavior.ReadOnly = True
+        Me.GVSQ.OptionsView.ShowGroupPanel = False
+        Me.GVSQ.RowHeight = 200
+        '
+        'GridColumn9
+        '
+        Me.GridColumn9.Caption = "Photo"
+        Me.GridColumn9.ColumnEdit = Me.RepositoryItemPictureEdit2
+        Me.GridColumn9.FieldName = "image"
+        Me.GridColumn9.MaxWidth = 200
+        Me.GridColumn9.MinWidth = 200
+        Me.GridColumn9.Name = "GridColumn9"
+        Me.GridColumn9.UnboundType = DevExpress.Data.UnboundColumnType.[Object]
+        Me.GridColumn9.Width = 200
+        '
+        'RepositoryItemPictureEdit2
+        '
+        Me.RepositoryItemPictureEdit2.Name = "RepositoryItemPictureEdit2"
+        '
+        'GridColumn10
+        '
+        Me.GridColumn10.Caption = "File Name"
+        Me.GridColumn10.FieldName = "file_name"
+        Me.GridColumn10.Name = "GridColumn10"
+        Me.GridColumn10.Visible = True
+        Me.GridColumn10.VisibleIndex = 0
+        '
+        'GridColumn11
+        '
+        Me.GridColumn11.Caption = "URL"
+        Me.GridColumn11.FieldName = "url"
+        Me.GridColumn11.Name = "GridColumn11"
+        '
+        'GridColumn12
+        '
+        Me.GridColumn12.Caption = "Sort"
+        Me.GridColumn12.FieldName = "sort"
+        Me.GridColumn12.Name = "GridColumn12"
+        '
+        'XtraTabPageTH
+        '
+        Me.XtraTabPageTH.Controls.Add(Me.GCTH)
+        Me.XtraTabPageTH.Name = "XtraTabPageTH"
+        Me.XtraTabPageTH.Size = New System.Drawing.Size(778, 500)
+        Me.XtraTabPageTH.Text = "Thumbnail"
+        '
+        'GCTH
+        '
+        Me.GCTH.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.GCTH.Location = New System.Drawing.Point(0, 0)
+        Me.GCTH.MainView = Me.GVTH
+        Me.GCTH.Name = "GCTH"
+        Me.GCTH.RepositoryItems.AddRange(New DevExpress.XtraEditors.Repository.RepositoryItem() {Me.RepositoryItemPictureEdit3})
+        Me.GCTH.Size = New System.Drawing.Size(778, 500)
+        Me.GCTH.TabIndex = 3
+        Me.GCTH.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() {Me.GVTH})
+        '
+        'GVTH
+        '
+        Me.GVTH.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() {Me.GridColumn13, Me.GridColumn14, Me.GridColumn15, Me.GridColumn16})
+        Me.GVTH.GridControl = Me.GCTH
+        Me.GVTH.Name = "GVTH"
+        Me.GVTH.OptionsBehavior.ReadOnly = True
+        Me.GVTH.OptionsView.ShowGroupPanel = False
+        Me.GVTH.RowHeight = 100
+        '
+        'GridColumn13
+        '
+        Me.GridColumn13.Caption = "Photo"
+        Me.GridColumn13.ColumnEdit = Me.RepositoryItemPictureEdit3
+        Me.GridColumn13.FieldName = "image"
+        Me.GridColumn13.MaxWidth = 100
+        Me.GridColumn13.MinWidth = 100
+        Me.GridColumn13.Name = "GridColumn13"
+        Me.GridColumn13.UnboundType = DevExpress.Data.UnboundColumnType.[Object]
+        Me.GridColumn13.Width = 100
+        '
+        'RepositoryItemPictureEdit3
+        '
+        Me.RepositoryItemPictureEdit3.Name = "RepositoryItemPictureEdit3"
+        '
+        'GridColumn14
+        '
+        Me.GridColumn14.Caption = "File Name"
+        Me.GridColumn14.FieldName = "file_name"
+        Me.GridColumn14.Name = "GridColumn14"
+        Me.GridColumn14.Visible = True
+        Me.GridColumn14.VisibleIndex = 0
+        '
+        'GridColumn15
+        '
+        Me.GridColumn15.Caption = "URL"
+        Me.GridColumn15.FieldName = "url"
+        Me.GridColumn15.Name = "GridColumn15"
+        '
+        'GridColumn16
+        '
+        Me.GridColumn16.Caption = "Sort"
+        Me.GridColumn16.FieldName = "sort"
+        Me.GridColumn16.Name = "GridColumn16"
+        '
+        'FormDesignImagesDetail
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(784, 528)
+        Me.Controls.Add(Me.XtraTabControl1)
+        Me.Name = "FormDesignImagesDetail"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Design Images Detail"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+        CType(Me.XtraTabControl1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.XtraTabControl1.ResumeLayout(False)
+        Me.XtraTabPageZ.ResumeLayout(False)
+        CType(Me.GCZ, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.GVZ, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.RepositoryItemPictureEdit, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.XtraTabPageV.ResumeLayout(False)
+        CType(Me.GCV, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.GVV, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.RepositoryItemPictureEdit1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.XtraTabPageSQ.ResumeLayout(False)
+        CType(Me.GCSQ, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.GVSQ, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.RepositoryItemPictureEdit2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.XtraTabPageTH.ResumeLayout(False)
+        CType(Me.GCTH, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.GVTH, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.RepositoryItemPictureEdit3, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents XtraTabControl1 As DevExpress.XtraTab.XtraTabControl
+    Friend WithEvents XtraTabPageZ As DevExpress.XtraTab.XtraTabPage
+    Friend WithEvents XtraTabPageV As DevExpress.XtraTab.XtraTabPage
+    Friend WithEvents XtraTabPageSQ As DevExpress.XtraTab.XtraTabPage
+    Friend WithEvents XtraTabPageTH As DevExpress.XtraTab.XtraTabPage
+    Friend WithEvents GCZ As DevExpress.XtraGrid.GridControl
+    Friend WithEvents GVZ As DevExpress.XtraGrid.Views.Grid.GridView
+    Friend WithEvents GridColumn1 As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GridColumn2 As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GridColumn3 As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GridColumn4 As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents RepositoryItemPictureEdit As DevExpress.XtraEditors.Repository.RepositoryItemPictureEdit
+    Friend WithEvents GCV As DevExpress.XtraGrid.GridControl
+    Friend WithEvents GVV As DevExpress.XtraGrid.Views.Grid.GridView
+    Friend WithEvents GridColumn5 As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents RepositoryItemPictureEdit1 As DevExpress.XtraEditors.Repository.RepositoryItemPictureEdit
+    Friend WithEvents GridColumn6 As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GridColumn7 As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GridColumn8 As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GCSQ As DevExpress.XtraGrid.GridControl
+    Friend WithEvents GVSQ As DevExpress.XtraGrid.Views.Grid.GridView
+    Friend WithEvents GridColumn9 As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents RepositoryItemPictureEdit2 As DevExpress.XtraEditors.Repository.RepositoryItemPictureEdit
+    Friend WithEvents GridColumn10 As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GridColumn11 As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GridColumn12 As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GCTH As DevExpress.XtraGrid.GridControl
+    Friend WithEvents GVTH As DevExpress.XtraGrid.Views.Grid.GridView
+    Friend WithEvents GridColumn13 As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents RepositoryItemPictureEdit3 As DevExpress.XtraEditors.Repository.RepositoryItemPictureEdit
+    Friend WithEvents GridColumn14 As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GridColumn15 As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GridColumn16 As DevExpress.XtraGrid.Columns.GridColumn
+End Class
