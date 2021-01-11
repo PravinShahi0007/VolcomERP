c55b0eb722bbebb8a352b9decff150f40b2d2f98
Satrya Anas <catur.satrya@gmail.com>
2021-01-11 12:21:56 +0800

    compare stock vios

---
+++ VolcomInventory/FormCompareStockWebHistory.vb
@@ -0,0 +1,29 @@
+﻿Public Class FormCompareStockWebHistory
+    Private Sub FormCompareStockWebHistory_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        viewData()
+    End Sub
+
+    Sub viewData()
+        Cursor = Cursors.WaitCursor
+        Dim query As String = "SELECT s.id_log_compare_shopify, s.sync_date, s.sync_by, emp.employee_name
+FROM tb_log_compare_shopify s
+INNER JOIN tb_m_user us ON us.id_user = s.sync_by
+INNER JOIN tb_m_employee emp ON emp.id_employee  = us.id_employee
+ORDER BY s.sync_date DESC"
+        Dim data As DataTable = execute_query(query, -1, True, "", "", "", "")
+        GCData.DataSource = data
+        GVData.BestFitColumns()
+        Cursor = Cursors.Default
+    End Sub
+
+    Private Sub FormCompareStockWebHistory_FormClosed(sender As Object, e As FormClosedEventArgs) Handles MyBase.FormClosed
+        Dispose()
+    End Sub
+
+    Private Sub GVData_DoubleClick(sender As Object, e As EventArgs) Handles GVData.DoubleClick
+        If GVData.RowCount > 0 And GVData.FocusedRowHandle >= 0 Then
+            FormCompareStockWebsite.viewCompare(GVData.GetFocusedRowCellValue("id_log_compare_shopify").ToString)
+            Close()
+        End If
+    End Sub
+End Class
--- VolcomInventory/FormCompareStockWebsite.vb
+++ VolcomInventory/FormCompareStockWebsite.vb
@@ -1,10 +1,17 @@
 ﻿Public Class FormCompareStockWebsite
+    Public id_last_sync As String = "-1"
     Private Sub FormCompareStockWebsite_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Cursor = Cursors.WaitCursor
-        Dim qry As String = "SELECT s.date AS `last_sync` FROM tb_m_stock_shopify s ORDER BY s.date DESC LIMIT 1 "
-        Dim dt As DataTable = execute_query(qry, -1, True, "", "", "", "")
-        LabelLast.Text = DateTime.Parse(dt.Rows(0)("last_sync").ToString).ToString("dd MMMM yyyy ")
+        getLastSync()
         Cursor = Cursors.Default
+    End Sub
+
+    Sub getLastSync()
+        Dim qry As String = "SELECT IFNULL(c.id_log_compare_shopify,0) AS `id_log_compare_shopify`,c.sync_date AS `sync_date`
+FROM tb_log_compare_shopify c ORDER BY c.sync_date DESC LIMIT 1 "
+        Dim dt As DataTable = execute_query(qry, -1, True, "", "", "", "")
+        id_last_sync = dt.Rows(0)("id_log_compare_shopify").ToString
+        LabelLast.Text = DateTime.Parse(dt.Rows(0)("sync_date").ToString).ToString("dd MMMM yyyy HH:mm:ss")
     End Sub
 
     Private Sub FormCompareStockWebsite_Activated(sender As Object, e As EventArgs) Handles MyBase.Activated
@@ -26,27 +33,38 @@
     End Sub
 
     Private Sub SBSync_Click(sender As Object, e As EventArgs) Handles SBSync.Click
-        Cursor = Cursors.WaitCursor
-        FormMain.SplashScreenManager1.ShowWaitForm()
+        Dim confirm As DialogResult = DevExpress.XtraEditors.XtraMessageBox.Show("Pastikan Anda sudah melakukan sinkronisasi untuk 'Paid Order', agar komparasi data lebih valid. Lanjut komparasi stok ?", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button2)
+        If confirm = Windows.Forms.DialogResult.Yes Then
+            Cursor = Cursors.WaitCursor
+            FormMain.SplashScreenManager1.ShowWaitForm()
 
-        FormMain.SplashScreenManager1.SetWaitFormDescription("Get stock from website")
-        Dim cls As ClassShopifyApi = New ClassShopifyApi
+            'save date sync
+            Dim query As String = "INSERT INTO tb_log_compare_shopify(sync_date, sync_by) VALUES(NOW(), '" + id_user + "'); SELECT LAST_INSERT_ID(); "
+            Dim id_new As String = execute_query(query, 0, True, "", "", "", "")
 
-        cls.sync_stock()
+            FormMain.SplashScreenManager1.SetWaitFormDescription("Get stock from website")
+            Dim cls As ClassShopifyApi = New ClassShopifyApi
+            cls.sync_stock(id_new)
 
-        FormMain.SplashScreenManager1.SetWaitFormDescription("Compare stock with ERP")
-        viewCompare()
-        FormMain.SplashScreenManager1.CloseWaitForm()
-        Cursor = Cursors.Default
+            FormMain.SplashScreenManager1.SetWaitFormDescription("Get open order")
+            cls.get_open_order(id_new)
+
+            FormMain.SplashScreenManager1.SetWaitFormDescription("Compare stock with ERP")
+            viewCompare(id_new)
+            FormMain.SplashScreenManager1.CloseWaitForm()
+            Cursor = Cursors.Default
+        End If
+
     End Sub
 
-    Sub viewCompare()
+    Sub viewCompare(ByVal id_log As String)
         Cursor = Cursors.WaitCursor
-        Dim query As String = "CALL view_stock_compare_erp_shopify()"
+        Dim query As String = "CALL view_stock_compare_erp_shopify(" + id_log + ")"
 
         Dim data As DataTable = execute_query(query, -1, True, "", "", "", "")
 
         GridControlStock.DataSource = data
+        GridViewStock.BestFitColumns()
         Cursor = Cursors.Default
     End Sub
 
@@ -68,6 +86,12 @@
     End Sub
 
     Private Sub SimpleButton1_Click(sender As Object, e As EventArgs) Handles SimpleButton1.Click
-        viewCompare()
+        viewCompare(id_last_sync)
+    End Sub
+
+    Private Sub BtnHistory_Click(sender As Object, e As EventArgs) Handles BtnHistory.Click
+        Cursor = Cursors.WaitCursor
+        FormCompareStockWebHistory.ShowDialog()
+        Cursor = Cursors.Default
     End Sub
 End Class
