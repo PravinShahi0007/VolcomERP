4775d11d4ca680cc0aa5e5a19226ec79bce20230
Agus Friastana <friastana@gmail.com>
2021-02-25 16:17:04 +0800

    adjustment og fix item list

--- VolcomInventory/FormAdjustmentOGPick.vb
+++ VolcomInventory/FormAdjustmentOGPick.vb
@@ -28,6 +28,50 @@
         Dim stc As New ClassPurcItemStock()
         Dim query As String = stc.queryGetStock(dept, cat, date_until_selected)
         Dim data As DataTable = execute_query(query, -1, True, "", "", "", "")
+
+        'add zero
+        If FormAdjustmentOGDet.SLUEType.EditValue.ToString = "1" Then
+            For i = 0 To data.Rows.Count - 1
+                in_item += ", " + data.Rows(i)("id_item").ToString
+            Next
+
+            Dim data_z As DataTable = execute_query("
+                SELECT uom_stock.uom, " + FormAdjustmentOGDet.SLUEFromDepartment.EditValue.ToString + " AS id_departement, '" + FormAdjustmentOGDet.SLUEFromDepartment.Text + "' AS departement, i.id_item, i.item_desc, i.id_item_cat, c.item_cat, 0 AS qty, 0 AS qty_req, t.avg_cost AS `value`, '' AS remark
+                FROM tb_item AS i
+                INNER JOIN tb_item_cat AS c ON c.id_item_cat = i.id_item_cat
+                INNER JOIN tb_m_uom uom ON uom.id_uom = i.id_uom
+                LEFT JOIN tb_m_uom uom_stock ON uom_stock.id_uom = i.id_uom_stock
+                LEFT JOIN (
+	                SELECT a.id_item, a.avg_cost 
+	                FROM (
+		                SELECT a.id_item, a.avg_cost 
+		                FROM tb_item_avg_cost a
+		                ORDER BY a.id_item_avg_cost DESC
+	                ) a
+	                GROUP BY a.id_item
+                ) t ON t.id_item = i.id_item
+                WHERE i.id_item NOT IN (" + in_item + ")
+            ", -1, True, "", "", "", "")
+
+            For i = 0 To data_z.Rows.Count - 1
+                Dim r As DataRow = data.NewRow
+
+                r("uom") = data_z.Rows(i)("uom")
+                r("id_departement") = data_z.Rows(i)("id_departement")
+                r("departement") = data_z.Rows(i)("departement")
+                r("id_item") = data_z.Rows(i)("id_item")
+                r("item_desc") = data_z.Rows(i)("item_desc")
+                r("id_item_cat") = data_z.Rows(i)("id_item_cat")
+                r("item_cat") = data_z.Rows(i)("item_cat")
+                r("qty") = data_z.Rows(i)("qty")
+                r("qty_req") = data_z.Rows(i)("qty_req")
+                r("value") = data_z.Rows(i)("value")
+                r("remark") = data_z.Rows(i)("remark")
+
+                data.Rows.Add(r)
+            Next
+        End If
+
         GCList.DataSource = data
         GVList.BestFitColumns()
         Cursor = Cursors.Default
