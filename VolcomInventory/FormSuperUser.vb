833b4cd74240760ee5123fad1f8366d77e0195ca
Putu Septian Primadewa <iansaviour@gmail.com>
2021-02-25 10:35:49 +0800

    temp table

--- VolcomInventory/FormSuperUser.vb
+++ VolcomInventory/FormSuperUser.vb
@@ -19,7 +19,7 @@
     End Function
 
     Private Sub BtnOther_Click(sender As Object, e As EventArgs) Handles BtnOther.Click
-
+        FormTempTable.ShowDialog()
         'gen stiker
         '        Dim q As String = "SELECT so.sales_order_ol_shop_number 
         'FROM tb_sales_order so 
