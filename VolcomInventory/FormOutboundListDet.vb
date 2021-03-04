19983dee424ba2e63e498f2ef2d37cb501be7795
Putu Septian Primadewa <iansaviour@gmail.com>
2021-03-04 13:12:52 +0800

    bisa banyak , scan pemisah hilangkan

--- VolcomInventory/FormOutboundListDet.vb
+++ VolcomInventory/FormOutboundListDet.vb
@@ -20,10 +20,15 @@
         '
         view_del_type()
         '
-        Dim q As String = "SELECT awb.id_awbill,awb.ol_number,dis.sub_district,c.comp_name
-FROM tb_wh_awbill awb 
+        Dim q As String = "SELECT awb.id_awbill,SUM(awbd.qty) AS qty,dis.sub_district,IFNULL(c.comp_name,cg.description) AS comp_name,awb.ol_number
+FROM `tb_wh_awbill` awb 
 INNER JOIN tb_m_sub_district dis ON dis.id_sub_district=awb.id_sub_district
-INNER JOIN tb_m_comp c ON c.id_comp=awb.id_store
+INNER JOIN tb_wh_awbill_det awbd ON awbd.id_awbill=awb.id_awbill
+LEFT JOIN tb_m_comp c ON c.id_comp=awb.id_store
+INNER JOIN tb_pl_sales_order_del pl ON pl.id_pl_sales_order_del=awbd.id_pl_sales_order_del
+INNER JOIN tb_m_comp_contact ccx ON ccx.id_comp_contact = pl.id_store_contact_to
+INNER JOIN tb_m_comp cx ON cx.id_comp = ccx.id_comp
+INNER JOIN tb_m_comp_group cg ON cg.`id_comp_group`=cx.`id_comp_group`
 WHERE awb.id_awbill='" & id_awb & "'"
         Dim dt As DataTable = execute_query(q, -1, True, "", "", "", "")
         If dt.Rows.Count > 0 Then
