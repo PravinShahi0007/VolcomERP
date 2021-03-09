ccba4428eefda4f90cede2f53d4523e0cc2dfffb
Satrya Anas <catur.satrya@gmail.com>
2021-03-09 12:30:30 +0800

    Hide RT2 di Rec. Repair

--- VolcomInventory/FormPopUpContact.vb
+++ VolcomInventory/FormPopUpContact.vb
@@ -183,7 +183,7 @@
         End If
 
         If id_pop_up = "87" Then
-            query += "AND tb_m_comp.is_only_for_alloc=2 "
+            query += "AND tb_m_comp.is_only_for_alloc=2 AND tb_m_comp.id_comp<>'" + get_setup_field("wh_temp") + "' "
         End If
 
 
