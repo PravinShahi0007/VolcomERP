34b733a78d668fc431dd57d155290aa5c3a23a78
Putu Septian Primadewa <iansaviour@gmail.com>
2021-03-24 17:48:03 +0800

    balance shee vs prev month done

--- VolcomInventory/FormReportBalanceSheet.vb
+++ VolcomInventory/FormReportBalanceSheet.vb
@@ -1461,17 +1461,37 @@ WHERE DATE(atx.`date_tax_report`)>='" + Date.Parse(DETaxFrom.EditValue.ToString)
         If XTCMonthlyReport.SelectedTabPageIndex = 0 Then
 
         ElseIf XTCMonthlyReport.SelectedTabPageIndex = 1 Then
-            Cursor = Cursors.WaitCursor
+            If GVMProfitLoss.RowCount > 0 Then
+                Cursor = Cursors.WaitCursor
 
-            Dim Report As New ReportMonthlyIS()
-            Report.dt = GCMProfitLoss.DataSource
-            Report.languange = "eng"
-            Report.LTitle.Text = "PT. Volcom Indonesia Income Statement for Period Ending " & Date.Parse(DEMonthlyReport.EditValue.ToString).ToString("MMMM yyyy")
+                Dim Report As New ReportMonthlyIS()
+                Report.dt = GCMProfitLoss.DataSource
+                Report.languange = "eng"
+                Report.LTitle.Text = "PT. Volcom Indonesia Income Statement for Period Ending " & Date.Parse(DEMonthlyReport.EditValue.ToString).ToString("MMMM yyyy")
 
-            Dim Tool As DevExpress.XtraReports.UI.ReportPrintTool = New DevExpress.XtraReports.UI.ReportPrintTool(Report)
-            Tool.ShowPreviewDialog()
+                Dim Tool As DevExpress.XtraReports.UI.ReportPrintTool = New DevExpress.XtraReports.UI.ReportPrintTool(Report)
+                Tool.ShowPreviewDialog()
 
-            Cursor = Cursors.Default
+                Cursor = Cursors.Default
+            End If
+        ElseIf XTCMonthlyReport.SelectedTabPageIndex = 2 Then
+            If GVMBSvsPrevMonth.RowCount > 0 Then
+                Cursor = Cursors.WaitCursor
+
+                Dim Report As New ReportMonthlyBSvs()
+                Report.dt = GCMBSvsPrevMonth.DataSource
+                Report.languange = "eng"
+
+                Dim q As String = "SELECT DATE_FORMAT('" & Date.Parse(DEMonthlyReport.EditValue.ToString).ToString("yyyy-MM-dd") & "','%M %Y') AS this_month,DATE_FORMAT(DATE_SUB('" & Date.Parse(DEMonthlyReport.EditValue.ToString).ToString("yyyy-MM-dd") & "',INTERVAL 1 MONTH),'%M %Y') AS prev_month "
+                Dim dt As DataTable = execute_query(q, -1, True, "", "", "", "")
+
+                Report.DataSource = dt
+
+                Dim Tool As DevExpress.XtraReports.UI.ReportPrintTool = New DevExpress.XtraReports.UI.ReportPrintTool(Report)
+                Tool.ShowPreviewDialog()
+
+                Cursor = Cursors.Default
+            End If
         End If
 
     End Sub
@@ -1497,8 +1517,8 @@ WHERE DATE(atx.`date_tax_report`)>='" + Date.Parse(DETaxFrom.EditValue.ToString)
         If e.SummaryProcess = DevExpress.Data.CustomSummaryProcess.Calculate Then
             Select Case summaryID
                 Case 1 'group
-                    bs_vpm_t_group += GVMBSvsPrevMonth.GetRowCellValue(e.RowHandle, "prev_month")
-                    bs_vpm_p_group += GVMBSvsPrevMonth.GetRowCellValue(e.RowHandle, "this_month")
+                    bs_vpm_t_group += GVMBSvsPrevMonth.GetRowCellValue(e.RowHandle, "this_month")
+                    bs_vpm_p_group += GVMBSvsPrevMonth.GetRowCellValue(e.RowHandle, "prev_month")
                 Case 2 'sum
                     bs_vpm_t_sum += GVMBSvsPrevMonth.GetRowCellValue(e.RowHandle, "this_month")
                     bs_vpm_p_sum += GVMBSvsPrevMonth.GetRowCellValue(e.RowHandle, "prev_month")
--- VolcomInventory/ReportMonthlyBSvs.vb
+++ VolcomInventory/ReportMonthlyBSvs.vb
@@ -5,29 +5,43 @@
     Private Sub ReportMonthlyBSvs_BeforePrint(sender As Object, e As Printing.PrintEventArgs) Handles MyBase.BeforePrint
         Dim font_row_style As New Font("Segoe UI", 9, FontStyle.Regular)
 
-        Dim tot_report_sub As Decimal = 0.00
-        Dim tot_report_head As Decimal = 0.00
-        Dim tot_sub As Decimal = 0.00
-        Dim tot_head As Decimal = 0.00
+        Dim tot_sub_t As Decimal = 0.00
+        Dim tot_head_t As Decimal = 0.00
+        Dim tot_sub_p As Decimal = 0.00
+        Dim tot_head_p As Decimal = 0.00
 
-        Dim total As Decimal = 0.00
+        Dim total_t As Decimal = 0.00
+        Dim total_p As Decimal = 0.00
 
         For i = 0 To dt.Rows.Count - 1
+            If i = 0 Then
+                add_head(dt.Rows(i)("head_desc").ToString, "", "", "")
+                add_head(dt.Rows(i)("sub_desc").ToString, "", "", "")
+            Else
+                If Not dt.Rows(i)("head_desc").ToString = dt.Rows(i - 1)("head_desc").ToString Then
+                    add_head(dt.Rows(i)("head_desc").ToString, "", "", "")
+                End If
+                If Not dt.Rows(i)("sub_desc").ToString = dt.Rows(i - 1)("sub_desc").ToString Then
+                    add_head(dt.Rows(i)("sub_desc").ToString, "", "", "")
+                End If
+            End If
+
+
             'detail
             Dim row_det As DevExpress.XtraReports.UI.XRTableRow = New DevExpress.XtraReports.UI.XRTableRow
             row_det.HeightF = 25
 
             'Desc
-            Dim no As DevExpress.XtraReports.UI.XRTableCell = New DevExpress.XtraReports.UI.XRTableCell
-            no.Text = dt.Rows(i)("acc_description").ToString
-            no.TextAlignment = DevExpress.XtraPrinting.TextAlignment.MiddleLeft
-            no.BackColor = Color.Transparent
-            no.Font = font_row_style
-            no.WidthF = 240
-            no.Borders = DevExpress.XtraPrinting.BorderSide.Left Or DevExpress.XtraPrinting.BorderSide.Bottom
-            no.Padding = 10
+            Dim desc_ As DevExpress.XtraReports.UI.XRTableCell = New DevExpress.XtraReports.UI.XRTableCell
+            desc_.Text = dt.Rows(i)("acc_description").ToString
+            desc_.TextAlignment = DevExpress.XtraPrinting.TextAlignment.MiddleLeft
+            desc_.BackColor = Color.Transparent
+            desc_.Font = font_row_style
+            desc_.WidthF = 220
+            desc_.Borders = DevExpress.XtraPrinting.BorderSide.Left Or DevExpress.XtraPrinting.BorderSide.Bottom
+            desc_.Padding = 10
 
-            row_det.Cells.Add(no)
+            row_det.Cells.Add(desc_)
 
             'prev month
             Dim prev_month As DevExpress.XtraReports.UI.XRTableCell = New DevExpress.XtraReports.UI.XRTableCell
@@ -35,11 +49,11 @@
             prev_month.TextAlignment = DevExpress.XtraPrinting.TextAlignment.MiddleRight
             prev_month.BackColor = Color.Transparent
             prev_month.Font = font_row_style
-            prev_month.WidthF = 240
+            prev_month.WidthF = 190
             prev_month.Borders = DevExpress.XtraPrinting.BorderSide.Left Or DevExpress.XtraPrinting.BorderSide.Bottom
             prev_month.Padding = 10
             '
-            total += dt.Rows(i)("prev_month")
+            total_p += dt.Rows(i)("prev_month")
             '
             row_det.Cells.Add(prev_month)
 
@@ -49,139 +63,95 @@
             this_month.TextAlignment = DevExpress.XtraPrinting.TextAlignment.MiddleRight
             this_month.BackColor = Color.Transparent
             this_month.Font = font_row_style
-            this_month.WidthF = 240
+            this_month.WidthF = 190
             this_month.Borders = DevExpress.XtraPrinting.BorderSide.Left Or DevExpress.XtraPrinting.BorderSide.Bottom
             this_month.Padding = 10
             '
-            total += dt.Rows(i)("this_month")
+            total_t += dt.Rows(i)("this_month")
             '
             row_det.Cells.Add(this_month)
 
             'Percentage
             Dim percent As DevExpress.XtraReports.UI.XRTableCell = New DevExpress.XtraReports.UI.XRTableCell
-            percent.Text = Decimal.Parse(Math.Abs(dt.Rows(i)("percentage")).ToString).ToString("N2") & " % "
+            percent.Text = Decimal.Parse(dt.Rows(i)("percentage").ToString).ToString("N2") & " % "
             percent.TextAlignment = DevExpress.XtraPrinting.TextAlignment.MiddleRight
             percent.BackColor = Color.Transparent
             percent.Font = font_row_style
-            percent.WidthF = 243
+            percent.WidthF = 123
             percent.Borders = DevExpress.XtraPrinting.BorderSide.Left Or DevExpress.XtraPrinting.BorderSide.Bottom Or DevExpress.XtraPrinting.BorderSide.Right
             percent.Padding = 10
 
             row_det.Cells.Add(percent)
             XrTable2.Rows.Add(row_det)
             '
-            '    If Not i = dt.Rows.Count - 1 Then
-            '        'sub
-            '        If Not dt.Rows(i)("sub_desc").ToString = dt.Rows(i + 1)("sub_desc").ToString Then
-            '            tot_sub += dt.Rows(i)("this_month")
+            If Not i = dt.Rows.Count - 1 Then
+                'sub
+                If Not dt.Rows(i)("sub_desc").ToString = dt.Rows(i + 1)("sub_desc").ToString Then
+                    tot_sub_t += dt.Rows(i)("this_month")
+                    tot_sub_p += dt.Rows(i)("prev_month")
 
-            '            Dim percentage As Decimal = (tot_sub / dt.Rows(i)("this_month_sale")) * 100
+                    Dim percentage As Decimal = 0.00
+                    Try
+                        percentage = ((tot_sub_t - tot_sub_p) / tot_sub_t) * 100
+                    Catch ex As Exception
 
-            '            If dt.Rows(i)("sub_name").ToString = "8" Or dt.Rows(i)("sub_name").ToString = "6" Then
-            '                'other income + cogs skip
-            '            Else
-            '                add_head(dt.Rows(i)("sub_desc").ToString, Decimal.Parse(Math.Abs(tot_sub).ToString).ToString("N2"), Decimal.Parse(Math.Abs(percentage).ToString).ToString("N0"))
-            '            End If
+                    End Try
 
-            '            tot_sub = 0
-            '        Else
-            '            tot_sub += dt.Rows(i)("this_month")
-            '        End If
-            '        'head
-            '        If Not dt.Rows(i)("head_desc").ToString = dt.Rows(i + 1)("head_desc").ToString Then
-            '            tot_head += dt.Rows(i)("this_month")
+                    add_head("Total " & dt.Rows(i)("sub_desc").ToString, Decimal.Parse(Math.Abs(tot_sub_p).ToString).ToString("N2"), Decimal.Parse(Math.Abs(tot_sub_t).ToString).ToString("N2"), Decimal.Parse(percentage.ToString).ToString("N2"))
 
-            '            Dim percentage As Decimal = (tot_head / dt.Rows(i)("this_month_sale")) * 100
+                    tot_sub_t = 0
+                    tot_sub_p = 0
+                Else
+                    tot_sub_t += dt.Rows(i)("this_month")
+                    tot_sub_p += dt.Rows(i)("prev_month")
+                End If
+                'head
+                If Not dt.Rows(i)("head_desc").ToString = dt.Rows(i + 1)("head_desc").ToString Then
+                    tot_head_t += dt.Rows(i)("this_month")
+                    tot_head_p += dt.Rows(i)("prev_month")
 
-            '            If dt.Rows(i)("head_name").ToString = "4" Then
-            '                'biaya skip
-            '            Else
-            '                add_head(dt.Rows(i)("head_desc").ToString, Decimal.Parse(Math.Abs(tot_head).ToString).ToString("N2"), Decimal.Parse(Math.Abs(percentage).ToString).ToString("N0"))
-            '            End If
+                    Dim percentage As Decimal = 0.00
+                    Try
+                        percentage = ((tot_head_t - tot_head_p) / tot_head_t) * 100
+                    Catch ex As Exception
 
-            '            tot_head = 0
-            '        Else
-            '            tot_head += dt.Rows(i)("this_month")
-            '        End If
-            '        'report sub
-            '        If Not dt.Rows(i)("report_sub").ToString = dt.Rows(i + 1)("report_sub").ToString Then
-            '            tot_report_sub += dt.Rows(i)("this_month")
+                    End Try
 
-            '            Dim percentage As Decimal = (tot_report_sub / dt.Rows(i)("this_month_sale")) * 100
+                    add_head("Total " & dt.Rows(i)("head_desc").ToString, Decimal.Parse(Math.Abs(tot_head_p).ToString).ToString("N2"), Decimal.Parse(Math.Abs(tot_head_t).ToString).ToString("N2"), Decimal.Parse(percentage.ToString).ToString("N2"))
 
-            '            If dt.Rows(i)("id_consolidation_report_sub").ToString = "1" Then 'operasional
-            '                If languange = "ind" Then
-            '                    If tot_report_sub > 0 Then
-            '                        add_head("Keuntungan Operasional", Decimal.Parse(Math.Abs(tot_report_sub).ToString).ToString("N2"), Decimal.Parse(Math.Abs(percentage).ToString).ToString("N0"))
-            '                    Else
-            '                        add_head("Kerugian Operasional", Decimal.Parse(Math.Abs(tot_report_sub).ToString).ToString("N2"), Decimal.Parse(Math.Abs(percentage).ToString).ToString("N0"))
-            '                    End If
-            '                Else
-            '                    If tot_report_sub > 0 Then
-            '                        add_head("Operational Profit", Decimal.Parse(Math.Abs(tot_report_sub).ToString).ToString("N2"), Decimal.Parse(Math.Abs(percentage).ToString).ToString("N0"))
-            '                    Else
-            '                        add_head("Operational Loss", Decimal.Parse(Math.Abs(tot_report_sub).ToString).ToString("N2"), Decimal.Parse(Math.Abs(percentage).ToString).ToString("N0"))
-            '                    End If
-            '                End If
-            '            ElseIf dt.Rows(i)("id_consolidation_report_sub").ToString = "1" Then 'biaya/pendapatan 
-            '                If languange = "ind" Then
-            '                    If tot_report_sub > 0 Then
-            '                        add_head("Pendapatan Lain-lain", Decimal.Parse(Math.Abs(tot_report_sub).ToString).ToString("N2"), Decimal.Parse(Math.Abs(percentage).ToString).ToString("N0"))
-            '                    Else
-            '                        add_head("Biaya Lain-lain", Decimal.Parse(Math.Abs(tot_report_sub).ToString).ToString("N2"), Decimal.Parse(Math.Abs(percentage).ToString).ToString("N0"))
-            '                    End If
-            '                Else
-            '                    If tot_report_sub > 0 Then
-            '                        add_head("Other Income", Decimal.Parse(Math.Abs(tot_report_sub).ToString).ToString("N2"), Decimal.Parse(Math.Abs(percentage).ToString).ToString("N0"))
-            '                    Else
-            '                        add_head("Other Expense", Decimal.Parse(Math.Abs(tot_report_sub).ToString).ToString("N2"), Decimal.Parse(Math.Abs(percentage).ToString).ToString("N0"))
-            '                    End If
-            '                End If
-            '            End If
+                    tot_head_t = 0
+                    tot_head_p = 0
+                Else
+                    tot_head_t += dt.Rows(i)("this_month")
+                    tot_head_p += dt.Rows(i)("prev_month")
+                End If
+            ElseIf i = dt.Rows.Count - 1 Then
+                'end footer net profit after tax
+                'sub
+                tot_sub_t += dt.Rows(i)("this_month")
+                tot_sub_p += dt.Rows(i)("prev_month")
 
-            '            tot_report_sub = 0
-            '        Else
-            '            tot_report_sub += dt.Rows(i)("this_month")
-            '        End If
-            '        'report head
-            '        If Not dt.Rows(i)("report_head").ToString = dt.Rows(i + 1)("report_head").ToString Then
-            '            tot_report_head += dt.Rows(i)("this_month")
+                Dim percentage As Decimal = 0.00
+                Try
+                    percentage = ((tot_sub_t - tot_sub_p) / tot_sub_t) * 100
+                Catch ex As Exception
 
-            '            Dim percentage As Decimal = (tot_report_head / dt.Rows(i)("this_month_sale")) * 100
-            '            If languange = "ind" Then
-            '                If tot_report_head > 0 Then
-            '                    add_head("Keuntungan Bersih Sebelum Pajak", Decimal.Parse(Math.Abs(tot_report_head).ToString).ToString("N2"), Decimal.Parse(Math.Abs(percentage).ToString).ToString("N0"))
-            '                Else
-            '                    add_head("Kerugian Bersih Sebelum Pajak", Decimal.Parse(Math.Abs(tot_report_head).ToString).ToString("N2"), Decimal.Parse(Math.Abs(percentage).ToString).ToString("N0"))
-            '                End If
-            '            Else
-            '                If tot_report_head > 0 Then
-            '                    add_head("Net Profit Before Tax", Decimal.Parse(Math.Abs(tot_report_head).ToString).ToString("N2"), Decimal.Parse(Math.Abs(percentage).ToString).ToString("N0"))
-            '                Else
-            '                    add_head("Net Loss Before Tax", Decimal.Parse(Math.Abs(tot_report_head).ToString).ToString("N2"), Decimal.Parse(Math.Abs(percentage).ToString).ToString("N0"))
-            '                End If
-            '            End If
+                End Try
 
-            '            tot_report_head = 0
-            '        Else
-            '            tot_report_head += dt.Rows(i)("this_month")
-            '        End If
-            '    ElseIf i = dt.Rows.Count - 1 Then
-            '        'end footer net profit after tax
-            '        If languange = "ind" Then
-            '            If total > 0 Then
-            '                add_head("Keuntungan Bersih Setelah Pajak", Decimal.Parse(Math.Abs(total).ToString).ToString("N2"), "100")
-            '            Else
-            '                add_head("Kerugian Bersih Setelah Pajak", Decimal.Parse(Math.Abs(total).ToString).ToString("N2"), "100")
-            '            End If
-            '        Else
-            '            If total > 0 Then
-            '                add_head("Net Profit After Tax", Decimal.Parse(Math.Abs(total).ToString).ToString("N2"), "100")
-            '            Else
-            '                add_head("Net Loss After Tax", Decimal.Parse(Math.Abs(total).ToString).ToString("N2"), "100")
-            '            End If
-            '        End If
-            '    End If
+                add_head("Total " & dt.Rows(i)("sub_desc").ToString, Decimal.Parse(Math.Abs(tot_sub_p).ToString).ToString("N2"), Decimal.Parse(Math.Abs(tot_sub_t).ToString).ToString("N2"), Decimal.Parse(percentage.ToString).ToString("N2"))
+                'header
+                tot_head_t += dt.Rows(i)("this_month")
+                tot_head_p += dt.Rows(i)("prev_month")
+
+                Dim percentage_head As Decimal = 0.00
+                Try
+                    percentage_head = ((tot_head_t - tot_head_p) / tot_head_t) * 100
+                Catch ex As Exception
+
+                End Try
+
+                add_head("Total " & dt.Rows(i)("head_desc").ToString, Decimal.Parse(Math.Abs(tot_head_p).ToString).ToString("N2"), Decimal.Parse(Math.Abs(tot_head_t).ToString).ToString("N2"), Decimal.Parse(percentage_head.ToString).ToString("N2"))
+            End If
         Next
         '
         pre_load_list_horz("303", 2, 1, XRTableMark)
@@ -212,7 +182,7 @@
         prev_month.BackColor = Color.Transparent
         prev_month.Font = font_row_style
         prev_month.WidthF = 190
-        prev_month.Borders = DevExpress.XtraPrinting.BorderSide.Left Or DevExpress.XtraPrinting.BorderSide.Bottom Or DevExpress.XtraPrinting.BorderSide.Right
+        prev_month.Borders = DevExpress.XtraPrinting.BorderSide.Left Or DevExpress.XtraPrinting.BorderSide.Bottom
         prev_month.Padding = 10
 
         row_det.Cells.Add(prev_month)
@@ -231,7 +201,12 @@
 
         'Percentage
         Dim percent As DevExpress.XtraReports.UI.XRTableCell = New DevExpress.XtraReports.UI.XRTableCell
-        percent.Text = percentage & " % "
+        If percentage = "" Then
+            percent.Text = ""
+        Else
+            percent.Text = percentage & " % "
+        End If
+
         percent.TextAlignment = DevExpress.XtraPrinting.TextAlignment.MiddleRight
         percent.BackColor = Color.Transparent
         percent.Font = font_row_style
