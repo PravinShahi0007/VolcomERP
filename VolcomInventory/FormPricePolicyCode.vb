0f5609a10bddbcaef5948c22b53ac052f43b5cfd
Satrya Anas <catur.satrya@gmail.com>
2021-03-30 16:08:41 +0800

    perbaikan view list

--- VolcomInventory/FormPricePolicyCode.vb
+++ VolcomInventory/FormPricePolicyCode.vb
@@ -5,8 +5,18 @@
 
     Sub viewData()
         Cursor = Cursors.WaitCursor
-        Dim cpc As New ClassPricePolicy()
-        GCData.DataSource = cpc.dataMain("-1", "1")
+        Dim query As String = "SELECT p.id_code_detail, cd.display_name AS `code`, cd.code_detail_name AS `description`,
+        CONCAT(CAST(MAX(CASE WHEN p.id_disc_type=1 THEN p.age_min END) AS DECIMAL(5,0)),'-',CAST(MAX(CASE WHEN p.id_disc_type=1 THEN p.age_max END) AS DECIMAL(5,0))) AS `normal_view`,
+        CONCAT(CAST(MAX(CASE WHEN p.id_disc_type=2 THEN p.age_min END) AS DECIMAL(5,0)),'-',CAST(MAX(CASE WHEN p.id_disc_type=2 THEN p.age_max END) AS DECIMAL(5,0))) AS `mkd_30_view`,
+        CONCAT(CAST(MAX(CASE WHEN p.id_disc_type=3 THEN p.age_min END) AS DECIMAL(5,0)),'-',CAST(MAX(CASE WHEN p.id_disc_type=3 THEN p.age_max END) AS DECIMAL(5,0))) AS `mkd_50_view`,
+        CONCAT(CAST(MAX(CASE WHEN p.id_disc_type=4 THEN p.age_min END) AS DECIMAL(5,0)),'-',CAST(MAX(CASE WHEN p.id_disc_type=4 THEN p.age_max END) AS DECIMAL(5,0))) AS `mkd_70_view`
+        FROM tb_m_design_price_policy p
+        INNER JOIN tb_lookup_disc_type dt ON dt.id_disc_type = p.id_disc_type
+        INNER JOIN tb_m_code_detail cd ON cd.id_code_detail = p.id_code_detail
+        GROUP BY p.id_code_detail
+        ORDER BY cd.display_name ASC "
+        Dim data As DataTable = execute_query(query, -1, True, "", "", "", "")
+        GCData.DataSource = data
         GVData.BestFitColumns()
         Cursor = Cursors.Default
     End Sub
