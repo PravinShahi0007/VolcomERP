8dfe023a85e91366c89f3fe288bb339a8f615241
Satrya Anas <catur.satrya@gmail.com>
2021-03-31 12:35:57 +0800

    prepare zalora comm addition

--- VolcomInventory/FormPayoutZaloraComm.vb
+++ VolcomInventory/FormPayoutZaloraComm.vb
@@ -40,6 +40,8 @@
 	        WHERE d.id_payout_zalora='" + id + "' AND (t.id_payout_zalora_cat=3 OR t.id_payout_zalora_cat=5) AND a.id_acc<>'" + id_acc_default_comm + "' AND a.is_use_ref=2
         ) a
         GROUP BY a.id_payout_zalora "
+        'addition
+        viewAddition()
         Dim dex As DataTable = execute_query(qex, -1, True, "", "", "", "")
         If dex.Rows.Count > 0 Then
             TxtOtherExpense.EditValue = dex.Rows(0)("erp_amount")
@@ -58,6 +60,7 @@
             TxtComm.Enabled = False
             TxtCommTax.Enabled = False
             PanelControl1.Visible = False
+            PanelControl4.Visible = False
         End If
         Cursor = Cursors.Default
     End Sub
@@ -103,5 +106,38 @@
         FormPayoutZaloraDet.XTCData.SelectedTabPageIndex = 1
         FormPayoutZaloraDet.GVData.ActiveFilterString = "[id_payout_zalora_cat]='3' OR [id_payout_zalora_cat]='5'"
         Close()
+    End Sub
+
+    Private Sub SimpleButton1_Click(sender As Object, e As EventArgs) Handles BtnDelete.Click
+        Dim confirm As DialogResult = DevExpress.XtraEditors.XtraMessageBox.Show("Are you sure want to delete this data ?", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button2)
+        If confirm = Windows.Forms.DialogResult.Yes Then
+            Dim id_zalora_comm_addition As String = GVData.GetFocusedRowCellValue("id_zalora_comm_addition").ToString
+            Dim query As String = "DELETE FROM tb_payout_zalora_comm_addition WHERE id_zalora_comm_addition='" + id_zalora_comm_addition + "' "
+            execute_non_query(query, True, "", "", "", "")
+            viewAddition()
+        End If
+    End Sub
+
+    Sub viewAddition()
+        Cursor = Cursors.WaitCursor
+        Dim query As String = "SELECT a.id_zalora_comm_addition, a.id_payout_zalora, 
+a.id_zalora_comm_type, ct.zalora_comm_type, 
+a.id_acc, coa.acc_name, coa.acc_description,
+a.`value`, a.note 
+FROM tb_payout_zalora_comm_addition a
+INNER JOIN tb_payout_zalora_comm_type ct ON ct.id_zalora_comm_type = a.id_zalora_comm_type
+INNER JOIN tb_a_acc coa ON coa.id_acc = a.id_acc
+WHERE a.id_payout_zalora=" + id + " "
+        Dim data As DataTable = execute_query(query, -1, True, "", "", "", "")
+        GCData.DataSource = data
+        GVData.BestFitColumns()
+        Cursor = Cursors.Default
+    End Sub
+
+    Private Sub BtnAdd_Click(sender As Object, e As EventArgs) Handles BtnAdd.Click
+        Cursor = Cursors.WaitCursor
+        FormPayoutZaloraCommAdd.id_payout = id
+        FormPayoutZaloraCommAdd.ShowDialog()
+        Cursor = Cursors.Default
     End Sub
 End Class
---
+++ VolcomInventory/FormPayoutZaloraCommAdd.vb
@@ -0,0 +1,44 @@
+﻿Public Class FormPayoutZaloraCommAdd
+    Public id_payout As String = "-1"
+    Private Sub FormPayoutZaloraCommAdd_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        viewCOA()
+        actionLoad()
+    End Sub
+
+    Private Sub FormPayoutZaloraCommAdd_FormClosed(sender As Object, e As FormClosedEventArgs) Handles MyBase.FormClosed
+        Dispose()
+    End Sub
+
+    Private Sub BtnDiscard_Click(sender As Object, e As EventArgs) Handles BtnDiscard.Click
+        Close()
+    End Sub
+
+    Private Sub BtnConfirm_Click(sender As Object, e As EventArgs) Handles BtnConfirm.Click
+        Dim id_payout_zalora As String = id_payout
+        Dim id_acc As String = SLECOA.EditValue.ToString
+        Dim adj_value As String = decimalSQL(TxtAmount.EditValue.ToString)
+        Dim note As String = addSlashes(MENote.Text)
+
+        'insert
+        Dim query As String = "INSERT INTO tb_payout_zalora_comm_addition(id_payout_zalora, id_zalora_comm_type, id_acc, value, note) 
+        VALUES('" + id_payout_zalora + "','" + +"'); "
+        execute_non_query(query, True, "", "", "", "")
+        FormPayoutZaloraComm.viewAddition()
+        actionLoad()
+    End Sub
+
+    Sub viewCOA()
+        Dim query As String = "SELECT a.id_acc, a.acc_name, a.acc_description,CONCAT(a.acc_name,' - ', a.acc_description) AS `acc`, a.id_acc_parent, 
+        a.id_acc_parent, a.id_acc_cat, a.id_is_det, a.id_status, a.id_comp
+        FROM tb_a_acc a WHERE a.id_status=1 AND a.id_is_det=2 AND a.id_coa_type='1' "
+        viewSearchLookupQuery(SLECOA, query, "id_acc", "acc", "id_acc")
+    End Sub
+
+    Sub actionLoad()
+        Cursor = Cursors.WaitCursor
+        MENote.Text = ""
+        TxtAmount.EditValue = 0.00
+        MENote.Focus()
+        Cursor = Cursors.Default
+    End Sub
+End Class
