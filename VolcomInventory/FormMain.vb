81277ca7134a03258e2d3cd0a52bd212a312367b
Satrya Anas <catur.satrya@gmail.com>
2021-04-16 10:17:57 +0800

    new menu : proposal turun harga

--- VolcomInventory/FormMain.vb
+++ VolcomInventory/FormMain.vb
@@ -16013,4 +16013,8 @@ WHERE pddr.id_prod_demand_design='" & FormProduction.GVDesign.GetFocusedRowCellV
         End Try
         Cursor = Cursors.Default
     End Sub
+
+    Private Sub NBProposePriceMKD_LinkClicked(sender As Object, e As DevExpress.XtraNavBar.NavBarLinkEventArgs) Handles NBProposePriceMKD.LinkClicked
+
+    End Sub
 End Class
