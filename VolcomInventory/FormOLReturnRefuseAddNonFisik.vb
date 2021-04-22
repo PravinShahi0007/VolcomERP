aaa898e261abf7f89a277615a8d3274b04cda3f3
Satrya Anas <catur.satrya@gmail.com>
2021-04-22 12:33:56 +0800

    Untuk return data hanya yang OOS saja

--- VolcomInventory/FormOLReturnRefuseAddNonFisik.vb
+++ VolcomInventory/FormOLReturnRefuseAddNonFisik.vb
@@ -34,41 +34,58 @@
     Sub choose()
         If GVData.RowCount > 0 And GVData.FocusedRowHandle >= 0 Then
             Cursor = Cursors.WaitCursor
+            Dim id_sales_order_det As String = GVData.GetFocusedRowCellValue("id_sales_order_det").ToString
             Dim code As String = GVData.GetFocusedRowCellValue("code_list").ToString
             Dim dtf As DataRow() = FormOLReturnRefuseDet.dt.Select("[code_list]='" + code + "' AND [is_used]='2'")
             If dtf.Length <= 0 Then
                 stopCustomDialog("Code : " + code + " not found or already scanned.")
                 Close()
             Else
-                makeSafeGV(GVData)
-                Dim newRow As DataRow = (TryCast(FormOLReturnRefuseDet.GCData.DataSource, DataTable)).NewRow()
-                newRow("id_return_refuse_det") = "0"
-                newRow("id_return_refuse") = "0"
-                newRow("id_sales_order_det") = dtf(0)("id_sales_order_det").ToString
-                newRow("id_sales_return_order_det") = "0"
-                newRow("id_sales_pos_det_cn") = "0"
-                newRow("id_product") = dtf(0)("id_product").ToString
-                newRow("id_pl_prod_order_rec_det_unique") = dtf(0)("id_pl_prod_order_rec_det_unique").ToString
-                newRow("scanned_code") = dtf(0)("code_list").ToString
-                newRow("name") = dtf(0)("name").ToString
-                newRow("size") = dtf(0)("size").ToString
-                newRow("item_id") = dtf(0)("item_id").ToString
-                newRow("ol_store_id") = dtf(0)("ol_store_id").ToString
-                newRow("id_design_price") = dtf(0)("id_design_price").ToString
-                newRow("design_price") = dtf(0)("design_price")
-                newRow("qty") = 1
+                'cek status returned
+                Dim qstt As String = "SELECT a.`status` FROM tb_sales_order_det_status a
+                WHERE a.id_sales_order_det='" + id_sales_order_det + "' 
+                ORDER BY a.status_date DESC LIMIT 1 "
+                Dim dstt As DataTable = execute_query(qstt, -1, True, "", "", "", "")
+                Dim stt_last As String = ""
+                If dstt.Rows.Count <= 0 Then
+                    stt_last = ""
+                Else
+                    stt_last = dstt.Rows(0)("status").ToString
+                End If
+                If stt_last <> "canceled" And stt_last <> "cancelled" And stt_last <> "failed" And stt_last <> "returned" Then
+                    stopCustom("This item can't be processed, because status is " + stt_last)
+                    Exit Sub
+                End If
 
-                TryCast(FormOLReturnRefuseDet.GCData.DataSource, DataTable).Rows.Add(newRow)
-                FormOLReturnRefuseDet.GCData.RefreshDataSource()
-                FormOLReturnRefuseDet.GVData.RefreshData()
-                FormOLReturnRefuseDet.GVData.FocusedRowHandle = FormOLReturnRefuseDet.GVData.RowCount - 1
-                '
-                dtf(0)("is_used") = "1"
-                '
-                'refresh
-                viewData()
-            End If
-            Cursor = Cursors.WaitCursor
+                makeSafeGV(GVData)
+                    Dim newRow As DataRow = (TryCast(FormOLReturnRefuseDet.GCData.DataSource, DataTable)).NewRow()
+                    newRow("id_return_refuse_det") = "0"
+                    newRow("id_return_refuse") = "0"
+                    newRow("id_sales_order_det") = dtf(0)("id_sales_order_det").ToString
+                    newRow("id_sales_return_order_det") = "0"
+                    newRow("id_sales_pos_det_cn") = "0"
+                    newRow("id_product") = dtf(0)("id_product").ToString
+                    newRow("id_pl_prod_order_rec_det_unique") = dtf(0)("id_pl_prod_order_rec_det_unique").ToString
+                    newRow("scanned_code") = dtf(0)("code_list").ToString
+                    newRow("name") = dtf(0)("name").ToString
+                    newRow("size") = dtf(0)("size").ToString
+                    newRow("item_id") = dtf(0)("item_id").ToString
+                    newRow("ol_store_id") = dtf(0)("ol_store_id").ToString
+                    newRow("id_design_price") = dtf(0)("id_design_price").ToString
+                    newRow("design_price") = dtf(0)("design_price")
+                    newRow("qty") = 1
+
+                    TryCast(FormOLReturnRefuseDet.GCData.DataSource, DataTable).Rows.Add(newRow)
+                    FormOLReturnRefuseDet.GCData.RefreshDataSource()
+                    FormOLReturnRefuseDet.GVData.RefreshData()
+                    FormOLReturnRefuseDet.GVData.FocusedRowHandle = FormOLReturnRefuseDet.GVData.RowCount - 1
+                    '
+                    dtf(0)("is_used") = "1"
+                    '
+                    'refresh
+                    viewData()
+                End If
+                Cursor = Cursors.WaitCursor
         End If
     End Sub
 
