526293711a404121b6bf73e7adf45e502c61ee8c
Satrya Anas <catur.satrya@gmail.com>
2021-05-07 15:29:30 +0800

    last approved

--- VolcomInventory/FormProposePriceMKD.vb
+++ VolcomInventory/FormProposePriceMKD.vb
@@ -6,8 +6,26 @@
 
     Sub viewSummary()
         Cursor = Cursors.WaitCursor
-        Dim mkd As New ClassProposePriceMKD()
-        Dim query As String = mkd.queryMain("-1", "2")
+        Dim query As String = "SELECT p.id_pp_change, p.`number`, p.created_date, p.effective_date, p.soh_sal_date, 
+        p.note, p.id_report_status, rs.report_status,p.id_design_price_type, p.id_design_mkd, pt.design_price_type, p.is_confirm, p.confirm_date,
+        la.employee_name
+        FROM tb_pp_change p 
+        INNER JOIN tb_lookup_report_status rs ON rs.id_report_status = p.id_report_status
+        LEFT JOIN tb_lookup_design_price_type pt ON pt.id_design_price_type = p.id_design_price_type
+        LEFT JOIN (
+	        SELECT a.id_report, a.id_user, a.username, a.employee_name 
+	        FROM (
+		        SELECT rm.id_report, rm.id_user, u.username, e.employee_name
+		        FROM tb_report_mark rm 
+		        INNER JOIN tb_m_user u ON u.id_user = rm.id_user
+		        INNER JOIN tb_m_employee e ON e.id_employee = u.id_employee
+		        WHERE (rm.report_mark_type=306) AND rm.id_report_status>1 AND rm.id_mark=2
+		        ORDER BY rm.report_mark_datetime DESC
+	        ) a
+	        GROUP BY a.id_report
+        ) la ON la.id_report = p.id_pp_change
+        WHERE p.id_pp_change>0 "
+        query += "ORDER BY p.id_pp_change DESC "
         Dim data As DataTable = execute_query(query, -1, True, "", "", "", "")
         GCSummary.DataSource = data
         GVSummary.BestFitColumns()
