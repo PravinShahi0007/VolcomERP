3ce3dedfbda178717f3d1531d103835ade61dd1b
Putu Septian Primadewa <iansaviour@gmail.com>
2021-06-09 17:38:10 +0800

    add form group email

--- VolcomInventory/FormCompGroupEmail.Designer.vb
+++ VolcomInventory/FormCompGroupEmail.Designer.vb
@@ -19,20 +19,302 @@ Partial Class FormCompGroupEmail
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FormCompGroupEmail))
+        Me.PanelControl3 = New DevExpress.XtraEditors.PanelControl()
+        Me.BDel = New DevExpress.XtraEditors.SimpleButton()
+        Me.LargeImageCollection = New DevExpress.Utils.ImageCollection(Me.components)
+        Me.BAdd = New DevExpress.XtraEditors.SimpleButton()
+        Me.SplitContainerControl1 = New DevExpress.XtraEditors.SplitContainerControl()
+        Me.GCGroupComp = New DevExpress.XtraGrid.GridControl()
+        Me.GVGroupComp = New DevExpress.XtraGrid.Views.Grid.GridView()
+        Me.id_company = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GridColumnGroup = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GridColumn1 = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GridColumnCompanyName = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GridColumnIdComp = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GridColumnStatus = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.is_active_company = New DevExpress.XtraEditors.Repository.RepositoryItemCheckEdit()
+        Me.RepositoryItemCheckEdit1 = New DevExpress.XtraEditors.Repository.RepositoryItemCheckEdit()
+        Me.GCEmail = New DevExpress.XtraGrid.GridControl()
+        Me.GVEmail = New DevExpress.XtraGrid.Views.Grid.GridView()
+        Me.GridColumn2 = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GridColumn8 = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GridColumn11 = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GridColumn12 = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.RepositoryItemCheckEdit2 = New DevExpress.XtraEditors.Repository.RepositoryItemCheckEdit()
+        Me.RepositoryItemCheckEdit3 = New DevExpress.XtraEditors.Repository.RepositoryItemCheckEdit()
+        CType(Me.PanelControl3, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.PanelControl3.SuspendLayout()
+        CType(Me.LargeImageCollection, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.SplitContainerControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SplitContainerControl1.SuspendLayout()
+        CType(Me.GCGroupComp, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.GVGroupComp, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.is_active_company, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.RepositoryItemCheckEdit1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.GCEmail, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.GVEmail, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.RepositoryItemCheckEdit2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.RepositoryItemCheckEdit3, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
+        '
+        'PanelControl3
+        '
+        Me.PanelControl3.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.NoBorder
+        Me.PanelControl3.Controls.Add(Me.BDel)
+        Me.PanelControl3.Controls.Add(Me.BAdd)
+        Me.PanelControl3.Dock = System.Windows.Forms.DockStyle.Top
+        Me.PanelControl3.Location = New System.Drawing.Point(0, 0)
+        Me.PanelControl3.Name = "PanelControl3"
+        Me.PanelControl3.Size = New System.Drawing.Size(982, 45)
+        Me.PanelControl3.TabIndex = 31
+        '
+        'BDel
+        '
+        Me.BDel.Dock = System.Windows.Forms.DockStyle.Right
+        Me.BDel.ImageIndex = 1
+        Me.BDel.ImageList = Me.LargeImageCollection
+        Me.BDel.Location = New System.Drawing.Point(792, 0)
+        Me.BDel.Name = "BDel"
+        Me.BDel.Size = New System.Drawing.Size(95, 45)
+        Me.BDel.TabIndex = 3
+        Me.BDel.Text = "Delete"
+        '
+        'LargeImageCollection
+        '
+        Me.LargeImageCollection.ImageSize = New System.Drawing.Size(24, 24)
+        Me.LargeImageCollection.ImageStream = CType(resources.GetObject("LargeImageCollection.ImageStream"), DevExpress.Utils.ImageCollectionStreamer)
+        Me.LargeImageCollection.Images.SetKeyName(0, "20_24x24.png")
+        Me.LargeImageCollection.Images.SetKeyName(1, "8_24x24.png")
+        Me.LargeImageCollection.Images.SetKeyName(2, "23_24x24.png")
+        Me.LargeImageCollection.Images.SetKeyName(3, "contact32.png")
+        '
+        'BAdd
+        '
+        Me.BAdd.Dock = System.Windows.Forms.DockStyle.Right
+        Me.BAdd.ImageIndex = 0
+        Me.BAdd.ImageList = Me.LargeImageCollection
+        Me.BAdd.Location = New System.Drawing.Point(887, 0)
+        Me.BAdd.Name = "BAdd"
+        Me.BAdd.Size = New System.Drawing.Size(95, 45)
+        Me.BAdd.TabIndex = 2
+        Me.BAdd.Text = "Add"
+        '
+        'SplitContainerControl1
+        '
+        Me.SplitContainerControl1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.SplitContainerControl1.Horizontal = False
+        Me.SplitContainerControl1.Location = New System.Drawing.Point(0, 45)
+        Me.SplitContainerControl1.Name = "SplitContainerControl1"
+        Me.SplitContainerControl1.Panel1.Controls.Add(Me.GCGroupComp)
+        Me.SplitContainerControl1.Panel1.Text = "Panel1"
+        Me.SplitContainerControl1.Panel2.Controls.Add(Me.GCEmail)
+        Me.SplitContainerControl1.Panel2.Text = "Panel2"
+        Me.SplitContainerControl1.Size = New System.Drawing.Size(982, 451)
+        Me.SplitContainerControl1.SplitterPosition = 212
+        Me.SplitContainerControl1.TabIndex = 32
+        Me.SplitContainerControl1.Text = "SplitContainerControl1"
+        '
+        'GCGroupComp
+        '
+        Me.GCGroupComp.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.GCGroupComp.Location = New System.Drawing.Point(0, 0)
+        Me.GCGroupComp.MainView = Me.GVGroupComp
+        Me.GCGroupComp.Name = "GCGroupComp"
+        Me.GCGroupComp.RepositoryItems.AddRange(New DevExpress.XtraEditors.Repository.RepositoryItem() {Me.is_active_company, Me.RepositoryItemCheckEdit1})
+        Me.GCGroupComp.Size = New System.Drawing.Size(982, 212)
+        Me.GCGroupComp.TabIndex = 5
+        Me.GCGroupComp.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() {Me.GVGroupComp})
+        '
+        'GVGroupComp
+        '
+        Me.GVGroupComp.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() {Me.id_company, Me.GridColumnGroup, Me.GridColumn1, Me.GridColumnCompanyName, Me.GridColumnIdComp, Me.GridColumnStatus})
+        Me.GVGroupComp.GridControl = Me.GCGroupComp
+        Me.GVGroupComp.Name = "GVGroupComp"
+        Me.GVGroupComp.OptionsBehavior.Editable = False
+        Me.GVGroupComp.OptionsFind.AlwaysVisible = True
+        Me.GVGroupComp.OptionsView.ShowGroupPanel = False
+        '
+        'id_company
+        '
+        Me.id_company.Caption = "ID"
+        Me.id_company.FieldName = "id_comp_group"
+        Me.id_company.Name = "id_company"
+        '
+        'GridColumnGroup
+        '
+        Me.GridColumnGroup.Caption = "Company Group"
+        Me.GridColumnGroup.FieldName = "comp_group"
+        Me.GridColumnGroup.Name = "GridColumnGroup"
+        Me.GridColumnGroup.Visible = True
+        Me.GridColumnGroup.VisibleIndex = 0
+        Me.GridColumnGroup.Width = 235
+        '
+        'GridColumn1
+        '
+        Me.GridColumn1.Caption = "Description"
+        Me.GridColumn1.FieldName = "description"
+        Me.GridColumn1.Name = "GridColumn1"
+        Me.GridColumn1.Visible = True
+        Me.GridColumn1.VisibleIndex = 1
+        Me.GridColumn1.Width = 242
+        '
+        'GridColumnCompanyName
+        '
+        Me.GridColumnCompanyName.Caption = "Company Name"
+        Me.GridColumnCompanyName.FieldName = "comp_name"
+        Me.GridColumnCompanyName.Name = "GridColumnCompanyName"
+        Me.GridColumnCompanyName.Visible = True
+        Me.GridColumnCompanyName.VisibleIndex = 2
+        Me.GridColumnCompanyName.Width = 242
+        '
+        'GridColumnIdComp
+        '
+        Me.GridColumnIdComp.FieldName = "id_comp"
+        Me.GridColumnIdComp.Name = "GridColumnIdComp"
+        '
+        'GridColumnStatus
+        '
+        Me.GridColumnStatus.Caption = "Status"
+        Me.GridColumnStatus.FieldName = "comp_status"
+        Me.GridColumnStatus.Name = "GridColumnStatus"
+        Me.GridColumnStatus.Visible = True
+        Me.GridColumnStatus.VisibleIndex = 3
+        Me.GridColumnStatus.Width = 245
+        '
+        'is_active_company
+        '
+        Me.is_active_company.AutoHeight = False
+        Me.is_active_company.DisplayValueChecked = "1"
+        Me.is_active_company.DisplayValueUnchecked = "0"
+        Me.is_active_company.Name = "is_active_company"
+        '
+        'RepositoryItemCheckEdit1
+        '
+        Me.RepositoryItemCheckEdit1.AutoHeight = False
+        Me.RepositoryItemCheckEdit1.Name = "RepositoryItemCheckEdit1"
+        Me.RepositoryItemCheckEdit1.ValueChecked = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.RepositoryItemCheckEdit1.ValueUnchecked = New Decimal(New Integer() {2, 0, 0, 0})
+        '
+        'GCEmail
+        '
+        Me.GCEmail.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.GCEmail.Location = New System.Drawing.Point(0, 0)
+        Me.GCEmail.MainView = Me.GVEmail
+        Me.GCEmail.Name = "GCEmail"
+        Me.GCEmail.RepositoryItems.AddRange(New DevExpress.XtraEditors.Repository.RepositoryItem() {Me.RepositoryItemCheckEdit2, Me.RepositoryItemCheckEdit3})
+        Me.GCEmail.Size = New System.Drawing.Size(982, 234)
+        Me.GCEmail.TabIndex = 6
+        Me.GCEmail.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() {Me.GVEmail})
+        '
+        'GVEmail
+        '
+        Me.GVEmail.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() {Me.GridColumn2, Me.GridColumn8, Me.GridColumn11, Me.GridColumn12})
+        Me.GVEmail.GridControl = Me.GCEmail
+        Me.GVEmail.Name = "GVEmail"
+        Me.GVEmail.OptionsBehavior.Editable = False
+        Me.GVEmail.OptionsFind.AlwaysVisible = True
+        Me.GVEmail.OptionsView.ShowGroupPanel = False
+        '
+        'GridColumn2
+        '
+        Me.GridColumn2.Caption = "ID"
+        Me.GridColumn2.FieldName = "id_mail_to_group"
+        Me.GridColumn2.Name = "GridColumn2"
+        '
+        'GridColumn8
+        '
+        Me.GridColumn8.Caption = "Contact Name"
+        Me.GridColumn8.FieldName = "contact_person"
+        Me.GridColumn8.Name = "GridColumn8"
+        Me.GridColumn8.Visible = True
+        Me.GridColumn8.VisibleIndex = 0
+        Me.GridColumn8.Width = 165
+        '
+        'GridColumn11
+        '
+        Me.GridColumn11.Caption = "Email"
+        Me.GridColumn11.FieldName = "email"
+        Me.GridColumn11.Name = "GridColumn11"
+        Me.GridColumn11.Visible = True
+        Me.GridColumn11.VisibleIndex = 1
+        Me.GridColumn11.Width = 162
+        '
+        'GridColumn12
+        '
+        Me.GridColumn12.Caption = "Send Type (To / CC)"
+        Me.GridColumn12.FieldName = "comp_status"
+        Me.GridColumn12.Name = "GridColumn12"
+        Me.GridColumn12.Visible = True
+        Me.GridColumn12.VisibleIndex = 2
+        Me.GridColumn12.Width = 90
+        '
+        'RepositoryItemCheckEdit2
+        '
+        Me.RepositoryItemCheckEdit2.AutoHeight = False
+        Me.RepositoryItemCheckEdit2.DisplayValueChecked = "1"
+        Me.RepositoryItemCheckEdit2.DisplayValueUnchecked = "0"
+        Me.RepositoryItemCheckEdit2.Name = "RepositoryItemCheckEdit2"
+        '
+        'RepositoryItemCheckEdit3
+        '
+        Me.RepositoryItemCheckEdit3.AutoHeight = False
+        Me.RepositoryItemCheckEdit3.Name = "RepositoryItemCheckEdit3"
+        Me.RepositoryItemCheckEdit3.ValueChecked = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.RepositoryItemCheckEdit3.ValueUnchecked = New Decimal(New Integer() {2, 0, 0, 0})
         '
         'FormCompGroupEmail
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1096, 496)
+        Me.ClientSize = New System.Drawing.Size(982, 496)
+        Me.Controls.Add(Me.SplitContainerControl1)
+        Me.Controls.Add(Me.PanelControl3)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
         Me.MaximizeBox = False
         Me.MinimizeBox = False
         Me.Name = "FormCompGroupEmail"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
         Me.Text = "Setup Email Company Group"
+        CType(Me.PanelControl3, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.PanelControl3.ResumeLayout(False)
+        CType(Me.LargeImageCollection, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.SplitContainerControl1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.SplitContainerControl1.ResumeLayout(False)
+        CType(Me.GCGroupComp, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.GVGroupComp, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.is_active_company, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.RepositoryItemCheckEdit1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.GCEmail, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.GVEmail, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.RepositoryItemCheckEdit2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.RepositoryItemCheckEdit3, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
+
+    Friend WithEvents PanelControl3 As DevExpress.XtraEditors.PanelControl
+    Public WithEvents LargeImageCollection As DevExpress.Utils.ImageCollection
+    Friend WithEvents BAdd As DevExpress.XtraEditors.SimpleButton
+    Friend WithEvents SplitContainerControl1 As DevExpress.XtraEditors.SplitContainerControl
+    Friend WithEvents BDel As DevExpress.XtraEditors.SimpleButton
+    Friend WithEvents GCEmail As DevExpress.XtraGrid.GridControl
+    Friend WithEvents GVEmail As DevExpress.XtraGrid.Views.Grid.GridView
+    Friend WithEvents GridColumn2 As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GridColumn8 As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GridColumn11 As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GridColumn12 As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents RepositoryItemCheckEdit2 As DevExpress.XtraEditors.Repository.RepositoryItemCheckEdit
+    Friend WithEvents RepositoryItemCheckEdit3 As DevExpress.XtraEditors.Repository.RepositoryItemCheckEdit
+    Friend WithEvents GCGroupComp As DevExpress.XtraGrid.GridControl
+    Friend WithEvents GVGroupComp As DevExpress.XtraGrid.Views.Grid.GridView
+    Friend WithEvents id_company As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GridColumnGroup As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GridColumn1 As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GridColumnCompanyName As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GridColumnIdComp As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GridColumnStatus As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents is_active_company As DevExpress.XtraEditors.Repository.RepositoryItemCheckEdit
+    Friend WithEvents RepositoryItemCheckEdit1 As DevExpress.XtraEditors.Repository.RepositoryItemCheckEdit
 End Class
--- VolcomInventory/FormCompGroupEmail.vb
+++ VolcomInventory/FormCompGroupEmail.vb
@@ -1,3 +1,15 @@
 ﻿Public Class FormCompGroupEmail
+    Private Sub FormPopUpCompGroup_Activated(sender As Object, e As EventArgs) Handles MyBase.Activated
+        FormMain.show_rb(Name)
+        checkFormAccess(Name)
+        button_main("0", "0", "0")
+    End Sub
 
+    Private Sub FormPopUpCompGroup_Deactivate(sender As Object, e As EventArgs) Handles MyBase.Deactivate
+        FormMain.hide_rb()
+    End Sub
+
+    Private Sub FormCompGroupEmail_FormClosed(sender As Object, e As FormClosedEventArgs) Handles MyBase.FormClosed
+        Dispose()
+    End Sub
 End Class
