6eb61805a4288d5c89339512fa4883199820af21
Satrya Anas <catur.satrya@gmail.com>
2021-06-11 17:52:56 +0800

    export to xls

--- VolcomInventory/FormProductPerform.vb
+++ VolcomInventory/FormProductPerform.vb
@@ -639,4 +639,23 @@ ORDER BY area ASC"
     Private Sub TEProductCode_EditValueChanged(sender As Object, e As EventArgs) Handles TEProductCode.EditValueChanged
         defaultView()
     End Sub
+
+    Private Sub SBExportExcel_Click(sender As Object, e As EventArgs) Handles SBExportExcel.Click
+        Cursor = Cursors.WaitCursor
+        Dim save As SaveFileDialog = New SaveFileDialog
+
+        save.Filter = "Excel File | *.xlsx"
+        save.ShowDialog()
+
+        If Not save.FileName = "" Then
+            Dim op As DevExpress.XtraPrinting.XlsxExportOptionsEx = New DevExpress.XtraPrinting.XlsxExportOptionsEx
+
+            op.ExportType = DevExpress.Export.ExportType.WYSIWYG
+
+            GVData.ExportToXlsx(save.FileName, op)
+
+            infoCustom("File saved.")
+        End If
+        Cursor = Cursors.Default
+    End Sub
 End Class
