4c6ccf1008bb68792098b287370f1104660a0972
Agus Friastana <friastana@gmail.com>
2021-06-30 18:15:03 +0800

    view unscan image

--- VolcomInventory/FormStockTakeStoreScanList.vb
+++ VolcomInventory/FormStockTakeStoreScanList.vb
@@ -8,7 +8,7 @@
     Sub viewData()
         Cursor = Cursors.WaitCursor
         Dim query As String = "SELECT s.id_product,s.scanned_code, p.product_name  AS `name`, cd.code_detail_name AS `size`,s.qty, s.created_date,
-        IF(s.is_unique_not_found=1,'Yes', 'No') AS `is_unique_not_found_view`, IF(s.is_no_tag=1,'Yes', 'No') AS `is_no_tag_view`
+        IF(s.is_unique_not_found=1,'Yes', 'No') AS `is_unique_not_found_view`, IF(s.is_no_tag=1,'Yes', 'No') AS `is_no_tag_view`, image
         FROM tb_st_store s
         INNER JOIN tb_m_product p ON p.id_product = s.id_product
         INNER JOIN tb_m_product_code pc ON pc.id_product = p.id_product
@@ -16,6 +16,13 @@
         WHERE s.id_st_store_period='" + id_period + "'
         ORDER BY s.created_date ASC "
         Dim data As DataTable = execute_query(query, -1, True, "", "", "", "")
+
+        For i = 0 To data.Rows.Count - 1
+            If Not data.Rows(i)("image").ToString = "" Then
+                data.Rows(i)("image") = get_setup_field("volcom_client_host") + "/stocktake/getImage?image=" + data.Rows(i)("image").ToString.Replace("[""", "").Replace("]", "").Replace("\", "").Replace("""", "")
+            End If
+        Next
+
         GCData.DataSource = data
         GVData.BestFitColumns()
         Cursor = Cursors.Default
