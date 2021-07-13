0ce91339f948f99896097f11bb74703a6719e59c
Putu Septian Primadewa <iansaviour@gmail.com>
2021-07-13 14:00:36 +0800

    pending sni serah terima

--- VolcomInventory/FormSNISerahTerimaDet.Designer.vb
+++ VolcomInventory/FormSNISerahTerimaDet.Designer.vb
@@ -19,24 +19,28 @@ Partial Class FormSNISerahTerimaDet
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FormSNISerahTerimaDet))
         Me.PanelControl1 = New DevExpress.XtraEditors.PanelControl()
-        Me.LabelControl1 = New DevExpress.XtraEditors.LabelControl()
-        Me.TextEdit1 = New DevExpress.XtraEditors.TextEdit()
+        Me.DEProposeDate = New DevExpress.XtraEditors.DateEdit()
+        Me.LabelControl3 = New DevExpress.XtraEditors.LabelControl()
         Me.TextEdit2 = New DevExpress.XtraEditors.TextEdit()
         Me.LabelControl2 = New DevExpress.XtraEditors.LabelControl()
-        Me.LabelControl3 = New DevExpress.XtraEditors.LabelControl()
-        Me.DEProposeDate = New DevExpress.XtraEditors.DateEdit()
+        Me.TextEdit1 = New DevExpress.XtraEditors.TextEdit()
+        Me.LabelControl1 = New DevExpress.XtraEditors.LabelControl()
         Me.PanelControl2 = New DevExpress.XtraEditors.PanelControl()
         Me.PanelControl3 = New DevExpress.XtraEditors.PanelControl()
         Me.GridControl1 = New DevExpress.XtraGrid.GridControl()
         Me.GridView1 = New DevExpress.XtraGrid.Views.Grid.GridView()
+        Me.BDel = New DevExpress.XtraEditors.SimpleButton()
+        Me.BAdd = New DevExpress.XtraEditors.SimpleButton()
         CType(Me.PanelControl1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.PanelControl1.SuspendLayout()
-        CType(Me.TextEdit1.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.TextEdit2.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.DEProposeDate.Properties.CalendarTimeProperties, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.DEProposeDate.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TextEdit2.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TextEdit1.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PanelControl2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.PanelControl2.SuspendLayout()
         CType(Me.PanelControl3, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.GridControl1, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.GridView1, System.ComponentModel.ISupportInitialize).BeginInit()
@@ -56,20 +60,27 @@ Partial Class FormSNISerahTerimaDet
         Me.PanelControl1.Size = New System.Drawing.Size(917, 71)
         Me.PanelControl1.TabIndex = 0
         '
-        'LabelControl1
+        'DEProposeDate
         '
-        Me.LabelControl1.Location = New System.Drawing.Point(11, 41)
-        Me.LabelControl1.Name = "LabelControl1"
-        Me.LabelControl1.Size = New System.Drawing.Size(54, 13)
-        Me.LabelControl1.TabIndex = 1
-        Me.LabelControl1.Text = "Created By"
+        Me.DEProposeDate.EditValue = Nothing
+        Me.DEProposeDate.Location = New System.Drawing.Point(501, 38)
+        Me.DEProposeDate.Name = "DEProposeDate"
+        Me.DEProposeDate.Properties.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() {New DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)})
+        Me.DEProposeDate.Properties.CalendarTimeProperties.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() {New DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)})
+        Me.DEProposeDate.Properties.DisplayFormat.FormatString = "dd MMMM yyyy"
+        Me.DEProposeDate.Properties.DisplayFormat.FormatType = DevExpress.Utils.FormatType.DateTime
+        Me.DEProposeDate.Properties.Mask.EditMask = "dd\/MM\/yyyy"
+        Me.DEProposeDate.Properties.ReadOnly = True
+        Me.DEProposeDate.Size = New System.Drawing.Size(266, 20)
+        Me.DEProposeDate.TabIndex = 25
         '
-        'TextEdit1
+        'LabelControl3
         '
-        Me.TextEdit1.Location = New System.Drawing.Point(91, 38)
-        Me.TextEdit1.Name = "TextEdit1"
-        Me.TextEdit1.Size = New System.Drawing.Size(333, 20)
-        Me.TextEdit1.TabIndex = 2
+        Me.LabelControl3.Location = New System.Drawing.Point(430, 41)
+        Me.LabelControl3.Name = "LabelControl3"
+        Me.LabelControl3.Size = New System.Drawing.Size(65, 13)
+        Me.LabelControl3.TabIndex = 5
+        Me.LabelControl3.Text = "Created Date"
         '
         'TextEdit2
         '
@@ -86,30 +97,25 @@ Partial Class FormSNISerahTerimaDet
         Me.LabelControl2.TabIndex = 3
         Me.LabelControl2.Text = "Number"
         '
-        'LabelControl3
+        'TextEdit1
         '
-        Me.LabelControl3.Location = New System.Drawing.Point(430, 41)
-        Me.LabelControl3.Name = "LabelControl3"
-        Me.LabelControl3.Size = New System.Drawing.Size(65, 13)
-        Me.LabelControl3.TabIndex = 5
-        Me.LabelControl3.Text = "Created Date"
+        Me.TextEdit1.Location = New System.Drawing.Point(91, 38)
+        Me.TextEdit1.Name = "TextEdit1"
+        Me.TextEdit1.Size = New System.Drawing.Size(333, 20)
+        Me.TextEdit1.TabIndex = 2
         '
-        'DEProposeDate
+        'LabelControl1
         '
-        Me.DEProposeDate.EditValue = Nothing
-        Me.DEProposeDate.Location = New System.Drawing.Point(501, 38)
-        Me.DEProposeDate.Name = "DEProposeDate"
-        Me.DEProposeDate.Properties.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() {New DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)})
-        Me.DEProposeDate.Properties.CalendarTimeProperties.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() {New DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)})
-        Me.DEProposeDate.Properties.DisplayFormat.FormatString = "dd MMMM yyyy"
-        Me.DEProposeDate.Properties.DisplayFormat.FormatType = DevExpress.Utils.FormatType.DateTime
-        Me.DEProposeDate.Properties.Mask.EditMask = "dd\/MM\/yyyy"
-        Me.DEProposeDate.Properties.ReadOnly = True
-        Me.DEProposeDate.Size = New System.Drawing.Size(266, 20)
-        Me.DEProposeDate.TabIndex = 25
+        Me.LabelControl1.Location = New System.Drawing.Point(11, 41)
+        Me.LabelControl1.Name = "LabelControl1"
+        Me.LabelControl1.Size = New System.Drawing.Size(54, 13)
+        Me.LabelControl1.TabIndex = 1
+        Me.LabelControl1.Text = "Created By"
         '
         'PanelControl2
         '
+        Me.PanelControl2.Controls.Add(Me.BDel)
+        Me.PanelControl2.Controls.Add(Me.BAdd)
         Me.PanelControl2.Dock = System.Windows.Forms.DockStyle.Top
         Me.PanelControl2.Location = New System.Drawing.Point(0, 71)
         Me.PanelControl2.Name = "PanelControl2"
@@ -139,6 +145,26 @@ Partial Class FormSNISerahTerimaDet
         Me.GridView1.GridControl = Me.GridControl1
         Me.GridView1.Name = "GridView1"
         '
+        'BDel
+        '
+        Me.BDel.Dock = System.Windows.Forms.DockStyle.Right
+        Me.BDel.Image = CType(resources.GetObject("BDel.Image"), System.Drawing.Image)
+        Me.BDel.Location = New System.Drawing.Point(694, 2)
+        Me.BDel.Name = "BDel"
+        Me.BDel.Size = New System.Drawing.Size(108, 38)
+        Me.BDel.TabIndex = 4
+        Me.BDel.Text = "Delete Item"
+        '
+        'BAdd
+        '
+        Me.BAdd.Dock = System.Windows.Forms.DockStyle.Right
+        Me.BAdd.Image = CType(resources.GetObject("BAdd.Image"), System.Drawing.Image)
+        Me.BAdd.Location = New System.Drawing.Point(802, 2)
+        Me.BAdd.Name = "BAdd"
+        Me.BAdd.Size = New System.Drawing.Size(113, 38)
+        Me.BAdd.TabIndex = 5
+        Me.BAdd.Text = "Add Item"
+        '
         'FormSNISerahTerimaDet
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
@@ -156,11 +182,12 @@ Partial Class FormSNISerahTerimaDet
         CType(Me.PanelControl1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.PanelControl1.ResumeLayout(False)
         Me.PanelControl1.PerformLayout()
-        CType(Me.TextEdit1.Properties, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.TextEdit2.Properties, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.DEProposeDate.Properties.CalendarTimeProperties, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.DEProposeDate.Properties, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.TextEdit2.Properties, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.TextEdit1.Properties, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.PanelControl2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.PanelControl2.ResumeLayout(False)
         CType(Me.PanelControl3, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.GridControl1, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.GridView1, System.ComponentModel.ISupportInitialize).EndInit()
@@ -179,4 +206,6 @@ Partial Class FormSNISerahTerimaDet
     Friend WithEvents PanelControl3 As DevExpress.XtraEditors.PanelControl
     Friend WithEvents GridControl1 As DevExpress.XtraGrid.GridControl
     Friend WithEvents GridView1 As DevExpress.XtraGrid.Views.Grid.GridView
+    Friend WithEvents BDel As DevExpress.XtraEditors.SimpleButton
+    Friend WithEvents BAdd As DevExpress.XtraEditors.SimpleButton
 End Class
