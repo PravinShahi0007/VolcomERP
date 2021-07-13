25f34f08c07ad647394820108fadbed3eccf7d3c
Putu Septian Primadewa <iansaviour@gmail.com>
2021-07-13 11:24:44 +0800

    add serah terima form

--- VolcomInventory/FormSNISerahTerima.Designer.vb
+++ VolcomInventory/FormSNISerahTerima.Designer.vb
@@ -19,20 +19,44 @@ Partial Class FormSNISerahTerima
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.GCList = New DevExpress.XtraGrid.GridControl()
+        Me.GVList = New DevExpress.XtraGrid.Views.Grid.GridView()
+        CType(Me.GCList, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.GVList, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
+        '
+        'GCList
+        '
+        Me.GCList.Location = New System.Drawing.Point(218, 253)
+        Me.GCList.MainView = Me.GVList
+        Me.GCList.Name = "GCList"
+        Me.GCList.Size = New System.Drawing.Size(400, 200)
+        Me.GCList.TabIndex = 0
+        Me.GCList.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() {Me.GVList})
+        '
+        'GVList
+        '
+        Me.GVList.GridControl = Me.GCList
+        Me.GVList.Name = "GVList"
         '
         'FormSNISerahTerima
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(1001, 566)
+        Me.Controls.Add(Me.GCList)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
         Me.MaximizeBox = False
         Me.MinimizeBox = False
         Me.Name = "FormSNISerahTerima"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
         Me.Text = "Serah Terima SNI"
+        CType(Me.GCList, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.GVList, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
+
+    Friend WithEvents GCList As DevExpress.XtraGrid.GridControl
+    Friend WithEvents GVList As DevExpress.XtraGrid.Views.Grid.GridView
 End Class
--- VolcomInventory/FormSNISerahTerimaDet.Designer.vb
+++ VolcomInventory/FormSNISerahTerimaDet.Designer.vb
@@ -19,19 +19,32 @@ Partial Class FormSNISerahTerimaDet
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.PanelControl1 = New DevExpress.XtraEditors.PanelControl()
+        CType(Me.PanelControl1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
+        '
+        'PanelControl1
+        '
+        Me.PanelControl1.Location = New System.Drawing.Point(118, 111)
+        Me.PanelControl1.Name = "PanelControl1"
+        Me.PanelControl1.Size = New System.Drawing.Size(200, 100)
+        Me.PanelControl1.TabIndex = 0
         '
         'FormSNISerahTerimaDet
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(917, 571)
+        Me.Controls.Add(Me.PanelControl1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
         Me.MinimizeBox = False
         Me.Name = "FormSNISerahTerimaDet"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "SNI Serah Terima"
+        CType(Me.PanelControl1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
+
+    Friend WithEvents PanelControl1 As DevExpress.XtraEditors.PanelControl
 End Class
