4688f3a1df3777740e0e4698813dff679d11505d
Putu Septian Primadewa <iansaviour@gmail.com>
2021-08-04 16:47:23 +0800

    add report sni realisasi

--- VolcomInventory/ClassShowPopUp.vb
+++ VolcomInventory/ClassShowPopUp.vb
@@ -3647,13 +3647,13 @@ WHERE tb.id_report_status='6' AND IF(ISNULL(rec.id_prod_order),2,1)=2 "
             gv.Columns("date_created").Caption = "Created Date"
             gv.Columns("date_created").DisplayFormat.FormatType = DevExpress.Utils.FormatType.DateTime
             gv.Columns("date_created").DisplayFormat.FormatString = "dd MMM yyyy"
-            gv.Columns("number").Caption = "Number"
+            gv.Columns("number").Caption = "FGPO Number"
             gv.Columns("qty").Caption = "Quantity"
             gv.Columns("qty").DisplayFormat.FormatType = DevExpress.Utils.FormatType.Numeric
             gv.Columns("qty").DisplayFormat.FormatString = "{0:n0}"
             gv.Columns("qty").SummaryItem.SummaryType = DevExpress.Data.SummaryItemType.Sum
             gv.Columns("qty").SummaryItem.DisplayFormat = "{0:n0}"
-            gv.Columns("currency").Caption = "Currency"
+            gv.Columns("currency").Caption = "Curr"
             gv.Columns("unit_price").Caption = "Price"
             gv.Columns("unit_price").DisplayFormat.FormatType = DevExpress.Utils.FormatType.Numeric
             gv.Columns("unit_price").DisplayFormat.FormatString = "{0:n2}"
---
+++ VolcomInventory/ReportSNIRealisasi.vb
@@ -0,0 +1,3 @@
+﻿Public Class ReportSNIRealisasi
+
+End Class
