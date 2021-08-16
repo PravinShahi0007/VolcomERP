8ec163272ca967e9a333ccb519a8e9afe3ca8318
Putu Septian Primadewa <iansaviour@gmail.com>
2021-08-16 10:33:29 +0800

    print done

---
+++ VolcomInventory/ReportSNIOut.vb
@@ -0,0 +1,16 @@
+﻿Public Class ReportSNIOut
+    Public Shared id_qc_sni_out As String = "-1"
+    Public Shared dt As DataTable
+
+    Private Sub GVDetail_CustomColumnDisplayText_1(ByVal sender As System.Object, ByVal e As DevExpress.XtraGrid.Views.Base.CustomColumnDisplayTextEventArgs) Handles GVDetail.CustomColumnDisplayText
+        If e.Column.FieldName = "no" Then
+            e.DisplayText = (e.ListSourceRowIndex + 1).ToString()
+        End If
+    End Sub
+
+    Private Sub ReportSNIOut_BeforePrint(sender As Object, e As Printing.PrintEventArgs) Handles MyBase.BeforePrint
+        GCDetail.DataSource = dt
+        Dim acc As String = LTo.Text
+        load_mark_horz("330", id_qc_sni_out, acc, "1", XrTable1)
+    End Sub
+End Class
