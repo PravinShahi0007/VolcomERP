8998cb06539fc8d7830f25655d1fe4b83c73d1fe
Satrya Anas <catur.satrya@gmail.com>
2021-08-24 11:41:34 +0800

    prepare menu silhoute

---
+++ VolcomInventory/FormMasterSilhouette.Designer.vb
@@ -0,0 +1,236 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FormMasterSilhouette
+    Inherits DevExpress.XtraEditors.XtraForm
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        If disposing AndAlso components IsNot Nothing Then
+            components.Dispose()
+        End If
+        MyBase.Dispose(disposing)
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FormMasterSilhouette))
+        Me.PanelControl1 = New DevExpress.XtraEditors.PanelControl()
+        Me.BtnDiscard = New DevExpress.XtraEditors.SimpleButton()
+        Me.BtnSaveChanges = New DevExpress.XtraEditors.SimpleButton()
+        Me.PanelControl2 = New DevExpress.XtraEditors.PanelControl()
+        Me.TECodeDet = New DevExpress.XtraEditors.TextEdit()
+        Me.LabelSeason = New DevExpress.XtraEditors.LabelControl()
+        Me.TEPrintedCode = New DevExpress.XtraEditors.TextEdit()
+        Me.LabelControl1 = New DevExpress.XtraEditors.LabelControl()
+        Me.GCData = New DevExpress.XtraGrid.GridControl()
+        Me.GVData = New DevExpress.XtraGrid.Views.Grid.GridView()
+        Me.GridColumnis_select = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.RepositoryItemCheckEdit1 = New DevExpress.XtraEditors.Repository.RepositoryItemCheckEdit()
+        Me.GridColumnid_code_detail = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GridColumndisplay_name = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GridColumncode_detail_name = New DevExpress.XtraGrid.Columns.GridColumn()
+        CType(Me.PanelControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.PanelControl1.SuspendLayout()
+        CType(Me.PanelControl2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.PanelControl2.SuspendLayout()
+        CType(Me.TECodeDet.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TEPrintedCode.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.GCData, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.GVData, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.RepositoryItemCheckEdit1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PanelControl1
+        '
+        Me.PanelControl1.Controls.Add(Me.BtnDiscard)
+        Me.PanelControl1.Controls.Add(Me.BtnSaveChanges)
+        Me.PanelControl1.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.PanelControl1.Location = New System.Drawing.Point(0, 384)
+        Me.PanelControl1.Name = "PanelControl1"
+        Me.PanelControl1.Size = New System.Drawing.Size(685, 41)
+        Me.PanelControl1.TabIndex = 0
+        '
+        'BtnDiscard
+        '
+        Me.BtnDiscard.Dock = System.Windows.Forms.DockStyle.Right
+        Me.BtnDiscard.Image = CType(resources.GetObject("BtnDiscard.Image"), System.Drawing.Image)
+        Me.BtnDiscard.Location = New System.Drawing.Point(465, 2)
+        Me.BtnDiscard.Name = "BtnDiscard"
+        Me.BtnDiscard.Size = New System.Drawing.Size(100, 37)
+        Me.BtnDiscard.TabIndex = 1
+        Me.BtnDiscard.Text = "Discard"
+        '
+        'BtnSaveChanges
+        '
+        Me.BtnSaveChanges.Dock = System.Windows.Forms.DockStyle.Right
+        Me.BtnSaveChanges.Image = CType(resources.GetObject("BtnSaveChanges.Image"), System.Drawing.Image)
+        Me.BtnSaveChanges.Location = New System.Drawing.Point(565, 2)
+        Me.BtnSaveChanges.Name = "BtnSaveChanges"
+        Me.BtnSaveChanges.Size = New System.Drawing.Size(118, 37)
+        Me.BtnSaveChanges.TabIndex = 0
+        Me.BtnSaveChanges.Text = "Save Changes"
+        '
+        'PanelControl2
+        '
+        Me.PanelControl2.Controls.Add(Me.TECodeDet)
+        Me.PanelControl2.Controls.Add(Me.LabelSeason)
+        Me.PanelControl2.Controls.Add(Me.TEPrintedCode)
+        Me.PanelControl2.Controls.Add(Me.LabelControl1)
+        Me.PanelControl2.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.PanelControl2.Location = New System.Drawing.Point(0, 312)
+        Me.PanelControl2.Name = "PanelControl2"
+        Me.PanelControl2.Size = New System.Drawing.Size(685, 72)
+        Me.PanelControl2.TabIndex = 1
+        '
+        'TECodeDet
+        '
+        Me.TECodeDet.EditValue = ""
+        Me.TECodeDet.Location = New System.Drawing.Point(120, 37)
+        Me.TECodeDet.Name = "TECodeDet"
+        Me.TECodeDet.Properties.Appearance.Font = New System.Drawing.Font("Calibri", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TECodeDet.Properties.Appearance.Options.UseFont = True
+        Me.TECodeDet.Size = New System.Drawing.Size(547, 22)
+        Me.TECodeDet.TabIndex = 42
+        Me.TECodeDet.ToolTipIconType = DevExpress.Utils.ToolTipIconType.Information
+        Me.TECodeDet.ToolTipTitle = "Information"
+        '
+        'LabelSeason
+        '
+        Me.LabelSeason.Appearance.Font = New System.Drawing.Font("Calibri", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LabelSeason.Location = New System.Drawing.Point(10, 40)
+        Me.LabelSeason.Name = "LabelSeason"
+        Me.LabelSeason.Size = New System.Drawing.Size(64, 15)
+        Me.LabelSeason.TabIndex = 43
+        Me.LabelSeason.Text = "Description"
+        '
+        'TEPrintedCode
+        '
+        Me.TEPrintedCode.EditValue = ""
+        Me.TEPrintedCode.Location = New System.Drawing.Point(120, 9)
+        Me.TEPrintedCode.Name = "TEPrintedCode"
+        Me.TEPrintedCode.Properties.Appearance.Font = New System.Drawing.Font("Calibri", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TEPrintedCode.Properties.Appearance.Options.UseFont = True
+        Me.TEPrintedCode.Properties.MaxLength = 25
+        Me.TEPrintedCode.Size = New System.Drawing.Size(547, 22)
+        Me.TEPrintedCode.TabIndex = 40
+        Me.TEPrintedCode.ToolTipIconType = DevExpress.Utils.ToolTipIconType.Information
+        Me.TEPrintedCode.ToolTipTitle = "Information"
+        '
+        'LabelControl1
+        '
+        Me.LabelControl1.Appearance.Font = New System.Drawing.Font("Calibri", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LabelControl1.Location = New System.Drawing.Point(10, 12)
+        Me.LabelControl1.Name = "LabelControl1"
+        Me.LabelControl1.Size = New System.Drawing.Size(102, 15)
+        Me.LabelControl1.TabIndex = 41
+        Me.LabelControl1.Text = "Printed in Barcode"
+        '
+        'GCData
+        '
+        Me.GCData.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.GCData.Location = New System.Drawing.Point(0, 0)
+        Me.GCData.MainView = Me.GVData
+        Me.GCData.Name = "GCData"
+        Me.GCData.RepositoryItems.AddRange(New DevExpress.XtraEditors.Repository.RepositoryItem() {Me.RepositoryItemCheckEdit1})
+        Me.GCData.Size = New System.Drawing.Size(685, 312)
+        Me.GCData.TabIndex = 2
+        Me.GCData.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() {Me.GVData})
+        '
+        'GVData
+        '
+        Me.GVData.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() {Me.GridColumnis_select, Me.GridColumnid_code_detail, Me.GridColumndisplay_name, Me.GridColumncode_detail_name})
+        Me.GVData.GridControl = Me.GCData
+        Me.GVData.Name = "GVData"
+        Me.GVData.OptionsFind.AlwaysVisible = True
+        Me.GVData.OptionsView.ColumnAutoWidth = False
+        Me.GVData.OptionsView.ShowGroupPanel = False
+        '
+        'GridColumnis_select
+        '
+        Me.GridColumnis_select.Caption = "Select"
+        Me.GridColumnis_select.ColumnEdit = Me.RepositoryItemCheckEdit1
+        Me.GridColumnis_select.FieldName = "is_select"
+        Me.GridColumnis_select.Name = "GridColumnis_select"
+        Me.GridColumnis_select.Visible = True
+        Me.GridColumnis_select.VisibleIndex = 0
+        '
+        'RepositoryItemCheckEdit1
+        '
+        Me.RepositoryItemCheckEdit1.AutoHeight = False
+        Me.RepositoryItemCheckEdit1.Name = "RepositoryItemCheckEdit1"
+        Me.RepositoryItemCheckEdit1.ValueChecked = "Yes"
+        Me.RepositoryItemCheckEdit1.ValueUnchecked = "No"
+        '
+        'GridColumnid_code_detail
+        '
+        Me.GridColumnid_code_detail.Caption = "id_code_detail"
+        Me.GridColumnid_code_detail.FieldName = "id_code_detail"
+        Me.GridColumnid_code_detail.Name = "GridColumnid_code_detail"
+        Me.GridColumnid_code_detail.OptionsColumn.AllowEdit = False
+        '
+        'GridColumndisplay_name
+        '
+        Me.GridColumndisplay_name.Caption = "Class"
+        Me.GridColumndisplay_name.FieldName = "display_name"
+        Me.GridColumndisplay_name.Name = "GridColumndisplay_name"
+        Me.GridColumndisplay_name.OptionsColumn.AllowEdit = False
+        Me.GridColumndisplay_name.Visible = True
+        Me.GridColumndisplay_name.VisibleIndex = 1
+        '
+        'GridColumncode_detail_name
+        '
+        Me.GridColumncode_detail_name.Caption = "Class Description"
+        Me.GridColumncode_detail_name.FieldName = "code_detail_name"
+        Me.GridColumncode_detail_name.Name = "GridColumncode_detail_name"
+        Me.GridColumncode_detail_name.OptionsColumn.AllowEdit = False
+        Me.GridColumncode_detail_name.Visible = True
+        Me.GridColumncode_detail_name.VisibleIndex = 2
+        '
+        'FormMasterSilhouette
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(685, 425)
+        Me.Controls.Add(Me.GCData)
+        Me.Controls.Add(Me.PanelControl2)
+        Me.Controls.Add(Me.PanelControl1)
+        Me.MinimizeBox = False
+        Me.Name = "FormMasterSilhouette"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Master Silhouette"
+        CType(Me.PanelControl1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.PanelControl1.ResumeLayout(False)
+        CType(Me.PanelControl2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.PanelControl2.ResumeLayout(False)
+        Me.PanelControl2.PerformLayout()
+        CType(Me.TECodeDet.Properties, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.TEPrintedCode.Properties, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.GCData, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.GVData, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.RepositoryItemCheckEdit1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents PanelControl1 As DevExpress.XtraEditors.PanelControl
+    Friend WithEvents PanelControl2 As DevExpress.XtraEditors.PanelControl
+    Friend WithEvents GCData As DevExpress.XtraGrid.GridControl
+    Friend WithEvents GVData As DevExpress.XtraGrid.Views.Grid.GridView
+    Friend WithEvents GridColumnis_select As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents RepositoryItemCheckEdit1 As DevExpress.XtraEditors.Repository.RepositoryItemCheckEdit
+    Friend WithEvents GridColumnid_code_detail As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GridColumndisplay_name As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GridColumncode_detail_name As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents BtnDiscard As DevExpress.XtraEditors.SimpleButton
+    Friend WithEvents BtnSaveChanges As DevExpress.XtraEditors.SimpleButton
+    Friend WithEvents TEPrintedCode As DevExpress.XtraEditors.TextEdit
+    Friend WithEvents LabelControl1 As DevExpress.XtraEditors.LabelControl
+    Friend WithEvents TECodeDet As DevExpress.XtraEditors.TextEdit
+    Friend WithEvents LabelSeason As DevExpress.XtraEditors.LabelControl
+End Class
---
+++ VolcomInventory/FormMasterSilhouette.vb
@@ -0,0 +1,29 @@
+﻿Public Class FormMasterSilhouette
+    Private Sub FormMasterSilhouette_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        viewClass()
+    End Sub
+
+    Sub viewClass()
+        Cursor = Cursors.WaitCursor
+        Dim query As String = "SELECT 'No' AS `is_select`,cd.id_code_detail, cd.display_name, cd.code_detail_name 
+        FROM tb_m_code_detail cd
+        WHERE cd.id_code IN (SELECT o.id_code_fg_class  FROM tb_opt o)
+        ORDER BY cd.display_name ASC "
+        Dim data As DataTable = execute_query(query, -1, True, "", "", "", "")
+        GCData.DataSource = data
+        GVData.BestFitColumns()
+        Cursor = Cursors.Default
+    End Sub
+
+    Private Sub FormMasterSilhouette_FormClosed(sender As Object, e As FormClosedEventArgs) Handles MyBase.FormClosed
+        Dispose()
+    End Sub
+
+    Private Sub BtnDiscard_Click(sender As Object, e As EventArgs) Handles BtnDiscard.Click
+        Close()
+    End Sub
+
+    Private Sub BtnSaveChanges_Click(sender As Object, e As EventArgs) Handles BtnSaveChanges.Click
+
+    End Sub
+End Class
