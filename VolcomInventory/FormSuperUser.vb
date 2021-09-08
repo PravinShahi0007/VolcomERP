c7772c7c080a6ebe253a30174ccfb8d5aaf7796e
Satrya Anas <catur.satrya@gmail.com>
2021-09-08 14:20:33 +0800

    revert super user

--- VolcomInventory/FormSuperUser.vb
+++ VolcomInventory/FormSuperUser.vb
@@ -112,8 +112,6 @@
     End Sub
 
     Private Sub BtnOther_Click(sender As Object, e As EventArgs) Handles BtnOther.Click
-        'vios sync price
-        FormPriceMKDVios.ShowDialog()
         'siluet list
         'FormMasterSilhouette.is_show_master_sht = True
         'FormMasterSilhouette.ShowDialog()
