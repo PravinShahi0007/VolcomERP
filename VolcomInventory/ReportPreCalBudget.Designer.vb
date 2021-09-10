6e8681cc2163a510dc608ee0d6bba81d0396ee4c
Putu Septian Primadewa <iansaviour@gmail.com>
2021-09-10 18:02:01 +0800

    almost

--- VolcomInventory/ReportPreCalBudget.Designer.vb
+++ VolcomInventory/ReportPreCalBudget.Designer.vb
@@ -23,16 +23,6 @@ Partial Public Class ReportPreCalBudget
         Me.XTDetail = New DevExpress.XtraReports.UI.XRTable()
         Me.XRRow = New DevExpress.XtraReports.UI.XRTableRow()
         Me.XrTableCell1 = New DevExpress.XtraReports.UI.XRTableCell()
-        Me.TopMargin = New DevExpress.XtraReports.UI.TopMarginBand()
-        Me.BottomMargin = New DevExpress.XtraReports.UI.BottomMarginBand()
-        Me.PageHeader = New DevExpress.XtraReports.UI.PageHeaderBand()
-        Me.XrTable3 = New DevExpress.XtraReports.UI.XRTable()
-        Me.XrTableRow3 = New DevExpress.XtraReports.UI.XRTableRow()
-        Me.LVendor1 = New DevExpress.XtraReports.UI.XRTableCell()
-        Me.ReportFooter = New DevExpress.XtraReports.UI.ReportFooterBand()
-        Me.PageFooter = New DevExpress.XtraReports.UI.PageFooterBand()
-        Me.XrTableCell4 = New DevExpress.XtraReports.UI.XRTableCell()
-        Me.XrTableCell5 = New DevExpress.XtraReports.UI.XRTableCell()
         Me.XrTableCell2 = New DevExpress.XtraReports.UI.XRTableCell()
         Me.XrTableCell3 = New DevExpress.XtraReports.UI.XRTableCell()
         Me.XrTableCell6 = New DevExpress.XtraReports.UI.XRTableCell()
@@ -41,8 +31,23 @@ Partial Public Class ReportPreCalBudget
         Me.XrTableCell9 = New DevExpress.XtraReports.UI.XRTableCell()
         Me.XrTableCell10 = New DevExpress.XtraReports.UI.XRTableCell()
         Me.XrTableCell11 = New DevExpress.XtraReports.UI.XRTableCell()
+        Me.TopMargin = New DevExpress.XtraReports.UI.TopMarginBand()
+        Me.BottomMargin = New DevExpress.XtraReports.UI.BottomMarginBand()
+        Me.PageHeader = New DevExpress.XtraReports.UI.PageHeaderBand()
+        Me.XrTable3 = New DevExpress.XtraReports.UI.XRTable()
+        Me.XrTableRow3 = New DevExpress.XtraReports.UI.XRTableRow()
+        Me.LVendor1 = New DevExpress.XtraReports.UI.XRTableCell()
+        Me.XrTableCell4 = New DevExpress.XtraReports.UI.XRTableCell()
+        Me.XrTableCell5 = New DevExpress.XtraReports.UI.XRTableCell()
+        Me.ReportFooter = New DevExpress.XtraReports.UI.ReportFooterBand()
+        Me.PageFooter = New DevExpress.XtraReports.UI.PageFooterBand()
+        Me.XrTable2 = New DevExpress.XtraReports.UI.XRTable()
+        Me.XrTableRow2 = New DevExpress.XtraReports.UI.XRTableRow()
+        Me.XrTableCell12 = New DevExpress.XtraReports.UI.XRTableCell()
+        Me.XrPageInfo1 = New DevExpress.XtraReports.UI.XRPageInfo()
         CType(Me.XTDetail, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.XrTable3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.XrTable2, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me, System.ComponentModel.ISupportInitialize).BeginInit()
         '
         'Detail
@@ -82,6 +87,57 @@ Partial Public Class ReportPreCalBudget
         Me.XrTableCell1.TextAlignment = DevExpress.XtraPrinting.TextAlignment.MiddleLeft
         Me.XrTableCell1.Weight = 2.272727272727272R
         '
+        'XrTableCell2
+        '
+        Me.XrTableCell2.Name = "XrTableCell2"
+        Me.XrTableCell2.Text = "Unit Price"
+        Me.XrTableCell2.Weight = 0.90909090909090917R
+        '
+        'XrTableCell3
+        '
+        Me.XrTableCell3.Name = "XrTableCell3"
+        Me.XrTableCell3.Text = "Qty"
+        Me.XrTableCell3.Weight = 0.36363636363636359R
+        '
+        'XrTableCell6
+        '
+        Me.XrTableCell6.Name = "XrTableCell6"
+        Me.XrTableCell6.Text = "Total"
+        Me.XrTableCell6.Weight = 1.0000002774325285R
+        '
+        'XrTableCell7
+        '
+        Me.XrTableCell7.Borders = CType((DevExpress.XtraPrinting.BorderSide.Left Or DevExpress.XtraPrinting.BorderSide.Right), DevExpress.XtraPrinting.BorderSide)
+        Me.XrTableCell7.Name = "XrTableCell7"
+        Me.XrTableCell7.StylePriority.UseBorders = False
+        Me.XrTableCell7.Weight = 0.454544344815341R
+        '
+        'XrTableCell8
+        '
+        Me.XrTableCell8.Name = "XrTableCell8"
+        Me.XrTableCell8.StylePriority.UseTextAlignment = False
+        Me.XrTableCell8.Text = "Description"
+        Me.XrTableCell8.TextAlignment = DevExpress.XtraPrinting.TextAlignment.MiddleLeft
+        Me.XrTableCell8.Weight = 2.2727273652048763R
+        '
+        'XrTableCell9
+        '
+        Me.XrTableCell9.Name = "XrTableCell9"
+        Me.XrTableCell9.Text = "Unit Price"
+        Me.XrTableCell9.Weight = 0.90909093551311448R
+        '
+        'XrTableCell10
+        '
+        Me.XrTableCell10.Name = "XrTableCell10"
+        Me.XrTableCell10.Text = "Qty"
+        Me.XrTableCell10.Weight = 0.36363636469329341R
+        '
+        'XrTableCell11
+        '
+        Me.XrTableCell11.Name = "XrTableCell11"
+        Me.XrTableCell11.Text = "Total"
+        Me.XrTableCell11.Weight = 1.0000000295173761R
+        '
         'TopMargin
         '
         Me.TopMargin.HeightF = 100.0!
@@ -91,7 +147,7 @@ Partial Public Class ReportPreCalBudget
         '
         'BottomMargin
         '
-        Me.BottomMargin.HeightF = 100.0!
+        Me.BottomMargin.HeightF = 18.75!
         Me.BottomMargin.Name = "BottomMargin"
         Me.BottomMargin.Padding = New DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100.0!)
         Me.BottomMargin.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft
@@ -123,16 +179,6 @@ Partial Public Class ReportPreCalBudget
         Me.LVendor1.Text = "[vendor1]"
         Me.LVendor1.Weight = 1.0317728380016711R
         '
-        'ReportFooter
-        '
-        Me.ReportFooter.HeightF = 100.0!
-        Me.ReportFooter.Name = "ReportFooter"
-        '
-        'PageFooter
-        '
-        Me.PageFooter.HeightF = 100.0!
-        Me.PageFooter.Name = "PageFooter"
-        '
         'XrTableCell4
         '
         Me.XrTableCell4.Name = "XrTableCell4"
@@ -144,60 +190,61 @@ Partial Public Class ReportPreCalBudget
         Me.XrTableCell5.Text = "[vendor2]"
         Me.XrTableCell5.Weight = 1.0317727413402504R
         '
-        'XrTableCell2
+        'ReportFooter
         '
-        Me.XrTableCell2.Name = "XrTableCell2"
-        Me.XrTableCell2.Text = "Unit Price"
-        Me.XrTableCell2.Weight = 0.90909090909090917R
+        Me.ReportFooter.Controls.AddRange(New DevExpress.XtraReports.UI.XRControl() {Me.XrTable2})
+        Me.ReportFooter.HeightF = 25.0!
+        Me.ReportFooter.Name = "ReportFooter"
         '
-        'XrTableCell3
+        'PageFooter
         '
-        Me.XrTableCell3.Name = "XrTableCell3"
-        Me.XrTableCell3.Text = "Qty"
-        Me.XrTableCell3.Weight = 0.36363636363636359R
+        Me.PageFooter.Controls.AddRange(New DevExpress.XtraReports.UI.XRControl() {Me.XrPageInfo1})
+        Me.PageFooter.HeightF = 22.99998!
+        Me.PageFooter.Name = "PageFooter"
         '
-        'XrTableCell6
+        'XrTable2
         '
-        Me.XrTableCell6.Name = "XrTableCell6"
-        Me.XrTableCell6.Text = "Total"
-        Me.XrTableCell6.Weight = 1.0000002774325285R
+        Me.XrTable2.Font = New System.Drawing.Font("Consolas", 8.0!)
+        Me.XrTable2.LocationFloat = New DevExpress.Utils.PointFloat(0!, 0!)
+        Me.XrTable2.Name = "XrTable2"
+        Me.XrTable2.Rows.AddRange(New DevExpress.XtraReports.UI.XRTableRow() {Me.XrTableRow2})
+        Me.XrTable2.SizeF = New System.Drawing.SizeF(1050.0!, 25.0!)
+        Me.XrTable2.StylePriority.UseFont = False
         '
-        'XrTableCell7
+        'XrTableRow2
         '
-        Me.XrTableCell7.Name = "XrTableCell7"
-        Me.XrTableCell7.Weight = 0.454544344815341R
+        Me.XrTableRow2.Cells.AddRange(New DevExpress.XtraReports.UI.XRTableCell() {Me.XrTableCell12})
+        Me.XrTableRow2.Name = "XrTableRow2"
+        Me.XrTableRow2.Weight = 1.0R
         '
-        'XrTableCell8
+        'XrTableCell12
         '
-        Me.XrTableCell8.Name = "XrTableCell8"
-        Me.XrTableCell8.StylePriority.UseTextAlignment = False
-        Me.XrTableCell8.Text = "Description"
-        Me.XrTableCell8.TextAlignment = DevExpress.XtraPrinting.TextAlignment.MiddleLeft
-        Me.XrTableCell8.Weight = 2.2727273652048763R
+        Me.XrTableCell12.Font = New System.Drawing.Font("Lucida Console", 8.0!)
+        Me.XrTableCell12.Name = "XrTableCell12"
+        Me.XrTableCell12.StylePriority.UseFont = False
+        Me.XrTableCell12.Text = "Here Table Mark Goes, Please Ignore This"
+        Me.XrTableCell12.Visible = False
+        Me.XrTableCell12.Weight = 2.99999986405489R
         '
-        'XrTableCell9
+        'XrPageInfo1
         '
-        Me.XrTableCell9.Name = "XrTableCell9"
-        Me.XrTableCell9.Text = "Unit Price"
-        Me.XrTableCell9.Weight = 0.90909093551311448R
-        '
-        'XrTableCell10
-        '
-        Me.XrTableCell10.Name = "XrTableCell10"
-        Me.XrTableCell10.Text = "Qty"
-        Me.XrTableCell10.Weight = 0.36363636469329341R
-        '
-        'XrTableCell11
-        '
-        Me.XrTableCell11.Name = "XrTableCell11"
-        Me.XrTableCell11.Text = "Total"
-        Me.XrTableCell11.Weight = 1.0000000295173761R
+        Me.XrPageInfo1.BorderColor = System.Drawing.Color.DimGray
+        Me.XrPageInfo1.Borders = DevExpress.XtraPrinting.BorderSide.None
+        Me.XrPageInfo1.Format = "Page {0} of {1}"
+        Me.XrPageInfo1.LocationFloat = New DevExpress.Utils.PointFloat(0!, 0!)
+        Me.XrPageInfo1.Name = "XrPageInfo1"
+        Me.XrPageInfo1.Padding = New DevExpress.XtraPrinting.PaddingInfo(2, 2, 0, 0, 100.0!)
+        Me.XrPageInfo1.SizeF = New System.Drawing.SizeF(1050.0!, 22.99998!)
+        Me.XrPageInfo1.StylePriority.UseBorderColor = False
+        Me.XrPageInfo1.StylePriority.UseBorders = False
+        Me.XrPageInfo1.StylePriority.UseTextAlignment = False
+        Me.XrPageInfo1.TextAlignment = DevExpress.XtraPrinting.TextAlignment.MiddleRight
         '
         'ReportPreCalBudget
         '
         Me.Bands.AddRange(New DevExpress.XtraReports.UI.Band() {Me.Detail, Me.TopMargin, Me.BottomMargin, Me.PageHeader, Me.ReportFooter, Me.PageFooter})
         Me.Landscape = True
-        Me.Margins = New System.Drawing.Printing.Margins(51, 68, 100, 100)
+        Me.Margins = New System.Drawing.Printing.Margins(51, 68, 100, 19)
         Me.PageHeight = 827
         Me.PageWidth = 1169
         Me.PaperKind = System.Drawing.Printing.PaperKind.A4
@@ -205,6 +252,7 @@ Partial Public Class ReportPreCalBudget
         Me.Version = "15.1"
         CType(Me.XTDetail, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.XrTable3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.XrTable2, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me, System.ComponentModel.ISupportInitialize).EndInit()
 
     End Sub
@@ -230,4 +278,8 @@ Partial Public Class ReportPreCalBudget
     Friend WithEvents XrTableCell9 As DevExpress.XtraReports.UI.XRTableCell
     Friend WithEvents XrTableCell10 As DevExpress.XtraReports.UI.XRTableCell
     Friend WithEvents XrTableCell11 As DevExpress.XtraReports.UI.XRTableCell
+    Friend WithEvents XrTable2 As DevExpress.XtraReports.UI.XRTable
+    Friend WithEvents XrTableRow2 As DevExpress.XtraReports.UI.XRTableRow
+    Friend WithEvents XrTableCell12 As DevExpress.XtraReports.UI.XRTableCell
+    Friend WithEvents XrPageInfo1 As DevExpress.XtraReports.UI.XRPageInfo
 End Class
--- VolcomInventory/ReportPreCalBudget.vb
+++ VolcomInventory/ReportPreCalBudget.vb
@@ -76,10 +76,11 @@
         sub_amount_colc.Font = font_row_style
     End Sub
 
-    Sub insert_header(ByRef row As DevExpress.XtraReports.UI.XRTableRow, ByVal txt As String, ByVal opt As String)
-        Dim font_row_style As New Font(XTDetail.Font.FontFamily, XTDetail.Font.Size, FontStyle.Regular)
+    Sub insert_header(ByRef row_det As DevExpress.XtraReports.UI.XRTableRow, ByVal txt As String, ByVal opt As String)
+        Dim font_row_style As New Font(XTDetail.Font.FontFamily, XTDetail.Font.Size, FontStyle.Underline)
+        Dim row As New DevExpress.XtraReports.UI.XRTableRow
 
-        row = XTDetail.InsertRowBelow(row)
+        row = XTDetail.InsertRowAbove(row_det)
 
         row.Borders = DevExpress.XtraPrinting.BorderSide.All
         row.BorderWidth = 1
@@ -135,10 +136,108 @@
         sub_amount_colc.Text = ""
         sub_amount_colc.TextAlignment = DevExpress.XtraPrinting.TextAlignment.MiddleRight
         sub_amount_colc.Font = font_row_style
+
+        row.DeleteCell(total_nilai)
+        row.DeleteCell(col_qty)
+        row.DeleteCell(sub_amount_col)
+        '
+        Dim separator As DevExpress.XtraReports.UI.XRTableCell = row.Cells.Item(1)
+        '
+        row.DeleteCell(total_nilaic)
+        row.DeleteCell(col_qtyc)
+        row.DeleteCell(sub_amount_colc)
+
+        desc.WidthF = 500
+        separator.WidthF = 50
+        descc.WidthF = 500
+
+        If opt = "sub" Then
+            desc.BackColor = Color.LightGray
+            descc.BackColor = Color.LightGray
+        End If
+    End Sub
+
+    Sub insert_row_manual(ByRef row As DevExpress.XtraReports.UI.XRTableRow, ByVal dt As DataTable, ByVal row_i As Integer)
+        Dim font_row_style As New Font(XTDetail.Font.FontFamily, XTDetail.Font.Size, FontStyle.Regular)
+
+        If Not row_i = 0 Then
+            row = XTDetail.InsertRowBelow(row)
+        End If
+
+        row.Borders = DevExpress.XtraPrinting.BorderSide.All
+        row.BorderWidth = 1
+        row.HeightF = 15
+        row.Font = font_row_style
+
+        '-------------------- vendor 1 ------------------------------------------
+        'desc
+        Dim desc As DevExpress.XtraReports.UI.XRTableCell = row.Cells.Item(0)
+        desc.Text = dt.Rows(row_i)("desc").ToString
+        desc.TextAlignment = DevExpress.XtraPrinting.TextAlignment.MiddleLeft
+        desc.Font = font_row_style
+
+        'value
+        Dim harga As String = Decimal.Parse(dt.Rows(row_i)("unit_price_in_rp").ToString).ToString("N2")
+
+        Dim total_nilai As DevExpress.XtraReports.UI.XRTableCell = row.Cells.Item(1)
+        total_nilai.Text = harga
+        total_nilai.TextAlignment = DevExpress.XtraPrinting.TextAlignment.MiddleRight
+        total_nilai.Font = font_row_style
+
+        'qty
+        Dim qty As String = Decimal.Parse(dt.Rows(row_i)("qty").ToString).ToString("N2")
+
+        Dim col_qty As DevExpress.XtraReports.UI.XRTableCell = row.Cells.Item(2)
+        col_qty.Text = qty
+        col_qty.TextAlignment = DevExpress.XtraPrinting.TextAlignment.MiddleRight
+        col_qty.Font = font_row_style
+
+        'sub_amount
+        Dim sub_amount As String = Decimal.Parse(dt.Rows(row_i)("total_in_rp").ToString).ToString("N2")
+
+        Dim sub_amount_col As DevExpress.XtraReports.UI.XRTableCell = row.Cells.Item(3)
+        sub_amount_col.Text = sub_amount
+        sub_amount_col.TextAlignment = DevExpress.XtraPrinting.TextAlignment.MiddleRight
+        sub_amount_col.Font = font_row_style
+
+        '-------------------- vendor 2 ------------------------------------------
+        'desc
+        Dim descc As DevExpress.XtraReports.UI.XRTableCell = row.Cells.Item(5)
+        descc.Text = dt.Rows(row_i)("descc").ToString
+        descc.TextAlignment = DevExpress.XtraPrinting.TextAlignment.MiddleLeft
+        descc.Font = font_row_style
+
+        'value
+        Dim hargac As String = Decimal.Parse(dt.Rows(row_i)("unit_price_in_rpc").ToString).ToString("N2")
+
+        Dim total_nilaic As DevExpress.XtraReports.UI.XRTableCell = row.Cells.Item(6)
+        total_nilaic.Text = hargac
+        total_nilaic.TextAlignment = DevExpress.XtraPrinting.TextAlignment.MiddleRight
+        total_nilaic.Font = font_row_style
+
+        'qty
+        Dim qtyc As String = Decimal.Parse(dt.Rows(row_i)("qtyc").ToString).ToString("N2")
+
+        Dim col_qtyc As DevExpress.XtraReports.UI.XRTableCell = row.Cells.Item(7)
+        col_qtyc.Text = qtyc
+        col_qtyc.TextAlignment = DevExpress.XtraPrinting.TextAlignment.MiddleRight
+        col_qtyc.Font = font_row_style
+
+        'sub_amount
+        Dim sub_amountc As String = Decimal.Parse(dt.Rows(row_i)("total_in_rpc").ToString).ToString("N2")
+
+        Dim sub_amount_colc As DevExpress.XtraReports.UI.XRTableCell = row.Cells.Item(8)
+        sub_amount_colc.Text = sub_amountc
+        sub_amount_colc.TextAlignment = DevExpress.XtraPrinting.TextAlignment.MiddleRight
+        sub_amount_colc.Font = font_row_style
     End Sub
 
     Private Sub ReportPreCalBudget_BeforePrint(sender As Object, e As Printing.PrintEventArgs) Handles MyBase.BeforePrint
         Dim row_baru As DevExpress.XtraReports.UI.XRTableRow = XRRow
+        Dim total_orign As Decimal = 0.00
+        Dim total_orignc As Decimal = 0.00
+        Dim total_dest As Decimal = 0.00
+        Dim total_destc As Decimal = 0.00
 
         Dim qorign As String = "SELECT d.`desc`,IFNULL(d.`unit_price`,0) AS unit_price,IFNULL(d.`qty`,0) AS qty,IFNULL(d.`unit_price_in_rp`,0) AS unit_price_in_rp,IFNULL(d.`total_in_rp`,0) AS total_in_rp, dc.`desc` AS descc,IFNULL(dc.`unit_price`,0) AS unit_pricec,IFNULL(dc.`qty`,0) AS qtyc,IFNULL(dc.`unit_price_in_rp`,0) AS unit_price_in_rpc,IFNULL(dc.`total_in_rp`,0) AS total_in_rpc
 FROM `tb_pre_cal_fgpo_det` d
@@ -148,12 +247,15 @@ WHERE d.`id_pre_cal_fgpo`='" & id_report & "'
 ORDER BY d.`id_pre_cal_temp` ASC"
         Dim dt_orign As DataTable = execute_query(qorign, -1, True, "", "", "", "")
 
-        insert_header(row_baru, "Orign Charges :", "sub")
-
         For i = 0 To dt_orign.Rows.Count - 1
+            If i = 0 Then
+                row_baru = XTDetail.InsertRowBelow(row_baru)
+                insert_header(row_baru, "ORIGN CHARGES :", "sub")
+            End If
             insert_row(row_baru, dt_orign, i)
+            total_orign += dt_orign.Rows(i)("total_in_rp")
+            total_orignc += dt_orign.Rows(i)("total_in_rpc")
         Next
-
         '
         Dim qdest As String = "SELECT d.`desc`,IFNULL(d.`unit_price`,0) AS unit_price,IFNULL(d.`qty`,0) AS qty,IFNULL(d.`unit_price_in_rp`,0) AS unit_price_in_rp,IFNULL(d.`total_in_rp`,0) AS total_in_rp, dc.`desc` AS descc,IFNULL(dc.`unit_price`,0) AS unit_pricec,IFNULL(dc.`qty`,0) AS qtyc,IFNULL(dc.`unit_price_in_rp`,0) AS unit_price_in_rpc,IFNULL(dc.`total_in_rp`,0) AS total_in_rpc
 FROM `tb_pre_cal_fgpo_det` d
@@ -164,7 +266,13 @@ ORDER BY d.`id_pre_cal_temp` ASC"
         Dim dt_dest As DataTable = execute_query(qdest, -1, True, "", "", "", "")
 
         For i = 0 To dt_dest.Rows.Count - 1
+            If i = 0 Then
+                row_baru = XTDetail.InsertRowBelow(row_baru)
+                insert_header(row_baru, "DESTINATION CHARGES :", "sub")
+            End If
             insert_row(row_baru, dt_dest, i)
+            total_dest += dt_orign.Rows(i)("total_in_rp")
+            total_destc += dt_orign.Rows(i)("total_in_rpc")
         Next
 
         '
@@ -177,6 +285,17 @@ ORDER BY d.`id_pre_cal_temp` ASC"
         Dim dt_adm As DataTable = execute_query(qadm, -1, True, "", "", "", "")
 
         For i = 0 To dt_adm.Rows.Count - 1
+            If i = 0 Then
+                row_baru = XTDetail.InsertRowBelow(row_baru)
+                insert_header(row_baru, "TOTAL AVERAGE", "")
+                '
+                Dim qsum As String = "SELECT 'FREIGHT' AS desc,0 AS unit_price,0 AS qty,0 AS unit_price_in_rp,'" & decimalSQL(Decimal.Parse(total_orign.ToString).ToString) & "' AS total_in_rp, 'FREIGHT' AS descc,0 AS unit_pricec,0 AS qtyc,0 AS unit_price_in_rpc,'" & decimalSQL(Decimal.Parse(total_orignc.ToString).ToString) & "' AS total_in_rpc
+UNION ALL
+SELECT 'DESTINATION CHARGES' AS desc,0 AS unit_price,0 AS qty,0 AS unit_price_in_rp,'" & decimalSQL(Decimal.Parse(total_dest.ToString).ToString) & "' AS total_in_rp, 'DESTINATION CHARGES' AS descc,0 AS unit_pricec,0 AS qtyc,0 AS unit_price_in_rpc,'" & decimalSQL(Decimal.Parse(total_destc.ToString).ToString) & "' AS total_in_rpc"
+                Dim dtsum As DataTable = execute_query(qsum, -1, True, "", "", "", "")
+                '
+                insert_row(row_baru, dtsum, i)
+            End If
             insert_row(row_baru, dt_adm, i)
         Next
     End Sub
