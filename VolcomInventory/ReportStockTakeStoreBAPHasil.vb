49e0c55127cbab93537be930c801f28b5e7054bd
Agus Friastana <friastana@gmail.com>
2021-09-17 17:26:54 +0800

    stocktake bap view draft

--- VolcomInventory/ReportStockTakeStoreBAPHasil.vb
+++ VolcomInventory/ReportStockTakeStoreBAPHasil.vb
@@ -3,6 +3,8 @@
     Public report_mark_type As String = "-1"
 
     Private Sub ReportStockTakeStoreBAPHasil_BeforePrint(sender As Object, e As Printing.PrintEventArgs) Handles MyBase.BeforePrint
-        pre_load_mark_horz(report_mark_type, id, "2", "2", XrTable1)
+        If Not report_mark_type = "0" Then
+            pre_load_mark_horz(report_mark_type, id, "2", "2", XrTable1)
+        End If
     End Sub
 End Class
