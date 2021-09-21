4d858ce5b1fd009ba6df45117c45756e1f8d2336
Agus Friastana <friastana@gmail.com>
2021-09-21 11:18:05 +0800

    fix rb

--- VolcomInventory/FormStockTakePropose.vb
+++ VolcomInventory/FormStockTakePropose.vb
@@ -28,4 +28,13 @@
         FormStockTakeProposeDet.id_st_store_propose = GVData.GetFocusedRowCellValue("id_st_store_propose").ToString
         FormStockTakeProposeDet.ShowDialog()
     End Sub
+
+    Private Sub FormStockTakePropose_Activated(sender As Object, e As EventArgs) Handles MyBase.Activated
+        FormMain.show_rb(Name)
+        checkFormAccess(Name)
+    End Sub
+
+    Private Sub FormStockTakePropose_Deactivate(sender As Object, e As EventArgs) Handles MyBase.Deactivate
+        FormMain.hide_rb()
+    End Sub
 End Class
