a7484cea19643192576a970f13c73e399f97af3a
Satrya Anas <catur.satrya@gmail.com>
2021-09-21 14:59:57 +0800

    prepare view

---
+++ VolcomInventory/FormDisplayAlloc.Designer.vb
@@ -0,0 +1,132 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FormDisplayAlloc
+    Inherits DevExpress.XtraEditors.XtraForm
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        If disposing AndAlso components IsNot Nothing Then
+            components.Dispose()
+        End If
+        MyBase.Dispose(disposing)
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FormDisplayAlloc))
+        Me.PanelControl1 = New DevExpress.XtraEditors.PanelControl()
+        Me.GCData = New DevExpress.XtraGrid.GridControl()
+        Me.GVData = New DevExpress.XtraGrid.Views.Grid.GridView()
+        Me.BtnPrint = New DevExpress.XtraEditors.SimpleButton()
+        Me.BtnExportToXLS = New DevExpress.XtraEditors.SimpleButton()
+        Me.BtnRefresh = New DevExpress.XtraEditors.SimpleButton()
+        Me.BtnClassGroup = New DevExpress.XtraEditors.SimpleButton()
+        CType(Me.PanelControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.PanelControl1.SuspendLayout()
+        CType(Me.GCData, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.GVData, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PanelControl1
+        '
+        Me.PanelControl1.Controls.Add(Me.BtnClassGroup)
+        Me.PanelControl1.Controls.Add(Me.BtnExportToXLS)
+        Me.PanelControl1.Controls.Add(Me.BtnPrint)
+        Me.PanelControl1.Controls.Add(Me.BtnRefresh)
+        Me.PanelControl1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.PanelControl1.Location = New System.Drawing.Point(0, 0)
+        Me.PanelControl1.Name = "PanelControl1"
+        Me.PanelControl1.Size = New System.Drawing.Size(717, 51)
+        Me.PanelControl1.TabIndex = 0
+        '
+        'GCData
+        '
+        Me.GCData.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.GCData.Location = New System.Drawing.Point(0, 51)
+        Me.GCData.MainView = Me.GVData
+        Me.GCData.Name = "GCData"
+        Me.GCData.Size = New System.Drawing.Size(717, 401)
+        Me.GCData.TabIndex = 1
+        Me.GCData.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() {Me.GVData})
+        '
+        'GVData
+        '
+        Me.GVData.GridControl = Me.GCData
+        Me.GVData.Name = "GVData"
+        Me.GVData.OptionsFind.AlwaysVisible = True
+        Me.GVData.OptionsView.ColumnAutoWidth = False
+        Me.GVData.OptionsView.ShowGroupPanel = False
+        '
+        'BtnPrint
+        '
+        Me.BtnPrint.Dock = System.Windows.Forms.DockStyle.Right
+        Me.BtnPrint.Image = CType(resources.GetObject("BtnPrint.Image"), System.Drawing.Image)
+        Me.BtnPrint.Location = New System.Drawing.Point(531, 2)
+        Me.BtnPrint.Name = "BtnPrint"
+        Me.BtnPrint.Size = New System.Drawing.Size(87, 47)
+        Me.BtnPrint.TabIndex = 0
+        Me.BtnPrint.Text = "Print"
+        '
+        'BtnExportToXLS
+        '
+        Me.BtnExportToXLS.Dock = System.Windows.Forms.DockStyle.Right
+        Me.BtnExportToXLS.Image = CType(resources.GetObject("BtnExportToXLS.Image"), System.Drawing.Image)
+        Me.BtnExportToXLS.Location = New System.Drawing.Point(412, 2)
+        Me.BtnExportToXLS.Name = "BtnExportToXLS"
+        Me.BtnExportToXLS.Size = New System.Drawing.Size(119, 47)
+        Me.BtnExportToXLS.TabIndex = 1
+        Me.BtnExportToXLS.Text = "Export to XLS"
+        '
+        'BtnRefresh
+        '
+        Me.BtnRefresh.Dock = System.Windows.Forms.DockStyle.Right
+        Me.BtnRefresh.Image = CType(resources.GetObject("BtnRefresh.Image"), System.Drawing.Image)
+        Me.BtnRefresh.Location = New System.Drawing.Point(618, 2)
+        Me.BtnRefresh.Name = "BtnRefresh"
+        Me.BtnRefresh.Size = New System.Drawing.Size(97, 47)
+        Me.BtnRefresh.TabIndex = 2
+        Me.BtnRefresh.Text = "Refresh"
+        '
+        'BtnClassGroup
+        '
+        Me.BtnClassGroup.Dock = System.Windows.Forms.DockStyle.Left
+        Me.BtnClassGroup.Image = CType(resources.GetObject("BtnClassGroup.Image"), System.Drawing.Image)
+        Me.BtnClassGroup.Location = New System.Drawing.Point(2, 2)
+        Me.BtnClassGroup.Name = "BtnClassGroup"
+        Me.BtnClassGroup.Size = New System.Drawing.Size(114, 47)
+        Me.BtnClassGroup.TabIndex = 3
+        Me.BtnClassGroup.Text = "Class Group"
+        '
+        'FormDisplayAlloc
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(717, 452)
+        Me.Controls.Add(Me.GCData)
+        Me.Controls.Add(Me.PanelControl1)
+        Me.MinimizeBox = False
+        Me.Name = "FormDisplayAlloc"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Display Allocation"
+        CType(Me.PanelControl1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.PanelControl1.ResumeLayout(False)
+        CType(Me.GCData, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.GVData, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents PanelControl1 As DevExpress.XtraEditors.PanelControl
+    Friend WithEvents GCData As DevExpress.XtraGrid.GridControl
+    Friend WithEvents GVData As DevExpress.XtraGrid.Views.Grid.GridView
+    Friend WithEvents BtnExportToXLS As DevExpress.XtraEditors.SimpleButton
+    Friend WithEvents BtnPrint As DevExpress.XtraEditors.SimpleButton
+    Friend WithEvents BtnRefresh As DevExpress.XtraEditors.SimpleButton
+    Friend WithEvents BtnClassGroup As DevExpress.XtraEditors.SimpleButton
+End Class
---
+++ VolcomInventory/FormDisplayAlloc.vb
@@ -0,0 +1,56 @@
+﻿Public Class FormDisplayAlloc
+    Private Sub FormDisplayAlloc_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub FormDisplayAlloc_FormClosed(sender As Object, e As FormClosedEventArgs) Handles MyBase.FormClosed
+        Dispose()
+    End Sub
+
+    Sub viewData()
+        Cursor = Cursors.WaitCursor
+
+        'build kolom
+        Dim qt As String = "SELECT dt.id_display_type, UPPER(dt.display_type) AS `display_type` FROM tb_display_type dt "
+        Dim dt As DataTable = execute_query(qt, -1, True, "", "", "", "")
+        Dim col_type1 As String = ""
+        Dim col_type2 As String = ""
+        For i As Integer = 0 To dt.Rows.Count - 1
+            If i > 0 Then
+                col_type1 += ","
+                col_type2 += ","
+            End If
+            col_type1 += "SUM(CASE WHEN a.id_display_type=" + dt.Rows(i)("id_display_type").ToString + " THEN a.capacity END) AS `" + dt.Rows(i)("display_type").ToString + "` "
+            col_type2 += "IFNULL(a.`" + dt.Rows(i)("display_type").ToString + "`,0) AS `" + dt.Rows(i)("display_type").ToString + "` "
+        Next
+
+        Dim query As String = "SELECT cg.id_class_group, cg.id_division, dv.display_name AS `division`, 
+        cg.id_class_type, ct.class_type, cg.id_class_cat, UPPER(cat.class_cat) AS `class_cat`, cg.class_group,
+        " + col_type2 + ",
+        IFNULL(a.`TOTAL DISPLAY`,0) AS `TOTAL DISPLAY`, 
+        IFNULL(a.`ESTIMASI SKU`,0) AS `ESTIMASI SKU`
+        FROM tb_class_group cg
+        INNER JOIN tb_m_code_detail dv ON dv.id_code_detail = cg.id_division
+        INNER JOIN tb_class_type ct ON ct.id_class_type = cg.id_class_type
+        INNER JOIN tb_class_cat cat ON cat.id_class_cat = cg.id_class_cat
+        LEFT JOIN (
+	        SELECT a.id_class_group, 
+	        " + col_type1 + ",
+	        SUM(a.capacity) AS `TOTAL DISPLAY`, 
+	        SUM(a.capacity / a.qty_size) AS `ESTIMASI SKU`
+	        FROM tb_display_alloc a
+	        GROUP BY a.id_class_group
+        ) a ON a.id_class_group = cg.id_class_group "
+        Dim data As DataTable = execute_query(query, -1, True, "", "", "", "")
+        GCData.DataSource = data
+        GVData.BestFitColumns()
+        Cursor = Cursors.Default
+    End Sub
+
+    Private Sub BtnClassGroup_Click(sender As Object, e As EventArgs) Handles BtnClassGroup.Click
+        Cursor = Cursors.WaitCursor
+        FormClassGroup.ShowDialog()
+        viewData()
+        Cursor = Cursors.Default
+    End Sub
+End Class
