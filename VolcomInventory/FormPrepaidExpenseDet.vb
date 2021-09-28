032301bdd2968d9ec9bccf44947395f9ed3575ac
Putu Septian Primadewa <iansaviour@gmail.com>
2021-09-28 12:14:36 +0800

    add extra warning

--- VolcomInventory/FormPrepaidExpenseDet.vb
+++ VolcomInventory/FormPrepaidExpenseDet.vb
@@ -2,4 +2,8 @@
     Private Sub FormPrepaidExpenseDet_FormClosed(sender As Object, e As FormClosedEventArgs) Handles MyBase.FormClosed
         Dispose()
     End Sub
+
+    Private Sub FormPrepaidExpenseDet_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
 End Class
