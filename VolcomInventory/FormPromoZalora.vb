de98f995e8ccf2b24ca74101b75ec007813d8107
Satrya Anas <catur.satrya@gmail.com>
2021-10-04 09:52:26 +0800

    show rb

--- VolcomInventory/FormPromoZalora.vb
+++ VolcomInventory/FormPromoZalora.vb
@@ -19,4 +19,13 @@
     Sub printList()
 
     End Sub
+
+    Private Sub FormPromoZalora_Activated(sender As Object, e As EventArgs) Handles MyBase.Activated
+        FormMain.show_rb(Name)
+        checkFormAccess(Name)
+    End Sub
+
+    Private Sub FormPromoZalora_Deactivate(sender As Object, e As EventArgs) Handles MyBase.Deactivate
+        FormMain.hide_rb()
+    End Sub
 End Class
