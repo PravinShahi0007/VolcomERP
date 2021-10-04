26d3f0f4cdb704525abc50b2b3e61ce2478234f2
Satrya Anas <catur.satrya@gmail.com>
2021-10-04 09:48:08 +0800

    New Menu : Promo Zalora

---
+++ VolcomInventory/FormPromoZalora.vb
@@ -0,0 +1,22 @@
+﻿Public Class FormPromoZalora
+
+    Private Sub FormPromoZalora_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Sub newPropose()
+
+    End Sub
+
+    Sub viewData()
+
+    End Sub
+
+    Sub viewDetail()
+
+    End Sub
+
+    Sub printList()
+
+    End Sub
+End Class
