d51e1576f3f5d02a489b5d9e6c29191cac31c69d
Putu Septian Primadewa <iansaviour@gmail.com>
2021-10-18 15:20:16 +0800

    add sni pop

---
+++ VolcomInventory/FormSNIRealisasiPop.vb
@@ -0,0 +1,17 @@
+﻿Public Class FormSNIRealisasiPop
+    Private Sub FormSNIRealisasiPop_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim q As String = "SELECT d.id_item_del,d.number AS item_del_number,r.`number` AS item_req_number,dd.`qty`,d.`note`
+FROM tb_item_del_det dd
+INNER JOIN tb_item_del d ON d.`id_item_del`=dd.`id_item_del` AND d.`id_report_status`=6
+INNER JOIN tb_item_req_det rd ON rd.`id_item_req_det`=dd.`id_item_req_det`
+INNER JOIN tb_item_req r ON r.id_item_req=rd.id_item_req
+WHERE rd.`id_item`='752'"
+        Dim dt As DataTable = execute_query(q, -1, True, "", "", "", "")
+
+
+    End Sub
+
+    Private Sub BApprove_Click(sender As Object, e As EventArgs) Handles BApprove.Click
+
+    End Sub
+End Class
