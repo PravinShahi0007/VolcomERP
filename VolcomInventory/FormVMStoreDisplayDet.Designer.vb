6eb791fbc870aa6619b2c516fc1767fd653c2f7f
Satrya Anas <catur.satrya@gmail.com>
2021-10-19 17:50:23 +0800

    prepare

--- VolcomInventory/FormVMStoreDisplayDet.Designer.vb
+++ VolcomInventory/FormVMStoreDisplayDet.Designer.vb
@@ -19,29 +19,41 @@ Partial Class FormVMStoreDisplayDet
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FormVMStoreDisplayDet))
         Me.PanelControl1 = New DevExpress.XtraEditors.PanelControl()
         Me.GCData = New DevExpress.XtraGrid.GridControl()
         Me.GVData = New DevExpress.XtraGrid.Views.Grid.GridView()
+        Me.LabelControl1 = New DevExpress.XtraEditors.LabelControl()
+        Me.TxtStoreCode = New DevExpress.XtraEditors.TextEdit()
+        Me.TxtStoreName = New DevExpress.XtraEditors.TextEdit()
+        Me.BtnPrint = New DevExpress.XtraEditors.SimpleButton()
         CType(Me.PanelControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.PanelControl1.SuspendLayout()
         CType(Me.GCData, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.GVData, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TxtStoreCode.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TxtStoreName.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'PanelControl1
         '
+        Me.PanelControl1.Controls.Add(Me.BtnPrint)
+        Me.PanelControl1.Controls.Add(Me.TxtStoreName)
+        Me.PanelControl1.Controls.Add(Me.TxtStoreCode)
+        Me.PanelControl1.Controls.Add(Me.LabelControl1)
         Me.PanelControl1.Dock = System.Windows.Forms.DockStyle.Top
         Me.PanelControl1.Location = New System.Drawing.Point(0, 0)
         Me.PanelControl1.Name = "PanelControl1"
-        Me.PanelControl1.Size = New System.Drawing.Size(552, 46)
+        Me.PanelControl1.Size = New System.Drawing.Size(584, 50)
         Me.PanelControl1.TabIndex = 0
         '
         'GCData
         '
         Me.GCData.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.GCData.Location = New System.Drawing.Point(0, 46)
+        Me.GCData.Location = New System.Drawing.Point(0, 50)
         Me.GCData.MainView = Me.GVData
         Me.GCData.Name = "GCData"
-        Me.GCData.Size = New System.Drawing.Size(552, 301)
+        Me.GCData.Size = New System.Drawing.Size(584, 311)
         Me.GCData.TabIndex = 1
         Me.GCData.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() {Me.GVData})
         '
@@ -49,13 +61,49 @@ Partial Class FormVMStoreDisplayDet
         '
         Me.GVData.GridControl = Me.GCData
         Me.GVData.Name = "GVData"
+        Me.GVData.OptionsFind.AlwaysVisible = True
+        Me.GVData.OptionsView.ColumnAutoWidth = False
+        Me.GVData.OptionsView.ShowFooter = True
         Me.GVData.OptionsView.ShowGroupPanel = False
+        '
+        'LabelControl1
+        '
+        Me.LabelControl1.Location = New System.Drawing.Point(12, 18)
+        Me.LabelControl1.Name = "LabelControl1"
+        Me.LabelControl1.Size = New System.Drawing.Size(26, 13)
+        Me.LabelControl1.TabIndex = 0
+        Me.LabelControl1.Text = "Store"
+        '
+        'TxtStoreCode
+        '
+        Me.TxtStoreCode.Enabled = False
+        Me.TxtStoreCode.Location = New System.Drawing.Point(44, 15)
+        Me.TxtStoreCode.Name = "TxtStoreCode"
+        Me.TxtStoreCode.Size = New System.Drawing.Size(61, 20)
+        Me.TxtStoreCode.TabIndex = 1
+        '
+        'TxtStoreName
+        '
+        Me.TxtStoreName.Enabled = False
+        Me.TxtStoreName.Location = New System.Drawing.Point(107, 15)
+        Me.TxtStoreName.Name = "TxtStoreName"
+        Me.TxtStoreName.Size = New System.Drawing.Size(276, 20)
+        Me.TxtStoreName.TabIndex = 2
+        '
+        'BtnPrint
+        '
+        Me.BtnPrint.Image = CType(resources.GetObject("BtnPrint.Image"), System.Drawing.Image)
+        Me.BtnPrint.Location = New System.Drawing.Point(389, 13)
+        Me.BtnPrint.Name = "BtnPrint"
+        Me.BtnPrint.Size = New System.Drawing.Size(63, 23)
+        Me.BtnPrint.TabIndex = 3
+        Me.BtnPrint.Text = "Print"
         '
         'FormVMStoreDisplayDet
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(552, 347)
+        Me.ClientSize = New System.Drawing.Size(584, 361)
         Me.Controls.Add(Me.GCData)
         Me.Controls.Add(Me.PanelControl1)
         Me.MaximizeBox = False
@@ -63,8 +111,12 @@ Partial Class FormVMStoreDisplayDet
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "VM Store Display Setup"
         CType(Me.PanelControl1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.PanelControl1.ResumeLayout(False)
+        Me.PanelControl1.PerformLayout()
         CType(Me.GCData, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.GVData, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.TxtStoreCode.Properties, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.TxtStoreName.Properties, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
@@ -72,4 +124,8 @@ Partial Class FormVMStoreDisplayDet
     Friend WithEvents PanelControl1 As DevExpress.XtraEditors.PanelControl
     Friend WithEvents GCData As DevExpress.XtraGrid.GridControl
     Friend WithEvents GVData As DevExpress.XtraGrid.Views.Grid.GridView
+    Friend WithEvents TxtStoreName As DevExpress.XtraEditors.TextEdit
+    Friend WithEvents TxtStoreCode As DevExpress.XtraEditors.TextEdit
+    Friend WithEvents LabelControl1 As DevExpress.XtraEditors.LabelControl
+    Friend WithEvents BtnPrint As DevExpress.XtraEditors.SimpleButton
 End Class
--- VolcomInventory/FormVMStoreDisplayDet.vb
+++ VolcomInventory/FormVMStoreDisplayDet.vb
@@ -1,3 +1,11 @@
 ﻿Public Class FormVMStoreDisplayDet
     Public id_store As String = "-1"
+
+    Private Sub BtnPrint_Click(sender As Object, e As EventArgs) Handles BtnPrint.Click
+        print(GCData, "Display Item List")
+    End Sub
+
+    Private Sub FormVMStoreDisplayDet_FormClosed(sender As Object, e As FormClosedEventArgs) Handles MyBase.FormClosed
+        Dispose()
+    End Sub
 End Class
