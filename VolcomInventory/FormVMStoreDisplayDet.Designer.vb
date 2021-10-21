57efb76c15713f366493f631042d9ed94fbec60e
Satrya Anas <catur.satrya@gmail.com>
2021-10-21 15:13:13 +0800

    prepare

--- VolcomInventory/FormVMStoreDisplayDet.Designer.vb
+++ VolcomInventory/FormVMStoreDisplayDet.Designer.vb
@@ -21,39 +21,105 @@ Partial Class FormVMStoreDisplayDet
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FormVMStoreDisplayDet))
         Me.PanelControl1 = New DevExpress.XtraEditors.PanelControl()
+        Me.LabelControl2 = New DevExpress.XtraEditors.LabelControl()
+        Me.BtnPrint = New DevExpress.XtraEditors.SimpleButton()
+        Me.TxtStoreName = New DevExpress.XtraEditors.TextEdit()
+        Me.TxtStoreCode = New DevExpress.XtraEditors.TextEdit()
+        Me.LabelControl1 = New DevExpress.XtraEditors.LabelControl()
+        Me.BtnBrowse = New DevExpress.XtraEditors.SimpleButton()
         Me.GCData = New DevExpress.XtraGrid.GridControl()
         Me.GVData = New DevExpress.XtraGrid.Views.Grid.GridView()
-        Me.LabelControl1 = New DevExpress.XtraEditors.LabelControl()
-        Me.TxtStoreCode = New DevExpress.XtraEditors.TextEdit()
-        Me.TxtStoreName = New DevExpress.XtraEditors.TextEdit()
-        Me.BtnPrint = New DevExpress.XtraEditors.SimpleButton()
         CType(Me.PanelControl1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.PanelControl1.SuspendLayout()
+        CType(Me.TxtStoreName.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TxtStoreCode.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.GCData, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.GVData, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.TxtStoreCode.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.TxtStoreName.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'PanelControl1
         '
+        Me.PanelControl1.Controls.Add(Me.LabelControl2)
         Me.PanelControl1.Controls.Add(Me.BtnPrint)
         Me.PanelControl1.Controls.Add(Me.TxtStoreName)
         Me.PanelControl1.Controls.Add(Me.TxtStoreCode)
         Me.PanelControl1.Controls.Add(Me.LabelControl1)
+        Me.PanelControl1.Controls.Add(Me.BtnBrowse)
         Me.PanelControl1.Dock = System.Windows.Forms.DockStyle.Top
         Me.PanelControl1.Location = New System.Drawing.Point(0, 0)
         Me.PanelControl1.Name = "PanelControl1"
-        Me.PanelControl1.Size = New System.Drawing.Size(584, 50)
+        Me.PanelControl1.Size = New System.Drawing.Size(731, 67)
         Me.PanelControl1.TabIndex = 0
+        '
+        'LabelControl2
+        '
+        Me.LabelControl2.Appearance.Font = New System.Drawing.Font("Tahoma", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LabelControl2.Location = New System.Drawing.Point(77, 13)
+        Me.LabelControl2.Name = "LabelControl2"
+        Me.LabelControl2.Size = New System.Drawing.Size(66, 13)
+        Me.LabelControl2.TabIndex = 5
+        Me.LabelControl2.Text = "Store Name"
+        '
+        'BtnPrint
+        '
+        Me.BtnPrint.Dock = System.Windows.Forms.DockStyle.Right
+        Me.BtnPrint.Image = CType(resources.GetObject("BtnPrint.Image"), System.Drawing.Image)
+        Me.BtnPrint.ImageLocation = DevExpress.XtraEditors.ImageLocation.TopCenter
+        Me.BtnPrint.Location = New System.Drawing.Point(522, 2)
+        Me.BtnPrint.Name = "BtnPrint"
+        Me.BtnPrint.Size = New System.Drawing.Size(97, 63)
+        Me.BtnPrint.TabIndex = 3
+        Me.BtnPrint.Text = "Print List"
+        '
+        'TxtStoreName
+        '
+        Me.TxtStoreName.EditValue = ""
+        Me.TxtStoreName.Enabled = False
+        Me.TxtStoreName.Location = New System.Drawing.Point(77, 29)
+        Me.TxtStoreName.Name = "TxtStoreName"
+        Me.TxtStoreName.Properties.Appearance.Font = New System.Drawing.Font("Tahoma", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TxtStoreName.Properties.Appearance.Options.UseFont = True
+        Me.TxtStoreName.Size = New System.Drawing.Size(369, 20)
+        Me.TxtStoreName.TabIndex = 2
+        '
+        'TxtStoreCode
+        '
+        Me.TxtStoreCode.EditValue = ""
+        Me.TxtStoreCode.Enabled = False
+        Me.TxtStoreCode.Location = New System.Drawing.Point(12, 29)
+        Me.TxtStoreCode.Name = "TxtStoreCode"
+        Me.TxtStoreCode.Properties.Appearance.Font = New System.Drawing.Font("Tahoma", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TxtStoreCode.Properties.Appearance.Options.UseFont = True
+        Me.TxtStoreCode.Size = New System.Drawing.Size(62, 20)
+        Me.TxtStoreCode.TabIndex = 1
+        '
+        'LabelControl1
+        '
+        Me.LabelControl1.Appearance.Font = New System.Drawing.Font("Tahoma", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LabelControl1.Location = New System.Drawing.Point(12, 13)
+        Me.LabelControl1.Name = "LabelControl1"
+        Me.LabelControl1.Size = New System.Drawing.Size(28, 13)
+        Me.LabelControl1.TabIndex = 0
+        Me.LabelControl1.Text = "Code"
+        '
+        'BtnBrowse
+        '
+        Me.BtnBrowse.Dock = System.Windows.Forms.DockStyle.Right
+        Me.BtnBrowse.Image = CType(resources.GetObject("BtnBrowse.Image"), System.Drawing.Image)
+        Me.BtnBrowse.ImageLocation = DevExpress.XtraEditors.ImageLocation.TopCenter
+        Me.BtnBrowse.Location = New System.Drawing.Point(619, 2)
+        Me.BtnBrowse.Name = "BtnBrowse"
+        Me.BtnBrowse.Size = New System.Drawing.Size(110, 63)
+        Me.BtnBrowse.TabIndex = 4
+        Me.BtnBrowse.Text = "Browse Item"
         '
         'GCData
         '
         Me.GCData.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.GCData.Location = New System.Drawing.Point(0, 50)
+        Me.GCData.Location = New System.Drawing.Point(0, 67)
         Me.GCData.MainView = Me.GVData
         Me.GCData.Name = "GCData"
-        Me.GCData.Size = New System.Drawing.Size(584, 311)
+        Me.GCData.Size = New System.Drawing.Size(731, 294)
         Me.GCData.TabIndex = 1
         Me.GCData.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() {Me.GVData})
         '
@@ -66,44 +132,11 @@ Partial Class FormVMStoreDisplayDet
         Me.GVData.OptionsView.ShowFooter = True
         Me.GVData.OptionsView.ShowGroupPanel = False
         '
-        'LabelControl1
-        '
-        Me.LabelControl1.Location = New System.Drawing.Point(12, 18)
-        Me.LabelControl1.Name = "LabelControl1"
-        Me.LabelControl1.Size = New System.Drawing.Size(26, 13)
-        Me.LabelControl1.TabIndex = 0
-        Me.LabelControl1.Text = "Store"
-        '
-        'TxtStoreCode
-        '
-        Me.TxtStoreCode.Enabled = False
-        Me.TxtStoreCode.Location = New System.Drawing.Point(44, 15)
-        Me.TxtStoreCode.Name = "TxtStoreCode"
-        Me.TxtStoreCode.Size = New System.Drawing.Size(61, 20)
-        Me.TxtStoreCode.TabIndex = 1
-        '
-        'TxtStoreName
-        '
-        Me.TxtStoreName.Enabled = False
-        Me.TxtStoreName.Location = New System.Drawing.Point(107, 15)
-        Me.TxtStoreName.Name = "TxtStoreName"
-        Me.TxtStoreName.Size = New System.Drawing.Size(276, 20)
-        Me.TxtStoreName.TabIndex = 2
-        '
-        'BtnPrint
-        '
-        Me.BtnPrint.Image = CType(resources.GetObject("BtnPrint.Image"), System.Drawing.Image)
-        Me.BtnPrint.Location = New System.Drawing.Point(389, 13)
-        Me.BtnPrint.Name = "BtnPrint"
-        Me.BtnPrint.Size = New System.Drawing.Size(63, 23)
-        Me.BtnPrint.TabIndex = 3
-        Me.BtnPrint.Text = "Print"
-        '
         'FormVMStoreDisplayDet
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(584, 361)
+        Me.ClientSize = New System.Drawing.Size(731, 361)
         Me.Controls.Add(Me.GCData)
         Me.Controls.Add(Me.PanelControl1)
         Me.MaximizeBox = False
@@ -113,10 +146,10 @@ Partial Class FormVMStoreDisplayDet
         CType(Me.PanelControl1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.PanelControl1.ResumeLayout(False)
         Me.PanelControl1.PerformLayout()
+        CType(Me.TxtStoreName.Properties, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.TxtStoreCode.Properties, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.GCData, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.GVData, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.TxtStoreCode.Properties, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.TxtStoreName.Properties, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
@@ -128,4 +161,6 @@ Partial Class FormVMStoreDisplayDet
     Friend WithEvents TxtStoreCode As DevExpress.XtraEditors.TextEdit
     Friend WithEvents LabelControl1 As DevExpress.XtraEditors.LabelControl
     Friend WithEvents BtnPrint As DevExpress.XtraEditors.SimpleButton
+    Friend WithEvents BtnBrowse As DevExpress.XtraEditors.SimpleButton
+    Friend WithEvents LabelControl2 As DevExpress.XtraEditors.LabelControl
 End Class
