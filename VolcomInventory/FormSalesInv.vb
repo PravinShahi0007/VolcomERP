b9dafc7af89861ef543c4aa7aee9e769a84fcd16
Satrya Anas <catur.satrya@gmail.com>
2021-11-22 14:57:14 +0800

    view_sales_inv_per_acc_by_sal_v2

--- VolcomInventory/FormSalesInv.vb
+++ VolcomInventory/FormSalesInv.vb
@@ -548,7 +548,7 @@
         'excecute
         'old query
         'Dim query As String = "CALL view_sales_inv_per_account('" + date_from_selected + "', '" + date_until_selected + "', '" + id_comp + "','" + id_design_per_outlet + "', '" + id_period_type + "', '" + opt_display_param + "', '" + where_param + "')"
-        Dim query As String = "CALL view_sales_inv_per_acc_by_sal('" + date_from_selected + "', '" + date_until_selected + "', '" + id_comp + "','" + id_design_per_outlet + "', '" + id_period_type + "', '" + opt_display_param + "', '" + where_param + "', '" + is_soh_sal_period + "')"
+        Dim query As String = "CALL view_sales_inv_per_acc_by_sal_v2('" + date_from_selected + "', '" + date_until_selected + "', '" + id_comp + "','" + id_design_per_outlet + "', '" + id_period_type + "', '" + opt_display_param + "', '" + where_param + "', '" + is_soh_sal_period + "')"
         Dim data As DataTable = execute_query(query, -1, True, "", "", "", "")
         GCByAccount.DataSource = data
         FormMain.SplashScreenManager1.CloseWaitForm()
