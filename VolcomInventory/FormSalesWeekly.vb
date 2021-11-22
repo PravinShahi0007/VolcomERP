36e799d6a5ec03ec3ebfa802c2b33fec6a9aa2ab
Satrya Anas <catur.satrya@gmail.com>
2021-11-22 18:01:58 +0800

    prepare

--- VolcomInventory/FormSalesWeekly.vb
+++ VolcomInventory/FormSalesWeekly.vb
@@ -1089,7 +1089,7 @@
         Dim band_desc As DevExpress.XtraGrid.Views.BandedGrid.GridBand = BGVSalesWeeklyByDate.Bands.AddBand("DESCRIPTION")
         band_desc.AppearanceHeader.Font = New Font(BGVSalesWeeklyByDate.Appearance.Row.Font.FontFamily, BGVSalesWeeklyByDate.Appearance.Row.Font.Size, FontStyle.Bold)
 
-        'cond promo
+        'cond gwp
         Dim include_promo As String = ""
         If CEPromoWeeklyByDate.EditValue = True Then
             include_promo = "1"
@@ -1097,8 +1097,16 @@
             include_promo = "2"
         End If
 
+        'cond promo/uni
+        Dim include_uni As String = ""
+        If CEIncludePrmUniWeekly.EditValue = True Then
+            include_uni = "1"
+        Else
+            include_uni = "2"
+        End If
+
         'excecute query
-        Dim query As String = "CALL view_sales_weekly_by_date('" + date_from_weekdate_selected + "', '" + date_until_weekdate_selected + "', " + include_promo + ")"
+        Dim query As String = "CALL view_sales_weekly_by_date_v2('" + date_from_weekdate_selected + "', '" + date_until_weekdate_selected + "', " + include_promo + ")"
         Dim data As DataTable = execute_query(query, -1, True, "", "", "", "")
         For i As Integer = 0 To data.Columns.Count - 1
             If data.Columns(i).ColumnName.ToString.Contains("_Qty") Then
