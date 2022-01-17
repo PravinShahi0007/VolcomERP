e11c175b88dd15195e26e16f8b2ddfa5a8c80301
Satrya Anas <catur.satrya@gmail.com>
2022-01-17 11:46:54 +0800

    VIOS PAYOUT CC :100:

--- VolcomInventory/FormImportExcel.vb
+++ VolcomInventory/FormImportExcel.vb
@@ -3997,7 +3997,7 @@ WHERE id_comp_cat='6'"
             Dim id_vios As String = get_setup_field("shopify_comp_group")
             Dim queryx As String = "(
 SELECT ol.id_virtual_acc_trans, ol.id AS `id_order`, ol.checkout_id, IFNULL(v.value,0.00) AS `other_price`,ol.sales_order_ol_shop_number,
-SUM(CAST(((pos.`sales_pos_total`*((100-pos.sales_pos_discount)/100))-pos.`sales_pos_potongan`) AS DECIMAL(15,2)))+IFNULL(sh.ship_amo,0)+IFNULL(v.value,0.00) AS amount,
+SUM(pos.netto)+IFNULL(sh.ship_amo,0)+IFNULL(v.value,0.00) AS amount,
 GROUP_CONCAT(DISTINCT(pos.`sales_pos_number`)) AS inv_number, sh.ship_number AS `ship_inv_number`,v.number AS `ver_number`,
 GROUP_CONCAT(DISTINCT(pos.`id_sales_pos`)) AS id_sales_pos, IFNULL(sh.id_invoice_ship,0) AS `id_invoice_ship`,  IFNULL(v.id_list_payout_ver,0) AS `id_list_payout_ver`
 FROM (
