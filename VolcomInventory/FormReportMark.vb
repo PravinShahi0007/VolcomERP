7ac7e887f2f12d50edeebcfb004c0a7362a8b8e1
Satrya Anas <catur.satrya@gmail.com>
2022-01-19 13:59:59 +0800

    - voucher amount zalora hilangkan koma - saat failed get shopify product close wait form

--- VolcomInventory/FormReportMark.vb
+++ VolcomInventory/FormReportMark.vb
@@ -2699,6 +2699,7 @@ INNER JOIN
                         Dim s As New ClassShopifyApi()
                         dtweb = s.get_product()
                     Catch ex As Exception
+                        FormMain.SplashScreenManager1.CloseWaitForm()
                         stopCustom("Failed get shopify product : " + ex.ToString)
                         Exit Sub
                     End Try
