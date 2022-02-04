3096ca82941b9a900b78b2b4bf32279804a75ef5
Satrya Anas <catur.satrya@gmail.com>
2022-02-04 16:09:50 +0800

    0.1.10.25

--- VolcomInventory/My Project/AssemblyInfo.vb
+++ VolcomInventory/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("0.1.10.24")>
-<Assembly: AssemblyFileVersion("0.1.10.24")>
+<Assembly: AssemblyVersion("0.1.10.25")>
+<Assembly: AssemblyFileVersion("0.1.10.25")>
