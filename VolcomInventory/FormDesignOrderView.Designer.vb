52aa216f4a406c04a896092a79f28442ee43f1f6
Satrya Anas <catur.satrya@gmail.com>
2022-03-11 16:12:23 +0800

    rekap form order untuk MD

---
+++ VolcomInventory/FormDesignOrderView.Designer.vb
@@ -0,0 +1,236 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FormDesignOrderView
+    Inherits DevExpress.XtraEditors.XtraForm
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        If disposing AndAlso components IsNot Nothing Then
+            components.Dispose()
+        End If
+        MyBase.Dispose(disposing)
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.PanelControl1 = New DevExpress.XtraEditors.PanelControl()
+        Me.PanelControlLeft = New DevExpress.XtraEditors.PanelControl()
+        Me.BtnPrint = New DevExpress.XtraEditors.SimpleButton()
+        Me.BtnView = New DevExpress.XtraEditors.SimpleButton()
+        Me.SLEComp = New DevExpress.XtraEditors.SearchLookUpEdit()
+        Me.GridView1 = New DevExpress.XtraGrid.Views.Grid.GridView()
+        Me.SLESeason = New DevExpress.XtraEditors.SearchLookUpEdit()
+        Me.SearchLookUpEdit1View = New DevExpress.XtraGrid.Views.Grid.GridView()
+        Me.LabelControl2 = New DevExpress.XtraEditors.LabelControl()
+        Me.LabelControl1 = New DevExpress.XtraEditors.LabelControl()
+        Me.SimpleButton1 = New DevExpress.XtraEditors.SimpleButton()
+        Me.GCData = New DevExpress.XtraGrid.GridControl()
+        Me.GVData = New DevExpress.XtraGrid.Views.Grid.GridView()
+        CType(Me.PanelControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.PanelControl1.SuspendLayout()
+        CType(Me.PanelControlLeft, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.PanelControlLeft.SuspendLayout()
+        CType(Me.SLEComp.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.GridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.SLESeason.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.SearchLookUpEdit1View, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.GCData, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.GVData, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PanelControl1
+        '
+        Me.PanelControl1.Controls.Add(Me.SimpleButton1)
+        Me.PanelControl1.Controls.Add(Me.BtnPrint)
+        Me.PanelControl1.Controls.Add(Me.PanelControlLeft)
+        Me.PanelControl1.Controls.Add(Me.BtnView)
+        Me.PanelControl1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.PanelControl1.Location = New System.Drawing.Point(0, 0)
+        Me.PanelControl1.Name = "PanelControl1"
+        Me.PanelControl1.Size = New System.Drawing.Size(771, 48)
+        Me.PanelControl1.TabIndex = 1
+        '
+        'PanelControlLeft
+        '
+        Me.PanelControlLeft.Controls.Add(Me.SLEComp)
+        Me.PanelControlLeft.Controls.Add(Me.SLESeason)
+        Me.PanelControlLeft.Controls.Add(Me.LabelControl2)
+        Me.PanelControlLeft.Controls.Add(Me.LabelControl1)
+        Me.PanelControlLeft.Dock = System.Windows.Forms.DockStyle.Left
+        Me.PanelControlLeft.Location = New System.Drawing.Point(2, 2)
+        Me.PanelControlLeft.Name = "PanelControlLeft"
+        Me.PanelControlLeft.Size = New System.Drawing.Size(507, 44)
+        Me.PanelControlLeft.TabIndex = 21
+        '
+        'BtnPrint
+        '
+        Me.BtnPrint.Appearance.BackColor = System.Drawing.Color.DimGray
+        Me.BtnPrint.Appearance.Font = New System.Drawing.Font("Tahoma", 8.25!, System.Drawing.FontStyle.Bold)
+        Me.BtnPrint.Appearance.ForeColor = System.Drawing.Color.White
+        Me.BtnPrint.Appearance.Options.UseBackColor = True
+        Me.BtnPrint.Appearance.Options.UseFont = True
+        Me.BtnPrint.Appearance.Options.UseForeColor = True
+        Me.BtnPrint.Location = New System.Drawing.Point(581, 15)
+        Me.BtnPrint.LookAndFeel.SkinMaskColor = System.Drawing.Color.Blue
+        Me.BtnPrint.LookAndFeel.SkinMaskColor2 = System.Drawing.Color.Blue
+        Me.BtnPrint.LookAndFeel.Style = DevExpress.LookAndFeel.LookAndFeelStyle.UltraFlat
+        Me.BtnPrint.LookAndFeel.UseDefaultLookAndFeel = False
+        Me.BtnPrint.Name = "BtnPrint"
+        Me.BtnPrint.Size = New System.Drawing.Size(61, 21)
+        Me.BtnPrint.TabIndex = 20
+        Me.BtnPrint.Text = "Print"
+        '
+        'BtnView
+        '
+        Me.BtnView.Appearance.BackColor = System.Drawing.Color.CornflowerBlue
+        Me.BtnView.Appearance.Font = New System.Drawing.Font("Tahoma", 8.25!, System.Drawing.FontStyle.Bold)
+        Me.BtnView.Appearance.ForeColor = System.Drawing.Color.White
+        Me.BtnView.Appearance.Options.UseBackColor = True
+        Me.BtnView.Appearance.Options.UseFont = True
+        Me.BtnView.Appearance.Options.UseForeColor = True
+        Me.BtnView.Location = New System.Drawing.Point(515, 15)
+        Me.BtnView.LookAndFeel.SkinMaskColor = System.Drawing.Color.Blue
+        Me.BtnView.LookAndFeel.SkinMaskColor2 = System.Drawing.Color.Blue
+        Me.BtnView.LookAndFeel.Style = DevExpress.LookAndFeel.LookAndFeelStyle.UltraFlat
+        Me.BtnView.LookAndFeel.UseDefaultLookAndFeel = False
+        Me.BtnView.Name = "BtnView"
+        Me.BtnView.Size = New System.Drawing.Size(63, 21)
+        Me.BtnView.TabIndex = 19
+        Me.BtnView.Text = "View"
+        '
+        'SLEComp
+        '
+        Me.SLEComp.Location = New System.Drawing.Point(251, 13)
+        Me.SLEComp.Name = "SLEComp"
+        Me.SLEComp.Properties.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() {New DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)})
+        Me.SLEComp.Properties.ShowClearButton = False
+        Me.SLEComp.Properties.View = Me.GridView1
+        Me.SLEComp.Size = New System.Drawing.Size(244, 20)
+        Me.SLEComp.TabIndex = 4
+        '
+        'GridView1
+        '
+        Me.GridView1.FocusRectStyle = DevExpress.XtraGrid.Views.Grid.DrawFocusRectStyle.RowFocus
+        Me.GridView1.Name = "GridView1"
+        Me.GridView1.OptionsSelection.EnableAppearanceFocusedCell = False
+        Me.GridView1.OptionsView.ShowGroupPanel = False
+        '
+        'SLESeason
+        '
+        Me.SLESeason.Location = New System.Drawing.Point(55, 13)
+        Me.SLESeason.Name = "SLESeason"
+        Me.SLESeason.Properties.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() {New DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)})
+        Me.SLESeason.Properties.ShowClearButton = False
+        Me.SLESeason.Properties.View = Me.SearchLookUpEdit1View
+        Me.SLESeason.Size = New System.Drawing.Size(158, 20)
+        Me.SLESeason.TabIndex = 1
+        '
+        'SearchLookUpEdit1View
+        '
+        Me.SearchLookUpEdit1View.FocusRectStyle = DevExpress.XtraGrid.Views.Grid.DrawFocusRectStyle.RowFocus
+        Me.SearchLookUpEdit1View.Name = "SearchLookUpEdit1View"
+        Me.SearchLookUpEdit1View.OptionsSelection.EnableAppearanceFocusedCell = False
+        Me.SearchLookUpEdit1View.OptionsView.ShowGroupPanel = False
+        '
+        'LabelControl2
+        '
+        Me.LabelControl2.Location = New System.Drawing.Point(219, 16)
+        Me.LabelControl2.Name = "LabelControl2"
+        Me.LabelControl2.Size = New System.Drawing.Size(26, 13)
+        Me.LabelControl2.TabIndex = 3
+        Me.LabelControl2.Text = "Store"
+        '
+        'LabelControl1
+        '
+        Me.LabelControl1.Location = New System.Drawing.Point(14, 16)
+        Me.LabelControl1.Name = "LabelControl1"
+        Me.LabelControl1.Size = New System.Drawing.Size(35, 13)
+        Me.LabelControl1.TabIndex = 2
+        Me.LabelControl1.Text = "Season"
+        '
+        'SimpleButton1
+        '
+        Me.SimpleButton1.Appearance.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.SimpleButton1.Appearance.Font = New System.Drawing.Font("Tahoma", 8.25!, System.Drawing.FontStyle.Bold)
+        Me.SimpleButton1.Appearance.ForeColor = System.Drawing.Color.White
+        Me.SimpleButton1.Appearance.Options.UseBackColor = True
+        Me.SimpleButton1.Appearance.Options.UseFont = True
+        Me.SimpleButton1.Appearance.Options.UseForeColor = True
+        Me.SimpleButton1.Location = New System.Drawing.Point(645, 15)
+        Me.SimpleButton1.LookAndFeel.SkinMaskColor = System.Drawing.Color.Blue
+        Me.SimpleButton1.LookAndFeel.SkinMaskColor2 = System.Drawing.Color.Blue
+        Me.SimpleButton1.LookAndFeel.Style = DevExpress.LookAndFeel.LookAndFeelStyle.UltraFlat
+        Me.SimpleButton1.LookAndFeel.UseDefaultLookAndFeel = False
+        Me.SimpleButton1.Name = "SimpleButton1"
+        Me.SimpleButton1.Size = New System.Drawing.Size(89, 21)
+        Me.SimpleButton1.TabIndex = 21
+        Me.SimpleButton1.Text = "Export to XLS"
+        '
+        'GCData
+        '
+        Me.GCData.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.GCData.Location = New System.Drawing.Point(0, 48)
+        Me.GCData.MainView = Me.GVData
+        Me.GCData.Name = "GCData"
+        Me.GCData.Size = New System.Drawing.Size(771, 425)
+        Me.GCData.TabIndex = 2
+        Me.GCData.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() {Me.GVData})
+        '
+        'GVData
+        '
+        Me.GVData.GridControl = Me.GCData
+        Me.GVData.Name = "GVData"
+        Me.GVData.OptionsBehavior.AutoExpandAllGroups = True
+        Me.GVData.OptionsBehavior.ReadOnly = True
+        Me.GVData.OptionsFind.AlwaysVisible = True
+        Me.GVData.OptionsView.ColumnAutoWidth = False
+        Me.GVData.OptionsView.ShowFooter = True
+        Me.GVData.OptionsView.ShowGroupPanel = False
+        '
+        'FormDesignOrderView
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(771, 473)
+        Me.Controls.Add(Me.GCData)
+        Me.Controls.Add(Me.PanelControl1)
+        Me.MinimizeBox = False
+        Me.Name = "FormDesignOrderView"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Article Order List"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+        CType(Me.PanelControl1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.PanelControl1.ResumeLayout(False)
+        CType(Me.PanelControlLeft, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.PanelControlLeft.ResumeLayout(False)
+        Me.PanelControlLeft.PerformLayout()
+        CType(Me.SLEComp.Properties, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.GridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.SLESeason.Properties, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.SearchLookUpEdit1View, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.GCData, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.GVData, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents PanelControl1 As DevExpress.XtraEditors.PanelControl
+    Friend WithEvents SimpleButton1 As DevExpress.XtraEditors.SimpleButton
+    Friend WithEvents BtnPrint As DevExpress.XtraEditors.SimpleButton
+    Friend WithEvents PanelControlLeft As DevExpress.XtraEditors.PanelControl
+    Friend WithEvents SLEComp As DevExpress.XtraEditors.SearchLookUpEdit
+    Friend WithEvents GridView1 As DevExpress.XtraGrid.Views.Grid.GridView
+    Friend WithEvents SLESeason As DevExpress.XtraEditors.SearchLookUpEdit
+    Friend WithEvents SearchLookUpEdit1View As DevExpress.XtraGrid.Views.Grid.GridView
+    Friend WithEvents LabelControl2 As DevExpress.XtraEditors.LabelControl
+    Friend WithEvents LabelControl1 As DevExpress.XtraEditors.LabelControl
+    Friend WithEvents BtnView As DevExpress.XtraEditors.SimpleButton
+    Friend WithEvents GCData As DevExpress.XtraGrid.GridControl
+    Friend WithEvents GVData As DevExpress.XtraGrid.Views.Grid.GridView
+End Class
---
+++ VolcomInventory/FormDesignOrderView.vb
@@ -0,0 +1,191 @@
+﻿Public Class FormDesignOrderView
+    Private Sub FormDesignOrderView_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        viewSeason()
+        viewComp()
+    End Sub
+
+    Private Sub FormDesignOrderView_FormClosed(sender As Object, e As FormClosedEventArgs) Handles MyBase.FormClosed
+        Dispose()
+    End Sub
+
+
+    Sub viewSeason()
+        Cursor = Cursors.WaitCursor
+        Dim query As String = "SELECT ss.id_season, rg.`range`, ss.season 
+        FROM tb_season ss 
+        INNER JOIN tb_range rg ON rg.id_range = ss.id_range
+        WHERE rg.is_md=1
+        ORDER BY `range` DESC "
+        viewSearchLookupQuery(SLESeason, query, "id_season", "season", "id_season")
+        Cursor = Cursors.Default
+    End Sub
+
+    Sub viewComp()
+        Cursor = Cursors.WaitCursor
+        Dim query As String = "(SELECT 0 AS `id_comp`, '- All Store -' AS `comp`) 
+        UNION ALL
+        (SELECT c.id_comp, CONCAT(c.comp_number, ' - ', c.comp_name) AS `comp` 
+        FROM tb_m_comp c 
+        WHERE c.id_comp_cat=6 
+        ORDER BY c.comp_number ASC) "
+        viewSearchLookupQuery(SLEComp, query, "id_comp", "comp", "id_comp")
+        Cursor = Cursors.Default
+    End Sub
+
+    Sub viewData()
+        Cursor = Cursors.WaitCursor
+        Dim where_string As String = ""
+        Dim id_season As String = SLESeason.EditValue.ToString
+        If id_season <> "0" Then
+            where_string += "AND dp.id_season=" + id_season + " "
+        End If
+        Dim id_comp As String = SLEComp.EditValue.ToString
+        If id_comp <> "0" Then
+            where_string += "AND dp.id_comp=" + id_comp + " "
+        End If
+
+        'generate column
+        Dim qs As String = "SELECT c.id_comp, CONCAT(c.comp_number,' (', c.comp_name,')') AS `comp` 
+        FROM tb_display_pps dp 
+        INNER JOIN tb_m_comp c ON c.id_comp = dp.id_comp
+        WHERE dp.id_report_status!=5 "
+        If id_season <> "0" Then
+            qs += "AND  dp.id_season=" + id_season + " "
+        End If
+        If id_comp <> "0" Then
+            qs += "AND dp.id_comp=" + id_comp + " "
+        End If
+        qs += "GROUP BY dp.id_comp
+        ORDER BY c.comp_number ASC "
+        Dim ds As DataTable = execute_query(qs, -1, True, "", "", "", "")
+        Dim col_comp As String = ""
+        If ds.Rows.Count > 0 Then
+            For s As Integer = 0 To ds.Rows.Count - 1
+                If s > 0 Then
+                    col_comp += ","
+                End If
+                col_comp += "IFNULL(COUNT(CASE WHEN dp.id_comp='" + ds.Rows(s)("id_comp").ToString + "' THEN dpd.id_design END),0) AS `" + ds.Rows(s)("comp").ToString + "` "
+            Next
+        Else
+            Cursor = Cursors.Default
+            stopCustom("Order is not available for this option")
+            Exit Sub
+        End If
+
+        Dim query As String = "SELECT d.id_design, d.design_code AS `Code`, cd.class AS `Class`, d.design_display_name AS `Description`, 
+        cd.color AS `Color`, cd.sht AS `Silhouette`, ss.season AS `Season`, sd.delivery AS `Del`,
+        " + col_comp + ",
+        IFNULL(COUNT(dpd.id_design),0) AS `Total Qty`
+        FROM tb_display_pps_det dpd
+        INNER JOIN tb_display_pps dp ON dp.id_display_pps = dpd.id_display_pps
+        INNER JOIN tb_m_design d ON d.id_design = dpd.id_design
+        LEFT JOIN (
+	        SELECT dc.id_design, 
+	        MAX(CASE WHEN cd.id_code=32 THEN cd.id_code_detail END) AS `id_division`,
+	        MAX(CASE WHEN cd.id_code=32 THEN cd.code_detail_name END) AS `division`,
+	        MAX(CASE WHEN cd.id_code=30 THEN cd.id_code_detail END) AS `id_class`,
+	        MAX(CASE WHEN cd.id_code=30 THEN cd.display_name END) AS `class`,
+	        MAX(CASE WHEN cd.id_code=14 THEN cd.id_code_detail END) AS `id_color`,
+	        MAX(CASE WHEN cd.id_code=14 THEN cd.display_name END) AS `color`,
+	        MAX(CASE WHEN cd.id_code=14 THEN cd.code_detail_name END) AS `color_desc`,
+	        MAX(CASE WHEN cd.id_code=43 THEN cd.id_code_detail END) AS `id_sht`,
+	        MAX(CASE WHEN cd.id_code=43 THEN cd.code_detail_name END) AS `sht`
+	        FROM tb_m_design_code dc
+	        INNER JOIN tb_m_code_detail cd ON cd.id_code_detail = dc.id_code_detail 
+	        AND cd.id_code IN (32,30,14, 43)
+	        GROUP BY dc.id_design
+        ) cd ON cd.id_design = d.id_design
+        INNER JOIN tb_season ss ON ss.id_season = d.id_season
+        INNER JOIN tb_season_delivery sd ON sd.id_delivery = d.id_delivery
+        WHERE dp.id_report_status!=5 AND dpd.is_selected=1 
+        " + where_string + "
+        GROUP BY dpd.id_design
+        ORDER BY `Class` ASC, `Description` ASC, `Del` ASC "
+        Dim data As DataTable = execute_query(query, -1, True, "", "", "", "")
+        GCData.DataSource = data
+        For c As Integer = 0 To data.Columns.Count - 1
+            Dim col As String = data.Columns(c).Caption
+            Console.WriteLine(col)
+            If col.Contains("(") Or col = "Total Qty" Then
+                'display format
+                GVData.Columns(col).DisplayFormat.FormatType = DevExpress.Utils.FormatType.Numeric
+                GVData.Columns(col).DisplayFormat.FormatString = "{0:n0}"
+
+                'summary
+                GVData.Columns(col).SummaryItem.SummaryType = DevExpress.Data.SummaryItemType.Sum
+                GVData.Columns(col).SummaryItem.DisplayFormat = "{0:n0}"
+
+
+                'group summary
+                Dim summary As DevExpress.XtraGrid.GridGroupSummaryItem = New DevExpress.XtraGrid.GridGroupSummaryItem
+                summary.DisplayFormat = "{0:N0}"
+                summary.FieldName = col
+                summary.ShowInGroupColumnFooter = GVData.Columns(col)
+                summary.SummaryType = DevExpress.Data.SummaryItemType.Sum
+                GVData.GroupSummary.Add(summary)
+            End If
+        Next
+        GVData.Columns("id_design").Visible = False
+        GVData.BestFitColumns()
+        Cursor = Cursors.Default
+    End Sub
+
+    Private Sub BtnView_Click(sender As Object, e As EventArgs) Handles BtnView.Click
+        viewData()
+    End Sub
+
+    Private Sub BtnPrint_Click(sender As Object, e As EventArgs) Handles BtnPrint.Click
+        print_raw(GCData, "")
+    End Sub
+
+    Private Sub SLESeason_EditValueChanged(sender As Object, e As EventArgs) Handles SLESeason.EditValueChanged
+        resetView()
+    End Sub
+
+    Sub resetView()
+        GCData.DataSource = Nothing
+    End Sub
+
+    Private Sub SLEComp_EditValueChanged(sender As Object, e As EventArgs) Handles SLEComp.EditValueChanged
+        resetView()
+    End Sub
+
+    Sub exportToXLS(ByVal path_par As String, ByVal sheet_name_par As String, ByVal gc_par As DevExpress.XtraGrid.GridControl)
+        Cursor = Cursors.WaitCursor
+        Dim path As String = path_par
+
+        ' Customize export options 
+        CType(gc_par.MainView, DevExpress.XtraGrid.Views.Grid.GridView).OptionsPrint.PrintHeader = True
+        Dim advOptions As DevExpress.XtraPrinting.XlsxExportOptionsEx = New DevExpress.XtraPrinting.XlsxExportOptionsEx()
+        advOptions.AllowSortingAndFiltering = DevExpress.Utils.DefaultBoolean.False
+        advOptions.ShowGridLines = DevExpress.Utils.DefaultBoolean.False
+        advOptions.AllowGrouping = DevExpress.Utils.DefaultBoolean.False
+        advOptions.ShowTotalSummaries = DevExpress.Utils.DefaultBoolean.False
+        advOptions.SheetName = sheet_name_par
+        advOptions.ExportType = DevExpress.Export.ExportType.DataAware
+
+        Try
+            gc_par.ExportToXlsx(path, advOptions)
+            Process.Start(path)
+            ' Open the created XLSX file with the default application. 
+        Catch ex As Exception
+            stopCustom(ex.ToString)
+        End Try
+        Cursor = Cursors.Default
+    End Sub
+
+    Private Sub SimpleButton1_Click(sender As Object, e As EventArgs) Handles SimpleButton1.Click
+        If GVData.RowCount > 0 Then
+            Cursor = Cursors.WaitCursor
+            Dim path As String = Application.StartupPath & "\download\"
+            'create directory if not exist
+            If Not IO.Directory.Exists(path) Then
+                System.IO.Directory.CreateDirectory(path)
+            End If
+            'path = path + "del_" + dt_from + "_" + dt_until + ".xlsx"
+            path = path + "order" + SLESeason.Text.Trim + ".xlsx"
+            exportToXLS(path, "order" + SLESeason.Text.Trim, GCData)
+            Cursor = Cursors.Default
+        End If
+    End Sub
+End Class
