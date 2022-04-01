246d6a5eab0bf31dd8b18e454f188c5c4ed1384d
Putu Septian Primadewa <iansaviour@gmail.com>
2022-04-01 14:48:02 +0800

    group by max

--- VolcomInventory/FormMatPurchasePDDup.vb
+++ VolcomInventory/FormMatPurchasePDDup.vb
@@ -63,21 +63,25 @@ UNION
 SELECT 'no' AS is_check,pdp.size,'' AS note,pdp.id_prod_demand_design,pdp.id_prod_demand_product,pdp.id_design,pdp.qty,dsg.design_display_name,dsg.design_code,pdp.prod_demand_number,pdp.qty,(" & decimalSQL(TEConsumption.EditValue.ToString) & "*pdp.qty) AS qty_order 
 FROM (
 	SELECT pd_dsg.id_prod_demand_design, pd_prd.`id_prod_demand_product`, pd_dsg.id_prod_demand, pd.prod_demand_number, pd_dsg.id_design, 
-	pd_dsg.prod_demand_design_propose_price, pd_dsg.prod_demand_design_total_cost, pd_dsg.msrp,
-	(SUM(pd_prd.prod_demand_product_qty)) AS qty
+	pd_dsg.prod_demand_design_propose_price, pd_dsg.prod_demand_design_total_cost, pd_dsg.msrp
+	,(SUM(pd_prd.prod_demand_product_qty)) AS qty 
 	,pc.size,pd_prd.id_product
 	FROM  tb_prod_demand_design pd_dsg
 	INNER JOIN tb_prod_demand pd ON pd.id_prod_demand = pd_dsg.id_prod_demand
-	LEFT JOIN tb_prod_demand_product pd_prd ON pd_prd.id_prod_demand_design = pd_dsg.id_prod_demand_design 
+	INNER JOIN tb_prod_demand_product pd_prd ON pd_prd.id_prod_demand_design = pd_dsg.id_prod_demand_design
+	INNER JOIN (
+		SELECT pd_prd.id_product,MAX(pd_prd.id_prod_demand_product) AS id_prod_demand_product
+		FROM  tb_prod_demand_design pd_dsg
+		INNER JOIN tb_prod_demand pd ON pd.id_prod_demand = pd_dsg.id_prod_demand
+		INNER JOIN tb_prod_demand_product pd_prd ON pd_prd.id_prod_demand_design = pd_dsg.id_prod_demand_design 
+		WHERE pd.id_report_status = '6' AND pd_dsg.is_void=2 AND pd.is_pd=1
+		GROUP BY pd_prd.id_product
+	)pdp ON pdp.id_prod_demand_product=pd_prd.id_prod_demand_product
 	INNER JOIN 
 	(
-		SELECT pc.`id_product`,cd.`code_detail_name` AS size FROM 
-		tb_m_product_code pc 
-		INNER JOIN tb_m_code_detail cd ON cd.`id_code_detail`=pc.`id_code_detail` AND cd.`id_code`='33'
+		SELECT pc.`id_product`,cd.`code_detail_name` AS size FROM tb_m_product_code pc INNER JOIN tb_m_code_detail cd ON cd.`id_code_detail`=pc.`id_code_detail` AND cd.`id_code`='33'
 	)pc ON pc.`id_product`=pd_prd.`id_product`
-	WHERE pd.id_report_status = '6' AND pd_dsg.is_void=2
 	GROUP BY pd_prd.id_prod_demand_product
-	ORDER BY pd_prd.id_prod_demand_product DESC
 ) pdp
 INNER JOIN tb_m_design dsg ON dsg.id_design=pdp.id_design
 LEFT JOIN
@@ -86,7 +90,6 @@ LEFT JOIN
 	INNER JOIN tb_mat_purc_list pl ON pl.`id_mat_purc_list`=plp.`id_mat_purc_list` AND pl.`is_cancel`=2 AND pl.id_mat_det='" & SLEMaterial.EditValue.ToString & "'
 ) pl ON pl.id_prod_demand_product=pdp.id_prod_demand_product
 WHERE ISNULL(pl.id_prod_demand_product)
-GROUP BY pdp.id_product
 ORDER BY pdp.id_prod_demand_design DESC"
         Else
             query = "SELECT 'yes' AS is_check,lp.note AS note,lp.id_prod_demand_design,IFNULL(lp.id_prod_demand_product,'') AS id_prod_demand_product,pdd.id_design,lp.total_qty_pd AS qty,dsg.design_display_name,dsg.design_code,pd.prod_demand_number,(" & decimalSQL(TEConsumption.EditValue.ToString) & "*lp.total_qty_pd) AS qty_order 
@@ -103,10 +106,16 @@ FROM (
 	(SUM(pd_prd.prod_demand_product_qty)) AS qty
 	FROM  tb_prod_demand_design pd_dsg
 	INNER JOIN tb_prod_demand pd ON pd.id_prod_demand = pd_dsg.id_prod_demand
-	LEFT JOIN tb_prod_demand_product pd_prd ON pd_prd.id_prod_demand_design = pd_dsg.id_prod_demand_design 
-	WHERE pd.id_report_status = '6' AND pd_dsg.is_void=2
-	GROUP BY pd_dsg.id_prod_demand_design
-	ORDER BY pd_dsg.id_prod_demand_design DESC
+	INNER JOIN tb_prod_demand_product pd_prd ON pd_prd.id_prod_demand_design = pd_dsg.id_prod_demand_design 
+	INNER JOIN 
+	(
+		SELECT pd_dsg.id_design,MAX(pd_dsg.id_prod_demand_design) AS id_prod_demand_design
+		FROM  tb_prod_demand_design pd_dsg
+		INNER JOIN tb_prod_demand pd ON pd.id_prod_demand = pd_dsg.id_prod_demand
+		WHERE pd.id_report_status = '6' AND pd_dsg.is_void=2 AND pd.is_pd=1
+		GROUP BY pd_dsg.id_design
+	)pdd ON pdd.id_prod_demand_design=pd_dsg.id_prod_demand_design
+	GROUP BY pd_dsg.id_design
 ) pdd
 INNER JOIN tb_m_design dsg ON dsg.id_design=pdd.id_design
 LEFT JOIN
@@ -116,7 +125,6 @@ LEFT JOIN
 	INNER JOIN tb_m_mat_det md ON md.`id_mat_det`=pl.`id_mat_det` AND md.`id_mat`=(SELECT id_mat FROM tb_m_mat_det WHERE id_mat_det='" & SLEMaterial.EditValue.ToString & "')
 ) pl ON pl.id_prod_demand_design=pdd.id_prod_demand_design
 WHERE ISNULL(pl.id_prod_demand_design)
-GROUP BY pdd.id_design
 ORDER BY is_check DESC,id_prod_demand_design DESC"
         End If
 
