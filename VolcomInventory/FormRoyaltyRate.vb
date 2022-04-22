0d2b52b9b54507442907d1cf4f0f6469a729131f
Satrya Anas <catur.satrya@gmail.com>
2022-04-22 14:37:13 +0800

    main :100:

--- VolcomInventory/FormRoyaltyRate.vb
+++ VolcomInventory/FormRoyaltyRate.vb
@@ -3,7 +3,10 @@
     Public is_load_new As Boolean = False
 
     Private Sub FormRoyaltyRate_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
+        viewData()
+        If is_test = "1" Then
+            createNew()
+        End If
     End Sub
 
     Sub viewData()
@@ -23,5 +26,46 @@
 
     Private Sub FormRoyaltyRate_Deactivate(sender As Object, e As EventArgs) Handles MyBase.Deactivate
         FormMain.hide_rb()
+    End Sub
+
+    Sub viewDetail()
+        If GVData.RowCount > 0 And GVData.FocusedRowHandle >= 0 Then
+            Cursor = Cursors.WaitCursor
+            FormRoyaltyRateDet.id = GVData.GetFocusedRowCellValue("id_royalty_rate").ToString
+            FormRoyaltyRateDet.action = "upd"
+            FormRoyaltyRateDet.ShowDialog()
+            Cursor = Cursors.Default
+        End If
+    End Sub
+
+    Sub printList()
+        Cursor = Cursors.WaitCursor
+        print(GCData, "Propose ABG Royalty Rate")
+        Cursor = Cursors.Default
+    End Sub
+
+    Sub createNew()
+        Cursor = Cursors.WaitCursor
+        Dim qcek As String = "SELECT * FROM tb_royalty_rate s WHERE s.id_report_status<5 "
+        Dim dcek As DataTable = execute_query(qcek, -1, True, "", "", "", "")
+        If dcek.Rows.Count > 0 Then
+            stopCustom("Please complete all pending transaction first")
+        Else
+            FormRoyaltyRateDet.action = "ins"
+            FormRoyaltyRateDet.ShowDialog()
+            loadNewDetail()
+        End If
+        Cursor = Cursors.Default
+    End Sub
+
+    Private Sub GVData_DoubleClick(sender As Object, e As EventArgs) Handles GVData.DoubleClick
+        viewDetail()
+    End Sub
+
+    Sub loadNewDetail()
+        If is_load_new Then
+            is_load_new = False
+            viewDetail()
+        End If
     End Sub
 End Class
