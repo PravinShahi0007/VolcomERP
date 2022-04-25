e63f19b6b557dc496e57ff786489c75fd456fe2c
Satrya Anas <catur.satrya@gmail.com>
2022-04-25 09:54:25 +0800

    Class Design (664) : mencari index size terkait line sheet, pd, pd revisi

--- VolcomInventory/ClassDesign.vb
+++ VolcomInventory/ClassDesign.vb
@@ -637,31 +637,33 @@
         Else
             typ = "pdd.id_prod_demand_design = d.id_prod_demand_design_line_final "
         End If
-        Dim query_caption As String = "SELECT cd.index_size,CONCAT('qty',cd.index_size,'_Breakdown') AS `col`,GROUP_CONCAT(DISTINCT cd.code_detail_name ORDER BY cd.code_detail_name ASC SEPARATOR '\n') AS `caption` FROM tb_m_code_detail cd
-         WHERE cd.id_code='33'
-         AND cd.`index_size` IN (
-             SELECT cd.`index_size` FROM tb_prod_demand_design pdd 
-             INNER JOIN tb_prod_demand_product pdp ON pdp.id_prod_demand_design =  pdd.id_prod_demand_design
-             INNER JOIN tb_m_product p ON p.id_product = pdp.id_product
-             INNER JOIN tb_m_product_code pc ON pc.id_product = p.id_product
-             INNER JOIN tb_m_code_detail cd ON cd.id_code_detail = pc.id_code_detail
-             INNER JOIN tb_m_design d ON d.id_design = p.id_design AND " + typ + "
-             WHERE (d.id_season =" + id_season_param + " OR d.id_season_move =" + id_season_param + ")
-	          AND pdp.prod_demand_product_qty>0
-             GROUP BY cd.`index_size`
-         )
-         AND cd.`size_type` IN (
-             SELECT cd.`size_type` FROM tb_prod_demand_design pdd 
-             INNER JOIN tb_prod_demand_product pdp ON pdp.id_prod_demand_design =  pdd.id_prod_demand_design
-             INNER JOIN tb_m_product p ON p.id_product = pdp.id_product
-             INNER JOIN tb_m_product_code pc ON pc.id_product = p.id_product
-             INNER JOIN tb_m_code_detail cd ON cd.id_code_detail = pc.id_code_detail
-             INNER JOIN tb_m_design d ON d.id_design = p.id_design AND " + typ + "
-             WHERE (d.id_season =" + id_season_param + " OR d.id_season_move =" + id_season_param + ")
-	         AND pdp.prod_demand_product_qty>0
-             GROUP BY cd.`size_type`
-         )
-         GROUP BY cd.index_size "
+        Dim query_caption As String = "SELECT cd.index_size,CONCAT('qty',cd.index_size,'_Breakdown') AS `col`,GROUP_CONCAT(DISTINCT cd.code_detail_name ORDER BY cd.code_detail_name ASC SEPARATOR '\n') AS `caption` 
+        FROM tb_m_code_detail cd
+        INNER JOIN (
+	        SELECT cd.`index_size` 
+	        FROM tb_prod_demand_design pdd 
+	        INNER JOIN tb_prod_demand_product pdp ON pdp.id_prod_demand_design =  pdd.id_prod_demand_design
+	        INNER JOIN tb_m_product p ON p.id_product = pdp.id_product
+	        INNER JOIN tb_m_product_code pc ON pc.id_product = p.id_product
+	        INNER JOIN tb_m_code_detail cd ON cd.id_code_detail = pc.id_code_detail
+	        INNER JOIN tb_m_design d ON d.id_design = p.id_design AND " + typ + "
+	        WHERE (d.id_season =" + id_season_param + " OR d.id_season_move =" + id_season_param + " )
+	        AND pdp.prod_demand_product_qty>0
+	        GROUP BY cd.`index_size`
+        ) i ON  i.index_size = cd.index_size
+        INNER JOIN (
+	        SELECT cd.`size_type` FROM tb_prod_demand_design pdd 
+	        INNER JOIN tb_prod_demand_product pdp ON pdp.id_prod_demand_design =  pdd.id_prod_demand_design
+	        INNER JOIN tb_m_product p ON p.id_product = pdp.id_product
+	        INNER JOIN tb_m_product_code pc ON pc.id_product = p.id_product
+	        INNER JOIN tb_m_code_detail cd ON cd.id_code_detail = pc.id_code_detail
+	        INNER JOIN tb_m_design d ON d.id_design = p.id_design AND " + typ + "
+	        WHERE (d.id_season =" + id_season_param + "  OR d.id_season_move =" + id_season_param + " )
+	        AND pdp.prod_demand_product_qty>0
+	        GROUP BY cd.`size_type`
+        ) s ON s.size_type = cd.size_type
+        WHERE cd.id_code='33'
+        GROUP BY cd.index_size "
         Dim data_caption As DataTable = execute_query(query_caption, -1, True, "", "", "", "")
         For c As Integer = 0 To data_caption.Rows.Count - 1
             BGVParam.Columns(data_caption.Rows(c)("col").ToString).Caption = data_caption.Rows(c)("caption").ToString
