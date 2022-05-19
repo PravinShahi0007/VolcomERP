f610e333a9602b3ac6f053f148dd1a22b34906c3
Satrya Anas <catur.satrya@gmail.com>
2022-05-19 17:51:35 +0800

    New Menu : Perubahan Delivery

--- VolcomInventory/FormMain.vb
+++ VolcomInventory/FormMain.vb
@@ -17240,4 +17240,17 @@ WHERE pddr.id_prod_demand_design='" & FormProduction.GVDesign.GetFocusedRowCellV
         End Try
         Cursor = Cursors.Default
     End Sub
+
+    Private Sub NBDropChanges_LinkClicked(sender As Object, e As DevExpress.XtraNavBar.NavBarLinkEventArgs) Handles NBDropChanges.LinkClicked
+        Cursor = Cursors.WaitCursor
+        Try
+            FormDropChanges.MdiParent = Me
+            FormDropChanges.Show()
+            FormDropChanges.WindowState = FormWindowState.Maximized
+            FormDropChanges.Focus()
+        Catch ex As Exception
+            errorProcess()
+        End Try
+        Cursor = Cursors.Default
+    End Sub
 End Class
