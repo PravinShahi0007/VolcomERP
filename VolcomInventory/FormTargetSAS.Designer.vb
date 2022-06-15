b6331b7be8b7198f4c7093034493f6b5e2e6b18a
Satrya Anas <catur.satrya@gmail.com>
2022-06-15 10:37:48 +0800

    prepare impor sas

--- VolcomInventory/FormTargetSAS.Designer.vb
+++ VolcomInventory/FormTargetSAS.Designer.vb
@@ -21,25 +21,25 @@ Partial Class FormTargetSAS
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FormTargetSAS))
         Me.PanelControl1 = New DevExpress.XtraEditors.PanelControl()
-        Me.GCData = New DevExpress.XtraGrid.GridControl()
-        Me.GVData = New DevExpress.XtraGrid.Views.Grid.GridView()
-        Me.BtnImportFromXLS = New DevExpress.XtraEditors.SimpleButton()
         Me.PanelControl2 = New DevExpress.XtraEditors.PanelControl()
-        Me.LabelControl1 = New DevExpress.XtraEditors.LabelControl()
-        Me.DateEdit1 = New DevExpress.XtraEditors.DateEdit()
         Me.DateEdit2 = New DevExpress.XtraEditors.DateEdit()
+        Me.DateEdit1 = New DevExpress.XtraEditors.DateEdit()
+        Me.LabelControl1 = New DevExpress.XtraEditors.LabelControl()
+        Me.BtnImportFromXLS = New DevExpress.XtraEditors.SimpleButton()
         Me.BtnView = New DevExpress.XtraEditors.SimpleButton()
         Me.SimpleButton1 = New DevExpress.XtraEditors.SimpleButton()
+        Me.GCData = New DevExpress.XtraGrid.GridControl()
+        Me.GVData = New DevExpress.XtraGrid.Views.Grid.GridView()
         CType(Me.PanelControl1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.PanelControl1.SuspendLayout()
-        CType(Me.GCData, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.GVData, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PanelControl2, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.PanelControl2.SuspendLayout()
-        CType(Me.DateEdit1.Properties.CalendarTimeProperties, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.DateEdit1.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.DateEdit2.Properties.CalendarTimeProperties, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.DateEdit2.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DateEdit1.Properties.CalendarTimeProperties, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DateEdit1.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.GCData, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.GVData, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'PanelControl1
@@ -54,33 +54,6 @@ Partial Class FormTargetSAS
         Me.PanelControl1.Size = New System.Drawing.Size(804, 63)
         Me.PanelControl1.TabIndex = 0
         '
-        'GCData
-        '
-        Me.GCData.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.GCData.Location = New System.Drawing.Point(0, 63)
-        Me.GCData.MainView = Me.GVData
-        Me.GCData.Name = "GCData"
-        Me.GCData.Size = New System.Drawing.Size(804, 419)
-        Me.GCData.TabIndex = 1
-        Me.GCData.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() {Me.GVData})
-        '
-        'GVData
-        '
-        Me.GVData.GridControl = Me.GCData
-        Me.GVData.Name = "GVData"
-        Me.GVData.OptionsView.ShowGroupPanel = False
-        '
-        'BtnImportFromXLS
-        '
-        Me.BtnImportFromXLS.Dock = System.Windows.Forms.DockStyle.Left
-        Me.BtnImportFromXLS.Image = CType(resources.GetObject("BtnImportFromXLS.Image"), System.Drawing.Image)
-        Me.BtnImportFromXLS.ImageLocation = DevExpress.XtraEditors.ImageLocation.TopCenter
-        Me.BtnImportFromXLS.Location = New System.Drawing.Point(2, 2)
-        Me.BtnImportFromXLS.Name = "BtnImportFromXLS"
-        Me.BtnImportFromXLS.Size = New System.Drawing.Size(93, 59)
-        Me.BtnImportFromXLS.TabIndex = 0
-        Me.BtnImportFromXLS.Text = "Import From XLS"
-        '
         'PanelControl2
         '
         Me.PanelControl2.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.NoBorder
@@ -93,13 +66,15 @@ Partial Class FormTargetSAS
         Me.PanelControl2.Size = New System.Drawing.Size(269, 59)
         Me.PanelControl2.TabIndex = 1
         '
-        'LabelControl1
+        'DateEdit2
         '
-        Me.LabelControl1.Location = New System.Drawing.Point(133, 21)
-        Me.LabelControl1.Name = "LabelControl1"
-        Me.LabelControl1.Size = New System.Drawing.Size(4, 13)
-        Me.LabelControl1.TabIndex = 2
-        Me.LabelControl1.Text = "-"
+        Me.DateEdit2.EditValue = Nothing
+        Me.DateEdit2.Location = New System.Drawing.Point(141, 18)
+        Me.DateEdit2.Name = "DateEdit2"
+        Me.DateEdit2.Properties.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() {New DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)})
+        Me.DateEdit2.Properties.CalendarTimeProperties.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() {New DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)})
+        Me.DateEdit2.Size = New System.Drawing.Size(119, 20)
+        Me.DateEdit2.TabIndex = 3
         '
         'DateEdit1
         '
@@ -111,15 +86,24 @@ Partial Class FormTargetSAS
         Me.DateEdit1.Size = New System.Drawing.Size(119, 20)
         Me.DateEdit1.TabIndex = 2
         '
-        'DateEdit2
+        'LabelControl1
         '
-        Me.DateEdit2.EditValue = Nothing
-        Me.DateEdit2.Location = New System.Drawing.Point(141, 18)
-        Me.DateEdit2.Name = "DateEdit2"
-        Me.DateEdit2.Properties.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() {New DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)})
-        Me.DateEdit2.Properties.CalendarTimeProperties.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() {New DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)})
-        Me.DateEdit2.Size = New System.Drawing.Size(119, 20)
-        Me.DateEdit2.TabIndex = 3
+        Me.LabelControl1.Location = New System.Drawing.Point(133, 21)
+        Me.LabelControl1.Name = "LabelControl1"
+        Me.LabelControl1.Size = New System.Drawing.Size(4, 13)
+        Me.LabelControl1.TabIndex = 2
+        Me.LabelControl1.Text = "-"
+        '
+        'BtnImportFromXLS
+        '
+        Me.BtnImportFromXLS.Dock = System.Windows.Forms.DockStyle.Left
+        Me.BtnImportFromXLS.Image = CType(resources.GetObject("BtnImportFromXLS.Image"), System.Drawing.Image)
+        Me.BtnImportFromXLS.ImageLocation = DevExpress.XtraEditors.ImageLocation.TopCenter
+        Me.BtnImportFromXLS.Location = New System.Drawing.Point(2, 2)
+        Me.BtnImportFromXLS.Name = "BtnImportFromXLS"
+        Me.BtnImportFromXLS.Size = New System.Drawing.Size(93, 59)
+        Me.BtnImportFromXLS.TabIndex = 0
+        Me.BtnImportFromXLS.Text = "Import From XLS"
         '
         'BtnView
         '
@@ -143,6 +127,26 @@ Partial Class FormTargetSAS
         Me.SimpleButton1.TabIndex = 3
         Me.SimpleButton1.Text = "Export to XLS"
         '
+        'GCData
+        '
+        Me.GCData.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.GCData.Location = New System.Drawing.Point(0, 63)
+        Me.GCData.MainView = Me.GVData
+        Me.GCData.Name = "GCData"
+        Me.GCData.Size = New System.Drawing.Size(804, 419)
+        Me.GCData.TabIndex = 1
+        Me.GCData.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() {Me.GVData})
+        '
+        'GVData
+        '
+        Me.GVData.GridControl = Me.GCData
+        Me.GVData.Name = "GVData"
+        Me.GVData.OptionsBehavior.AutoExpandAllGroups = True
+        Me.GVData.OptionsBehavior.ReadOnly = True
+        Me.GVData.OptionsFind.AlwaysVisible = True
+        Me.GVData.OptionsView.ShowFooter = True
+        Me.GVData.OptionsView.ShowGroupPanel = False
+        '
         'FormTargetSAS
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
@@ -156,15 +160,15 @@ Partial Class FormTargetSAS
         Me.Text = "Sell Through & SAS Target"
         CType(Me.PanelControl1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.PanelControl1.ResumeLayout(False)
-        CType(Me.GCData, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.GVData, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.PanelControl2, System.ComponentModel.ISupportInitialize).EndInit()
         Me.PanelControl2.ResumeLayout(False)
         Me.PanelControl2.PerformLayout()
-        CType(Me.DateEdit1.Properties.CalendarTimeProperties, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.DateEdit1.Properties, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.DateEdit2.Properties.CalendarTimeProperties, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.DateEdit2.Properties, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DateEdit1.Properties.CalendarTimeProperties, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DateEdit1.Properties, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.GCData, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.GVData, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
--- VolcomInventory/FormTargetSAS.vb
+++ VolcomInventory/FormTargetSAS.vb
@@ -1,3 +1,25 @@
 ﻿Public Class FormTargetSAS
+    Private Sub BtnImportFromXLS_Click(sender As Object, e As EventArgs) Handles BtnImportFromXLS.Click
+        Cursor = Cursors.WaitCursor
+        FormImportExcel.id_pop_up = "68"
+        FormImportExcel.ShowDialog()
+        Cursor = Cursors.Default
+    End Sub
 
+    Private Sub FormTargetSAS_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub FormTargetSAS_FormClosed(sender As Object, e As FormClosedEventArgs) Handles MyBase.FormClosed
+        Dispose()
+    End Sub
+
+    Private Sub FormTargetSAS_Activated(sender As Object, e As EventArgs) Handles MyBase.Activated
+        FormMain.show_rb(Name)
+        checkFormAccess(Name)
+    End Sub
+
+    Private Sub FormTargetSAS_Deactivate(sender As Object, e As EventArgs) Handles MyBase.Deactivate
+        FormMain.hide_rb()
+    End Sub
 End Class
