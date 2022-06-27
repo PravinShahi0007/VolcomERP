bd402a984ed12264fe507f9c46a650d511a46eb8
Satrya Anas <catur.satrya@gmail.com>
2022-06-27 10:08:20 +0800

    prepare view

--- VolcomInventory/FormSalthruCompare.vb
+++ VolcomInventory/FormSalthruCompare.vb
@@ -3,13 +3,28 @@
 
     Private Sub FormSalthruCompare_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         dt_json = volcomErpApiGetJson(volcom_erp_api_host & "api/salthru-compare")
+        DEUntil.EditValue = volcomErpApiGetDT(dt_json, 0).Rows(0)("current_date")
+        viewSeason()
     End Sub
 
     Private Sub FormSalthruCompare_FormClosed(sender As Object, e As FormClosedEventArgs) Handles MyBase.FormClosed
         Dispose()
     End Sub
 
+    Sub viewSeason()
+        Dim data As DataTable = volcomErpApiGetDT(dt_json, 1)
+
+        For i = 0 To data.Rows.Count - 1
+            Dim c As DevExpress.XtraEditors.Controls.CheckedListBoxItem = New DevExpress.XtraEditors.Controls.CheckedListBoxItem
+
+            c.Description = data.Rows(i)("season").ToString
+            c.Value = data.Rows(i)("id_season").ToString
+
+            CCBESeason.Properties.Items.Add(c)
+        Next
+    End Sub
+
     Sub viewData()
-        DEUntil.EditValue = volcomErpApiGetDT(dt_json, 0).Rows(0)("current_date")
+
     End Sub
 End Class
