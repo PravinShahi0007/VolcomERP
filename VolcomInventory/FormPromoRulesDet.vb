221f8d3dce2720017f932eff1a32a8a094fc3d27
Agus Friastana <friastana@gmail.com>
2022-06-30 15:13:57 +0800

    Add class & color

--- VolcomInventory/FormPromoRulesDet.vb
+++ VolcomInventory/FormPromoRulesDet.vb
@@ -107,11 +107,23 @@
         If e.KeyCode = Keys.Enter Then
             Cursor = Cursors.WaitCursor
             Dim code As String = addSlashes(TxtCode.Text)
-            Dim query As String = "SELECT p.id_product, p.product_full_code AS `code`, p.product_display_name AS `name`, cd.code_detail_name AS `size` 
+            Dim query As String = "SELECT p.id_product, p.product_full_code AS `code`, CONCAT(class.code, ' ', d.design_name, ' ', color.display_name) AS `name`, cd.code_detail_name AS `size` 
             FROM tb_m_product p
             INNER JOIN tb_m_product_code pc ON pc.id_product = p.id_product
             INNER JOIN tb_m_code_detail cd ON cd.id_code_detail = pc.id_code_detail
             INNER JOIN tb_m_design d ON d.id_design = p.id_design
+            LEFT JOIN (
+                SELECT c.id_design, d.code
+                FROM tb_m_design_code AS c
+                LEFT JOIN tb_m_code_detail AS d ON d.id_code_detail = c.id_code_detail
+                WHERE d.id_code = 30
+            ) AS class ON d.id_design = class.id_design
+            LEFT JOIN (
+                SELECT c.id_design, d.display_name
+                FROM tb_m_design_code AS c
+                LEFT JOIN tb_m_code_detail AS d ON d.id_code_detail = c.id_code_detail
+                WHERE d.id_code = 14
+            ) AS color ON d.id_design = color.id_design
             WHERE d.id_lookup_status_order!=2 AND d.id_active=1 AND p.product_full_code='" + code + "' "
             Dim data As DataTable = execute_query(query, -1, True, "", "", "", "")
             If data.Rows.Count > 0 Then
