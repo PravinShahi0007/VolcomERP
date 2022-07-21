c88121dd4e1904c8d0097c4d0049f39ce3d50a4d
Putu Septian Primadewa <iansaviour@gmail.com>
2022-07-21 10:09:55 +0800

    pending

--- VolcomInventory/FormPackaging.Designer.vb
+++ VolcomInventory/FormPackaging.Designer.vb
@@ -24,13 +24,13 @@ Partial Class FormPackaging
         Me.LabelControl1 = New DevExpress.XtraEditors.LabelControl()
         Me.SearchLookUpEdit1 = New DevExpress.XtraEditors.SearchLookUpEdit()
         Me.SearchLookUpEdit1View = New DevExpress.XtraGrid.Views.Grid.GridView()
-        Me.SplitContainerControl1 = New DevExpress.XtraEditors.SplitContainerControl()
         Me.GCClass = New DevExpress.XtraGrid.GridControl()
         Me.GVClass = New DevExpress.XtraGrid.Views.Grid.GridView()
         Me.GridColumn1 = New DevExpress.XtraGrid.Columns.GridColumn()
         Me.GridColumn2 = New DevExpress.XtraGrid.Columns.GridColumn()
-        Me.GCPackaging = New DevExpress.XtraGrid.GridControl()
-        Me.GVPackaging = New DevExpress.XtraGrid.Views.Grid.GridView()
+        Me.PanelControl2 = New DevExpress.XtraEditors.PanelControl()
+        Me.PanelControl3 = New DevExpress.XtraEditors.PanelControl()
+        Me.BCreatePenawaran = New DevExpress.XtraEditors.SimpleButton()
         Me.GridColumn3 = New DevExpress.XtraGrid.Columns.GridColumn()
         Me.GridColumn4 = New DevExpress.XtraGrid.Columns.GridColumn()
         Me.GridColumn5 = New DevExpress.XtraGrid.Columns.GridColumn()
@@ -39,12 +39,12 @@ Partial Class FormPackaging
         Me.PanelControl1.SuspendLayout()
         CType(Me.SearchLookUpEdit1.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.SearchLookUpEdit1View, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.SplitContainerControl1, System.ComponentModel.ISupportInitialize).BeginInit()
-        Me.SplitContainerControl1.SuspendLayout()
         CType(Me.GCClass, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.GVClass, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.GCPackaging, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.GVPackaging, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PanelControl2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.PanelControl2.SuspendLayout()
+        CType(Me.PanelControl3, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.PanelControl3.SuspendLayout()
         Me.SuspendLayout()
         '
         'PanelControl1
@@ -90,33 +90,19 @@ Partial Class FormPackaging
         Me.SearchLookUpEdit1View.OptionsSelection.EnableAppearanceFocusedCell = False
         Me.SearchLookUpEdit1View.OptionsView.ShowGroupPanel = False
         '
-        'SplitContainerControl1
-        '
-        Me.SplitContainerControl1.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.SplitContainerControl1.Location = New System.Drawing.Point(0, 48)
-        Me.SplitContainerControl1.Name = "SplitContainerControl1"
-        Me.SplitContainerControl1.Panel1.Controls.Add(Me.GCClass)
-        Me.SplitContainerControl1.Panel1.Text = "Panel1"
-        Me.SplitContainerControl1.Panel2.Controls.Add(Me.GCPackaging)
-        Me.SplitContainerControl1.Panel2.Text = "Panel2"
-        Me.SplitContainerControl1.Size = New System.Drawing.Size(1008, 489)
-        Me.SplitContainerControl1.SplitterPosition = 420
-        Me.SplitContainerControl1.TabIndex = 1
-        Me.SplitContainerControl1.Text = "SplitContainerControl1"
-        '
         'GCClass
         '
         Me.GCClass.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.GCClass.Location = New System.Drawing.Point(0, 0)
+        Me.GCClass.Location = New System.Drawing.Point(2, 2)
         Me.GCClass.MainView = Me.GVClass
         Me.GCClass.Name = "GCClass"
-        Me.GCClass.Size = New System.Drawing.Size(420, 489)
+        Me.GCClass.Size = New System.Drawing.Size(1004, 441)
         Me.GCClass.TabIndex = 0
         Me.GCClass.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() {Me.GVClass})
         '
         'GVClass
         '
-        Me.GVClass.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() {Me.GridColumn1, Me.GridColumn2})
+        Me.GVClass.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() {Me.GridColumn1, Me.GridColumn2, Me.GridColumn3, Me.GridColumn4, Me.GridColumn5, Me.GridColumn6})
         Me.GVClass.GridControl = Me.GCClass
         Me.GVClass.Name = "GVClass"
         Me.GVClass.OptionsView.ShowGroupPanel = False
@@ -135,57 +121,78 @@ Partial Class FormPackaging
         Me.GridColumn2.Visible = True
         Me.GridColumn2.VisibleIndex = 0
         '
-        'GCPackaging
+        'PanelControl2
         '
-        Me.GCPackaging.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.GCPackaging.Location = New System.Drawing.Point(0, 0)
-        Me.GCPackaging.MainView = Me.GVPackaging
-        Me.GCPackaging.Name = "GCPackaging"
-        Me.GCPackaging.Size = New System.Drawing.Size(583, 489)
-        Me.GCPackaging.TabIndex = 1
-        Me.GCPackaging.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() {Me.GVPackaging})
+        Me.PanelControl2.Controls.Add(Me.GCClass)
+        Me.PanelControl2.Controls.Add(Me.PanelControl3)
+        Me.PanelControl2.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.PanelControl2.Location = New System.Drawing.Point(0, 48)
+        Me.PanelControl2.Name = "PanelControl2"
+        Me.PanelControl2.Size = New System.Drawing.Size(1008, 489)
+        Me.PanelControl2.TabIndex = 2
         '
-        'GVPackaging
+        'PanelControl3
         '
-        Me.GVPackaging.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() {Me.GridColumn3, Me.GridColumn4, Me.GridColumn5, Me.GridColumn6})
-        Me.GVPackaging.GridControl = Me.GCPackaging
-        Me.GVPackaging.Name = "GVPackaging"
-        Me.GVPackaging.OptionsView.ShowGroupPanel = False
+        Me.PanelControl3.Controls.Add(Me.BCreatePenawaran)
+        Me.PanelControl3.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.PanelControl3.Location = New System.Drawing.Point(2, 443)
+        Me.PanelControl3.Name = "PanelControl3"
+        Me.PanelControl3.Size = New System.Drawing.Size(1004, 44)
+        Me.PanelControl3.TabIndex = 1
+        '
+        'BCreatePenawaran
+        '
+        Me.BCreatePenawaran.Appearance.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.BCreatePenawaran.Appearance.Font = New System.Drawing.Font("Tahoma", 8.25!, System.Drawing.FontStyle.Bold)
+        Me.BCreatePenawaran.Appearance.ForeColor = System.Drawing.Color.White
+        Me.BCreatePenawaran.Appearance.Options.UseBackColor = True
+        Me.BCreatePenawaran.Appearance.Options.UseFont = True
+        Me.BCreatePenawaran.Appearance.Options.UseForeColor = True
+        Me.BCreatePenawaran.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.BCreatePenawaran.Location = New System.Drawing.Point(2, 2)
+        Me.BCreatePenawaran.LookAndFeel.SkinMaskColor = System.Drawing.Color.Blue
+        Me.BCreatePenawaran.LookAndFeel.SkinMaskColor2 = System.Drawing.Color.Blue
+        Me.BCreatePenawaran.LookAndFeel.Style = DevExpress.LookAndFeel.LookAndFeelStyle.UltraFlat
+        Me.BCreatePenawaran.LookAndFeel.UseDefaultLookAndFeel = False
+        Me.BCreatePenawaran.Name = "BCreatePenawaran"
+        Me.BCreatePenawaran.Size = New System.Drawing.Size(1000, 40)
+        Me.BCreatePenawaran.TabIndex = 15
+        Me.BCreatePenawaran.Text = "Update"
         '
         'GridColumn3
         '
-        Me.GridColumn3.Caption = "GridColumn3"
+        Me.GridColumn3.Caption = "Weight"
         Me.GridColumn3.Name = "GridColumn3"
         Me.GridColumn3.Visible = True
-        Me.GridColumn3.VisibleIndex = 0
+        Me.GridColumn3.VisibleIndex = 1
         '
         'GridColumn4
         '
         Me.GridColumn4.Caption = "GridColumn4"
         Me.GridColumn4.Name = "GridColumn4"
         Me.GridColumn4.Visible = True
-        Me.GridColumn4.VisibleIndex = 1
+        Me.GridColumn4.VisibleIndex = 2
         '
         'GridColumn5
         '
         Me.GridColumn5.Caption = "GridColumn5"
         Me.GridColumn5.Name = "GridColumn5"
         Me.GridColumn5.Visible = True
-        Me.GridColumn5.VisibleIndex = 2
+        Me.GridColumn5.VisibleIndex = 3
         '
         'GridColumn6
         '
         Me.GridColumn6.Caption = "GridColumn6"
         Me.GridColumn6.Name = "GridColumn6"
         Me.GridColumn6.Visible = True
-        Me.GridColumn6.VisibleIndex = 3
+        Me.GridColumn6.VisibleIndex = 4
         '
         'FormPackaging
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(1008, 537)
-        Me.Controls.Add(Me.SplitContainerControl1)
+        Me.Controls.Add(Me.PanelControl2)
         Me.Controls.Add(Me.PanelControl1)
         Me.MaximizeBox = False
         Me.MinimizeBox = False
@@ -197,28 +204,28 @@ Partial Class FormPackaging
         Me.PanelControl1.PerformLayout()
         CType(Me.SearchLookUpEdit1.Properties, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.SearchLookUpEdit1View, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.SplitContainerControl1, System.ComponentModel.ISupportInitialize).EndInit()
-        Me.SplitContainerControl1.ResumeLayout(False)
         CType(Me.GCClass, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.GVClass, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.GCPackaging, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.GVPackaging, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PanelControl2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.PanelControl2.ResumeLayout(False)
+        CType(Me.PanelControl3, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.PanelControl3.ResumeLayout(False)
         Me.ResumeLayout(False)
 
     End Sub
 
     Friend WithEvents PanelControl1 As DevExpress.XtraEditors.PanelControl
-    Friend WithEvents SplitContainerControl1 As DevExpress.XtraEditors.SplitContainerControl
     Friend WithEvents GCClass As DevExpress.XtraGrid.GridControl
     Friend WithEvents GVClass As DevExpress.XtraGrid.Views.Grid.GridView
-    Friend WithEvents GCPackaging As DevExpress.XtraGrid.GridControl
-    Friend WithEvents GVPackaging As DevExpress.XtraGrid.Views.Grid.GridView
     Friend WithEvents BView As DevExpress.XtraEditors.SimpleButton
     Friend WithEvents LabelControl1 As DevExpress.XtraEditors.LabelControl
     Friend WithEvents SearchLookUpEdit1 As DevExpress.XtraEditors.SearchLookUpEdit
     Friend WithEvents SearchLookUpEdit1View As DevExpress.XtraGrid.Views.Grid.GridView
     Friend WithEvents GridColumn1 As DevExpress.XtraGrid.Columns.GridColumn
     Friend WithEvents GridColumn2 As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents PanelControl2 As DevExpress.XtraEditors.PanelControl
+    Friend WithEvents PanelControl3 As DevExpress.XtraEditors.PanelControl
+    Friend WithEvents BCreatePenawaran As DevExpress.XtraEditors.SimpleButton
     Friend WithEvents GridColumn3 As DevExpress.XtraGrid.Columns.GridColumn
     Friend WithEvents GridColumn4 As DevExpress.XtraGrid.Columns.GridColumn
     Friend WithEvents GridColumn5 As DevExpress.XtraGrid.Columns.GridColumn
